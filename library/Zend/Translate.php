abf3acf12103a917db71c686153596d9d8b9d06e
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-05-11 20:14:13 +0000

    [ZF-6500] Zend_Translate:
    - added missing svn property
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@15541 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Translate.php
+++ library/Zend/Translate.php
@@ -15,8 +15,8 @@
  * @category   Zend
  * @package    Zend_Translate
  * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
- * @version    $Id: $
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id$
  */
 
 /**

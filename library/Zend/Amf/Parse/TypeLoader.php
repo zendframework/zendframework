7fa16420e7b9c22f1e5ff9f3d0765c0643f218f0
stas <stas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-05-13 01:36:07 +0000

    map unknown classes to stdClass
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@15566 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Amf/Parse/TypeLoader.php
+++ library/Zend/Amf/Parse/TypeLoader.php
@@ -71,14 +71,13 @@ final class Zend_Amf_Parse_TypeLoader
      */
     public static function loadType($className)
     {
-        $class    = false;
-        $callBack = false;
         $class    = self::getMappedClassName($className);
-        if (!class_exists($class)) {
-            require_once 'Zend/Amf/Exception.php';
-            throw new Zend_Amf_Exception($className .' mapped class '. $class . ' is not defined');
+        if(!$class) {
+            $class = str_replace('.', '_', $className);
         }
-
+        if (!class_exists($class)) {
+            return "stdClass";
+        }
         return $class;
     }
 

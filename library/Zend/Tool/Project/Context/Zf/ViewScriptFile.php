d1545c8fb5c79d94c0d8e4083ec40fcfdbac621e
yoshida@zend.co.jp <yoshida@zend.co.jp@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-06-11 23:03:57 +0000

    [ZF-6988]error.phtml produced with invalid doctype
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@16012 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Tool/Project/Context/Zf/ViewScriptFile.php
+++ library/Zend/Tool/Project/Context/Zf/ViewScriptFile.php
@@ -116,7 +116,7 @@ class Zend_Tool_Project_Context_Zf_ViewScriptFile extends Zend_Tool_Project_Cont
 
         if ($this->_filesystemName == 'error.phtml') {  // should also check that the above directory is forController=error
             $contents .= <<<EOS
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"; "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />

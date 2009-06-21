5b60b76768b65847c7eb6ac6726404c7f96defe3
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-06-21 19:22:17 +0000

    [ZF-6295] Generic:
     - fixed license date (partitial)
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@16210 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Soap/Client/Exception.php
+++ library/Zend/Soap/Client/Exception.php
@@ -15,7 +15,7 @@
  * @category   Zend
  * @package    Zend_Soap
  * @subpackage Client
- * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
@@ -26,7 +26,7 @@ require_once 'Zend/Exception.php';
  * @category   Zend
  * @package    Zend_Soap
  * @subpackage Client
- * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version    $Id: Exception.php 8064 2008-02-16 10:58:39Z thomas $
  */

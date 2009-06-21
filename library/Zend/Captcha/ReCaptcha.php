ca4b3459bd2c2d4a5536cd4105dd7697e423a0c2
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-06-21 18:51:15 +0000

    [ZF-6295] Generic:
     - fixed license date (partitial)
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@16201 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Captcha/ReCaptcha.php
+++ library/Zend/Captcha/ReCaptcha.php
@@ -15,7 +15,7 @@
  * @category   Zend
  * @package    Zend_Captcha
  * @subpackage Adapter
- * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
@@ -35,7 +35,7 @@ require_once 'Zend/Service/ReCaptcha.php';
  * @category   Zend
  * @package    Zend_Captcha
  * @subpackage Adapter
- * @copyright  Copyright (c) 2005-2008 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version    $Id$
  */

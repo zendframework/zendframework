fc947397b3d1fe27f2628ef851855ed35c31a1bf
sidhighwind <sidhighwind@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-07-14 15:10:38 +0000

    ZF-7263: Have the Twitter Exception extend the Zend Service Exception.
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@16706 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Service/Twitter/Exception.php
+++ library/Zend/Service/Twitter/Exception.php
@@ -27,6 +27,6 @@
  * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Zend_Service_Twitter_Exception extends Exception
+class Zend_Service_Twitter_Exception extends Zend_Service_Exception
 {
 }

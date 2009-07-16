681a8f2832b39137a826eee9cb089e3acec2d361
yoshida@zend.co.jp <yoshida@zend.co.jp@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-07-16 01:26:21 +0000

    [ZF-7274]Zend_Service_Strikelron_Exception extends Zend_Exception instead of Zend_Service_Exception
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@16750 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Service/StrikeIron/Exception.php
+++ library/Zend/Service/StrikeIron/Exception.php
@@ -30,5 +30,5 @@ require_once 'Zend/Exception.php';
  * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Zend_Service_StrikeIron_Exception extends Zend_Exception
+class Zend_Service_StrikeIron_Exception extends Zend_Service_Exception
 {}

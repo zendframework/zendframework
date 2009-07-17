33a836deaaf1d880ea012883ad58be71f6d03b1c
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-07-17 14:27:56 +0000

    GENERAL - updated Zend_Version version number
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@16800 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.8.0dev';
+    const VERSION = '1.9.0a1';
 
     /**
      * Compare the specified Zend Framework version string $version

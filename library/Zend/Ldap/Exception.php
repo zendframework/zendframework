ac002ede3bcf370a75a2d1ecd6d4382c49d33c8b
mikaelkael <mikaelkael@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-07-20 13:12:36 +0000

    ZF-7315: review phpDoc for API documentation generation (Zend_Ldap)
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@16880 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Ldap/Exception.php
+++ library/Zend/Ldap/Exception.php
@@ -25,6 +25,13 @@
  */
 require_once 'Zend/Exception.php';
 
+/**
+ * @category   Zend
+ * @package    Zend_Ldap
+ * @uses       Zend_Exception
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 class Zend_Ldap_Exception extends Zend_Exception
 {
     const LDAP_SUCCESS                        = 0x00;

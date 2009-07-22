14ad39dd831e2c27cce42150893424eafd71060c
shahar <shahar@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-07-22 12:59:32 +0000

    Adding Id property on some new files
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@16960 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Http/Client/CommonHttpTests.php
+++ tests/Zend/Http/Client/CommonHttpTests.php
@@ -30,7 +30,7 @@ require_once 'Zend/Uri/Http.php';
  * @category   Zend
  * @package    Zend_Http_Client
  * @subpackage UnitTests
- * @version    $Id: SocketTest.php 16898 2009-07-20 21:31:33Z shahar $
+ * @version    $Id$
  * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */

700ea736089b4939303f7e2829964b7706d74a68
yoshida@zend.co.jp <yoshida@zend.co.jp@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-08-20 23:50:01 +0000

    [ZF-7647]fetchAssoc() and fetchPairs() have wrong docblock return type
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@17701 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Db/Adapter/Abstract.php
+++ library/Zend/Db/Adapter/Abstract.php
@@ -738,7 +738,7 @@ abstract class Zend_Db_Adapter_Abstract
      *
      * @param string|Zend_Db_Select $sql An SQL SELECT statement.
      * @param mixed $bind Data to bind into SELECT placeholders.
-     * @return string
+     * @return array
      */
     public function fetchAssoc($sql, $bind = array())
     {
@@ -775,7 +775,7 @@ abstract class Zend_Db_Adapter_Abstract
      *
      * @param string|Zend_Db_Select $sql An SQL SELECT statement.
      * @param mixed $bind Data to bind into SELECT placeholders.
-     * @return string
+     * @return array
      */
     public function fetchPairs($sql, $bind = array())
     {

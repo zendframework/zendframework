fe2d7960c654e256e040d41d32ee657571729c9c
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-09-12 11:42:47 +0000

    ZF-7834: add Zend_AuthTest to Zend_AllTests
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@18090 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/AllTests.php
+++ tests/Zend/AllTests.php
@@ -33,6 +33,7 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 require_once 'Zend/Acl/AclTest.php';
 require_once 'Zend/Amf/AllTests.php';
 require_once 'Zend/Application/AllTests.php';
+require_once 'Zend/AuthTest.php';
 require_once 'Zend/Auth/AllTests.php';
 require_once 'Zend/Cache/AllTests.php';
 require_once 'Zend/Captcha/AllTests.php';
@@ -162,6 +163,7 @@ class Zend_AllTests
         $suite->addTestSuite('Zend_Acl_AclTest');
         $suite->addTest(Zend_Amf_AllTests::suite());
         $suite->addTest(Zend_Application_AllTests::suite());
+        $suite->addTestSuite('Zend_AuthTest');
         $suite->addTest(Zend_Auth_AllTests::suite());
         $suite->addTest(Zend_Cache_AllTests::suite());
         $suite->addTest(Zend_Captcha_AllTests::suite());

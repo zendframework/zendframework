a8919811d49ca37827425c6145213c799989a07c
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-09-18 20:52:05 +0000

    [GENERIC] Zend_Validate_Postcode:
    - promoted Zend_Validate_Postcode to core
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@18288 44c647ce-9c0f-0410-b52a-842ac1e357ba

---
+++ tests/Zend/Validate/PostCodeTest.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Validate
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id: PostCodeTest.php 17798 2009-08-24 20:07:53Z thomas $
+ */
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Zend_Validate_PostCodeTest::main');
+}
+
+/**
+ * Test helper
+ */
+require_once dirname(__FILE__) . '/../../TestHelper.php';
+
+/**
+ * @see Zend_Validate_PostCode
+ */
+require_once 'Zend/Validate/PostCode.php';
+
+/**
+ * @category   Zend
+ * @package    Zend_Validate
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @group      Zend_Validate
+ */
+class Zend_Validate_PostCodeTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Zend_Validate_PostCode object
+     *
+     * @var Zend_Validate_PostCode
+     */
+    protected $_validator;
+
+    /**
+     * Runs this test suite
+     *
+     * @return void
+     */
+    public static function main()
+    {
+        $suite  = new PHPUnit_Framework_TestSuite('Zend_Validate_PostCodeTest');
+        $result = PHPUnit_TextUI_TestRunner::run($suite);
+    }
+
+    /**
+     * Creates a new Zend_Validate_PostCode object for each test method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->_validator = new Zend_Validate_PostCode('de_AT');
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testBasic()
+    {
+        $valuesExpected = array(
+            array('2292', true),
+            array('1000', true),
+            array('0000', true),
+            array('12345', false),
+            array(1234, true),
+            array(9821, true),
+            array('21A4', false),
+            array('ABCD', false),
+            array(true, false),
+            array('AT-2292', false),
+            array(1.56, false)
+        );
+
+        foreach ($valuesExpected as $element) {
+            $this->assertEquals($element[1], $this->_validator->isValid($element[0]),
+                'Test failed with ' . var_export($element, 1));
+        }
+    }
+
+    /**
+     * Ensures that getMessages() returns expected default value
+     *
+     * @return void
+     */
+    public function testGetMessages()
+    {
+        $this->assertEquals(array(), $this->_validator->getMessages());
+    }
+
+    /**
+     * Ensures that a region is available
+     */
+    public function testSettingLocalesWithoutRegion()
+    {
+        try {
+            $this->_validator->setLocale('de');
+            $this->fail();
+        } catch (Zend_Validate_Exception $e) {
+            $this->assertContains('Unable to detect a region', $e->getMessage());
+        }
+    }
+
+    /**
+     * Ensures that the region contains postal codes
+     */
+    public function testSettingLocalesWithoutPostalCodes()
+    {
+        try {
+            $this->_validator->setLocale('gez_ER');
+            $this->fail();
+        } catch (Zend_Validate_Exception $e) {
+            $this->assertContains('Unable to detect a format', $e->getMessage());
+        }
+    }
+
+    /**
+     * Ensures locales can be retrieved
+     */
+    public function testGettingLocale()
+    {
+        $this->assertEquals('de_AT', $this->_validator->getLocale());
+    }
+
+    /**
+     * Ensures format can be set and retrieved
+     */
+    public function testSetGetFormat()
+    {
+        $this->_validator->setFormat('\d{1}');
+        $this->assertEquals('/^\d{1}$/', $this->_validator->getFormat());
+
+        $this->_validator->setFormat('/^\d{1}');
+        $this->assertEquals('/^\d{1}$/', $this->_validator->getFormat());
+
+        $this->_validator->setFormat('/^\d{1}$/');
+        $this->assertEquals('/^\d{1}$/', $this->_validator->getFormat());
+
+        $this->_validator->setFormat('\d{1}$/');
+        $this->assertEquals('/^\d{1}$/', $this->_validator->getFormat());
+
+        try {
+            $this->_validator->setFormat(null);
+            $this->fail();
+        } catch (Zend_Validate_Exception $e) {
+            $this->assertContains('a not empty string', $e->getMessage());
+        }
+
+        try {
+            $this->_validator->setFormat('');
+            $this->fail();
+        } catch (Zend_Validate_Exception $e) {
+            $this->assertContains('a not empty string', $e->getMessage());
+        }
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Zend_Validate_PostCodeTest::main') {
+    Zend_Validate_PostCodeTest::main();
+}

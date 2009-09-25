63592a958a8e7255aa0434c8ea87be63a50feaae
mikaelkael <mikaelkael@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-09-25 17:41:25 +0000

    ZF-7946: Zend_Db_Adapter_Oracle triggers E_NOTICE without oci extension
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@18414 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Db/Adapter/Oracle.php
+++ library/Zend/Db/Adapter/Oracle.php
@@ -81,7 +81,7 @@ class Zend_Db_Adapter_Oracle extends Zend_Db_Adapter_Abstract
     /**
      * @var integer
      */
-    protected $_execute_mode = OCI_COMMIT_ON_SUCCESS;
+    protected $_execute_mode = null;
 
     /**
      * Default class name for a DB statement.
@@ -118,6 +118,8 @@ class Zend_Db_Adapter_Oracle extends Zend_Db_Adapter_Abstract
             require_once 'Zend/Db/Adapter/Oracle/Exception.php';
             throw new Zend_Db_Adapter_Oracle_Exception('The OCI8 extension is required for this adapter but the extension is not loaded');
         }
+
+        $this->_setExecuteMode(OCI_COMMIT_ON_SUCCESS);
 
         $connectionFuncName = ($this->_config['persistent'] == true) ? 'oci_pconnect' : 'oci_connect';
 

bb7cb86c2946a5091509a40a1b266d28b35b40f8
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-12-08 14:29:56 +0000

    MAINTENANCE - Updating Zend_Version to alpha state
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@19526 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.10.0dev';
+    const VERSION = '1.10.0alpha';
 
     /**
      * Compare the specified Zend Framework version string $version

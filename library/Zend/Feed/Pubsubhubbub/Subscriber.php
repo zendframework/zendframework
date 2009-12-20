cf57bce569a23872fdcaa9ab1fd2e892729d02ad
padraic <padraic@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2009-12-20 20:02:11 +0000

    Removed a redundant require_once file which doesn't exist
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@19817 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Feed/Pubsubhubbub/Subscriber.php
+++ library/Zend/Feed/Pubsubhubbub/Subscriber.php
@@ -17,8 +17,6 @@
  * @copyright  Copyright (c) 2005-2009 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
- 
-require_once 'Zend/Feed/Pubsubhubbub/Entity/TopicSubscription.php';
 
 /**
  * @see Zend_Feed_Pubsubhubbub

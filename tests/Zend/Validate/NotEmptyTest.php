32acdaa50a355bfa1aedacbc6217fc04ac655532
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-01-01 22:45:05 +0000

    [GENERIC] Zend_Validate_NotEmpty:
    - new reworked implementation of Zend_Validate_NotEmpty
    - see documentation for details
    - 
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@20020 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/Zend/Validate/NotEmptyTest.php
+++ tests/Zend/Validate/NotEmptyTest.php
@@ -106,6 +106,435 @@ class Zend_Validate_NotEmptyTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlyBoolean()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::BOOLEAN);
+        $this->assertFalse($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertTrue($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertTrue($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertTrue($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertTrue($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertTrue($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertTrue($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlyInteger()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::INTEGER);
+        $this->assertTrue($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertFalse($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertTrue($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertTrue($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertTrue($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertTrue($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertTrue($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlyFloat()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::FLOAT);
+        $this->assertTrue($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertTrue($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertFalse($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertTrue($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertTrue($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertTrue($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertTrue($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlyString()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::STRING);
+        $this->assertTrue($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertTrue($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertTrue($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertFalse($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertTrue($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertTrue($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertTrue($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlyZero()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::ZERO);
+        $this->assertTrue($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertTrue($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertTrue($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertTrue($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertFalse($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertTrue($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertTrue($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlyArray()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::EMPTY_ARRAY);
+        $this->assertTrue($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertTrue($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertTrue($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertTrue($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertTrue($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertFalse($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertTrue($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlyNull()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::NULL);
+        $this->assertTrue($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertTrue($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertTrue($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertTrue($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertTrue($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertTrue($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertFalse($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlyPHP()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::PHP);
+        $this->assertFalse($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertFalse($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertFalse($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertFalse($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertFalse($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertFalse($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertFalse($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlySpace()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::SPACE);
+        $this->assertTrue($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertTrue($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertTrue($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertTrue($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertTrue($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertTrue($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertTrue($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testOnlyAll()
+    {
+        $this->_validator->setType(Zend_Validate_NotEmpty::ALL);
+        $this->assertFalse($this->_validator->isValid(false));
+        $this->assertTrue($this->_validator->isValid(true));
+        $this->assertFalse($this->_validator->isValid(0));
+        $this->assertTrue($this->_validator->isValid(1));
+        $this->assertFalse($this->_validator->isValid(0.0));
+        $this->assertTrue($this->_validator->isValid(1.0));
+        $this->assertFalse($this->_validator->isValid(''));
+        $this->assertTrue($this->_validator->isValid('abc'));
+        $this->assertFalse($this->_validator->isValid('0'));
+        $this->assertTrue($this->_validator->isValid('1'));
+        $this->assertFalse($this->_validator->isValid(array()));
+        $this->assertTrue($this->_validator->isValid(array('xxx')));
+        $this->assertFalse($this->_validator->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testArrayConstantNotation()
+    {
+        $filter = new Zend_Validate_NotEmpty(
+            array(
+                'type' => array(
+                    Zend_Validate_NotEmpty::ZERO,
+                    Zend_Validate_NotEmpty::STRING,
+                    Zend_Validate_NotEmpty::BOOLEAN
+                )
+            )
+        );
+
+        $this->assertFalse($filter->isValid(false));
+        $this->assertTrue($filter->isValid(true));
+        $this->assertTrue($filter->isValid(0));
+        $this->assertTrue($filter->isValid(1));
+        $this->assertTrue($filter->isValid(0.0));
+        $this->assertTrue($filter->isValid(1.0));
+        $this->assertFalse($filter->isValid(''));
+        $this->assertTrue($filter->isValid('abc'));
+        $this->assertFalse($filter->isValid('0'));
+        $this->assertTrue($filter->isValid('1'));
+        $this->assertTrue($filter->isValid(array()));
+        $this->assertTrue($filter->isValid(array('xxx')));
+        $this->assertTrue($filter->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testArrayConfigNotation()
+    {
+        $filter = new Zend_Validate_NotEmpty(
+            array(
+                'type' => array(
+                    Zend_Validate_NotEmpty::ZERO,
+                    Zend_Validate_NotEmpty::STRING,
+                    Zend_Validate_NotEmpty::BOOLEAN),
+                'test' => false
+            )
+        );
+
+        $this->assertFalse($filter->isValid(false));
+        $this->assertTrue($filter->isValid(true));
+        $this->assertTrue($filter->isValid(0));
+        $this->assertTrue($filter->isValid(1));
+        $this->assertTrue($filter->isValid(0.0));
+        $this->assertTrue($filter->isValid(1.0));
+        $this->assertFalse($filter->isValid(''));
+        $this->assertTrue($filter->isValid('abc'));
+        $this->assertFalse($filter->isValid('0'));
+        $this->assertTrue($filter->isValid('1'));
+        $this->assertTrue($filter->isValid(array()));
+        $this->assertTrue($filter->isValid(array('xxx')));
+        $this->assertTrue($filter->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testMultiConstantNotation()
+    {
+        $filter = new Zend_Validate_NotEmpty(
+            Zend_Validate_NotEmpty::ZERO + Zend_Validate_NotEmpty::STRING + Zend_Validate_NotEmpty::BOOLEAN
+        );
+
+        $this->assertFalse($filter->isValid(false));
+        $this->assertTrue($filter->isValid(true));
+        $this->assertTrue($filter->isValid(0));
+        $this->assertTrue($filter->isValid(1));
+        $this->assertTrue($filter->isValid(0.0));
+        $this->assertTrue($filter->isValid(1.0));
+        $this->assertFalse($filter->isValid(''));
+        $this->assertTrue($filter->isValid('abc'));
+        $this->assertFalse($filter->isValid('0'));
+        $this->assertTrue($filter->isValid('1'));
+        $this->assertTrue($filter->isValid(array()));
+        $this->assertTrue($filter->isValid(array('xxx')));
+        $this->assertTrue($filter->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testStringNotation()
+    {
+        $filter = new Zend_Validate_NotEmpty(
+            array(
+                'type' => array('zero', 'string', 'boolean')
+            )
+        );
+
+        $this->assertFalse($filter->isValid(false));
+        $this->assertTrue($filter->isValid(true));
+        $this->assertTrue($filter->isValid(0));
+        $this->assertTrue($filter->isValid(1));
+        $this->assertTrue($filter->isValid(0.0));
+        $this->assertTrue($filter->isValid(1.0));
+        $this->assertFalse($filter->isValid(''));
+        $this->assertTrue($filter->isValid('abc'));
+        $this->assertFalse($filter->isValid('0'));
+        $this->assertTrue($filter->isValid('1'));
+        $this->assertTrue($filter->isValid(array()));
+        $this->assertTrue($filter->isValid(array('xxx')));
+        $this->assertTrue($filter->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testSingleStringNotation()
+    {
+        $filter = new Zend_Validate_NotEmpty(
+            'boolean'
+        );
+
+        $this->assertFalse($filter->isValid(false));
+        $this->assertTrue($filter->isValid(true));
+        $this->assertTrue($filter->isValid(0));
+        $this->assertTrue($filter->isValid(1));
+        $this->assertTrue($filter->isValid(0.0));
+        $this->assertTrue($filter->isValid(1.0));
+        $this->assertTrue($filter->isValid(''));
+        $this->assertTrue($filter->isValid('abc'));
+        $this->assertTrue($filter->isValid('0'));
+        $this->assertTrue($filter->isValid('1'));
+        $this->assertTrue($filter->isValid(array()));
+        $this->assertTrue($filter->isValid(array('xxx')));
+        $this->assertTrue($filter->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testConfigObject()
+    {
+        require_once 'Zend/Config.php';
+        $options = array('type' => 'all');
+        $config  = new Zend_Config($options);
+
+        $filter = new Zend_Validate_NotEmpty(
+            $config
+        );
+
+        $this->assertFalse($filter->isValid(false));
+        $this->assertTrue($filter->isValid(true));
+        $this->assertFalse($filter->isValid(0));
+        $this->assertTrue($filter->isValid(1));
+        $this->assertFalse($filter->isValid(0.0));
+        $this->assertTrue($filter->isValid(1.0));
+        $this->assertFalse($filter->isValid(''));
+        $this->assertTrue($filter->isValid('abc'));
+        $this->assertFalse($filter->isValid('0'));
+        $this->assertTrue($filter->isValid('1'));
+        $this->assertFalse($filter->isValid(array()));
+        $this->assertTrue($filter->isValid(array('xxx')));
+        $this->assertFalse($filter->isValid(null));
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testSettingFalseType()
+    {
+        try {
+            $this->_validator->setType(true);
+            $this->fail();
+        } catch (Zend_Exception $e) {
+            $this->assertContains('Unknown', $e->getMessage());
+        }
+    }
+
+    /**
+     * Ensures that the validator follows expected behavior
+     *
+     * @return void
+     */
+    public function testGetType()
+    {
+        $this->assertEquals(237, $this->_validator->getType());
+    }
+
+    /**
      * @see ZF-3236
      */
     public function testStringWithZeroShouldNotBeTreatedAsEmpty()

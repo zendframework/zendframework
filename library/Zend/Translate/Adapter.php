23aaab52f3a4766757d7569c21f284a967bb85a5
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-01-18 21:26:37 +0000

    [ZF-8448] Zend_Translate:
    - omit warning on isTranslated
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@20402 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Translate/Adapter.php
+++ library/Zend/Translate/Adapter.php
@@ -699,14 +699,14 @@ abstract class Zend_Translate_Adapter {
         }
 
         $locale = (string) $locale;
-        if (isset($this->_translate[$locale][$messageId]) === true) {
+        if ((is_string($messageId) || is_int($messageId)) && isset($this->_translate[$locale][$messageId])) {
             // return original translation
             return true;
         } else if ((strlen($locale) != 2) and ($original === false)) {
             // faster than creating a new locale and separate the leading part
             $locale = substr($locale, 0, -strlen(strrchr($locale, '_')));
 
-            if (isset($this->_translate[$locale][$messageId]) === true) {
+            if ((is_string($messageId) || is_int($messageId)) && isset($this->_translate[$locale][$messageId])) {
                 // return regionless translation (en_US -> en)
                 return true;
             }

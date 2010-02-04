64ba76c6ae6bf3f29110cdf11090206dca899820
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-02-04 07:20:38 +0000

    GENERAL - updating trunk Version to 1.11.0dev
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@20899 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -32,7 +32,7 @@ final class Zend_Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '1.10.0beta';
+    const VERSION = '1.11.0dev';
 
     /**
      * Compare the specified Zend Framework version string $version

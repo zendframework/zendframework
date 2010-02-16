157a2f27c3f81a129310629c5997889c186ee0e6
padraic <padraic@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-02-16 14:20:35 +0000

    Removed commented out line from Zend_Cache_Manager Fixed a parameter missing bug in Zend_Feed_Pubsubhubbub (0.3 Compatibility)
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@21066 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Cache/Manager.php
+++ library/Zend/Cache/Manager.php
@@ -86,7 +86,6 @@ class Zend_Cache_Manager
                 'name'    => 'Capture',
                 'options' => array(
                     'ignore_user_abort' => true,
-//                    'automatic_serialization' => true
                 ),
             ),
             'backend' => array(
--- library/Zend/Feed/Pubsubhubbub/Subscriber.php
+++ library/Zend/Feed/Pubsubhubbub/Subscriber.php
@@ -722,7 +722,7 @@ class Zend_Feed_Pubsubhubbub_Subscriber
          * Establish a persistent verify_token and attach key to callback
          * URL's path/querystring
          */
-        $key   = $this->_generateSubscriptionKey($params);
+        $key   = $this->_generateSubscriptionKey($params, $hubUrl);
         $token = $this->_generateVerifyToken();
         $params['hub.verify_token'] = $token;
 
@@ -792,9 +792,9 @@ class Zend_Feed_Pubsubhubbub_Subscriber
      * @param  string $hubUrl The Hub Server URL for which this token will apply
      * @return string
      */
-    protected function _generateSubscriptionKey(array $params)
+    protected function _generateSubscriptionKey(array $params, $hubUrl)
     {
-        $keyBase = $params['hub.topic'] . $params['hub.callback'];
+        $keyBase = $params['hub.topic'] . $hubUrl;
         $key     = md5($keyBase);
         return $key;
     }

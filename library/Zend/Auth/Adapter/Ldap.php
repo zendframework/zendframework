d7dc7bdf859c98456e6dcae6d197abda35dbd2da
sgehrig <sgehrig@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-02-28 15:40:21 +0000

    [ZF-9269] fixed typo
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@21236 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Auth/Adapter/Ldap.php
+++ library/Zend/Auth/Adapter/Ldap.php
@@ -1,4 +1,4 @@
-<?php
+f<?php
 /**
  * Zend Framework
  *
@@ -286,7 +286,7 @@ class Zend_Auth_Adapter_Ldap implements Zend_Auth_Adapter_Interface
                  * @see Zend_Auth_Adapter_Exception
                  */
                 require_once 'Zend/Auth/Adapter/Exception.php';
-                throw new Zend_Auth_Adapter_Exception('Adapter options array not in array');
+                throw new Zend_Auth_Adapter_Exception('Adapter options array not an array');
             }
             $adapterOptions = $this->_prepareOptions($ldap, $options);
             $dname = '';

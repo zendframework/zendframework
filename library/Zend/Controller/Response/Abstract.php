e843b70407f5b04d19a8a38208162cfbe87bfb52
yoshida@zend.co.jp <yoshida@zend.co.jp@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-03-02 23:01:19 +0000

    [ZF-9283]Zend_Controller_Response_Abstract contain tabs
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@21301 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Controller/Response/Abstract.php
+++ library/Zend/Controller/Response/Abstract.php
@@ -184,26 +184,26 @@ abstract class Zend_Controller_Response_Abstract
         return $this;
     }
 
-	/**
-	 * Clears the specified HTTP header
-	 *
-	 * @param  string $name
-	 * @return Zend_Controller_Response_Abstract
-	 */
-	public function clearHeader($name)
-	{
-		if (! count($this->_headers)) {
-			return $this;
-		}
+    /**
+     * Clears the specified HTTP header
+     *
+     * @param  string $name
+     * @return Zend_Controller_Response_Abstract
+     */
+    public function clearHeader($name)
+    {
+        if (! count($this->_headers)) {
+            return $this;
+        }
 
-		foreach ($this->_headers as $index => $header) {
-			if ($name == $header['name']) {
-				unset($this->_headers[$index]);
-			}
-		}
+        foreach ($this->_headers as $index => $header) {
+            if ($name == $header['name']) {
+                unset($this->_headers[$index]);
+            }
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
     /**
      * Set raw HTTP header
@@ -244,23 +244,23 @@ abstract class Zend_Controller_Response_Abstract
         return $this;
     }
 
-	/**
-	 * Clears the specified raw HTTP header
-	 *
-	 * @param  string $headerRaw
-	 * @return Zend_Controller_Response_Abstract
-	 */
-	public function clearRawHeader($headerRaw)
-	{
-		if (! count($this->_headersRaw)) {
-			return $this;
-		}
+    /**
+     * Clears the specified raw HTTP header
+     *
+     * @param  string $headerRaw
+     * @return Zend_Controller_Response_Abstract
+     */
+    public function clearRawHeader($headerRaw)
+    {
+        if (! count($this->_headersRaw)) {
+            return $this;
+        }
 
-		$key = array_search($headerRaw, $this->_headersRaw);
-		unset($this->_headersRaw[$key]);
+        $key = array_search($headerRaw, $this->_headersRaw);
+        unset($this->_headersRaw[$key]);
 
-		return $this;
-	}
+        return $this;
+    }
 
     /**
      * Clear all headers, normal and raw

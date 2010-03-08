947e228e35efbd23695aefff810d34adf9d63cb5
matthew <matthew@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-03-08 20:42:25 +0000

    [2.0] Added group annotation "resources" to translation resource tests
    git-svn-id: http://framework.zend.com/svn/framework/standard/branches/development-2.0@21391 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- tests/resources/languages/Zend_ValidateTest.php
+++ tests/resources/languages/Zend_ValidateTest.php
@@ -24,7 +24,7 @@
  * @category   Zend
  * @package    Zend_resources
  * @subpackage UnitTests
- * @group      Zend_Exception
+ * @group      resources
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */

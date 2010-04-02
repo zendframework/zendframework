2374e4ed488f2a1485dcea1f8c7fa581b7adc575
Matthew Weier O'Phinney <matthew@zend.com>
2010-04-02 12:33:25 -0400

    Updated test-iterator to make it work on more systems

--- working/test-iterator.php
+++ working/test-iterator.php
@@ -1,26 +1,32 @@
 <?php
-
+ini_set('display_errors', false);
 $component = $_SERVER['argv'][1];
 
 if (!$component) {
     die('A component is required');
 }
 
-$path = realpath('../tests/Zend/' . $component . '/');
-$tests_path = realpath('../tests/');
-$output = realpath('./tmp');
+$path       = realpath(__DIR__ . '/../tests/Zend/' . $component . '/');
+$tests_path = realpath(__DIR__ . '/../tests/');
+$output     = __DIR__ . '/tmp';
+
+if (!is_dir($output)) {
+    echo "Attempting to create output directory '$output'\n";
+    mkdir($output, 0755, true);
+}
 
 foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path)) as $fs_item) {
     $relative_to_tests = preg_replace('#^' . preg_quote($tests_path) . '/#', '', $fs_item->getRealPath());
     
     $subdir = $output . '/' . substr($relative_to_tests, 0, strrpos($relative_to_tests, '/'));
     if (strpos($relative_to_tests, '/') && !file_exists($subdir)) {
+        echo 'Making dir: ' . $subdir . PHP_EOL;
         mkdir($subdir, 0777, true);
-        //echo 'Making dir: ' . $subdir . PHP_EOL;
     }
     
-    if (preg_match('#\.php$#', $fs_item->getRealPath()) && !preg_match('#_files#', $fs_item->getRealPath())) {
-        $command = '/usr/bin/php ./test-namespacer.php ../tests ' . $relative_to_tests . ' > ' . $output . '/' . $relative_to_tests;
+    // if (preg_match('#\.php$#', $fs_item->getRealPath()) && !preg_match('#_files#', $fs_item->getRealPath())) {
+    if (preg_match('#\.php$#', $fs_item->getRealPath())) {
+        $command = 'php -d display_errors=0 ./test-namespacer.php ../tests ' . $relative_to_tests . ' > ' . $output . '/' . $relative_to_tests;
         system($command);
     } else {
         echo copy($fs_item->getRealPath(), $output . '/' . $relative_to_tests);

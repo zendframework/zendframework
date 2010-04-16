0182b602f3e455b595652aedb10b5d0c3df16de6
juokaz <juokaz@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-04-16 19:15:20 +0000

    ZF-9674 - Fixed subpackage declaration
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@21892 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Log/Filter/Abstract.php
+++ library/Zend/Log/Filter/Abstract.php
@@ -29,7 +29,7 @@ require_once 'Zend/Log/FactoryInterface.php';
 /**
  * @category   Zend
  * @package    Zend_Log
- * @subpackage Writer
+ * @subpackage Filter
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @version    $Id$

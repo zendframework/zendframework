52c9408033240c330bc84b852cf52e76597557bd
alexander <alexander@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-04-16 14:46:43 +0000

    Zend_Service_Flickr: Fix for ZF-8864. (applying patch provided by  Christian Münch).
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@21880 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Service/Flickr.php
+++ library/Zend/Service/Flickr.php
@@ -410,7 +410,7 @@ class Zend_Service_Flickr
     protected function _validateUserSearch(array $options)
     {
         $validOptions = array('api_key', 'method', 'user_id', 'per_page', 'page', 'extras', 'min_upload_date',
-                              'min_taken_date', 'max_upload_date', 'max_taken_date');
+                              'min_taken_date', 'max_upload_date', 'max_taken_date', 'safe_search');
 
         $this->_compareOptions($options, $validOptions);
 
@@ -465,7 +465,9 @@ class Zend_Service_Flickr
     {
         $validOptions = array('method', 'api_key', 'user_id', 'tags', 'tag_mode', 'text', 'min_upload_date',
                               'max_upload_date', 'min_taken_date', 'max_taken_date', 'license', 'sort',
-                              'privacy_filter', 'bbox', 'accuracy', 'machine_tags', 'machine_tag_mode', 'group_id',
+                              'privacy_filter', 'bbox', 'accuracy', 'safe_search', 'content_type', 'machine_tags',
+                              'machine_tag_mode', 'group_id', 'contacts', 'woe_id', 'place_id', 'media', 'has_geo',
+                              'geo_context', 'lat'. 'lon', 'radius', 'radius_units', 'is_commons', 'is_gallery',
                               'extras', 'per_page', 'page');
 
         $this->_compareOptions($options, $validOptions);

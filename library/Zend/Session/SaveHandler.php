acd0ddc7af2954c314bc3335bc9127214eb51fee
Matthew Weier O'Phinney <matthew@zend.com>
2010-04-19 15:46:03 -0400

    [session] Updated docblocks for SaveHandler
    - What you did

--- library/Zend/Session/SaveHandler.php
+++ library/Zend/Session/SaveHandler.php
@@ -27,7 +27,6 @@ namespace Zend\Session;
  *
  * @category   Zend
  * @package    Zend_Session
- * @subpackage SaveHandler
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  * @see        http://php.net/session_set_save_handler
--- library/Zend/Session/SaveHandler/Exception.php
+++ library/Zend/Session/SaveHandler/Exception.php
@@ -27,6 +27,7 @@ namespace Zend\Session\SaveHandler;
  * @uses       Zend\Session\Exception
  * @category   Zend
  * @package    Zend_Session
+ * @subpackage SaveHandler
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */

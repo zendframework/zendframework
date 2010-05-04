620a565b5c84964365cf83892d0d66a499387363
wadearnold <wadearnold@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-05-04 15:37:23 +0000

    ZF-9799 Updated headers to reflect W3C cach-Control recomendations http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@22096 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Amf/Response/Http.php
+++ library/Zend/Amf/Response/Http.php
@@ -41,8 +41,9 @@ class Zend_Amf_Response_Http extends Zend_Amf_Response
     public function getResponse()
     {
         if (!headers_sent()) {
-            header('Cache-Control: cache, must-revalidate');
-            header('Pragma: public');
+            header('Cache-Control: no-cache, must-revalidate');
+            header('Expires: Thu, 19 Nov 1981 08:52:00 GMT');
+            header('Pragma: no-cache');
             header('Content-Type: application/x-amf');
         }
         return parent::getResponse();

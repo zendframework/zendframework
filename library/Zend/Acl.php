6a4047353cef2af6848df2ac5d8f651b32c3353a
ralph <ralph@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-06-18 22:39:32 +0000

    ZF-9716 Applied patch for missing require statements near thrown exceptions in Zend_Acl
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@22455 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Acl.php
+++ library/Zend/Acl.php
@@ -309,6 +309,7 @@ class Zend_Acl
                 }
                 $resourceParent = $this->get($resourceParentId);
             } catch (Zend_Acl_Exception $e) {
+                require_once 'Zend/Acl/Exception.php';
                 throw new Zend_Acl_Exception("Parent Resource id '$resourceParentId' does not exist", 0, $e);
             }
             $this->_resources[$resourceParentId]['children'][$resourceId] = $resource;
@@ -407,6 +408,7 @@ class Zend_Acl
             $resourceId     = $this->get($resource)->getResourceId();
             $inheritId = $this->get($inherit)->getResourceId();
         } catch (Zend_Acl_Exception $e) {
+            require_once 'Zend/Acl/Exception.php';
             throw new Zend_Acl_Exception($e->getMessage(), $e->getCode(), $e);
         }
 
@@ -445,6 +447,7 @@ class Zend_Acl
         try {
             $resourceId = $this->get($resource)->getResourceId();
         } catch (Zend_Acl_Exception $e) {
+            require_once 'Zend/Acl/Exception.php';
             throw new Zend_Acl_Exception($e->getMessage(), $e->getCode(), $e);
         }
 

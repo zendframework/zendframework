6983e96dc5c3775fd2a74955c83850665c4a0e0f
Shahar Evron <shahar.e@zend.com>
2010-06-26 12:50:17 +0300

    Minor fixes to PHPDoc references

--- library/Zend/URI/URL.php
+++ library/Zend/URI/URL.php
@@ -28,8 +28,8 @@ use Zend\Validator\Hostname;
 /**
  * URL handler
  *
- * @uses      \Zend\Uri\Uri
- * @uses      \Zend\Uri\Exception
+ * @uses      \Zend\URI\URI
+ * @uses      \Zend\URI\Exception
  * @uses      \Zend\Validator\Hostname\Hostname
  * @category  Zend
  * @package   Zend_Uri
@@ -38,7 +38,6 @@ use Zend\Validator\Hostname;
  */
 class URL implements URI
 {
-    
     /**
      * Character classes for validation regular expressions
      * (null) values here will be initialized during calls to _getRegex
@@ -226,7 +225,7 @@ class URL implements URI
         }
 
         // Check the port against the allowed values
-        return ctype_digit((string) $port) and 1 <= $port and $port <= 65535;
+        return (ctype_digit((string) $port) && 1 <= $port && $port <= 0xffff);
     }
     
     /**

b0bfc3595bd5a4c2bebb9a244c4d379be1268d96
thomas <thomas@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-07-04 11:05:10 +0000

    [ZF-10072] Zend_Translate:
    - fixed polish plural rule for 3rd genetiv
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@22518 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Translate/Plural.php
+++ library/Zend/Translate/Plural.php
@@ -180,7 +180,7 @@ class Zend_Translate_Plural
                 return ($number == 0) ? 0 : ((($number % 10 == 1) && ($number % 100 != 11)) ? 1 : 2);
 
             case 'pl':
-                return ($number == 1) ? 0 : ((($number % 10 >= 2) && ($number % 10 <= 4) && (($number % 100 < 10) || ($number % 100 > 29))) ? 1 : 2);
+                return ($number == 1) ? 0 : ((($number % 10 >= 2) && ($number % 10 <= 4) && (($number % 100 < 12) || ($number % 100 > 14))) ? 1 : 2);
 
             case 'cy':
                 return ($number == 1) ? 0 : (($number == 2) ? 1 : ((($number == 8) || ($number == 11)) ? 2 : 3));

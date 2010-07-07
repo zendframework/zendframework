cbe66cc1e22e8ce5b4cbbd32614880e096a24b05
Matthew Weier O'Phinney <matthew@zend.com>
2010-07-07 08:54:20 -0400

    Zend\Memory cleanup
    - s/Container\ContainerInterface/Container/
    - Updated code to reflect the above

--- library/Zend/Memory/Container.php
+++ library/Zend/Memory/Container.php
@@ -22,7 +22,7 @@
 /**
  * @namespace
  */
-namespace Zend\Memory\Container;
+namespace Zend\Memory;
 
 /**
  * Memory value container interface
@@ -32,7 +32,7 @@ namespace Zend\Memory\Container;
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-interface ContainerInterface
+interface Container
 {
     /**
      * Get string value reference

85a932b0e09c167c2648be6b7b3d52251f736515
ramon <ramon@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-07-07 02:17:52 +0000

    [ZF-10081] Zend_Log:
    - fixed warning ZendPlatform
    
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@22531 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Log/Writer/ZendMonitor.php
+++ library/Zend/Log/Writer/ZendMonitor.php
@@ -51,7 +51,7 @@ class Zend_Log_Writer_ZendMonitor extends Zend_Log_Writer_Abstract
 
     /**
      * Create a new instance of Zend_Log_Writer_ZendMonitor
-     * 
+     *
      * @param  array|Zend_Config $config
      * @return Zend_Log_Writer_Syslog
      * @throws Zend_Log_Exception
@@ -64,10 +64,10 @@ class Zend_Log_Writer_ZendMonitor extends Zend_Log_Writer_Abstract
     /**
      * Is logging to this writer enabled?
      *
-     * If the Zend Monitor extension is not enabled, this log writer will 
-     * fail silently. You can query this method to determine if the log 
+     * If the Zend Monitor extension is not enabled, this log writer will
+     * fail silently. You can query this method to determine if the log
      * writer is enabled.
-     * 
+     *
      * @return bool
      */
     public function isEnabled()
@@ -103,7 +103,7 @@ class Zend_Log_Writer_ZendMonitor extends Zend_Log_Writer_Abstract
         unset($event['priority'], $event['message']);
 
         if (!empty($event)) {
-            monitor_custom_event($priority, $message, $event);
+            monitor_custom_event($priority, $message, false, $event);
         } else {
             monitor_custom_event($priority, $message);
         }

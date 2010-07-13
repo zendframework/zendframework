bdde2fd9a4648164d2938036f8f57d1047044983
Victor Farazdagi <simple.square@gmail.com>
2010-07-13 15:41:37 +0400

    Service\Amazon\Sqs UTs update

--- library/Zend/Service/Amazon/AbstractAmazon.php
+++ library/Zend/Service/Amazon/AbstractAmazon.php
@@ -37,7 +37,7 @@ namespace Zend\Service\Amazon;
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-abstract class AbstractAmazon extends \\Zend\\Service\\AbstractService
+abstract class AbstractAmazon extends \Zend\Service\AbstractService
 {
     /**
      * @var string Amazon Access Key

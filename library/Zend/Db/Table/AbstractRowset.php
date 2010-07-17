83b4d8e93c259d403d00d0b83db0817f1ef37fa1
ramon <ramon@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-07-17 15:33:11 +0000

    [ZF-10173] Zend_Db_Table:
    - Fixed throw a exception case illegal index.
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@22615 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Db/Table/AbstractRowset.php
+++ library/Zend/Db/Table/AbstractRowset.php
@@ -354,7 +354,12 @@ abstract class AbstractRowset implements \SeekableIterator, \Countable, \ArrayAc
      */
     public function offsetGet($offset)
     {
-        $this->_pointer = (int) $offset;
+        $offset = (int) $offset;
+        if ($offset < 0 || $offset >= $this->_count) {
+            require_once 'Zend/Db/Table/Rowset/Exception.php';
+            throw new Zend_Db_Table_Rowset_Exception("Illegal index $offset");
+        }
+        $this->_pointer = $offset;
 
         return $this->current();
     }

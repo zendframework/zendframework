1db4c46b24144fd1b9d7225dffd8358e6bf8f573
ramon <ramon@44c647ce-9c0f-0410-b52a-842ac1e357ba>
2010-07-25 19:35:20 +0000

    [ZF-5413 & ZF-10224] Zend_Soap_Wsdl:
    - fixed syntax error.
    
    git-svn-id: http://framework.zend.com/svn/framework/standard/trunk@22674 44c647ce-9c0f-0410-b52a-842ac1e357ba

--- library/Zend/Soap/Wsdl/Strategy/DefaultComplexType.php
+++ library/Zend/Soap/Wsdl/Strategy/DefaultComplexType.php
@@ -74,7 +74,7 @@ class Zend_Soap_Wsdl_Strategy_DefaultComplexType extends Zend_Soap_Wsdl_Strategy
                 $element->setAttribute('type', $this->getContext()->getType(trim($matches[1][0])));
 
                 // If the default value is null, then this property is nillable.
-                if ($defaultProperties[$propertyName] === NULL)) {
+                if ($defaultProperties[$propertyName] === null) {
                     $element->setAttribute('nillable', 'true');
                 }
 

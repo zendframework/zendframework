488cfca8f6b7b7d3dc7edbe045dd21ae28fdb41a
Matthew Weier O'Phinney <matthew@zend.com>
2010-07-30 08:10:58 -0400

    QuickStart readiness
    - Fixed several errors found when building the Quick Start app via ZF2
      - Hash element needed to do some normalization of the class name when creating
        a session container key
      - SessionManager needed to populate the storage and $_SESSION superglobal
        during start()
      - Several class names in Zend\Tool\Project\Provider\Test were not properly
        namespaced

--- library/Zend/Form/Element/Hash.php
+++ library/Zend/Form/Element/Hash.php
@@ -184,7 +184,7 @@ class Hash extends Xhtml
      */
     public function getSessionName()
     {
-        return __CLASS__ . '_' . $this->getSalt() . '_' . $this->getName();
+        return str_replace('\\', '_', __CLASS__) . '_' . $this->getSalt() . '_' . $this->getName();
     }
 
     /**

3ddbe678843f47b0bf598b7a200170bc22c31f20
Mickael Perraud <perraud.mickael@orange.fr>
2010-10-03 14:19:07 +0200

    Memory: review exceptions, add more tests to cover them, all tests passed

--- library/Zend/Memory/Exception/InvalidArgumentException.php
+++ library/Zend/Memory/Exception/InvalidArgumentException.php
@@ -22,15 +22,20 @@
 /**
  * @namespace
  */
-namespace Zend\Memory;
+namespace Zend\Memory\Exception;
+use Zend\Memory\Exception;
 
 /**
- * @uses       \Zend\Exception
+ * Exception for Zend_Memory component.
+ *
+ * @uses       Zend\Exception
  * @category   Zend
  * @package    Zend_Memory
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-interface Exception
-{}
-
+class InvalidArgumentException
+    extends \InvalidArgumentException
+    implements Exception
+{
+}

b5ccba9092814dc6e22794d12cf2c47cbdf4a9bd
Mickael Perraud <perraud.mickael@orange.fr>
2010-10-04 19:43:10 +0200

    Mime: review exceptions, all tests passed

--- library/Zend/Mime/Message.php
+++ library/Zend/Mime/Message.php
@@ -221,7 +221,7 @@ class Message
         // no more parts, find end boundary
         $p = strpos($body, '--' . $boundary . '--', $start);
         if ($p===false) {
-            throw new Zend\Exception('Not a valid Mime Message: End Missing');
+            throw new Exception\RuntimeException('Not a valid Mime Message: End Missing');
         }
 
         // the remaining part also needs to be parsed:
@@ -273,7 +273,7 @@ class Message
                         $newPart->language = $value;
                         break;
                     default:
-                        throw new Zend\Exception('Unknown header ignored for MimePart:' . $key);
+                        throw new Exception\RuntimeException('Unknown header ignored for MimePart:' . $key);
                 }
             }
             $res->addPart($newPart);

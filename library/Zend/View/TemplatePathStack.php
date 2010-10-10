e83157d2e98e44c21f2cf8931421d5a3d47fd852
Matthew Weier O'Phinney <matthew@zend.com>
2010-10-10 12:59:01 -0400

    [al milestone] Refactor helper/filter usage in Zend\View
    - Added HelperLoader and HelperBroker, extending PluginClassLoader and
      PluginBroker, respectively
      - Refactored Zend\View to use HelperBroker for resolving/registering helpers
    - Refactored Zend\View to use FilterChain for filter chain
    - Added "Variables" class:
      - Registry for variables
      - Provides escape mechanism, including encoding
        - Auto-escapes variables
        - Provides access to raw values
      - Provides "strict" access (trigger notice on undefined variables)
    - Refactored Zend\View to use Variables object by default to register variables
      - todo: allow creating a "default" variable container when render() is passed
        variables
    - Added TemplateResolver interface, with method getScriptName()
    - Added TemplatePathStack:
      - Uses Zend\Stdlib\SplStack internally
      - Implements view stream for short tags
      - Implements LFI protections
    - Refactored Zend\View to use TemplatePathStack by default
    - Renamed ViewEngine interface to Renderer
      - Removed all methods except render() and getEngine()
    - Removed View class
    - Renamed AbstractView to PhpRenderer
      - no longer abstract
    - TODO
      - allow passing classes and options for each helping class (resolver,
        variables, broker, filter chain)
      - move engine implementation into a separate directory?
        - would include PhpRenderer, potentially TemplateResolver classes
      - refactor existing unit tests and write new ones

---
+++ library/Zend/View/TemplatePathStack.php
@@ -0,0 +1,248 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_View
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * @namespace
+ */
+namespace Zend\View;
+
+use SplFileInfo,
+    Zend\Stdlib\SplStack;
+
+/**
+ * Resolves view scripts based on a stack of paths
+ *
+ * @category   Zend
+ * @package    Zend_View
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class TemplatePathStack implements TemplateResolver
+{
+    /**
+     * @var SplStack
+     */
+    protected $paths;
+
+    /**
+     * Flag indicating whether or not LFI protection for rendering view scripts is enabled
+     * @var bool
+     */
+    protected $lfiProtectionOn = true;
+
+    /**@+
+     * Flags used to determine if a stream wrapper should be used for enabling short tags
+     * @var bool
+     */
+    protected $useViewStream    = false;
+    protected $useStreamWrapper = false;
+    /**@-*/
+
+    /**
+     * Constructor
+     * 
+     * @param  null|array|Traversable $options 
+     * @return void
+     */
+    public function __construct($options = null)
+    {
+        $this->useViewStream = (bool) ini_get('short_open_tag') ? false : true;
+        if ($this->useViewStream) {
+            if (!in_array('zend.view', stream_get_wrappers())) {
+                stream_wrapper_register('zend.view', 'Zend\View\Stream');
+            }
+        }
+
+        $this->paths         = new SplStack;
+        if (null !== $options) {
+            $this->setOptions($options);
+        }
+    }
+
+    /**
+     * Configure object
+     * 
+     * @param  array|Traversable $options 
+     * @return void
+     */
+    public function setOptions($options)
+    {
+        if (!is_array($options) && !$options instanceof \Traversable) {
+            throw new Exception(sprintf(
+                'Expected array or Traversable object; received "%s"',
+                (is_object($options) ? get_class($options) : gettype($options))
+            ));
+        }
+
+        foreach ($options as $key => $value) {
+            switch (strtolower($key)) {
+                case 'lfi_protection':
+                    $this->setLfiProtection($value);
+                    break;
+                case 'script_paths':
+                    $this->setPaths((array) $value);
+                    break;
+                case 'use_stream_wrapper':
+                    $this->setUseStreamWrapper($value);
+                default:
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Add many paths to the stack at once
+     * 
+     * @param  array $paths 
+     * @return ViewScriptPaths
+     */
+    public function setPaths(array $paths)
+    {
+        foreach ($paths as $path) {
+            $this->addPath($path);
+        }
+        return $this;
+    }
+
+    /**
+     * Add a single path to the stack
+     * 
+     * @param  string $path 
+     * @return ViewScriptPaths
+     */
+    public function addPath($path)
+    {
+        if (!is_string($path)) {
+            throw new Exception(sprintf(
+                'Invalid path provided; must be a string, received %s',
+                gettype($path)
+            ));
+        }
+        $this->paths[] = $path;
+        return $this;
+    }
+
+    /**
+     * Clear all paths
+     * 
+     * @return void
+     */
+    public function clearPaths()
+    {
+        $this->paths = new SplStack;
+    }
+
+    /**
+     * Returns stack of paths
+     * 
+     * @return SplStack
+     */
+    public function getPaths()
+    {
+        return $this->paths;
+    }
+
+    /**
+     * Set LFI protection flag
+     *
+     * @param  bool $flag
+     * @return \Zend\View\AbstractView
+     */
+    public function setLfiProtection($flag)
+    {
+        $this->lfiProtectionOn = (bool) $flag;
+        return $this;
+    }
+
+    /**
+     * Return status of LFI protection flag
+     *
+     * @return bool
+     */
+    public function isLfiProtectionOn()
+    {
+        return $this->lfiProtectionOn;
+    }
+
+    /**
+     * Set flag indicating if stream wrapper should be used if short_open_tag is off
+     *
+     * @param  bool $flag
+     * @return \Zend\View\View
+     */
+    public function setUseStreamWrapper($flag)
+    {
+        $this->useStreamWrapper = (bool) $flag;
+        return $this;
+    }
+
+    /**
+     * Should the stream wrapper be used if short_open_tag is off?
+     *
+     * Returns true if the use_stream_wrapper flag is set, and if short_open_tag
+     * is disabled.
+     *
+     * @return bool
+     */
+    public function useStreamWrapper()
+    {
+        return ($this->useViewStream && $this->useStreamWrapper);
+    }
+
+    /**
+     * Retrieve the filesystem path to a view script
+     * 
+     * @param  string $name 
+     * @return string
+     */
+    public function getScriptPath($name)
+    {
+        if ($this->isLfiProtectionOn() && preg_match('#\.\.[\\\/]#', $name)) {
+            $e = new Exception('Requested scripts may not include parent directory traversal ("../", "..\\" notation)');
+            throw $e;
+        }
+
+        if (!count($this->scriptPaths)) {
+            $e = new Exception('No view script directory set; unable to determine location for view script');
+            throw $e;
+        }
+
+        $paths   = PATH_SEPARATOR;
+        foreach ($this->scriptPaths as $path) {
+            $file = new SplFileInfo($path . $name);
+            if ($file->isReadable()) {
+                // Found! Return it.
+                $filePath = $file->getRealPath();
+                if ($this->useStreamWrapper()) {
+                    // If using a stream wrapper, prepend the spec to the path
+                    $filePath = 'zend.view://' . $filePath;
+                }
+                return $filePath;
+            }
+            $paths .= $path . PATH_SEPARATOR;
+        }
+
+        $e = new Exception(sprintf(
+            'Script "%s" not found in path (%s)',
+            $name, trim($paths, PATH_SEPARATOR)
+        ));
+        throw $e;
+    }
+}

ae9cda2e2be2749c2193a3634d14338a9c6cda2e
Matthew Weier O'Phinney <matthew@zend.com>
2010-10-15 13:46:10 -0400

    Refactored application plugins to use plugin spec broker
    - Added ResourceBroker, which extends Zend\Loader\PluginSpecBroker
    - Modified ResourceBootstrapper to only have set/getBroker() methods; all others
      are functionality present in PluginSpecBroker
    - Updated AbstractBootstrap:
      - Created set/getBroker() methods
      - Removed all plugin loading/manipulation methods from class

---
+++ library/Zend/Application/ResourceBroker.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_View
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace Zend\Application;
+
+use Zend\Loader\PluginSpecBroker;
+
+/**
+ * Plugin Broker implementation for bootstrap resources.
+ *
+ * @category   Zend
+ * @package    Zend_View
+ * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class ResourceBroker extends PluginSpecBroker
+{
+    /**
+     * @var string Default plugin loading strategy
+     */
+    protected $defaultClassLoader = 'Zend\Application\ResourceLoader';
+
+    /**
+     * Bootstrap object with which to inject plugins
+     * 
+     * @var Bootstrap
+     */
+    protected $bootstrap;
+
+    /**
+     * Set boostrap object with which to inject resources
+     * 
+     * @param  ResourceBootstrapper $bootstrap 
+     * @return ResourceBroker
+     */
+    public function setBootstrap(ResourceBootstrapper $bootstrap)
+    {
+        $this->bootstrap = $bootstrap;
+        return $this;
+    }
+
+    /**
+     * Retrieve bootstrap object
+     * 
+     * @return null|ResourceBootstrapper
+     */
+    public function getBootstrap()
+    {
+        return $this->bootstrap;
+    }
+
+    /**
+     * Load a resource
+     * 
+     * @param  mixed $plugin 
+     * @param  array $options 
+     * @return Zend\Application\Resource
+     */
+    public function load($plugin, array $options = null)
+    {
+        $resource = parent::load($plugin, $options);
+        if (null !== $bootstrap = $this->getBootstrap()) {
+            $resource->setBootstrap($bootstrap);
+        }
+        return $resource;
+    }
+
+    /**
+     * Determine if we have a valid resource
+     * 
+     * @param  mixed $plugin 
+     * @return true
+     * @throws InvalidArgumentException
+     */
+    protected function validatePlugin($plugin)
+    {
+        if (!$plugin instanceof Resource) {
+            throw new \InvalidArgumentException('Bootstrap resources must implement Zend\Application\Resource');
+        }
+        return true;
+    }
+}

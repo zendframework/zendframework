b6a8e05f14a8b0af94442dddfeb108956b812467
Victor Farazdagi <simple.square@gmail.com>
2010-10-22 21:57:08 +0400

    Service\Amazon: SimpleDb\OfflineTest updated

--- tests/Zend/Service/Amazon/SimpleDb/OfflineTest.php
+++ tests/Zend/Service/Amazon/SimpleDb/OfflineTest.php
@@ -17,13 +17,14 @@
  * @subpackage UnitTests
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id: OfflineTest.php 8064 2008-02-16 10:58:39Z thomas $
+ * @version    $Id$
  */
 
+
 /**
- * Test helper
+ * @namespace
  */
-require_once dirname(__FILE__) . '/../../../../TestHelper.php';
+namespace ZendTest\Service\Amazon\SimpleDb;
 
 /**
  * @category   Zend
@@ -32,7 +33,7 @@ require_once dirname(__FILE__) . '/../../../../TestHelper.php';
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Zend_Service_Amazon_SimpleDb_OfflineTest extends PHPUnit_Framework_TestCase
+class OfflineTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {

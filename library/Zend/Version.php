b957fb92cd2c1b770742d3480fa67e29fee53357
Matthew Weier O'Phinney <matthew@zend.com>
2010-10-30 13:02:18 -0400

    Updated version strings

--- library/Zend/Version.php
+++ library/Zend/Version.php
@@ -36,7 +36,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '2.0.0dev1';
+    const VERSION = '2.0.0dev2';
 
     /**
      * Compare the specified Zend Framework version string $version

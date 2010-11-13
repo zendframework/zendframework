29ddd05d184a08804809b34bfb07fe0d78887061
Shahar Evron <shahar.e@zend.com>
2010-11-13 19:50:16 +0200

    Modifying existing exceptions to implement marker interface

--- library/Zend/Uri/InvalidUriException.php
+++ library/Zend/Uri/InvalidUriException.php
@@ -34,7 +34,7 @@ namespace Zend\Uri;
  * @copyright Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd     New BSD License
  */
-class InvalidUriException extends Exception
+class InvalidUriException extends \InvalidArgumentException implements Exception
 {
     
 }

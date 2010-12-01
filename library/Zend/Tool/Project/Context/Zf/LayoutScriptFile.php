89f03d6e9839507a08232748f0c49a09df82af56
Matthew Weier O'Phinney <matthew@zend.com>
2010-12-01 12:17:23 -0500

    Zend_Tool: fix view variable assignment and retrieval
    - Updated LayoutScriptFile to pull layout helper from the broker
    - Updated ControllerFile to assign variables using Variables container of View
      when in the ErrorController
    - Updated ViewScriptFile to pull variables from the Variables container of View
      when in error.phtml

--- library/Zend/Tool/Project/Context/Zf/LayoutScriptFile.php
+++ library/Zend/Tool/Project/Context/Zf/LayoutScriptFile.php
@@ -102,7 +102,7 @@ class LayoutScriptFile extends \Zend\Tool\Project\Context\Filesystem\File
     public function getContents()
     {
         $contents = <<<'EOS'
-<?php echo $this->layout()->content; ?>
+<?php echo $this->broker('layout')->content; ?>
 
 EOS;
 

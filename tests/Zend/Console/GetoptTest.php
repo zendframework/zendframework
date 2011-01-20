374bebc7e0ce914755c66ac2b8f4c4ea1e47bc5f
Alexey S. Kachayev <kachayev@gmail.com>
2011-01-20 23:07:15 +0200

    Test case for cumulative params configuration, test works in red

--- tests/Zend/Console/GetoptTest.php
+++ tests/Zend/Console/GetoptTest.php
@@ -536,4 +536,18 @@ class GetoptTest extends \PHPUnit_Framework_TestCase
                 '--colors', 'blue'));
         $this->assertEquals('blue', $opts->colors, 'Should be equal to last variable');
     }
+
+    public function testGetoptWithCumulativeParamsOptionHandleArrayValues()
+    {
+        $opts = new Getopt(
+            array('colors|c' => 'Colors-option'),
+            array(
+                '--colors', 'red',
+                '--colors', 'green',
+                '--colors', 'blue'
+            ),
+            array(Getopt::CONFIG_CUMULATIVE_PARAMETERS => true));
+        $this->assertType('array', $opts->colors, 'Colors value should be an array');
+        $this->assertEquals('red,green,blue', implode(',', $opts->colors));
+    }
 }

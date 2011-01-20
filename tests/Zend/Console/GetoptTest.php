9afc7ede2be17c11f6198364c7c854635c5e5d79
Alexey S. Kachayev <kachayev@gmail.com>
2011-01-20 23:05:59 +0200

    Test case for ignoring cumulative params by default

--- tests/Zend/Console/GetoptTest.php
+++ tests/Zend/Console/GetoptTest.php
@@ -525,4 +525,15 @@ class GetoptTest extends \PHPUnit_Framework_TestCase
         );
         $this->assertEquals($fooValue, $opts->foo);
     }
+
+    public function testGetoptIgnoreCumulativeParamsByDefault()
+    {
+        $opts = new Getopt(
+            array('colors|c' => 'Colors-option'),
+            array(
+                '--colors', 'red',
+                '--colors', 'green',
+                '--colors', 'blue'));
+        $this->assertEquals('blue', $opts->colors, 'Should be equal to last variable');
+    }
 }

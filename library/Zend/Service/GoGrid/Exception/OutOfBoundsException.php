82b65203a502db77f59d6f5ac16cf4e0acfae153
ezimuel <enrico@zend.com>
2011-04-27 12:44:05 +0200

    Changes the comment

--- library/Zend/Service/GoGrid/Exception/OutOfBoundsException.php
+++ library/Zend/Service/GoGrid/Exception/OutOfBoundsException.php
@@ -14,7 +14,7 @@
  *
  * @uses       \Zend\Service\GoGrid\Exception
  * @category   Zend
- * @package    Zend_Service
+ * @package    Zend\Service
  * @subpackage GoGrid
  * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
@@ -27,7 +27,7 @@ namespace Zend\Service\Amazon\Exception;
 
 /**
  * @category   Zend
- * @package    Zend_Service
+ * @package    Zend\Service
  * @subpackage GoGrid
  * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License

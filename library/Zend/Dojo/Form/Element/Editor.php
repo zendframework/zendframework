84a0b9dbacd9de400f25390f0a31844ccf3461d1
Robert Basic <robertbasic.com@gmail.com>
2011-08-01 16:14:18 +0200

    importing the fix for ZF-11511 also fixes ZF2-28

--- library/Zend/Dojo/Form/Element/Editor.php
+++ library/Zend/Dojo/Form/Element/Editor.php
@@ -598,4 +598,102 @@ class Editor extends Dijit
         }
         return $this->getDijitParam('updateInterval');
     }
+    
+    /**
+     * Add a single editor extra plugin.
+     *
+     * @param  string $plugin
+     * @return Zend\Dojo\Form\Element\Editor
+     */
+    public function addExtraPlugin($plugin)
+    {
+        $plugin = (string) $plugin;
+        $extraPlugins = $this->getExtraPlugins();
+        if (in_array($plugin, $extraPlugins)) {
+            return $this;
+        }
+    
+        $extraPlugins[] = (string) $plugin;
+        $this->setDijitParam('extraPlugins', $extraPlugins);
+        return $this;
+    }
+    
+    /**
+     * Add multiple extra plugins.
+     *
+     * @param  array $extraPlugins
+     * @return Zend\Dojo\Form\Element\Editor
+     */
+    public function addExtraPlugins(array $plugins)
+    {
+        foreach ($plugins as $plugin) {
+            $this->addExtraPlugin($plugin);
+        }
+        return $this;
+    }
+    
+    /**
+     * Overwrite many extra plugins at once.
+     *
+     * @param  array $plugins
+     * @return Zend\Dojo\Form\Element\Editor
+     */
+    public function setExtraPlugins(array $plugins)
+    {
+        $this->clearExtraPlugins();
+        $this->addExtraPlugins($plugins);
+        return $this;
+    }
+    
+    /**
+     * Get all extra plugins.
+     *
+     * @return array
+     */
+    public function getExtraPlugins()
+    {
+        if (!$this->hasDijitParam('extraPlugins')) {
+            return array();
+        }
+        return $this->getDijitParam('extraPlugins');
+    }
+    
+    /**
+     * Is a given extra plugin registered?
+     *
+     * @param  string $plugin
+     * @return bool
+     */
+    public function hasExtraPlugin($plugin)
+    {
+        $extraPlugins = $this->getExtraPlugins();
+        return in_array((string) $plugin, $extraPlugins);
+    }
+    
+    /**
+     * Remove a given extra plugin.
+     *
+     * @param  string $plugin
+     * @return Zend\Dojo\Form\Element\Editor
+     */
+    public function removeExtraPlugin($plugin)
+    {
+        $extraPlugins = $this->getExtraPlugins();
+        if (false === ($index = array_search($plugin, $extraPlugins))) {
+            return $this;
+        }
+        unset($extraPlugins[$index]);
+        $this->setDijitParam('extraPlugins', $extraPlugins);
+        return $this;
+    }
+    
+    /**
+     * Clear all extra plugins.
+     *
+     * @return Zend\Dojo\Form\Element\Editor
+     */
+    public function clearExtraPlugins()
+    {
+        return $this->removeDijitParam('extraPlugins');
+    }
 }
--- tests/Zend/Dojo/Form/Element/EditorTest.php
+++ tests/Zend/Dojo/Form/Element/EditorTest.php
@@ -229,4 +229,23 @@ class EditorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($this->element->getDijitParam('updateInterval'), $this->element->getUpdateInterval());
         $this->assertEquals(300, $this->element->getUpdateInterval());
     }
+    
+    public function testShouldNotHaveExtraPluginsByDefault()
+    {
+        $extraPlugins = $this->element->getExtraPlugins();
+        $this->assertTrue(empty($extraPlugins));
+    }
+
+    public function testExtraPluginAccessorsShouldProxyToDijitParams()
+    {
+        $this->element->setExtraPlugins(array('undo', 'bold', 'italic'));
+        $this->assertTrue($this->element->hasDijitParam('extraPlugins'));
+        $this->assertTrue($this->element->hasExtraPlugin('bold'));
+        $this->assertEquals($this->element->getDijitParam('extraPlugins'), $this->element->getExtraPlugins());
+
+        $this->element->removeExtraPlugin('bold');
+        $this->assertFalse($this->element->hasExtraPlugin('bold'), var_export($this->element->getExtraPlugins(), 1));
+        $extraPlugins = $this->element->getDijitParam('extraPlugins');
+        $this->assertNotContains('bold', $extraPlugins, var_export($extraPlugins, 1));
+    }
 }

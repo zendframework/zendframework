ec5e528a19325d51c3c7f72d7b8acbf27cdfea8b
Robert Basic <robertbasic.com@gmail.com>
2011-08-01 16:02:44 +0200

    importing the fix for ZF-5711

--- library/Zend/Dojo/View/Helper/Editor.php
+++ library/Zend/Dojo/View/Helper/Editor.php
@@ -62,7 +62,7 @@ class Editor extends Dijit
      * JSON-encoded parameters
      * @var array
      */
-    protected $_jsonParams = array('captureEvents', 'events', 'plugins');
+    protected $_jsonParams = array('captureEvents', 'events', 'plugins', 'extraPlugins');
 
     /**
      * dijit.Editor

599f541876afd3e703f16896c6f435a990a1d499
Matthew Weier O'Phinney <matthew@zend.com>
2011-09-02 15:01:38 -0500

    Added information/metadata class

---
+++ modules/Zf2Mvc/Information.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Zf2Mvc;
+
+/**
+ * Class detailing metadata about this module
+ *
+ * Ideas include exposing DI definitions, event handlers, configuration, etc.
+ */
+class Information
+{
+}

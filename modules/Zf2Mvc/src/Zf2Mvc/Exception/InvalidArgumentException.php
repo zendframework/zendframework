14ec5f7f2af53ccf76e06596bf629d71870462ae
Matthew Weier O'Phinney <matthew@zend.com>
2011-09-02 15:44:35 -0500

    Added locator functionality

---
+++ modules/Zf2Mvc/src/Zf2Mvc/Exception/InvalidArgumentException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zf2Mvc\Exception;
+
+use Zf2Mvc\Exception;
+
+class InvalidArgumentException 
+    extends \InvalidArgumentException
+    implements Exception
+{}
---
+++ modules/Zf2Mvc/tests/Zf2Mvc/TestAsset/Locator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zf2Mvc\TestAsset;
+
+/**
+ * Dummy locator used to test handling of locator objects by Application
+ */
+class Locator
+{
+    public function get($name, array $params = array())
+    {
+    }
+}

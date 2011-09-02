d35a07d86bf82aed82cd007e92a9f20ceb709711
Matthew Weier O'Phinney <matthew@zend.com>
2011-09-02 15:18:26 -0500

    Updated AppContext interface
    - Removed configuration and dispatcher/resolver from interface;
      unnecessary
    
    - Documented all methods with intent

--- modules/Zf2Mvc/src/Zf2Mvc/AppContext.php
+++ modules/Zf2Mvc/src/Zf2Mvc/AppContext.php
@@ -6,18 +6,64 @@ use Zend\EventManager\EventCollection;
 
 interface AppContext
 {
-    public function setConfiguration($config);
-    public function setDispatcher(Dispatcher $dispatcher);
+    /**
+     * Set the event manager instance used by this context
+     * 
+     * @param  EventCollection $events 
+     * @return AppContext
+     */
     public function setEventManager(EventCollection $events);
+
+    /**
+     * Set a service locator object
+     *
+     * Since the DI DependencyInjection and ServiceLocation objects do not 
+     * share a common interface, we will not specify an interface here. That
+     * said, both implement the same "get()" method signature, and this is 
+     * what we will enforce.
+     * 
+     * @param  mixed $locator 
+     * @return AppContext
+     */
     public function setLocator($locator);
+
+    /**
+     * Set the router used to decompose the request
+     *
+     * A router should return a metadata object containing a controller key.
+     * 
+     * @param  Router $router 
+     * @return AppContext
+     */
     public function setRouter(Router $router);
 
-    public function getConfiguration();
-    public function getDispatcher();
+    /**
+     * Get the locator object
+     * 
+     * @return mixed
+     */
     public function getLocator();
+
+    /**
+     * Get the router object
+     * 
+     * @return Router
+     */
     public function getRouter();
 
+    /**
+     * Retrieve the event manager
+     *
+     * Lazy-loads an EventManager instance if none registered.
+     * 
+     * @return EventCollection
+     */
     public function events();
 
+    /**
+     * Run the application
+     * 
+     * @return \Zend\Http\Response
+     */
     public function run();
 }

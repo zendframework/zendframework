0f036187c812a1a1836a642d4952905dcad5ef9e
Matthew Weier O'Phinney <matthew@zend.com>
2011-09-04 12:17:54 -0500

    Committed basic action and RESTful controller implementations
    - These would not _need_ to be used, but provide a solid foundation, and
      also a predictable upgrade path from ZF1.

--- modules/Zf2Mvc/src/Zf2Mvc/Application.php
+++ modules/Zf2Mvc/src/Zf2Mvc/Application.php
@@ -197,7 +197,11 @@ class Application implements AppContext
     /**
      * Run the application
      * 
+<<<<<<< HEAD
      * @events route.pre, route.post, dispatch.pre, dispatch.post, dispatch.error
+=======
+     * @events route.pre, route.post, dispatch.pre, dispatch.post
+>>>>>>> Committed basic action and RESTful controller implementations
      * @return Response
      */
     public function run()
@@ -309,7 +313,6 @@ class Application implements AppContext
         $request  = $this->getRequest();
         $request->setMetadata('route-match', $routeMatch);
         $response = $this->getResponse();
-
         $return   = $controller->dispatch($request, $response);
 
         complete:

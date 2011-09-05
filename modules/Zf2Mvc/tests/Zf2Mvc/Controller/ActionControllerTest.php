c7d5bf88decba073ce25efd2f4fc76efd4e63f36
Matthew Weier O'Phinney <matthew@zend.com>
2011-09-05 16:18:41 -0500

    Tested action controller
    - Tested basics: are actions dispatched, is index action dispatched, is
      not found action dispatched
    - Ensured event manager short-circuits correctly, and listens on a
      variety of classes/interfaces

---
+++ modules/Zf2Mvc/tests/Zf2Mvc/Controller/ActionControllerTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Zf2Mvc\Controller;
+
+use PHPUnit_Framework_TestCase as TestCase,
+    Zend\EventManager\StaticEventManager,
+    Zend\Http\Request,
+    Zend\Http\Response,
+    Zf2Mvc\Router\RouteMatch;
+
+class ActionControllerTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->controller = new TestAsset\SampleController();
+        $this->routeMatch = new RouteMatch(array('controller' => 'controller-sample'));
+        $this->request    = new Request();
+        $this->request->setMetadata('route-match', $this->routeMatch);
+
+        StaticEventManager::resetInstance();
+    }
+
+    public function testDispatchInvokesIndexActionWhenNoActionPresentInRouteMatch()
+    {
+        $result = $this->controller->dispatch($this->request);
+        $this->assertArrayHasKey('content', $result);
+        $this->assertContains('Placeholder page', $result['content']);
+    }
+
+    public function testDispatchInvokesNotFoundActionWhenInvalidActionPresentInRouteMatch()
+    {
+        $this->routeMatch->setParam('action', 'totally-made-up-action');
+        $result = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertArrayHasKey('content', $result);
+        $this->assertContains('Page not found', $result['content']);
+    }
+
+    public function testDispatchInvokesProvidedActionWhenMethodExists()
+    {
+        $this->routeMatch->setParam('action', 'test');
+        $result = $this->controller->dispatch($this->request);
+        $this->assertArrayHasKey('content', $result);
+        $this->assertContains('test', $result['content']);
+    }
+
+    public function testDispatchCallsActionMethodBasedOnNormalizingAction()
+    {
+        $this->routeMatch->setParam('action', 'test.some-strangely_separated.words');
+        $result = $this->controller->dispatch($this->request);
+        $this->assertArrayHasKey('content', $result);
+        $this->assertContains('Test Some Strangely Separated Words', $result['content']);
+    }
+
+    public function testShortCircuitsBeforeActionIfPreDispatchReturnsAResponse()
+    {
+        $response = new Response();
+        $response->setContent('short circuited!');
+        $this->controller->events()->attach('dispatch.pre', function($e) use ($response) {
+            return $response;
+        });
+        $result = $this->controller->dispatch($this->request);
+        $this->assertSame($response, $result);
+    }
+
+    public function testPostDispatchEventAllowsReplacingResponse()
+    {
+        $response = new Response();
+        $response->setContent('short circuited!');
+        $this->controller->events()->attach('dispatch.post', function($e) use ($response) {
+            return $response;
+        });
+        $result = $this->controller->dispatch($this->request);
+        $this->assertSame($response, $result);
+    }
+
+    public function testEventManagerListensOnDispatchableInterfaceByDefault()
+    {
+        $response = new Response();
+        $response->setContent('short circuited!');
+        $events = StaticEventManager::getInstance();
+        $events->attach('Zend\Stdlib\Dispatchable', 'dispatch.pre', function($e) use ($response) {
+            return $response;
+        });
+        $result = $this->controller->dispatch($this->request);
+        $this->assertSame($response, $result);
+    }
+
+    public function testEventManagerListensOnActionControllerClassByDefault()
+    {
+        $response = new Response();
+        $response->setContent('short circuited!');
+        $events = StaticEventManager::getInstance();
+        $events->attach('Zf2Mvc\Controller\ActionController', 'dispatch.pre', function($e) use ($response) {
+            return $response;
+        });
+        $result = $this->controller->dispatch($this->request);
+        $this->assertSame($response, $result);
+    }
+
+    public function testEventManagerListensOnClassNameByDefault()
+    {
+        $response = new Response();
+        $response->setContent('short circuited!');
+        $events = StaticEventManager::getInstance();
+        $events->attach(get_class($this->controller), 'dispatch.pre', function($e) use ($response) {
+            return $response;
+        });
+        $result = $this->controller->dispatch($this->request);
+        $this->assertSame($response, $result);
+    }
+}

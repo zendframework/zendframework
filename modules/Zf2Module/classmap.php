8af352653955fb4062a142c518237d8c1159892e
Evan Coury <me@evancoury.com>
2011-09-08 07:45:04 -0700

    Initial placeholder for module loader work

---
+++ modules/Zf2Module/classmap.php
@@ -0,0 +1,5 @@
+<?php
+return array (
+  'Zf2Module\\Information' => __DIR__ . DIRECTORY_SEPARATOR . 'Information.php',
+  'Zf2Module\\ModuleLoader' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleLoader.php',
+);

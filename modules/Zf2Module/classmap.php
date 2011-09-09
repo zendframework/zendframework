edd0bfd90f0a8b3aac4b9fa45bd1da9fd40d7a88
Evan Coury <me@evancoury.com>
2011-09-09 09:07:22 -0700

    Updated classmaps for Zf2Mvc and Zf2Module

--- modules/Zf2Module/classmap.php
+++ modules/Zf2Module/classmap.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Zf2Module\\ModuleCollectionTest' => __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleCollectionTest.php',
-  'Zf2Module\\Information' => __DIR__ . DIRECTORY_SEPARATOR . 'Information.php',
+  'Zf2Module\\Module' => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
   'Zf2Module\\Exception' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'Exception.php',
   'Zf2Module\\ModuleLoader' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleLoader.php',
   'Zf2Module\\ModuleResolver' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleResolver.php',

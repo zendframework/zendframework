8604c9ab2bd439648ee8cb1a75be1418a65f5dbd
Evan Coury <me@evancoury.com>
2011-09-14 13:39:27 -0700

    s/ModuleCollection/ModuleManager

--- modules/Zf2Module/classmap.php
+++ modules/Zf2Module/classmap.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  'Zf2Module\\ModuleCollectionTest' => __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleCollectionTest.php',
+  'Zf2Module\\ModuleManagerTest' => __DIR__ . DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleManagerTest.php',
   'Zf2Module\\Module' => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
   'Zf2Module\\Exception' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'Exception.php',
   'Zf2Module\\ModuleLoader' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleLoader.php',
   'Zf2Module\\ModuleResolver' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleResolver.php',
-  'Zf2Module\\ModuleCollection' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleCollection.php',
+  'Zf2Module\\ModuleManager' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Zf2Module' . DIRECTORY_SEPARATOR . 'ModuleManager.php',
 );

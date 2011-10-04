43c1453677da7b04f202a2aaa948de8ead60e33c
Matthew Weier O'Phinney <matthew@zend.com>
2011-10-04 13:54:48 -0500

    Added Redirect controller plugin

--- library/Zend/Mvc/Controller/PluginLoader.php
+++ library/Zend/Mvc/Controller/PluginLoader.php
@@ -10,6 +10,7 @@ class PluginLoader extends PluginClassLoader
      * @var array Pre-aliased plugins
      */
     protected $plugins = array(
-        'url' => 'Zend\Mvc\Controller\Plugin\Url',
+        'url'      => 'Zend\Mvc\Controller\Plugin\Url',
+        'redirect' => 'Zend\Mvc\Controller\Plugin\Redirect',
     );
 }

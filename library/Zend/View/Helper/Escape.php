7187b86ec21c2289642f95928656dcaf1167d14d
Matthew Weier O'Phinney <matthew@zend.com>
2011-10-06 10:53:07 -0500

    Added recursive escape functionalities
    - Object passed with no recursion flag will attempt to cast to string
      - If cannot cast, an exception is raised
    - Object passed with recursion flag will:
      - if toArray() is defined, recurse that structure
      - otherwise, cast the object to an array and recurse that
      - but always return an array of escaped values
    - Array passed with no recursion flag will raise an exception
    - Array passed with recursion flag will recurse the array to escape
      values, and return the escaped version

--- library/Zend/View/Helper/Escape.php
+++ library/Zend/View/Helper/Escape.php
@@ -37,6 +37,14 @@ use Zend\View\Exception;
  */
 class Escape extends AbstractHelper
 {
+    /**@+
+     * @const Recursion constants
+     */
+    const RECURSE_NONE   = 0x00;
+    const RECURSE_ARRAY  = 0x01;
+    const RECURSE_OBJECT = 0x02;
+    /**@-*/
+
     /**
      * @var callback
      */
@@ -108,12 +116,42 @@ class Escape extends AbstractHelper
     /**
      * Invoke this helper: escape a value
      * 
-     * @param  string $value 
-     * @return string
+     * @param  mixed $value 
+     * @param  int $recurse Expects one of the recursion constants; used to decide whether or not to recurse the given value when escaping
+     * @return mixed Given a scalar, a scalar value is returned. Given an object, with the $recurse flag not allowing object recursion, returns a string. Otherwise, returns an array.
      */
-    public function __invoke($value)
+    public function __invoke($value, $recurse = self::RECURSE_NONE)
     {
-        $callback = $this->getCallback();
-        return call_user_func($callback, $value);
+        if (is_string($value)) {
+            $callback = $this->getCallback();
+            return call_user_func($callback, $value);
+        }
+        if (is_array($value)) {
+            if (!(self::RECURSE_ARRAY & $recurse)) {
+                throw new Exception('Array provided to Escape helper, but flags do not allow recursion');
+            }
+            foreach ($value as $k => $v) {
+                $value[$k] = $this->__invoke($v, $recurse);
+            }
+            return $value;
+        }
+        if (is_object($value)) {
+            if (!(self::RECURSE_OBJECT & $recurse)) {
+                // Attempt to cast it to a string
+                if (method_exists($value, '__toString')) {
+                    $callback = $this->getCallback();
+                    return call_user_func($callback, (string) $value);
+                }
+
+                throw new Exception('Object provided to Escape helper, but flags do not allow recursion');
+            }
+            if (method_exists($value, 'toArray')) {
+                return $this->__invoke($value->toArray(), $recurse | self::RECURSE_ARRAY);
+            }
+            return $this->__invoke((array) $value, $recurse | self::RECURSE_ARRAY);
+        }
+
+        // At this point, we have a scalar; simply return it
+        return $value;
     }
 }
--- tests/Zend/View/Helper/EscapeTest.php
+++ tests/Zend/View/Helper/EscapeTest.php
@@ -3,6 +3,7 @@
 namespace ZendTest\View\Helper;
 
 use PHPUnit_Framework_TestCase as TestCase,
+    stdClass,
     Zend\View\Helper\Escape as EscapeHelper;
 
 class EscapeTest extends TestCase
@@ -46,6 +47,93 @@ class EscapeTest extends TestCase
         $text     = 'Hey! <b>This is some Text!</b> Yo!';
         $expected = htmlspecialchars($text, ENT_COMPAT, 'UTF-8', false);
         $test     = $this->helper->__invoke($text);
+        $this->assertEquals($expected, $test);
+    }
+
+    public function testAllowsRecursiveEscapingOfArrays()
+    {
+        $original = array(
+            'foo' => '<b>bar</b>',
+            'baz' => array(
+                '<em>bat</em>',
+                'second' => array(
+                    '<i>third</i>',
+                ),
+            ),
+        );
+        $expected = array(
+            'foo' => '&lt;b&gt;bar&lt;/b&gt;',
+            'baz' => array(
+                '&lt;em&gt;bat&lt;/em&gt;',
+                'second' => array(
+                    '&lt;i&gt;third&lt;/i&gt;',
+                ),
+            ),
+        );
+        $test = $this->helper->__invoke($original, EscapeHelper::RECURSE_ARRAY);
+        $this->assertEquals($expected, $test);
+    }
+
+    public function testWillCastObjectsToStringsBeforeEscaping()
+    {
+        $object = new TestAsset\Stringified;
+        $test = $this->helper->__invoke($object);
+        $this->assertEquals(get_class($object), $test);
+    }
+
+    public function testCanRecurseObjectImplementingToArray()
+    {
+        $original = array(
+            'foo' => '<b>bar</b>',
+            'baz' => array(
+                '<em>bat</em>',
+                'second' => array(
+                    '<i>third</i>',
+                ),
+            ),
+        );
+        $object = new TestAsset\ToArray();
+        $object->array = $original;
+
+        $expected = array(
+            'foo' => '&lt;b&gt;bar&lt;/b&gt;',
+            'baz' => array(
+                '&lt;em&gt;bat&lt;/em&gt;',
+                'second' => array(
+                    '&lt;i&gt;third&lt;/i&gt;',
+                ),
+            ),
+        );
+        $test = $this->helper->__invoke($object, EscapeHelper::RECURSE_OBJECT);
+        $this->assertEquals($expected, $test);
+    }
+
+    public function testCanRecurseObjectProperties()
+    {
+        $original = array(
+            'foo' => '<b>bar</b>',
+            'baz' => array(
+                '<em>bat</em>',
+                'second' => array(
+                    '<i>third</i>',
+                ),
+            ),
+        );
+        $object = new stdClass();
+        foreach ($original as $key => $value) {
+            $object->$key = $value;
+        }
+
+        $expected = array(
+            'foo' => '&lt;b&gt;bar&lt;/b&gt;',
+            'baz' => array(
+                '&lt;em&gt;bat&lt;/em&gt;',
+                'second' => array(
+                    '&lt;i&gt;third&lt;/i&gt;',
+                ),
+            ),
+        );
+        $test = $this->helper->__invoke($object, EscapeHelper::RECURSE_OBJECT);
         $this->assertEquals($expected, $test);
     }
 }

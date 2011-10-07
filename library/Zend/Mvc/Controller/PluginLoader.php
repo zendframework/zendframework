5c1cdffab9f7de3e5df356ee25e8bd6b2bbd594c
Matthew Weier O'Phinney <matthew@zend.com>
2011-10-07 09:26:13 -0500

    Added FlashMessenger plugin
    - Adapted from ZF1 action helper
    - Fully unit tested at this time

--- library/Zend/Mvc/Controller/PluginLoader.php
+++ library/Zend/Mvc/Controller/PluginLoader.php
@@ -10,8 +10,10 @@ class PluginLoader extends PluginClassLoader
      * @var array Pre-aliased plugins
      */
     protected $plugins = array(
-        'forward'  => 'Zend\Mvc\Controller\Plugin\Forward',
-        'redirect' => 'Zend\Mvc\Controller\Plugin\Redirect',
-        'url'      => 'Zend\Mvc\Controller\Plugin\Url',
+        'flash_messenger' => 'Zend\Mvc\Controller\Plugin\FlashMessenger',
+        'flashmessenger'  => 'Zend\Mvc\Controller\Plugin\FlashMessenger',
+        'forward'         => 'Zend\Mvc\Controller\Plugin\Forward',
+        'redirect'        => 'Zend\Mvc\Controller\Plugin\Redirect',
+        'url'             => 'Zend\Mvc\Controller\Plugin\Url',
     );
 }

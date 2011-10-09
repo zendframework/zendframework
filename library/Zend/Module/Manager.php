294151fe5143050789b7a26b7c1e90f0591dff56
Matt Cockayne <matt@zucchi.co.uk>
2011-10-09 16:03:31 +0100

    removed tabs entered in to file in error

--- library/Zend/Module/Manager.php
+++ library/Zend/Module/Manager.php
@@ -45,12 +45,13 @@ class Manager
      * @var array
      */
     protected $dependencies = array();
-    
+
     /**
      * regex for getting version operators
      * @var string
      */
     protected $operators = '/(<|lt|<=|le|>|gt|>=|ge|==|=|eq|!=|<>|ne)?(\d.*)/';
+
     /**
      * __construct 
      * 
@@ -109,7 +110,7 @@ class Manager
         if (!isset($this->loadedModules[$moduleName])) {
             $class = $moduleName . '\Module';
             $module = new $class;
-            
+
             $this->addProvision($module);
             $this->addDependency($module);
             $this->runModuleInit($module);
@@ -139,19 +140,19 @@ class Manager
      */
     public function addProvision($module)
     {
-     	// check for and load provides
+         // check for and load provides
         if (method_exists($module, 'getProvides')) {
-        	$provision = $module->getProvides();
-        	foreach ($provision AS $name => $info) {
-	        	if (isset($this->provided[$name])) {
-	        		throw new \RuntimeException("Double provision has occured for: {$name} {$info['version']}");
-	        	}
-	        	$this->provided[$name] = $info;	
+            $provision = $module->getProvides();
+               foreach ($provision AS $name => $info) {
+                if (isset($this->provided[$name])) {
+                    throw new \RuntimeException("Double provision has occured for: {$name} {$info['version']}");
+                }
+                $this->provided[$name] = $info;	
         	}
         }
     	return $this;
     }
-    
+
     /**
      * add dependencies from module
      * 
@@ -161,12 +162,12 @@ class Manager
      * 
      * <code>
      * return array(
-	 *		'php' => array(
-     *			'version' => '5.3.0',
-     *			'required' => true,
-     *		),
-     *		'ext/pdo_mysql' => true
-     *	);
+     *      'php' => array(
+     *          'version' => '5.3.0',
+     *          'required' => true,
+     *      ),
+     *      'ext/pdo_mysql' => true
+     * );
      * </code>
      * 
      * version and required data are optional
@@ -176,32 +177,31 @@ class Manager
      */
     public function addDependency($module)
     {
-    	// check for an load dependencies required
-    	if (method_exists($module, 'getDependencies')) {
-        	// iterate over dependencies to evaluate min required version
-        	foreach ($module->getDependencies() AS $dep => $depInfo) {
-        		if (!isset($this->dependencies[$dep])) { // if the dep isnt present just add it
-        			$this->dependencies[$dep] = $depInfo; 	
-        		} else { // dep already present
-        			if (is_array($depInfo)) { // if is array need to check versions
-        				if (isset($this->dependencies[$dep]['version']) && isset($depInfo)) {
-	        				if (version_compare($this->dependencies[$dep]['version'], $depInfo['version']) >= 0) {
-	        					$depInfo['version'] = $this->dependencies[$dep]['version'];// set to highest version
-	        				}
-        				}
-        				if (!is_array($this->dependencies[$dep])) { 
-        					$this->dependencies[$dep] = $depInfo;
-        				} else {
-        					$this->dependencies[$dep] = array_merge($this->dependencies[$dep], $depInfo);
-        				}
-        			}
-        			
-        		}
-        	}
+        // check for an load dependencies required
+        if (method_exists($module, 'getDependencies')) {
+            // iterate over dependencies to evaluate min required version
+            foreach ($module->getDependencies() AS $dep => $depInfo) {
+                if (!isset($this->dependencies[$dep])) { // if the dep isnt present just add it
+                    $this->dependencies[$dep] = $depInfo; 	
+                } else { // dep already present
+                    if (is_array($depInfo)) { // if is array need to check versions
+                        if (isset($this->dependencies[$dep]['version']) && isset($depInfo)) {
+                            if (version_compare($this->dependencies[$dep]['version'], $depInfo['version']) >= 0) {
+                                $depInfo['version'] = $this->dependencies[$dep]['version'];// set to highest version
+                            }
+                        }
+                        if (!is_array($this->dependencies[$dep])) { 
+                            $this->dependencies[$dep] = $depInfo;
+                        } else {
+                            $this->dependencies[$dep] = array_merge($this->dependencies[$dep], $depInfo);
+                        }
+                    }
+                }
+            }
         }
         return $this;
     }
-    
+
     /**
      * return the currently loaded dependencies
      * 
@@ -209,9 +209,9 @@ class Manager
      */
     public function getDependencies()
     {
-    	return $this->dependencies;
+        return $this->dependencies;
     }
-    
+
     /**
      * return the currently loaded provisions
      * 
@@ -219,7 +219,7 @@ class Manager
      */
     public function getProvisions()
     {
-    	return $this->provided;
+        return $this->provided;
     }
     
     /**
@@ -238,17 +238,17 @@ class Manager
     public function resolveDependencies()
     {
         foreach ($this->getDependencies() as $dep => $depInfo) {
-        	if (isset($depInfo['version'])) {
-        		preg_match($this->operators,$depInfo['version'], $matches, PREG_OFFSET_CAPTURE);
-        		$version = $matches[2][0];
-        		$operator = $matches[1][0] ?: '>=';
-        	} else {
-        		$version = 0;
-        		$operator = '>=';
-        	}
-            
+            if (isset($depInfo['version'])) {
+                preg_match($this->operators,$depInfo['version'], $matches, PREG_OFFSET_CAPTURE);
+                $version = $matches[2][0];
+                $operator = $matches[1][0] ?: '>=';
+            } else {
+                $version = 0;
+                $operator = '>=';
+            }
+
             if ($dep === 'php') { // is php version requirement
-            	$this->dependencies[$dep]['satisfied'] = true;
+                $this->dependencies[$dep]['satisfied'] = true;
                 if (!version_compare(PHP_VERSION, $version, $operator)) {
                     if (isset($depInfo['required']) && $depInfo['required'] == true) {
                         throw new \RuntimeException("Required dependency unsatisfied: {$dep} {$depInfo['version']}");
@@ -267,25 +267,24 @@ class Manager
                     }
                 }
             } elseif (substr($dep, 0, 4) === 'lib/') { // is library requirement
-            // @todo: add library detection
-            
-            
+                // @todo: add library detection
+
             } else { // is module requirement
-            	if (is_scalar($this->dependencies[$dep])) {
-            		$this->dependencies[$dep] = array();
-            	}
-             	if (!isset($depInfo['satisfied'])) {
-             		$this->dependencies[$dep]['satisfied'] = false;
-	                if (isset($this->provided[$dep])) { // if provided set satisfaction
-	                  	if (isset($depInfo['version'])){ // if dep have version requirement
-	                   		if (version_compare($this->provided[$dep]['version'], $version, $operator) >= 0) {
-			                   	$this->dependencies[$dep]['satisfied'] = true;
-			                }
-	                  	} else {
-	                  		$this->dependencies[$dep]['satisfied'] = true;
-	                   	}
+                if (is_scalar($this->dependencies[$dep])) {
+                    $this->dependencies[$dep] = array();
+                }
+                if (!isset($depInfo['satisfied'])) {
+                    $this->dependencies[$dep]['satisfied'] = false;
+                    if (isset($this->provided[$dep])) { // if provided set satisfaction
+                        if (isset($depInfo['version'])){ // if dep have version requirement
+                            if (version_compare($this->provided[$dep]['version'], $version, $operator) >= 0) {
+                                $this->dependencies[$dep]['satisfied'] = true;
+                            }
+                        } else {
+                            $this->dependencies[$dep]['satisfied'] = true;
+                        }
 	                 }
-               	}
+                }
                 if (!$this->dependencies[$dep]['satisfied']) {
                     if (isset($depInfo['required']) && $depInfo['required'] == true ) {
                         throw new \RuntimeException("Required dependency unsatisfied: {$dep} " . (isset($depInfo['version']) ?: ''));

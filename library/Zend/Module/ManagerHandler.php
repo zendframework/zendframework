b4051747e95ba5d6a83b7bc88583e09c5bddab7f
Evan Coury <me@evancoury.com>
2011-11-13 09:33:20 -0700

    Add Zend\Module\ManagerHandler interface for alternative module manager implementations

---
+++ library/Zend/Module/ManagerHandler.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Zend\Module;
+
+use Zend\EventManager\EventCollection,
+    Zend\Module\Listener\ConfigMerger;
+
+interface ManagerHandler
+{
+    /**
+     * Load the provided modules.
+     * 
+     * @return ManagerHandler
+     */
+    public function loadModules();
+
+    /**
+     * Load a specific module by name.
+     * 
+     * @param string $moduleName 
+     * @return mixed Module's Module class
+     */
+    public function loadModule($moduleName);
+
+    /**
+     * Get an array of the loaded modules.
+     *
+     * @param bool $loadModules If true, load modules if they're not already
+     * @return array An array of Module objects, keyed by module name
+     */
+    public function getLoadedModules($loadModules);
+
+    /**
+     * Get the array of modules that this manager should load.
+     *
+     * @return array
+     */
+    public function getModules();
+ 
+    /**
+     * Set an array of module names that this module manager should load. 
+     *
+     * @param array $modules
+     * @return ModuleHandler
+     */
+    public function setModules(array $modules);
+
+    /**
+     * Get the listener that's in charge of merging module configs.
+     *
+     * @return ConfigMerger
+     */
+    public function getConfigListener();
+ 
+    /**
+     * Set the listener that's in charge of merging module configs.
+     *
+     * @param ConfigMerger $configListener
+     * @return ModuleHandler
+     */
+    public function setConfigListener(ConfigMerger $configListener);
+
+    /**
+     * Set the event manager instance used by this module manager.
+     * 
+     * @param  EventCollection $events 
+     * @return ManagerHandler
+     */
+    public function setEventManager(EventCollection $events);
+    
+    /**
+     * Retrieve the event manager
+     *
+     * Lazy-loads an EventManager instance if none registered.
+     * 
+     * @return EventCollection
+     */
+    public function events();
+}

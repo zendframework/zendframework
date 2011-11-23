7bcda18d68d1afb1b3a61d6fa331a6d6843d0bee
dchusovitin <dchusovitin@gmail.com>
2011-11-23 14:38:29 +0600

    Fixed Zend\Validator\InArray::__construct

--- tests/Zend/Validator/InArrayTest.php
+++ tests/Zend/Validator/InArrayTest.php
@@ -23,7 +23,8 @@
  * @namespace
  */
 namespace ZendTest\Validator;
-use Zend\Validator;
+use Zend\Validator,
+    ReflectionClass;
 
 /**
  * Test helper
@@ -202,5 +203,41 @@ class InArrayTest extends \PHPUnit_Framework_TestCase
 
         $validator->setRecursive(true);
         $this->assertTrue($validator->isValid('A'));
+    }
+    
+    public function testEqualsMessageTemplates()
+    {
+        $validator = new Validator\InArray(array());
+        $reflection = new ReflectionClass($validator);
+        
+        if(!$reflection->hasProperty('_messageTemplates')) {
+            return;
+        }
+        
+        $property = $reflection->getProperty('_messageTemplates');
+        $property->setAccessible(true);
+
+        $this->assertEquals(
+            $property->getValue($validator),
+            $validator->getOption('messageTemplates')
+        );
+    }
+    
+    public function testEqualsMessageVariables()
+    {
+        $validator = new Validator\InArray(array());
+        $reflection = new ReflectionClass($validator);
+        
+        if(!$reflection->hasProperty('_messageVariables')) {
+            return;
+        }
+        
+        $property = $reflection->getProperty('_messageVariables');
+        $property->setAccessible(true);
+
+        $this->assertEquals(
+            $property->getValue($validator),
+            $validator->getOption('messageVariables')
+        );
     }
 }

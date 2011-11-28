6e95d7d7caddb7f6169a713998a7e9f06c53638d
Matthew Weier O'Phinney <matthew@zend.com>
2011-11-28 12:42:05 -0600

    Implemented accessors for To, Cc, Bcc, and Reply-To headers
    - Implemented all accessors for To, CC, BCC, and Reply-To headers
    - Added helper methods for the above to remove code duplication (also
      affects From header manipulation):
      - clearHeaderByName()
      - getAddressListFromHeader()
      - updateAddressList()

--- library/Zend/Mail/Message.php
+++ library/Zend/Mail/Message.php
@@ -63,6 +63,13 @@ class Message
         return $this;
     }
 
+    /**
+     * Access headers collection
+     *
+     * Lazy-loads if not already attached.
+     * 
+     * @return Headers
+     */
     public function headers()
     {
         if (null === $this->headers) {
@@ -75,18 +82,13 @@ class Message
     /**
      * Set (overwrite) From addresses
      * 
-     * @param  string|Address|AddressList $emailOrAddressList 
+     * @param  string|AddressDescription|array|AddressList|Traversable $emailOrAddressList 
      * @param  string|null $name 
      * @return Message
      */
     public function setFrom($emailOrAddressList, $name = null)
     {
-        $headers = $this->headers();
-        if ($headers->has('from')) {
-            $header = $headers->get('from');
-            $headers->removeHeader($header);
-        }
-
+        $this->clearHeaderByName('from');
         return $this->addFrom($emailOrAddressList, $name);
     }
 
@@ -100,90 +102,170 @@ class Message
     public function addFrom($emailOrAddressOrList, $name = null)
     {
         $addressList = $this->from();
-        if ($emailOrAddressOrList instanceof Traversable) {
-            foreach ($emailOrAddressOrList as $address) {
-                $addressList->add($address);
-            }
-            return $this;
-        }
-        if (is_array($emailOrAddressOrList)) {
-            $addressList->addMany($emailOrAddressOrList);
-            return $this;
-        }
-        if (!is_string($emailOrAddressOrList) && !$emailOrAddressOrList instanceof AddressDescription) {
-            throw new Exception\InvalidArgumentException(sprintf(
-                '%s expects a string, AddressDescription, array, AddressList, or Traversable as its first argument; received "%s"',
-                __METHOD__,
-                (is_object($emailOrAddressOrList) ? get_class($emailOrAddressOrList) : gettype($emailOrAddressOrList))
-            ));
-        }
-        $addressList->add($emailOrAddressOrList, $name);
+        $this->updateAddressList(__METHOD__, $addressList, $emailOrAddressOrList, $name);
         return $this;
     }
 
     /**
-     * Retrieve list of From recipients
+     * Retrieve list of From senders
      * 
      * @return AddressList
      */
     public function from()
     {
-        $headers = $this->headers();
-        if ($headers->has('from')) {
-            $header = $headers->get('from');
-        } else {
-            $header = new Header\From();
-            $headers->addHeader($header);
-        }
-        return $header->getAddressList();
+        return $this->getAddressListFromHeader('from', __NAMESPACE__ . '\Header\From');
     }
 
+    /**
+     * Overwrite the address list in the To recipients
+     * 
+     * @param  string|AddressDescription|array|AddressList|Traversable $emailOrAddressList 
+     * @param  null|string $name 
+     * @return Message
+     */
     public function setTo($emailOrAddressList, $name = null)
     {
+        $this->clearHeaderByName('to');
+        return $this->addTo($emailOrAddressList, $name);
     }
 
+    /**
+     * Add one or more addresses to the To recipients
+     *
+     * Appends to the list.
+     * 
+     * @param  string|AddressDescription|array|AddressList|Traversable $emailOrAddressOrList 
+     * @param  null|string $name 
+     * @return Message
+     */
     public function addTo($emailOrAddressOrList, $name = null)
     {
+        $addressList = $this->to();
+        $this->updateAddressList(__METHOD__, $addressList, $emailOrAddressOrList, $name);
+        return $this;
     }
 
+    /**
+     * Access the address list of the To header
+     * 
+     * @return AddressList
+     */
     public function to()
     {
+        return $this->getAddressListFromHeader('to', __NAMESPACE__ . '\Header\To');
     }
 
+    /**
+     * Set (overwrite) CC addresses
+     * 
+     * @param  string|AddressDescription|array|AddressList|Traversable $emailOrAddressList 
+     * @param  string|null $name 
+     * @return Message
+     */
     public function setCc($emailOrAddressList, $name = null)
     {
+        $this->clearHeaderByName('cc');
+        return $this->addCc($emailOrAddressList, $name);
     }
 
+    /**
+     * Add a "Cc" address
+     * 
+     * @param  string|Address|array|AddressList|Traversable $emailOrAddressOrList 
+     * @param  string|null $name 
+     * @return Message
+     */
     public function addCc($emailOrAddressOrList, $name = null)
     {
+        $addressList = $this->cc();
+        $this->updateAddressList(__METHOD__, $addressList, $emailOrAddressOrList, $name);
+        return $this;
     }
 
+    /**
+     * Retrieve list of CC recipients
+     * 
+     * @return AddressList
+     */
     public function cc()
     {
+        return $this->getAddressListFromHeader('cc', __NAMESPACE__ . '\Header\Cc');
     }
 
+    /**
+     * Set (overwrite) BCC addresses
+     * 
+     * @param  string|AddressDescription|array|AddressList|Traversable $emailOrAddressList 
+     * @param  string|null $name 
+     * @return Message
+     */
     public function setBcc($emailOrAddressList, $name = null)
     {
+        $this->clearHeaderByName('bcc');
+        return $this->addBcc($emailOrAddressList, $name);
     }
 
+    /**
+     * Add a "Bcc" address
+     * 
+     * @param  string|Address|array|AddressList|Traversable $emailOrAddressOrList 
+     * @param  string|null $name 
+     * @return Message
+     */
     public function addBcc($emailOrAddressOrList, $name = null)
     {
+        $addressList = $this->bcc();
+        $this->updateAddressList(__METHOD__, $addressList, $emailOrAddressOrList, $name);
+        return $this;
     }
 
+    /**
+     * Retrieve list of BCC recipients
+     * 
+     * @return AddressList
+     */
     public function bcc()
     {
+        return $this->getAddressListFromHeader('bcc', __NAMESPACE__ . '\Header\Bcc');
     }
 
+    /**
+     * Overwrite the address list in the Reply-To recipients
+     * 
+     * @param  string|AddressDescription|array|AddressList|Traversable $emailOrAddressList 
+     * @param  null|string $name 
+     * @return Message
+     */
     public function setReplyTo($emailOrAddressList, $name = null)
     {
+        $this->clearHeaderByName('reply-to');
+        return $this->addReplyTo($emailOrAddressList, $name);
     }
 
+    /**
+     * Add one or more addresses to the Reply-To recipients
+     *
+     * Appends to the list.
+     * 
+     * @param  string|AddressDescription|array|AddressList|Traversable $emailOrAddressOrList 
+     * @param  null|string $name 
+     * @return Message
+     */
     public function addReplyTo($emailOrAddressOrList, $name = null)
     {
+        $addressList = $this->replyTo();
+        $this->updateAddressList(__METHOD__, $addressList, $emailOrAddressOrList, $name);
+        return $this;
     }
 
+    /**
+     * Access the address list of the Reply-To header
+     * 
+     * @return AddressList
+     */
     public function replyTo()
     {
+        return $this->getAddressListFromHeader('reply-to', __NAMESPACE__ . '\Header\ReplyTo');
     }
 
     public function setSender($emailOrAddress, $name = null)
@@ -212,5 +294,75 @@ class Message
 
     public function getBodyText()
     {
+    }
+
+    /**
+     * Clear a header by name
+     * 
+     * @param  string $headerName 
+     * @return void
+     */
+    protected function clearHeaderByName($headerName)
+    {
+        $headers = $this->headers();
+        if ($headers->has($headerName)) {
+            $header = $headers->get($headerName);
+            $headers->removeHeader($header);
+        }
+    }
+
+    /**
+     * Retrieve the AddressList from a named header
+     *
+     * Used with To, From, Cc, Bcc, and ReplyTo headers. If the header does not
+     * exist, instantiates it.
+     * 
+     * @param  string $headerName 
+     * @param  string $headerClass 
+     * @return AddressList
+     */
+    protected function getAddressListFromHeader($headerName, $headerClass)
+    {
+        $headers = $this->headers();
+        if ($headers->has($headerName)) {
+            $header = $headers->get($headerName);
+        } else {
+            $header = new $headerClass();
+            $headers->addHeader($header);
+        }
+        return $header->getAddressList();
+    }
+
+    /**
+     * Update an address list
+     *
+     * Proxied to this from addFrom, addTo, addCc, addBcc, and addReplyTo.
+     * 
+     * @param  string $callingMethod 
+     * @param  AddressList $addressList 
+     * @param  string|AddressDescription|array|AddressList|Traversable $emailOrAddressOrList 
+     * @param  null|string $name 
+     * @return void
+     */
+    protected function updateAddressList($callingMethod, AddressList $addressList, $emailOrAddressOrList, $name)
+    {
+        if ($emailOrAddressOrList instanceof Traversable) {
+            foreach ($emailOrAddressOrList as $address) {
+                $addressList->add($address);
+            }
+            return;
+        }
+        if (is_array($emailOrAddressOrList)) {
+            $addressList->addMany($emailOrAddressOrList);
+            return;
+        }
+        if (!is_string($emailOrAddressOrList) && !$emailOrAddressOrList instanceof AddressDescription) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                '%s expects a string, AddressDescription, array, AddressList, or Traversable as its first argument; received "%s"',
+                $callingMethod,
+                (is_object($emailOrAddressOrList) ? get_class($emailOrAddressOrList) : gettype($emailOrAddressOrList))
+            ));
+        }
+        $addressList->add($emailOrAddressOrList, $name);
     }
 }

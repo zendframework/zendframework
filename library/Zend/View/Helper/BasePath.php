2137623690e91c79e68449a8c185d0918a4599ae
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2011-12-30 11:17:24 +0100

    Fixed slashes stripping in basePath() view helper.

--- library/Zend/View/Helper/BasePath.php
+++ library/Zend/View/Helper/BasePath.php
@@ -58,7 +58,7 @@ class BasePath extends AbstractHelper
         }
 
         if (null !== $file) {
-            $file = '/' . ltrim($file, '/');
+            $file = ltrim($file, '/');
         }
 
         return $this->basePath . $file;
@@ -72,7 +72,7 @@ class BasePath extends AbstractHelper
      */
     public function setBasePath($basePath)
     {
-        $this->basePath = $basePath;
+        $this->basePath = rtrim($basePath, '/') . '/';
         return $this;
     }
 }

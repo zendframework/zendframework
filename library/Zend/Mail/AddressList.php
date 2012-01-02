3086047f8779e357d1336a96482fbb77ef2a3fa0
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2012-01-02 23:11:39 +0100

    Updated test cases for addMany() fix.

--- library/Zend/Mail/AddressList.php
+++ library/Zend/Mail/AddressList.php
@@ -84,7 +84,7 @@ class AddressList implements Countable, Iterator
             if (is_int($key) || is_numeric($key)) {
                 $this->add($value);
             } elseif (is_string($key)) {
-                $this->add($key, value);
+                $this->add($key, $value);
             } else {
                 throw new Exception\RuntimeException(sprintf(
                     'Invalid key type in provided addresses array ("%s")',

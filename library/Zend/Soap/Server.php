d29e1b600a94a0067136c0551413c4af236d2b91
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2012-01-06 16:46:14 +0100

    Allow to pass object instances to \Zend\Soap\Server::setClass().

--- library/Zend/Soap/Server.php
+++ library/Zend/Soap/Server.php
@@ -535,7 +535,7 @@ class Server implements \Zend\Server\Server
      *
      * See {@link setObject()} to set preconfigured object instances as request handlers.
      *
-     * @param string $class Class Name which executes SOAP Requests at endpoint.
+     * @param string|object $class Class name or object instance which executes SOAP Requests at endpoint.
      * @return \Zend\Soap\Server
      * @throws \Zend\Soap\ServerException if called more than once, or if class
      * does not exist
@@ -544,6 +544,10 @@ class Server implements \Zend\Server\Server
     {
         if (isset($this->_class)) {
             throw new Exception\InvalidArgumentException('A class has already been registered with this soap server instance');
+        }
+
+        if (is_object($class)) {
+            return $this->setObject($class);
         }
 
         if (!is_string($class)) {

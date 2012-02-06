a2fad6e938c64ad7c4b9d285d58d7ffddeea62cf
Evan Coury <me@evancoury.com>
2012-02-06 07:25:33 -0700

    [Zend\Module] Coding standard fixes

--- library/Zend/Module/Listener/LocatorRegistrationListener.php
+++ library/Zend/Module/Listener/LocatorRegistrationListener.php
@@ -19,12 +19,12 @@ class LocatorRegistrationListener extends AbstractListener
 
     public function addTypePreference(Event $e)
     {
-        $moduleInstance = $this->moduleEvent->getModule();
+        $im              = $e->getParam('application')->getLocator()->instanceManager();
+        $moduleInstance  = $this->moduleEvent->getModule();
         $moduleClassName = get_class($moduleInstance);
 
-        $di = $e->getParam('application')->getLocator();
-        $im = $di->instanceManager();
-        if ($im->hasTypePreferences($moduleClassName)) return;
-        $im->addTypePreference($moduleClassName, $moduleInstance);
+        if (!$im->hasTypePreferences($moduleClassName)) {
+            $im->addTypePreference($moduleClassName, $moduleInstance);
+        }
     }
 }

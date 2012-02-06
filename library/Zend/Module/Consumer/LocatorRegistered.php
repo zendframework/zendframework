cc14b42d5c3893ef378b57b0bf764f94ee4af4a9
Evan Coury <me@evancoury.com>
2012-02-06 08:55:25 -0700

    [Zend\Module] Update LocatorRegistrationListener to work properly for multiple modules
    Before we were using the MvcEvent which only holds one module at a time. Now the
    listener keeps its own internal array of modules that are enabled.

---
+++ library/Zend/Module/Consumer/LocatorRegistered.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Zend\Module\Consumer;
+
+interface LocatorRegistered {}
--- library/Zend/Module/Listener/LocatorRegistrationListener.php
+++ library/Zend/Module/Listener/LocatorRegistrationListener.php
@@ -3,28 +3,88 @@
 namespace Zend\Module\Listener;
 
 use Zend\EventManager\StaticEventManager,
+    Zend\EventManager\Event,
     Zend\Module\ModuleEvent,
-    Zend\EventManager\Event;
+    Zend\Module\Consumer\LocatorRegistered,
+    Zend\EventManager\EventCollection,
+    Zend\EventManager\ListenerAggregate;
 
-class LocatorRegistrationListener extends AbstractListener
+class LocatorRegistrationListener extends AbstractListener implements ListenerAggregate
 {
-    protected $moduleEvent;
+    /**
+     * @var array
+     */
+    protected $modules = array();
+
+    /**
+     * @var array
+     */
+    protected $listeners = array();
 
     public function __invoke(ModuleEvent $e)
     {
-        $this->moduleEvent = $e;
+        return $this->loadModule($e);
+    }
+
+    public function loadModule(ModuleEvent $e)
+    {
+        if (!$e->getModule() instanceof LocatorRegistered) {
+            return;
+        }
+        $this->modules[] = $e->getModule();
+        $events = StaticEventManager::getInstance();
+        $events->attach('bootstrap', 'bootstrap', array($this, 'addTypePreference'), 1000);
+    }
+
+    public function loadModulesPost(Event $e)
+    {
+        if (0 === count($this->modules)) {
+            return;
+        }
+
+        // Attach to the bootstrap event if there are modules we need to process
         $events = StaticEventManager::getInstance();
         $events->attach('bootstrap', 'bootstrap', array($this, 'addTypePreference'), 1000);
     }
 
     public function addTypePreference(Event $e)
     {
-        $im              = $e->getParam('application')->getLocator()->instanceManager();
-        $moduleInstance  = $this->moduleEvent->getModule();
-        $moduleClassName = get_class($moduleInstance);
+        $im = $e->getParam('application')->getLocator()->instanceManager();
 
-        if (!$im->hasTypePreferences($moduleClassName)) {
-            $im->addTypePreference($moduleClassName, $moduleInstance);
+        foreach ($this->modules as $module) {
+            $moduleClassName = get_class($module);
+            if (!$im->hasTypePreferences($moduleClassName)) {
+                $im->addTypePreference($moduleClassName, $module);
+            }
         }
+    }
+
+    /**
+     * Attach one or more listeners
+     *
+     * @param EventCollection $events
+     * @return ConfigListener
+     */
+    public function attach(EventCollection $events)
+    {
+        $this->listeners[] = $events->attach('loadModule', array($this, 'loadModule'), 1000);
+        $this->listeners[] = $events->attach('loadModules.post', array($this, 'loadModulesPost'), 9000);
+        return $this;
+    }
+
+    /**
+     * Detach all previously attached listeners
+     *
+     * @param EventCollection $events
+     * @return void
+     */
+    public function detach(EventCollection $events)
+    {
+        foreach ($this->listeners as $key => $listener) {
+            $events->detach($listener);
+            unset($this->listeners[$key]);
+        }
+        $this->listeners = array();
+        return $this;
     }
 }

110b51de1ef792eeb532aaa81cccf5456486166b
Matthew Weier O'Phinney <matthew@zend.com>
2012-02-09 16:35:40 -0600

    Created initial ViewModelListener
    - Takes the result of an MVC dispatch and tests if it is a ViewModel.
      - If not, no action is taken.
      - If it is marked terminable, replace the ViewModel already in the
        event
      - Otherwise, add it as a child of the event's ViewModel

---
+++ library/Zend/Mvc/View/ViewModelListener.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Mvc
+ * @subpackage View
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace Zend\Mvc\View;
+
+use Zend\EventManager\EventCollection as Events,
+    Zend\EventManager\ListenerAggregate,
+    Zend\Mvc\MvcEvent,
+    Zend\View\Model as ViewModel;
+
+class ViewModelListener implements ListenerAggregate
+{
+    protected $listeners = array();
+
+    /**
+     * Attach listeners
+     * 
+     * @param  Events $events 
+     * @return void
+     */
+    public function attach(Events $events)
+    {
+        $this->listeners[] = $events->attach('dispatch', array($this, 'insertViewModel'), -100);
+    }
+
+    /**
+     * Detach listeners
+     * 
+     * @param  Events $events 
+     * @return void
+     */
+    public function detach(Events $events)
+    {
+        foreach ($this->listeners as $index => $listener) {
+            if ($events->detach($listener)) {
+                unset($this->listeners[$index]);
+            }
+        }
+    }
+
+    /**
+     * Insert the view model into the event
+     *
+     * Inspects the MVC result; if it is a view model, it then either (a) adds 
+     * it as a child to the default, composed view model, or (b) replaces it, 
+     * if the result  is marked as terminable.
+     * 
+     * @param  MvcEvent $e 
+     * @return void
+     */
+    public function insertViewModel(MvcEvent $e)
+    {
+        $result = $e->getResult();
+        if (!$result instanceof ViewModel) {
+            return;
+        }
+
+        $model = $e->getViewModel();
+
+        if ($result->terminate()) {
+            $e->setViewModel($result);
+            return;
+        }
+
+        $model->addChild($result);
+    }
+}

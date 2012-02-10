2239a7b34983752b18dba3af5e2b00d86f124034
Matthew Weier O'Phinney <matthew@zend.com>
2012-02-10 16:30:37 -0600

    Stripped exception, 404, layout strategies from DefaultRenderingStrategy

--- library/Zend/Mvc/View/DefaultRenderingStrategy.php
+++ library/Zend/Mvc/View/DefaultRenderingStrategy.php
@@ -44,30 +44,6 @@ use Zend\EventManager\EventCollection,
 class DefaultRenderingStrategy implements ListenerAggregate
 {
     /**
-     * @var string
-     */
-    protected $defaultLayout = 'layout';
-
-    /**
-     * @var bool
-     */
-    protected $displayExceptions = false;
-
-    /**
-     * @var bool
-     */
-    protected $enableLayoutForErrors = true;
-
-    /**
-     * View models incapable of layouts
-     * @var array
-     */
-    protected $layoutIncapableModels = array(
-        'Zend\View\Model\FeedModel',
-        'Zend\View\Model\JsonModel',
-    );
-
-    /**
      * @var \Zend\Stdlib\CallbackHandler[]
      */
     protected $listeners = array();
@@ -92,94 +68,6 @@ class DefaultRenderingStrategy implements ListenerAggregate
     {
         $this->view = $view;
         return $this;
-    }
-
-    /**
-     * Set value for default layout
-     *
-     * @param  string $defaultLayout
-     * @return DefaultRenderingStrategy
-     */
-    public function setDefaultLayout($defaultLayout)
-    {
-        $this->defaultLayout = (string) $defaultLayout;
-        return $this;
-    }
-    
-    /**
-     * Get default layout
-     *
-     * @return string
-     */
-    public function getDefaultLayout()
-    {
-        return $this->defaultLayout;
-    }
-
-    /**
-     * Flag: display exceptions in error pages?
-     * 
-     * @param  bool $flag 
-     * @return DefaultRenderingStrategy
-     */
-    public function setDisplayExceptions($displayExceptions)
-    {
-        $this->displayExceptions = (bool) $displayExceptions;
-        return $this;
-    }
-
-    /**
-     * Should we display exceptions in error pages?
-     * 
-     * @return bool
-     */
-    public function displayExceptions()
-    {
-        return $this->displayExceptions;
-    }
-
-    /**
-     * Set flag indicating whether or not to enable layouts for errors
-     *
-     * @param  bool $enableLayoutForErrors
-     * @return DefaultRenderingStrategy
-     */
-    public function setEnableLayoutForErrors($enableLayoutForErrors)
-    {
-        $this->enableLayoutForErrors = (bool) $enableLayoutForErrors;
-        return $this;
-    }
-    
-    /**
-     * Are layouts for errors enabled?
-     *
-     * @return bool
-     */
-    public function enableLayoutForErrors()
-    {
-        return $this->enableLayoutForErrors;
-    }
-
-    /**
-     * Set value for modelTypes
-     *
-     * @param  array $modelTypes
-     * @return DefaultRenderingStrategy
-     */
-    public function setLayoutIncapableModels(array $modelTypes)
-    {
-        $this->layoutIncapableModels = $modelTypes;
-        return $this;
-    }
-    
-    /**
-     * Get list of models incapable of layouts
-     *
-     * @return array
-     */
-    public function getLayoutIncapableModels()
-    {
-        return $this->layoutIncapableModels;
     }
 
     /**
@@ -212,9 +100,7 @@ class DefaultRenderingStrategy implements ListenerAggregate
      */
     public function attach(EventCollection $events)
     {
-        $this->listeners[] = $events->attach('dispatch', array($this, 'render404'), -1000);
         $this->listeners[] = $events->attach('dispatch', array($this, 'render'), -10000);
-        $this->listeners[] = $events->attach('dispatch.error', array($this, 'renderError'));
     }
 
     /**
@@ -269,145 +155,6 @@ class DefaultRenderingStrategy implements ListenerAggregate
 
         $view->render($viewModel);
         return $response;
-    }
-
-    /**
-     * Create an error view model and render it
-     * 
-     * @param  MvcEvent $e 
-     * @return HttpResponse
-     */
-    public function renderError(MvcEvent $e)
-    {
-        $error    = $e->getError();
-        $response = $e->getResponse();
-        if (!$response) {
-            $response = new HttpResponse();
-            $e->setResponse($response);
-        }
-
-        switch ($error) {
-            case Application::ERROR_CONTROLLER_NOT_FOUND:
-            case Application::ERROR_CONTROLLER_INVALID:
-                $vars = array(
-                    'message' => 'Page not found.',
-                );
-                $options = array(
-                    'template' => 'pages/404',
-                );
-                $response->setStatusCode(404);
-                break;
-
-            case Application::ERROR_EXCEPTION:
-            default:
-                $exception = $e->getParam('exception');
-                $vars = array(
-                    'message'            => 'An error occurred during execution; please try again later.',
-                    'exception'          => $e->getParam('exception'),
-                    'display_exceptions' => $this->displayExceptions(),
-                );
-                $options = array(
-                    'template' => 'error',
-                );
-                $response->setStatusCode(500);
-                break;
-        }
-        $options['enable_layout'] = $this->enableLayoutForErrors();
-
-        $model = new ViewModel\ViewModel($vars, $options);
-        $e->setResult($model);
-        return $this->render($e);
-    }
-
-    /**
-     * Create and return a 404 page
-     * 
-     * @param  MvcEvent $e 
-     * @return null|HttpResponse
-     */
-    public function render404(MvcEvent $e)
-    {
-        $vars = $e->getResult();
-        if ($vars instanceof Response) {
-            return;
-        }
-
-        $response = $e->getResponse();
-        if ($response->getStatusCode() != 404) {
-            // Only handle 404's
-            return;
-        }
-
-        $model = new ViewModel\ViewModel(
-            array('message' => 'Page not found.'),
-            array(
-                'template'      => 'pages/404',
-                'enable_layout' => $this->enableLayoutForErrors(),
-            )
-        );
-        $e->setResult($model);
-        return $this->render($e);
-    }
-
-
-    /**
-     * Select layout
-     *
-     * Listens on 'renderer' event. Instead of returning a renderer, it simply
-     * checks the following:
-     *
-     * - is the view model in a blacklist? if so, do nothing
-     * - is it an XHR request? if so, do nothing
-     * - if enable_layout was not set as a renderer option, do nothing
-     * - if enable_layout was set as a renderer option, but set to a boolean
-     *   false, do nothing
-     * - if the PhpRenderer is not available, do nothing.
-     *
-     * Finally, if the conditions above are not met, it adds the layout to the
-     * stack. The assumption is that the view script rendered will capture
-     * content into placeholders that the layout viewscript will retrieve and
-     * render.
-     * 
-     * @param  ViewEvent $e 
-     * @return void
-     */
-    public function selectLayout(ViewEvent $e)
-    {
-        $model = $e->getModel();
-        if (!$model instanceof ViewModel
-            || in_array(get_class($model), $this->getLayoutIncapableModels())
-        ) {
-            return;
-        }
-
-        $request   = $e->getRequest();
-        if ($request instanceof HttpRequest) {
-            $headers = $request->headers();
-            if ($headers->has('x-requested-with')) {
-                $header = $headers->get('x-requested-with');
-                if (strtolower($header->getFieldValue()) == 'xmlhttprequest') {
-                    // XHR request; ignore
-                    return;
-                }
-            }
-        }
-
-        $options = $model->getOptions();
-        if (array_key_exists('enable_layout', $options) && !$options['enable_layout']) {
-            return;
-        }
-
-        $view = $e->getTarget();
-        if (!$view->hasRenderer('Zend\View\PhpRenderer')) {
-            return;
-        }
-        $renderer = $view->getRenderer('Zend\View\PhpRenderer');
-
-        $layout = $this->getDefaultLayout();
-        if (isset($options['layout'])) {
-            $layout = $options['layout'];
-        }
-        $renderer->enqueue($layout);
     }
 
     /**
@@ -541,7 +288,6 @@ class DefaultRenderingStrategy implements ListenerAggregate
             $this->view->addRenderer(new PhpRenderer);
         }
 
-        $this->view->addRenderingStrategy(array($this, 'selectLayout'), -99);
         $this->view->addRenderingStrategy(array($this, 'selectRendererByContext'), -100);
         $this->view->addResponseStrategy(array($this, 'populateResponse'), -100);
     }
--- tests/Zend/Mvc/View/DefaultRendereringStrategyTest.php
+++ tests/Zend/Mvc/View/DefaultRendereringStrategyTest.php
@@ -78,54 +78,6 @@ class DefaultRenderingStrategyTest extends TestCase
         $this->strategy = new DefaultRenderingStrategy($this->view);
     }
 
-    public function testLayoutIsSetByDefault()
-    {
-        $this->assertEquals('layout', $this->strategy->getDefaultLayout());
-    }
-
-    public function testLayoutIsMutable()
-    {
-        $this->strategy->setDefaultLayout('foobar');
-        $this->assertEquals('foobar', $this->strategy->getDefaultLayout());
-    }
-
-    public function testErrorExceptionsAreNotDisplayedByDefault()
-    {
-        $this->assertFalse($this->strategy->displayExceptions());
-    }
-
-    public function testErrorExceptionDisplayFlagIsMutable()
-    {
-        $this->strategy->setDisplayExceptions('true');
-        $this->assertTrue($this->strategy->displayExceptions());
-    }
-
-    public function testLayoutIsEnabledForErrorsByDefault()
-    {
-        $this->assertTrue($this->strategy->enableLayoutForErrors());
-    }
-
-    public function testErrorEnabledLayoutsAreMutable()
-    {
-        $this->strategy->setEnableLayoutForErrors(false);
-        $this->assertFalse($this->strategy->enableLayoutForErrors());
-    }
-
-    public function testLayoutIncapableModelsIncludeJsonAndFeedByDefault()
-    {
-        $list = $this->strategy->getLayoutIncapableModels();
-        $this->assertContains('Zend\View\Model\JsonModel', $list);
-        $this->assertContains('Zend\View\Model\FeedModel', $list);
-    }
-
-    public function testLayoutIncapableModelsListIsMutable()
-    {
-        $this->strategy->setLayoutIncapableModels(array(
-            'Zend\View\Model\ViewModel',
-        ));
-        $this->assertEquals(array('Zend\View\Model\ViewModel'), $this->strategy->getLayoutIncapableModels());
-    }
-
     public function testEnablesDefaultRenderingStrategiesByDefault()
     {
         $this->assertTrue($this->strategy->useDefaultRenderingStrategy());
@@ -135,27 +87,6 @@ class DefaultRenderingStrategyTest extends TestCase
     {
         $this->strategy->setUseDefaultRenderingStrategy(false);
         $this->assertFalse($this->strategy->useDefaultRenderingStrategy());
-    }
-
-    public function testAttaches404RendererAtExpectedPriority()
-    {
-        $events = new EventManager();
-        $events->attachAggregate($this->strategy);
-        $listeners = $events->getListeners('dispatch');
-
-        $expectedCallback = array($this->strategy, 'render404');
-        $expectedPriority = -1000;
-        $found            = false;
-        foreach ($listeners as $listener) {
-            $callback = $listener->getCallback();
-            if ($callback === $expectedCallback) {
-                if ($listener->getMetadatum('priority') == $expectedPriority) {
-                    $found = true;
-                    break;
-                }
-            }
-        }
-        $this->assertTrue($found, '404 Renderer not found');
     }
 
     public function testAttachesRendererAtExpectedPriority()
@@ -179,33 +110,14 @@ class DefaultRenderingStrategyTest extends TestCase
         $this->assertTrue($found, 'Renderer not found');
     }
 
-    public function testAttachesErrorRendererAtExpectedPriority()
-    {
-        $events = new EventManager();
-        $events->attachAggregate($this->strategy);
-        $listeners = $events->getListeners('dispatch.error');
-
-        $expectedCallback = array($this->strategy, 'renderError');
-        $found            = false;
-        foreach ($listeners as $listener) {
-            $callback = $listener->getCallback();
-            if ($callback === $expectedCallback) {
-                $found = true;
-            }
-        }
-        $this->assertTrue($found, 'Error Renderer not found');
-    }
-
     public function testCanDetachListenersFromEventManager()
     {
         $events = new EventManager();
         $events->attachAggregate($this->strategy);
-        $this->assertEquals(2, count($events->getListeners('dispatch')));
-        $this->assertEquals(1, count($events->getListeners('dispatch.error')));
+        $this->assertEquals(1, count($events->getListeners('dispatch')));
 
         $events->detachAggregate($this->strategy);
         $this->assertEquals(0, count($events->getListeners('dispatch')));
-        $this->assertEquals(0, count($events->getListeners('dispatch.error')));
     }
 
     public function testRenderReturnsNullForNonMvcEvent()
@@ -220,35 +132,6 @@ class DefaultRenderingStrategyTest extends TestCase
         $this->event->setResult(new stdClass);
         $result = $this->strategy->render($this->event);
         $this->assertNull($result);
-    }
-
-    public function testRendersContentInLayout()
-    {
-        $this->resolver->add('content', __DIR__ . '/_files/content.phtml');
-        $model = new Model\ViewModel();
-        $model->setOption('template', 'content')
-              ->setOption('enable_layout', true);
-        $this->event->setResult($model);
-        $this->view->addRenderer($this->renderer);
-
-        $result = $this->strategy->render($this->event);
-        $this->assertSame($this->response, $result);
-        $this->assertContains('<layout>content</layout>', $result->getContent());
-    }
-
-    public function testRendersContentByItselfWhenLayoutDisabled()
-    {
-        $this->resolver->add('content', __DIR__ . '/_files/content.phtml');
-        $model = new Model\ViewModel();
-        $model->setOption('template', 'content')
-              ->setOption('enable_layout', false);
-        $this->event->setResult($model);
-        $this->view->addRenderer($this->renderer);
-
-        $result = $this->strategy->render($this->event);
-        $this->assertSame($this->response, $result);
-        $this->assertContains('content', $result->getContent());
-        $this->assertNotContains('<layout>', $result->getContent());
     }
 
     public function testRendersJson()
@@ -349,142 +232,6 @@ class DefaultRenderingStrategyTest extends TestCase
         $expected = sprintf('content (%s): %s', json_encode(array('template' => 'content')), json_encode(array('foo' => 'bar')));
     }
 
-    public function testSets404StatusForControllerNotFoundError()
-    {
-        $this->resolver->add('pages/404', __DIR__ . '/_files/error.phtml');
-        $this->view->addRenderer($this->renderer);
-        $this->strategy->setEnableLayoutForErrors(false);
-        $this->event->setError(Application::ERROR_CONTROLLER_NOT_FOUND);
-
-        $result = $this->strategy->renderError($this->event);
-        $this->assertSame($this->response, $result);
-
-        $this->assertTrue($this->response->isNotFound());
-        $this->assertContains('Page not found.', $this->response->getContent());
-    }
-
-    public function testSets404StatusForInvalidController()
-    {
-        $this->resolver->add('pages/404', __DIR__ . '/_files/error.phtml');
-        $this->view->addRenderer($this->renderer);
-        $this->strategy->setEnableLayoutForErrors(false);
-        $this->event->setError(Application::ERROR_CONTROLLER_INVALID);
-
-        $result = $this->strategy->renderError($this->event);
-        $this->assertSame($this->response, $result);
-
-        $this->assertTrue($this->response->isNotFound());
-        $this->assertContains('Page not found.', $this->response->getContent());
-    }
-
-    public function testSets500StatusForDetectedException()
-    {
-        $this->resolver->add('error', __DIR__ . '/_files/error.phtml');
-        $this->view->addRenderer($this->renderer);
-        $this->strategy->setEnableLayoutForErrors(false);
-        $this->strategy->setDisplayExceptions(false);
-        $this->event->setError(Application::ERROR_EXCEPTION);
-        $this->event->setParam('exception', new \Exception('Test exception'));
-
-        $result = $this->strategy->renderError($this->event);
-        $this->assertSame($this->response, $result);
-
-        $this->assertTrue($this->response->isServerError());
-        $content = $this->response->getContent();
-        $this->assertContains('error occurred during execution', $content);
-        $this->assertNotContains('Test exception', $content, $content);
-    }
-
-    public function testRendersStackTraceForDetectedExceptionWhenDisplayExceptionsEnabled()
-    {
-        $this->resolver->add('error', __DIR__ . '/_files/error.phtml');
-        $this->view->addRenderer($this->renderer);
-        $this->strategy->setEnableLayoutForErrors(false);
-        $this->strategy->setDisplayExceptions(true);
-        $this->event->setError(Application::ERROR_EXCEPTION);
-        $this->event->setParam('exception', new \Exception('Test exception'));
-
-        $result = $this->strategy->renderError($this->event);
-        $this->assertSame($this->response, $result);
-
-        $this->assertTrue($this->response->isServerError());
-        $content = $this->response->getContent();
-        $this->assertContains('error occurred during execution', $content);
-        $this->assertContains('Test exception', $content, $content);
-    }
-
-    public function testErrorInjectedIntoLayoutWhenErrorLayoutsAreEnabled()
-    {
-        $this->resolver->add('error', __DIR__ . '/_files/error.phtml');
-        $this->view->addRenderer($this->renderer);
-        $this->strategy->setEnableLayoutForErrors(true);
-        $this->strategy->setDisplayExceptions(true);
-        $this->event->setError(Application::ERROR_EXCEPTION);
-        $this->event->setParam('exception', new \Exception('Test exception'));
-
-        $result = $this->strategy->renderError($this->event);
-        $this->assertSame($this->response, $result);
-
-        $this->assertTrue($this->response->isServerError());
-        $content = $this->response->getContent();
-        $this->assertContains('error occurred during execution', $content);
-        $this->assertContains('Test exception', $content, $content);
-        $this->assertContains('<layout>', $content, $content);
-    }
-
-    public function test404RendererIsSkippedIfEventResultIsAResponseObject()
-    {
-        $this->resolver->add('pages/404', __DIR__ . '/_files/error.phtml');
-        $this->view->addRenderer($this->renderer);
-        $this->strategy->setEnableLayoutForErrors(false);
-        $this->strategy->setDisplayExceptions(false);
-
-        $this->event->setResult($this->response);
-        $result = $this->strategy->render404($this->event);
-        $this->assertNull($result);
-    }
-
-    public function test404RendererIsSkippedIfNon404StatusDetected()
-    {
-        $this->resolver->add('pages/404', __DIR__ . '/_files/error.phtml');
-        $this->view->addRenderer($this->renderer);
-        $this->strategy->setEnableLayoutForErrors(false);
-        $this->strategy->setDisplayExceptions(false);
-
-        $this->response->setStatusCode(200);
-        $result = $this->strategy->render404($this->event);
-        $this->assertNull($result);
-    }
-
-    public function test404RendererWillRenderContentWhenLayoutsAreDisabled()
-    {
-        $this->resolver->add('pages/404', __DIR__ . '/_files/error.phtml');
-        $this->view->addRenderer($this->renderer);
-        $this->strategy->setEnableLayoutForErrors(false);
-        $this->strategy->setDisplayExceptions(false);
-
-        $this->response->setStatusCode(404);
-        $result = $this->strategy->render404($this->event);
-        $this->assertSame($this->response, $result);
-        $this->assertEquals(404, $result->getStatusCode());
-        $this->assertContains('Page not found.', $result->getContent());
-    }
-
-    public function test404RendererWillRenderContentWithLayout()
-    {
-        $this->resolver->add('pages/404', __DIR__ . '/_files/error.phtml');
-        $this->view->addRenderer($this->renderer);
-        $this->strategy->setEnableLayoutForErrors(true);
-        $this->strategy->setDisplayExceptions(false);
-
-        $this->response->setStatusCode(404);
-        $result = $this->strategy->render404($this->event);
-        $this->assertSame($this->response, $result);
-        $this->assertEquals(404, $result->getStatusCode());
-        $this->assertContains('Page not found.', $result->getContent());
-        $this->assertContains('<layout>', $result->getContent());
-    }
-
     public function makeRendererExtensionsVisible()
     {
         $r = new ReflectionClass($this->renderer);
@@ -492,112 +239,6 @@ class DefaultRenderingStrategyTest extends TestCase
         $prop->setAccessible(true);
         $this->assertEquals(array(), $prop->getValue($this->renderer));
         $this->extensions = $prop;
-    }
-
-    public function testSelectLayoutExitsEarlyWithNonViewModel()
-    {
-        $event = new ViewEvent();
-        $event->setTarget($this->view)
-              ->setRequest($this->request)
-              ->setResponse($this->response);
-        $this->makeRendererExtensionsVisible();
-        $this->view->addRenderer($this->renderer);
-
-        $result = $this->strategy->selectLayout($event);
-        $this->assertNull($result);
-        $this->assertEquals(array(), $this->extensions->getValue($this->renderer));
-    }
-
-    public function testSelectLayoutExitsEarlyWithInvalidViewModel()
-    {
-        $event = new ViewEvent();
-        $event->setTarget($this->view)
-              ->setRequest($this->request)
-              ->setResponse($this->response)
-              ->setModel(new Model\JsonModel);
-        $this->makeRendererExtensionsVisible();
-        $this->view->addRenderer($this->renderer);
-
-        $result = $this->strategy->selectLayout($event);
-        $this->assertNull($result);
-        $this->assertEquals(array(), $this->extensions->getValue($this->renderer));
-    }
-
-    public function testSelectLayoutExitsEarlyWithXhrRequest()
-    {
-        $event = new ViewEvent();
-        $event->setTarget($this->view)
-              ->setRequest($this->request)
-              ->setResponse($this->response)
-              ->setModel(new Model\ViewModel);
-        $this->request->headers()->addHeaderLine('X-Requested-With', 'XmlHttpRequest');
-        $this->makeRendererExtensionsVisible();
-        $this->view->addRenderer($this->renderer);
-
-        $result = $this->strategy->selectLayout($event);
-        $this->assertNull($result);
-        $this->assertEquals(array(), $this->extensions->getValue($this->renderer));
-    }
-
-    public function testSelectLayoutExitsEarlyIfModelDisablesLayouts()
-    {
-        $model = new Model\ViewModel(array(), array('enable_layout' => false));
-        $event = new ViewEvent();
-        $event->setTarget($this->view)
-              ->setRequest($this->request)
-              ->setResponse($this->response)
-              ->setModel($model);
-        $this->makeRendererExtensionsVisible();
-        $this->view->addRenderer($this->renderer);
-
-        $result = $this->strategy->selectLayout($event);
-        $this->assertNull($result);
-        $this->assertEquals(array(), $this->extensions->getValue($this->renderer));
-    }
-
-    public function testSelectLayoutExitsEarlyIfNoPhpRendererAttached()
-    {
-        $model = new Model\ViewModel(array(), array('enable_layout' => true));
-        $event = new ViewEvent();
-        $event->setTarget($this->view)
-              ->setRequest($this->request)
-              ->setResponse($this->response)
-              ->setModel($model);
-
-        $result = $this->strategy->selectLayout($event);
-        $this->assertNull($result);
-    }
-
-    public function testSelectLayoutUsesDefaultLayoutIfNoneSpecifiedInModel()
-    {
-        $model = new Model\ViewModel(array(), array('enable_layout' => true));
-        $event = new ViewEvent();
-        $event->setTarget($this->view)
-              ->setRequest($this->request)
-              ->setResponse($this->response)
-              ->setModel($model);
-        $this->makeRendererExtensionsVisible();
-        $this->view->addRenderer($this->renderer);
-
-        $result = $this->strategy->selectLayout($event);
-        $this->assertNull($result);
-        $this->assertEquals(array('layout'), $this->extensions->getValue($this->renderer));
-    }
-
-    public function testSelectLayoutUsesLayoutSpecifiedInModel()
-    {
-        $model = new Model\ViewModel(array(), array('enable_layout' => true, 'layout' => 'my/layout'));
-        $event = new ViewEvent();
-        $event->setTarget($this->view)
-              ->setRequest($this->request)
-              ->setResponse($this->response)
-              ->setModel($model);
-        $this->makeRendererExtensionsVisible();
-        $this->view->addRenderer($this->renderer);
-
-        $result = $this->strategy->selectLayout($event);
-        $this->assertNull($result);
-        $this->assertEquals(array('my/layout'), $this->extensions->getValue($this->renderer));
     }
 
     public function testPhpRendererIsSelectedForJsonModelIfNoJsonRendererAttached()

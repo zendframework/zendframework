529935547ea33bd55645c6147c7fa5dbe2945507
Matthew Weier O'Phinney <matthew@zend.com>
2012-02-10 17:07:28 -0600

    Created JsonStrategy
    - Selects JsonRenderer when:
      - JsonModel present, OR
      - application/json Accept header present
    - Injects response when:
      - Event renderer matches its internal renderer, AND
      - string result present, resulting in
      - injecting result into response content, and application/json header
        into response headers

---
+++ tests/Zend/View/Strategy/JsonStrategyTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_View
+ * @subpackage UnitTest
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace ZendTest\View\Strategy;
+
+use PHPUnit_Framework_TestCase as TestCase,
+    Zend\Http\Request as HttpRequest,
+    Zend\Http\Response as HttpResponse,
+    Zend\View\Model,
+    Zend\View\Renderer\JsonRenderer,
+    Zend\View\Strategy\JsonStrategy,
+    Zend\View\ViewEvent;
+
+/**
+ * @category   Zend
+ * @package    Zend_View
+ * @subpackage UnitTest
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class JsonStrategyTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->renderer = new JsonRenderer;
+        $this->strategy = new JsonStrategy($this->renderer);
+        $this->event    = new ViewEvent();
+        $this->response = new HttpResponse();
+    }
+
+    public function testJsonModelSelectsJsonStrategy()
+    {
+        $this->event->setModel(new Model\JsonModel());
+        $result = $this->strategy->selectJsonRenderer($this->event);
+        $this->assertSame($this->renderer, $result);
+    }
+
+    public function testJsonAcceptHeaderSelectsJsonStrategy()
+    {
+        $request = new HttpRequest();
+        $request->headers()->addHeaderLine('Accept', 'application/json');
+        $this->event->setRequest($request);
+        $result = $this->strategy->selectJsonRenderer($this->event);
+        $this->assertSame($this->renderer, $result);
+    }
+
+    public function testLackOfJsonModelOrAcceptHeaderDoesNotSelectJsonStrategy()
+    {
+        $result = $this->strategy->selectJsonRenderer($this->event);
+        $this->assertNotSame($this->renderer, $result);
+        $this->assertNull($result);
+    }
+
+    protected function assertResponseNotInjected()
+    {
+        $content = $this->response->getContent();
+        $headers = $this->response->headers();
+        $this->assertTrue(empty($content));
+        $this->assertFalse($headers->has('content-type'));
+    }
+
+    public function testNonMatchingRendererDoesNotInjectResponse()
+    {
+        $this->event->setResponse($this->response);
+
+        // test empty renderer
+        $this->strategy->injectJsonResponse($this->event);
+        $this->assertResponseNotInjected();
+
+        // test non-matching renderer
+        $renderer = new JsonRenderer();
+        $this->event->setRenderer($renderer);
+        $this->strategy->injectJsonResponse($this->event);
+        $this->assertResponseNotInjected();
+    }
+
+    public function testNonStringResultDoesNotInjectResponse()
+    {
+        $this->event->setResponse($this->response);
+        $this->event->setRenderer($this->renderer);
+        $this->event->setResult($this->response);
+
+        $this->strategy->injectJsonResponse($this->event);
+        $this->assertResponseNotInjected();
+    }
+
+    public function testMatchingRendererAndStringResultInjectsResponse()
+    {
+        $expected = json_encode(array('foo' => 'bar'));
+        $this->event->setResponse($this->response);
+        $this->event->setRenderer($this->renderer);
+        $this->event->setResult($expected);
+
+        $this->strategy->injectJsonResponse($this->event);
+        $content = $this->response->getContent();
+        $headers = $this->response->headers();
+        $this->assertEquals($expected, $content);
+        $this->assertTrue($headers->has('content-type'));
+        $this->assertEquals('application/json', $headers->get('content-type')->getFieldValue());
+    }
+}

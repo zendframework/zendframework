8714c398f0bd5474fbc843e048df30ccda0ef3f8
David Roheim <david.roheim@gmail.com>
2012-02-22 22:25:05 +0200

    Copy English resource lang files to Norwegian dir

---
+++ resources/languages/no/Zend_Captcha.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Translate
+ * @subpackage Resource
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * EN-Revision: 30.Jul.2011
+ */
+return array(
+    // Zend_Captcha_ReCaptcha
+    "Missing captcha fields" => "Missing captcha fields",
+    "Failed to validate captcha" => "Failed to validate captcha",
+    "Captcha value is wrong: %value%" => "Captcha value is wrong: %value%",
+
+    // Zend_Captcha_Word
+    "Empty captcha value" => "Empty captcha value",
+    "Captcha ID field is missing" => "Captcha ID field is missing",
+    "Captcha value is wrong" => "Captcha value is wrong",
+);

bed047f0c97ae46044cc30129fc94efd76d64a2d
Matthew Weier O'Phinney <matthew@zend.com>
2012-02-23 10:54:15 -0600

    Removed Zend\Tool and related scripts
    - Removed. Currently, ZendSkeletonApplication is the route to go for scaffolding
      a new ZF2 project, and the future will likely be a combination of that,
      Zend\Console, and Zend\Code\Generator.

--- library/Zend/Tool/Project/Profile/Exception/InvalidArgumentException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zend\Tool\Project\Profile\Exception;
-
-class InvalidArgumentException
-    extends \Zend\Tool\Project\Exception\InvalidArgumentException
-    implements \Zend\Tool\Project\Profile\Exception
-{
-}
--- library/Zend/Tool/Project/Provider/Exception/RuntimeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zend\Tool\Project\Provider\Exception;
-
-class RuntimeException
-    extends \Zend\Tool\Project\Exception\RuntimeException
-    implements \Zend\Tool\Project\Exception
-{
-}

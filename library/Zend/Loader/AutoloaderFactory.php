6a0d2a9f4bc5eac572d25a0e0af0042d41c7294c
Mike Willbanks <mike@digitalstruct.com>
2012-02-24 08:31:59 -0600

    fixed parse error

--- library/Zend/Loader/AutoloaderFactory.php
+++ library/Zend/Loader/AutoloaderFactory.php
@@ -105,11 +105,12 @@ abstract class AutoloaderFactory
                 // unfortunately is_subclass_of is broken on some 5.3 versions
                 // additionally instanceof is also broken for this use case
                 if (version_compare(PHP_VERSION, '5.3.6', '>')) {
-                    if (!is_subclass_of($class, 'Zend\Loader\SplAutoloader')) {
-                    require_once 'Exception/InvalidArgumentException.php';
-                    throw new Exception\InvalidArgumentException(
-                                sprintf('Autoloader class %s must implement Zend\\Loader\\SplAutoloader', $class)
-                    );
+                        if (!is_subclass_of($class, 'Zend\Loader\SplAutoloader')) {
+                        require_once 'Exception/InvalidArgumentException.php';
+                        throw new Exception\InvalidArgumentException(
+                                    sprintf('Autoloader class %s must implement Zend\\Loader\\SplAutoloader', $class)
+                        );
+                    }
                 }
 
                 if ($class === static::STANDARD_AUTOLOADER) {

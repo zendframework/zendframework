da2da70bcb60b2b9bce0966b36118c887182174a
Matthew Weier O'Phinney <matthew@zend.com>
2012-03-01 23:56:48 -0600

    Tested PredicateSet
    - Added tests for PredicateSet
    - Added docblocks for PredicateSet and Predicate
    - Fixed logic error in PredicateSet::__construct()

--- library/Zend/Db/Sql/Predicate/PredicateSet.php
+++ library/Zend/Db/Sql/Predicate/PredicateSet.php
@@ -1,52 +1,112 @@
 <?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Db
+ * @subpackage Sql
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 
 namespace Zend\Db\Sql\Predicate;
 
-class PredicateSet implements PredicateInterface
+use Countable;
+
+/**
+ * @category   Zend
+ * @package    Zend_Db
+ * @subpackage Sql
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class PredicateSet implements PredicateInterface, Countable
 {
     const COMBINED_BY_AND = 'AND';
-    const OP_AND = 'AND';
+    const OP_AND          = 'AND';
 
-    const COMBINED_BY_OR = 'OR';
-    const OP_OR = 'OR';
+    const COMBINED_BY_OR  = 'OR';
+    const OP_OR           = 'OR';
     
     protected $defaultCombination = self::COMBINED_BY_AND;
-
-    protected $predicates = array();
+    protected $predicates         = array();
     
+    /**
+     * Constructor
+     * 
+     * @param  null|array $predicates 
+     * @param  string $defaultCombination 
+     * @return void
+     */
     public function __construct(array $predicates = null, $defaultCombination = self::COMBINED_BY_AND)
     {
         $this->defaultCombination = $defaultCombination;
         if ($predicates) {
-            foreach ($this->predicates as $predicate) {
+            foreach ($predicates as $predicate) {
                 $this->addPredicate($predicate);
             }
         }
     }
 
+    /**
+     * Add predicate to set
+     * 
+     * @param  PredicateInterface $predicate 
+     * @param  string $combination 
+     * @return PredicateSet
+     */
     public function addPredicate(PredicateInterface $predicate, $combination = null)
     {
         if ($combination === null || !in_array($combination, array(self::OP_AND, self::OP_OR))) {
             $combination = $this->defaultCombination;
         }
+
         if ($combination == self::OP_OR) {
             $this->orPredicate($predicate);
-        } else {
-            $this->andPredicate($predicate);
+            return $this;
         }
-    }
-
-    public function orPredicate(PredicateInterface $predicate)
-    {
-        $this->predicates[] = array(self::OP_OR, $predicate);
-    }
-
-    public function andPredicate(PredicateInterface $predicate)
-    {
-        $this->predicates[] = array(self::OP_AND, $predicate);
+        
+        $this->andPredicate($predicate);
+        return $this;
     }
 
     /**
+     * Add predicate using OR operator
+     * 
+     * @param  PredicateInterface $predicate 
+     * @return PredicateSet
+     */
+    public function orPredicate(PredicateInterface $predicate)
+    {
+        $this->predicates[] = array(self::OP_OR, $predicate);
+        return $this;
+    }
+
+    /**
+     * Add predicate using AND operator
+     * 
+     * @param  PredicateInterface $predicate 
+     * @return PredicateSet
+     */
+    public function andPredicate(PredicateInterface $predicate)
+    {
+        $this->predicates[] = array(self::OP_AND, $predicate);
+        return $this;
+    }
+
+    /**
+     * Get predicate parts for where statement
+     *
      * @return array
      */
     public function getWhereParts()
@@ -68,12 +128,17 @@ class PredicateSet implements PredicateInterface
             }
 
             if (isset($this->predicates[$i+1])) {
-                $parts[] .= ' ' . $this->predicates[$i+1][0] . ' ';
+                $parts[] = sprintf(' %s ', $this->predicates[$i+1][0]);
             }
         }
         return $parts;
     }
 
+    /**
+     * Get count of attached predicates
+     * 
+     * @return int
+     */
     public function count()
     {
         return count($this->predicates);

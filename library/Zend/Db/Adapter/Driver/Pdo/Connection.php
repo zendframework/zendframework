2094493515a40bc4332d6efbb918a87b8e887f85
Rob Allen <rob@akrabat.com>
2012-03-03 08:55:29 +0000

    Fix Pdo\Connection so that it works properly without a dsn parameter

--- library/Zend/Db/Adapter/Driver/Pdo/Connection.php
+++ library/Zend/Db/Adapter/Driver/Pdo/Connection.php
@@ -149,7 +149,7 @@ class Connection implements ConnectionInterface
 
         // @todo method createKnownDsn
 
-        $dsn = $username = $password = $database = null;
+        $dsn = $username = $password = $hostname = $database = null;
         $options = array();
         foreach ($this->connectionParameters as $key => $value) {
             switch (strtolower($key)) {
@@ -173,6 +173,10 @@ class Connection implements ConnectionInterface
                 case 'password':
                     $password = (string) $value;
                     break;
+                case 'host':
+                case 'hostname':
+                    $hostname = (string) $value;
+                    break;
                 case 'database':
                 case 'dbname':
                     $database = (string) $value;
@@ -190,8 +194,11 @@ class Connection implements ConnectionInterface
 
         if (!isset($dsn) && isset($pdoDriver)) {
             $dsn = $pdoDriver . ':';
+            if (isset($hostname)) {
+                $dsn .= "hostname=$hostname;";
+            }
             if (isset($database)) {
-                $dsn .= $database;
+                $dsn .= "dbname=$database;";
             }
         } elseif (!isset($dsn)) {
             throw new \Exception('A dsn was not provided or could not be constructed from your parameters');

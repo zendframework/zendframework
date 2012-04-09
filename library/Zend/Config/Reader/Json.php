3ff14ccc17ef3e76428b51ec5b47db36e7e82ae6
Dave Stephenson <sxndave@gmail.com>
2012-04-09 15:18:05 +0100

    Addresses issue ZF2-257 "Update all docblocks to use consistent copyright/license header. Version: 2012"
    http://framework.zend.com/issues/browse/ZF2-257
    
    Updates existing copyright notices to read 2005-2012

--- library/Zend/Config/Reader/Json.php
+++ library/Zend/Config/Reader/Json.php
@@ -31,7 +31,7 @@ use Zend\Config\Reader,
  * @category   Zend
  * @package    Zend_Config
  * @subpackage Reader
- * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Json implements Reader

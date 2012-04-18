ff72626a251312ad6e30692a9164366b0cfc56a2
Matthew Weier O'Phinney <matthew@zend.com>
2012-04-18 12:21:30 -0500

    [zen-47] Initial Factory scaffolding
    - Created test case file with incomplete tests
    - Created stub class file

---
+++ tests/Zend/Form/FactoryTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage UnitTest
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace ZendTest\Form;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Filter;
+use Zend\Form;
+use Zend\Form\Factory as FormFactory;
+use Zend\InputFilter;
+use Zend\Validator;
+
+/**
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage UnitTest
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class FactoryTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->factory = new FormFactory();
+    }
+
+    public function testCanCreateElements()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanCreateFieldsets()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanCreateFieldsetsWithElements()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanCreateNestedFieldsets()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanCreateForms()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanCreateFormsWithNamedInputFilters()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanCreateFormsWithInputFilterSpecifications()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanCreateFormsAndSpecifyHydrator()
+    {
+        $this->markTestIncomplete();
+    }
+}

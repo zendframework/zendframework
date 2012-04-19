e569e2d861ad6ef9385584ed70c740e3a648270f
sasa <sasa@sasa-W500.(none)>
2012-04-19 15:11:37 +0200

    Renamed interfaces in Zend\Uri

--- library/Zend/Uri/Exception/InvalidArgumentException.php
+++ library/Zend/Uri/Exception/InvalidArgumentException.php
@@ -4,5 +4,5 @@ namespace Zend\Uri\Exception;
 
 class InvalidArgumentException
     extends \InvalidArgumentException
-    implements \Zend\Uri\Exception
+    implements \Zend\Uri\Exception\ExceptionInterface
 {}

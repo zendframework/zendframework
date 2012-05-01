e0dd992da7630f6f42a35509e51bcc22d69f06fb
Ralph Schindler <ralph.schindler@zend.com>
2012-05-01 11:29:12 -0500

    Improved unit tests for ServiceManager [zen-33]

--- library/Zend/ServiceManager/Di/DiServiceInitializer.php
+++ library/Zend/ServiceManager/Di/DiServiceInitializer.php
@@ -2,7 +2,6 @@
 
 namespace Zend\ServiceManager\Di;
 
-
 use Zend\ServiceManager\InitializerInterface,
     Zend\ServiceManager\ServiceLocatorInterface,
     Zend\ServiceManager\Exception,
@@ -11,11 +10,26 @@ use Zend\ServiceManager\InitializerInterface,
 
 class DiServiceInitializer extends Di implements InitializerInterface
 {
-
+    /**
+     * @var Di
+     */
     protected $di = null;
+
+    /**
+     * @var DiInstanceManagerProxy
+     */
     protected $diInstanceManagerProxy = null;
+
+    /**
+     * @var ServiceLocatorInterface
+     */
     protected $serviceLocator = null;
 
+    /**
+     * @param \Zend\Di\Di $di
+     * @param \Zend\ServiceManager\ServiceLocatorInterface $serviceLocator
+     * @param null|DiInstanceManagerProxy $diImProxy
+     */
     public function __construct(Di $di, ServiceLocatorInterface $serviceLocator, DiInstanceManagerProxy $diImProxy = null)
     {
         $this->di = $di;
@@ -23,6 +37,9 @@ class DiServiceInitializer extends Di implements InitializerInterface
         $this->diInstanceManagerProxy = ($diImProxy) ?: new DiInstanceManagerProxy($di->instanceManager(), $serviceLocator);
     }
 
+    /**
+     * @param $instance
+     */
     public function initialize($instance)
     {
         $instanceManager = $this->di->instanceManager;
@@ -30,7 +47,7 @@ class DiServiceInitializer extends Di implements InitializerInterface
         try {
             $this->di->injectDependencies($instance);
             $this->di->instanceManager = $instanceManager;
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             $this->di->instanceManager = $instanceManager;
             throw $e;
         }

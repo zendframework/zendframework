b9a50eeb5b890b7c9a2ce1fd78a3765d43c29a23
Enrico Zimuel <e.zimuel@gmail.com>
2012-05-02 20:29:50 +0200

    Added docblocks to Zend\Module

--- library/Zend/Module/Listener/LocatorRegistrationListener.php
+++ library/Zend/Module/Listener/LocatorRegistrationListener.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Module
+ */
 namespace Zend\Module\Listener;
 
 use Zend\EventManager\StaticEventManager,
@@ -10,6 +17,13 @@ use Zend\EventManager\StaticEventManager,
     Zend\EventManager\EventCollection,
     Zend\EventManager\ListenerAggregate;
 
+/**
+ * Locator registration listener
+ * 
+ * @category   Zend
+ * @package    Zend_Module
+ * @subpackage Listener
+ */
 class LocatorRegistrationListener extends AbstractListener implements ListenerAggregate
 {
     /**

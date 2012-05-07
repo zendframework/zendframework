8e203365204f008f868bebd15f9aa8e23ea8dcf3
Maks3w <github.maks3w@virtualplanets.net>
2012-05-08 00:20:59 +0200

    [CodeGenerator] Remove hierarchy from Zend\Code\Generator\Exception

--- library/Zend/Code/Generator/Exception.php
+++ library/Zend/Code/Generator/Exception.php
@@ -27,7 +27,7 @@ namespace Zend\Code\Generator;
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-interface Exception extends \Zend\CodeGenerator\Exception
+interface Exception extends \Zend\Code\Exception
 {
 
 }

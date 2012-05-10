d6babb237d2f648f3b83bf1d72639ee1997911cf
Maks3w <github.maks3w@virtualplanets.net>
2012-05-10 17:30:17 +0200

    [Validator] Improve Magic Mime Exceptions and Tests
    As of PHP >= 5.3.11 and >= 5.4.1 the magic database format has changed.
    http://doc.php.net/downloads/pdf/split/de/File-Information.pdf

---
+++ library/Zend/Validator/Exception/InvalidMagicMimeFileException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Zend\Validator\Exception;
+
+class InvalidMagicMimeFileException
+    extends \InvalidArgumentException
+    implements \Zend\Validator\Exception\ExceptionInterface
+{
+}

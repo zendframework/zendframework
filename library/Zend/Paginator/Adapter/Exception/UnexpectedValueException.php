dc55b67193348523f2e729d9f0555c92ac4c8add
prolic <saschaprolic@googlemail.com>
2012-05-18 21:13:37 +0200

    fixed exceptions in Zend\Paginator

--- library/Zend/Paginator/Adapter/Exception/UnexpectedValueException.php
+++ library/Zend/Paginator/Adapter/Exception/UnexpectedValueException.php
@@ -10,6 +10,8 @@
 
 namespace Zend\Paginator\Adapter\Exception;
 
+use Zend\Paginator\Exception;
+
 /**
  * @category   Zend
  * @package    Zend\Paginator\Adapter
@@ -18,6 +20,6 @@ namespace Zend\Paginator\Adapter\Exception;
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class UnexpectedValueException
-    extends \Zend\Paginator\Exception\UnexpectedValueException
+    extends Exception\UnexpectedValueException
     implements ExceptionInterface
 {}

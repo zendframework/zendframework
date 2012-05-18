9f178f059ecb48bdf867373980c880dfe9f7dbe3
prolic <saschaprolic@googlemail.com>
2012-05-18 21:24:45 +0200

    fixed exceptions in Zend\Cache

--- library/Zend/Cache/Exception/ExtensionNotLoadedException.php
+++ library/Zend/Cache/Exception/ExtensionNotLoadedException.php
@@ -26,6 +26,7 @@ namespace Zend\Cache\Exception;
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class ExtensionNotLoadedException extends RuntimeException implements ExceptionInterface
+class ExtensionNotLoadedException
+    extends RuntimeException
 {
 }
--- library/Zend/Cache/Exception/RuntimeException.php
+++ library/Zend/Cache/Exception/RuntimeException.php
@@ -26,6 +26,8 @@ namespace Zend\Cache\Exception;
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class RuntimeException extends \RuntimeException implements ExceptionInterface
+class RuntimeException
+    extends \RuntimeException
+    implements ExceptionInterface
 {
 }

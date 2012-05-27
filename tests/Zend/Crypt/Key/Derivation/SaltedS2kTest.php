94bb061d16a6f02c873aa4563cf7748b75c428b1
Evan Coury <me@evancoury.com>
2012-05-26 21:18:14 -0700

    Skip SaltedS2k tests if mhash extension is not loaded
    Also small tab/whitespace fix.

--- tests/Zend/Crypt/Key/Derivation/SaltedS2kTest.php
+++ tests/Zend/Crypt/Key/Derivation/SaltedS2kTest.php
@@ -44,6 +44,10 @@ class SaltedS2kTest extends \PHPUnit_Framework_TestCase
 
     public function testCalc()
     {
+        if (!extension_loaded('mhash')) {
+            $this->markTestSkipped('The mhash extension is not available');
+            return;
+        }
         $password = SaltedS2k::calc('sha256', 'test', $this->salt, 32);
         $this->assertEquals(32, strlen($password));
         $this->assertEquals('qzQISUBUSP1iqYtwe/druhdOVqluc/Y2TetdSHSbaw8=', base64_encode($password));
@@ -51,6 +55,10 @@ class SaltedS2kTest extends \PHPUnit_Framework_TestCase
 
     public function testCalcWithWrongHash()
     {
+        if (!extension_loaded('mhash')) {
+            $this->markTestSkipped('The mhash extension is not available');
+            return;
+        }
         $this->setExpectedException('Zend\Crypt\Key\Derivation\Exception\InvalidArgumentException',
                                     'The hash algorihtm wrong is not supported by Zend\Crypt\Key\Derivation\SaltedS2k');
         $password = SaltedS2k::calc('wrong', 'test', $this->salt, 32);
@@ -58,6 +66,10 @@ class SaltedS2kTest extends \PHPUnit_Framework_TestCase
 
     public function testCalcWithWrongSalt()
     {
+        if (!extension_loaded('mhash')) {
+            $this->markTestSkipped('The mhash extension is not available');
+            return;
+        }
         $this->setExpectedException('Zend\Crypt\Key\Derivation\Exception\InvalidArgumentException',
                                     'The salt size must be at least of 8 bytes');
         $password = SaltedS2k::calc('sha256', 'test', substr($this->salt,-1), 32);

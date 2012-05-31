e8eea2d2506f7070f59209743a844bea4383c268
Maks3w <github.maks3w@virtualplanets.net>
2012-05-31 10:24:11 +0200

    [Loader] Move legacy ZendTest\LoaderTest to the correct place

--- tests/Zend/LoaderTest.php
+++ tests/Zend/LoaderTest.php
@@ -13,16 +13,17 @@
  * to license@zend.com so we can send you a copy immediately.
  *
  * @category   Zend
- * @package    Loader
+ * @package    Zend_Loader
  * @subpackage UnitTests
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
-namespace ZendTest\Loader;
-use \stdClass,
-    \Phar,
-    \Zend\Loader;
+namespace ZendTest;
+
+use stdClass,
+    Phar,
+    Zend\Loader;
 
 /**
  * @category   Zend

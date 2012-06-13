484fcf2e3d78d8ed74293bddcd2eee15a1228f7a
Marc Bennewitz <marc-bennewitz@arcor.de>
2012-06-13 04:18:07 +0200

    phpdoc: Use class level method definition to re-define method return type

--- library/Zend/Cache/Storage/IterableInterface.php
+++ library/Zend/Cache/Storage/IterableInterface.php
@@ -27,15 +27,9 @@ namespace Zend\Cache\Storage;
  * @subpackage Storage
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ *
+ * @method IteratorInterface getIterator() Get the storage iterator
  */
 interface IterableInterface extends \IteratorAggregate
 {
-    /**
-     * Get the storage iterator
-     *
-     * @return IteratorInterface
-     */
-    // PHP 5.3.3: Fatal error: Can't inherit abstract function IteratorAggregate::getIterator()
-    // (previously declared abstract in Zend\Cache\Storage\IterableInterface)
-    //public function getIterator();
 }

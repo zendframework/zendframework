96a54473332787e8aa7ed2d86153cdacff23dd57
Christopher Martin <cgmartin@gmail.com>
2012-06-14 22:19:26 -0400

    [Form] New form view helpers for HTML4/XHTML1 element types

---
+++ library/Zend/Form/View/Helper/FormImage.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage View
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace Zend\Form\View\Helper;
+
+use Zend\Form\ElementInterface;
+use Zend\Form\Exception;
+
+/**
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage View
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class FormImage extends FormInput
+{
+    /**
+     * Attributes valid for the input tag type="image"
+     *
+     * @var array
+     */
+    protected $validTagAttributes = array(
+        'name'           => true,
+        'alt'            => true,
+        'autofocus'      => true,
+        'disabled'       => true,
+        'form'           => true,
+        'formaction'     => true,
+        'formenctype'    => true,
+        'formmethod'     => true,
+        'formnovalidate' => true,
+        'formtarget'     => true,
+        'height'         => true,
+        'src'            => true,
+        'type'           => true,
+        'value'          => true,
+        'width'          => true,
+    );
+
+    /**
+     * Render a form <input> element from the provided $element
+     *
+     * @param  ElementInterface $element
+     * @return string
+     */
+    public function render(ElementInterface $element)
+    {
+        $src = $element->getAttribute('src');
+        if (empty($src)) {
+            throw new Exception\DomainException(sprintf(
+                '%s requires that the element has an assigned src; none discovered',
+                __METHOD__
+            ));
+        }
+
+        return parent::render($element);
+    }
+
+    /**
+     * Determine input type to use
+     *
+     * @param  ElementInterface $element
+     * @return string
+     */
+    protected function getType(ElementInterface $element)
+    {
+        return 'image';
+    }
+}
---
+++ library/Zend/Form/View/Helper/FormSearch.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage View
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace Zend\Form\View\Helper;
+
+use Zend\Form\ElementInterface;
+
+/**
+ * FormSearch view helper
+ *
+ * The difference between the Text state and the Search state is primarily stylistic:
+ * on platforms where search fields are distinguished from regular text fields,
+ * the Search state might result in an appearance consistent with the platform's
+ * search fields rather than appearing like a regular text field.
+ *
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage View
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class FormSearch extends FormText
+{
+    /**
+     * Determine input type to use
+     *
+     * @param  ElementInterface $element
+     * @return string
+     */
+    protected function getType(ElementInterface $element)
+    {
+        return 'search';
+    }
+}

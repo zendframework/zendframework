c0fe7a81631e07c2e92f82166b0a44e4c661d6bd
Matthew Weier O'Phinney <matthew@zend.com>
2012-06-19 12:55:35 -0500

    Added unit tests for existing annotation manager functionality
    - Preparing to add a feature...

--- library/Zend/Code/Annotation/AnnotationManager.php
+++ library/Zend/Code/Annotation/AnnotationManager.php
@@ -1,19 +1,44 @@
 <?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Code
+ * @subpackage Annotation
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 
 namespace Zend\Code\Annotation;
 
 use Zend\Code\Exception;
 
+/**
+ * @category   Zend
+ * @package    Zend_Code
+ * @subpackage Annotation
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
 class AnnotationManager
 {
-
     /**
      * @var string[]
      */
     protected $annotationNames = array();
 
     /**
-     * @var Annotation[]
+     * @var AnnotationInterface[]
      */
     protected $annotations = array();
 
@@ -34,8 +59,8 @@ class AnnotationManager
     /**
      * Register annotations
      *
-     * @param AnnotationInterface $annotation
-     * @throws \Zend\Code\Exception\InvalidArgumentException
+     * @param  AnnotationInterface $annotation
+     * @throws Exception\InvalidArgumentException
      */
     public function registerAnnotation(AnnotationInterface $annotation)
     {
@@ -57,16 +82,16 @@ class AnnotationManager
      */
     public function hasAnnotation($class)
     {
-        // otherwise, only if its name exists as a key
+        // Only if its name exists as a key
         return in_array($class, $this->annotationNames);
     }
 
     /**
      * Create Annotation
      *
-     * @param string $class
-     * @param        $content
-     * @throws \Zend\Code\Exception\RuntimeException
+     * @param  string $class
+     * @param  null|string $content
+     * @throws Exception\RuntimeException
      * @return AnnotationInterface
      */
     public function createAnnotation($class, $content = null)

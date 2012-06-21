cd296db6790ae45fd049633bd7748682f67e1676
Michaël Gallego <mic.gallego@gmail.com>
2012-06-21 16:47:02 +0200

    Add form row view helper

---
+++ library/Zend/Form/View/Helper/FormRow.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage View
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace Zend\Form\View\Helper;
+
+use Zend\Form\ElementInterface;
+use Zend\Form\Exception;
+use Zend\Loader\Pluggable;
+use Zend\View\Helper\AbstractHelper as BaseAbstractHelper;
+
+/**
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage View
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class FormRow extends BaseAbstractHelper
+{
+    const LABEL_APPEND  = 'append';
+    const LABEL_PREPEND = 'prepend';
+
+    /**
+     * @var string
+     */
+    protected $labelPosition = self::LABEL_PREPEND;
+
+
+    /**
+     * Utility form helper that renders a label (if it exists), an element and errors
+     *
+     * @param ElementInterface $element
+     * @return string
+     * @throws \Zend\Form\Exception\DomainException
+     */
+    public function render(ElementInterface $element)
+    {
+        $renderer = $this->getView();
+        if (!$renderer instanceof Pluggable) {
+            // Bail early if renderer is not pluggable
+            return '';
+        }
+
+        $labelHelper = $renderer->plugin('form_label');
+        $elementHelper = $renderer->plugin('form_element');
+        $elementErrorsHelper = $renderer->plugin('form_element_errors');
+
+        $labelOpen      = $labelHelper->openTag();
+        $labelClose     = $labelHelper->closeTag();
+        $label = $element->getAttribute('label');
+        $elementString = $elementHelper->render($element);
+        $elementErrors = $elementErrorsHelper->render($element);
+
+        if (!empty($label)) {
+            switch ($this->labelPosition) {
+                case self::LABEL_PREPEND:
+                    $markup = $labelOpen . $label . $elementString . $labelClose . $elementErrors;
+                    break;
+                case self::LABEL_APPEND:
+                default:
+                    $markup = $labelOpen . $elementString . $label . $labelClose . $elementErrors;
+                    break;
+            }
+        } else {
+            $markup = $element . $elementErrors;
+        }
+
+        return $markup;
+    }
+
+    /**
+     * Invoke helper as functor
+     *
+     * Proxies to {@link render()}.
+     *
+     * @param  ElementInterface|null $element
+     * @return string
+     */
+    public function __invoke(ElementInterface $element = null)
+    {
+        if (!$element) {
+            return $this;
+        }
+
+        return $this->render($element);
+    }
+
+    /**
+     * Set the label position
+     *
+     * @param $labelPosition
+     * @return FormRow
+     * @throws \Zend\Form\Exception\InvalidArgumentException
+     */
+    public function setLabelPosition($labelPosition)
+    {
+        $labelPosition = strtolower($labelPosition);
+        if (!in_array($labelPosition, array(self::LABEL_APPEND, self::LABEL_PREPEND))) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                '%s expects either %s::LABEL_APPEND or %s::LABEL_PREPEND; received "%s"',
+                __METHOD__,
+                __CLASS__,
+                __CLASS__,
+                (string) $labelPosition
+            ));
+        }
+        $this->labelPosition = $labelPosition;
+        return $this;
+    }
+
+    /**
+     * Get the label position
+     *
+     * @return string
+     */
+    public function getLabelPosition()
+    {
+        return $this->labelPosition;
+    }
+}
---
+++ tests/Zend/Form/View/Helper/FormRowTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage UnitTest
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace ZendTest\Form\View\Helper;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Form\Element;
+use Zend\Form\View\Helper\FormRow as FormRowHelper;
+use Zend\Form\View\HelperLoader;
+use Zend\View\Renderer\PhpRenderer;
+
+/**
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage UnitTest
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class FormRowTest extends TestCase
+{
+    protected $helper;
+    protected $renderer;
+
+    public function setUp()
+    {
+        $this->helper = new FormRowHelper();
+
+        $this->renderer = new PhpRenderer;
+        $broker = $this->renderer->getBroker();
+        $loader = $broker->getClassLoader();
+        $loader->registerPlugins(new HelperLoader());
+        $this->helper->setView($this->renderer);
+    }
+
+    public function testCanGenerateLabel()
+    {
+        $element = new Element('foo');
+        $element->setAttribute('label', 'The value for foo:');
+        $markup = $this->helper->render($element);
+        $this->assertContains('>The value for foo:<', $markup);
+        $this->assertContains('<label', $markup);
+        $this->assertContains('</label>', $markup);
+    }
+
+    public function testCanCreateLabelValueBeforeInput()
+    {
+        $element = new Element('foo');
+        $element->setAttribute('label', 'The value for foo:');
+        $this->helper->setLabelPosition('prepend');
+        $markup = $this->helper->render($element);
+        $this->assertContains('<label>The value for foo:<', $markup);
+    }
+
+    public function testCanCreateLabelValueAfterInput()
+    {
+        $element = new Element('foo');
+        $element->setAttribute('label', 'The value for foo:');
+        $this->helper->setLabelPosition('append');
+        $markup = $this->helper->render($element);
+        $this->assertContains('<label><input', $markup);
+    }
+
+    public function testInvokeWithNoElementChainsHelper()
+    {
+        $this->assertSame($this->helper, $this->helper->__invoke());
+    }
+}

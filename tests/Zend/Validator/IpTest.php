e68334700e89bf365aa7c943dd2698a80b5e9f82
Maks3w <github.maks3w@virtualplanets.net>
2012-06-25 20:11:10 +0200

    [Validator] Add support for IPvFuture and literal format
    Add support to IPvFuture as described in RFC 3986
    Add support for IPs in literal format

--- tests/Zend/Validator/IpTest.php
+++ tests/Zend/Validator/IpTest.php
@@ -39,6 +39,14 @@ class IpTest extends \PHPUnit_Framework_TestCase
     protected $validator;
 
     /**
+     * The list with the options supported.
+     * By default all options are disabled.
+     *
+     * @var array
+     */
+    protected $options;
+
+    /**
      * Creates a new IP Validator for each test
      *
      * @return void
@@ -46,6 +54,12 @@ class IpTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->validator = new Ip();
+        $this->options   = array(
+            'allowipv4'      => false,
+            'allowipv6'      => false,
+            'allowipvfuture' => false,
+            'allowliteral' => false,
+        );
     }
 
     /**
@@ -68,34 +82,75 @@ class IpTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($this->validator->isValid('0.0.0.0'));
     }
 
-    /**
-     * Ensures that getMessages() returns expected default value
-     *
-     * @return void
-     */
-    public function testGetMessages()
-    {
-        $this->assertEquals(array(), $this->validator->getMessages());
-    }
 
     public function testOnlyIpv4()
     {
-        $this->validator->setOptions(array('allowipv6' => false));
+        $this->options['allowipv4'] = true;
+        $this->validator->setOptions($this->options);
         $this->assertTrue($this->validator->isValid('1.2.3.4'));
         $this->assertFalse($this->validator->isValid('a:b:c:d:e::1.2.3.4'));
+        $this->assertFalse($this->validator->isValid('v1.09azAZ-._~!$&\'()*+,;='));
     }
 
     public function testOnlyIpv6()
     {
-        $this->validator->setOptions(array('allowipv4' => false));
+        $this->options['allowipv6'] = true;
+        $this->validator->setOptions($this->options);
         $this->assertFalse($this->validator->isValid('1.2.3.4'));
         $this->assertTrue($this->validator->isValid('a:b:c:d:e::1.2.3.4'));
+        $this->assertFalse($this->validator->isValid('v1.09azAZ-._~!$&\'()*+,;='));
+    }
+
+    public function testOnlyIpvfuture()
+    {
+        $this->options['allowipvfuture'] = true;
+        $this->validator->setOptions($this->options);
+        $this->assertFalse($this->validator->isValid('1.2.3.4'));
+        $this->assertFalse($this->validator->isValid('a:b:c:d:e::1.2.3.4'));
+        $this->assertTrue($this->validator->isValid("v1.09azAZ-._~!$&'()*+,;=:"));
+    }
+
+    public function testLiteral()
+    {
+        $this->options   = array(
+            'allowipv4'      => true,
+            'allowipv6'      => true,
+            'allowipvfuture' => true,
+            'allowliteral'   => true,
+        );
+        $this->validator->setOptions($this->options);
+
+        $this->assertFalse($this->validator->isValid('[1.2.3.4]'));
+        $this->assertTrue($this->validator->isValid('[a:b:c:d:e::1.2.3.4]'));
+        $this->assertFalse($this->validator->isValid('[[a:b:c:d:e::1.2.3.4]]'));
+        $this->assertFalse($this->validator->isValid('[[a:b:c:d:e::1.2.3.4]'));
+        $this->assertFalse($this->validator->isValid('[[a:b:c:d:e::1.2.3.4'));
+        $this->assertFalse($this->validator->isValid('[a:b:c:d:e::1.2.3.4]]'));
+        $this->assertFalse($this->validator->isValid('a:b:c:d:e::1.2.3.4]]'));
+        $this->assertTrue($this->validator->isValid("[v1.ZZ:ZZ]"));
+    }
+
+    /**
+     * Versions 4 and 6 are not allowed in IPvFuture
+     *
+     * @depends testOnlyIpvfuture
+     */
+    public function testVersionsAllowedIpvfuture()
+    {
+        $this->options['allowipvfuture'] = true;
+        $this->validator->setOptions($this->options);
+        $this->assertTrue($this->validator->isValid('v1.A', 'IPvFuture: Version 1 disallowed'));
+        $this->assertTrue($this->validator->isValid('vD.A', 'IPvFuture: Version D disallowed'));
+        $this->assertTrue($this->validator->isValid('v46.A', 'IPvFuture: Version 46 disallowed'));
+
+        $this->assertFalse($this->validator->isValid('v4.A', 'IPvFuture: Version 4 allowed'));
+        $this->assertFalse($this->validator->isValid('v6.A', 'IPvFuture: Version 6 allowed'));
     }
 
     public function testNoValidation()
     {
         $this->setExpectedException('Zend\Validator\Exception\InvalidArgumentException', 'Nothing to validate');
-        $this->validator->setOptions(array('allowipv4' => false, 'allowipv6' => false));
+        $this->validator->setOptions($this->options);
     }
 
     public function testInvalidIpForZF4809()
@@ -114,90 +169,94 @@ class IpTest extends \PHPUnit_Framework_TestCase
      */
     public function testIPv6addresses()
     {
-        $IPs = array(
-            '2001:0db8:0000:0000:0000:0000:1428:57ab'   => true,
-            '2001:0DB8:0000:0000:0000:0000:1428:57AB'   => true,
-            '2001:00db8:0000:0000:0000:0000:1428:57ab'  => false,
-            '2001:0db8:xxxx:0000:0000:0000:1428:57ab'   => false,
+        $ips = array(
+            '2001:0db8:0000:0000:0000:0000:1428:57ab'      => true,
+            '2001:0DB8:0000:0000:0000:0000:1428:57AB'      => true,
+            '[2001:0DB8:0000:0000:0000:0000:1428:57AB]'    => true,
+            '2001:00db8:0000:0000:0000:0000:1428:57ab'     => false,
+            '2001:0db8:xxxx:0000:0000:0000:1428:57ab'      => false,
+            '2001:0DB8:0000:0000:0000:0000:1428:57AB:90'   => false,
+            '[2001:0DB8:0000:0000:0000:0000:1428:57AB]:90' => false,
 
-            '2001:db8::1428:57ab'                       => true,
-            '2001:db8::1428::57ab'                      => false,
-            '2001:dx0::1234'                            => false,
-            '2001:db0::12345'                           => false,
+            '2001:db8::1428:57ab'                          => true,
+            '2001:db8::1428::57ab'                         => false,
+            '2001:dx0::1234'                               => false,
+            '2001:db0::12345'                              => false,
 
-            ''                                          => false,
-            ':'                                         => false,
-            '::'                                        => true,
-            ':::'                                       => false,
-            '::::'                                      => false,
-            '::1'                                       => true,
-            ':::1'                                      => false,
+            ''                                             => false,
+            ':'                                            => false,
+            '::'                                           => true,
+            ':::'                                          => false,
+            '::::'                                         => false,
+            '::1'                                          => true,
+            ':::1'                                         => false,
 
-            '::1.2.3.4'                                 => true,
-            '::127.0.0.1'                               => true,
-            '::256.0.0.1'                               => false,
-            '::01.02.03.04'                             => true,
+            '[::1.2.3.4]'                                  => true,
+            '::1.2.3.4'                                    => true,
+            '::127.0.0.1'                                  => true,
+            '::256.0.0.1'                                  => false,
+            '::01.02.03.04'                                => true,
             // according to RFC this can be interpreted as hex notation IPv4
-            'a:b:c::1.2.3.4'                            => true,
-            'a:b:c:d::1.2.3.4'                          => true,
-            'a:b:c:d:e::1.2.3.4'                        => true,
-            'a:b:c:d:e:f:1.2.3.4'                       => true,
-            'a:b:c:d:e:f:1.256.3.4'                     => false,
-            'a:b:c:d:e:f::1.2.3.4'                      => false,
+            'a:b:c::1.2.3.4'                               => true,
+            'a:b:c:d::1.2.3.4'                             => true,
+            'a:b:c:d:e::1.2.3.4'                           => true,
+            'a:b:c:d:e:f:1.2.3.4'                          => true,
+            'a:b:c:d:e:f:1.256.3.4'                        => false,
+            'a:b:c:d:e:f::1.2.3.4'                         => false,
 
-            'a:b:c:d:e:f:0:1:2'                         => false,
-            'a:b:c:d:e:f:0:1'                           => true,
-            'a::b:c:d:e:f:0:1'                          => false,
-            'a::c:d:e:f:0:1'                            => true,
-            'a::d:e:f:0:1'                              => true,
-            'a::e:f:0:1'                                => true,
-            'a::f:0:1'                                  => true,
-            'a::0:1'                                    => true,
-            'a::1'                                      => true,
-            'a::'                                       => true,
+            'a:b:c:d:e:f:0:1:2'                            => false,
+            'a:b:c:d:e:f:0:1'                              => true,
+            'a::b:c:d:e:f:0:1'                             => false,
+            'a::c:d:e:f:0:1'                               => true,
+            'a::d:e:f:0:1'                                 => true,
+            'a::e:f:0:1'                                   => true,
+            'a::f:0:1'                                     => true,
+            'a::0:1'                                       => true,
+            'a::1'                                         => true,
+            'a::'                                          => true,
 
-            '::0:1:a:b:c:d:e:f'                         => false,
-            '::0:a:b:c:d:e:f'                           => true,
-            '::a:b:c:d:e:f'                             => true,
-            '::b:c:d:e:f'                               => true,
-            '::c:d:e:f'                                 => true,
-            '::d:e:f'                                   => true,
-            '::e:f'                                     => true,
-            '::f'                                       => true,
+            '::0:1:a:b:c:d:e:f'                            => false,
+            '::0:a:b:c:d:e:f'                              => true,
+            '::a:b:c:d:e:f'                                => true,
+            '::b:c:d:e:f'                                  => true,
+            '::c:d:e:f'                                    => true,
+            '::d:e:f'                                      => true,
+            '::e:f'                                        => true,
+            '::f'                                          => true,
 
-            '0:1:a:b:c:d:e:f::'                         => false,
-            '0:a:b:c:d:e:f::'                           => true,
-            'a:b:c:d:e:f::'                             => true,
-            'b:c:d:e:f::'                               => true,
-            'c:d:e:f::'                                 => true,
-            'd:e:f::'                                   => true,
-            'e:f::'                                     => true,
-            'f::'                                       => true,
+            '0:1:a:b:c:d:e:f::'                            => false,
+            '0:a:b:c:d:e:f::'                              => true,
+            'a:b:c:d:e:f::'                                => true,
+            'b:c:d:e:f::'                                  => true,
+            'c:d:e:f::'                                    => true,
+            'd:e:f::'                                      => true,
+            'e:f::'                                        => true,
+            'f::'                                          => true,
 
-            'a:b:::e:f'                                 => false,
-            '::a:'                                      => false,
-            '::a::'                                     => false,
-            ':a::b'                                     => false,
-            'a::b:'                                     => false,
-            '::a:b::c'                                  => false,
-            'abcde::f'                                  => false,
+            'a:b:::e:f'                                    => false,
+            '::a:'                                         => false,
+            '::a::'                                        => false,
+            ':a::b'                                        => false,
+            'a::b:'                                        => false,
+            '::a:b::c'                                     => false,
+            'abcde::f'                                     => false,
 
-            ':10.0.0.1'                                 => false,
-            '0:0:0:255.255.255.255'                     => false,
-            '1fff::a88:85a3::172.31.128.1'              => false,
+            ':10.0.0.1'                                    => false,
+            '0:0:0:255.255.255.255'                        => false,
+            '1fff::a88:85a3::172.31.128.1'                 => false,
 
-            'a:b:c:d:e:f:0::1'                          => false,
-            'a:b:c:d:e:f:0::'                           => true,
-            'a:b:c:d:e:f::0'                            => true,
+            'a:b:c:d:e:f:0::1'                             => false,
+            'a:b:c:d:e:f:0::'                              => true,
+            'a:b:c:d:e:f::0'                               => true,
 
-            'total gibberish'                           => false
+            'total gibberish'                              => false
         );
 
-        foreach ($IPs as $ip => $expectedOutcome) {
+        foreach ($ips as $ip => $expectedOutcome) {
             if ($expectedOutcome) {
-                $this->assertTrue($this->validator->isValid($ip), $ip . ' failed validation');
+                $this->assertTrue($this->validator->isValid($ip), $ip . ' failed validation (expects true)');
             } else {
-                $this->assertFalse($this->validator->isValid($ip), $ip . ' failed validation');
+                $this->assertFalse($this->validator->isValid($ip), $ip . ' failed validation (expects false)');
             }
         }
 
@@ -226,28 +285,95 @@ class IpTest extends \PHPUnit_Framework_TestCase
     {
         $ips = array(
             // binary notation
-            '00000001.00000010.00000011.00000100' => true,
-            '10000000.02000000.00000000.00000001' => false,
+            '00000001.00000010.00000011.00000100'    => true,
+            '10000000.02000000.00000000.00000001'    => false,
+            '10000000.02000000.00000000.00000001:80' => false,
 
             // octal notation (always seen as integer!)
-            '001.002.003.004'                     => true,
-            '009.008.007.006'                     => true,
-            '0a0.100.001.010'                     => false,
+            '001.002.003.004'                        => true,
+            '009.008.007.006'                        => true,
+            '0a0.100.001.010'                        => false,
+            '0a0.100.001.010:80'                     => false,
 
             // hex notation
-            '01.02.03.04'                         => true,
-            'a0.b0.c0.d0'                         => true,
-            'g0.00.00.00'                         => false
+            '01.02.03.04'                            => true,
+            'a0.b0.c0.d0'                            => true,
+            'g0.00.00.00'                            => false,
+            'g0.00.00.00:80'                         => false,
         );
 
         foreach ($ips as $ip => $expectedOutcome) {
             if ($expectedOutcome) {
-                $this->assertTrue($this->validator->isValid($ip), $ip . ' failed validation');
+                $this->assertTrue($this->validator->isValid($ip), $ip . ' failed validation (expects true)');
             } else {
-                $this->assertFalse($this->validator->isValid($ip), $ip . ' failed validation');
+                $this->assertFalse($this->validator->isValid($ip), $ip . ' failed validation (expects false)');
             }
         }
+    }
 
+    /**
+     * @dataProvider iPvFutureAddressesProvider
+     */
+    public function testIPvFutureAddresses($ip, $expected)
+    {
+        $this->options['allowipvfuture'] = true;
+        $this->options['allowliteral'] = true;
+        $this->validator->setOptions($this->options);
+        $this->assertEquals($expected, $this->validator->isValid($ip));
+    }
+
+    public function iPvFutureAddressesProvider()
+    {
+        return array(
+            array("[v1.09azAZ-._~!$&'()*+,;=:]:80", false),
+            array("[v1.09azAZ-._~!$&'()*+,;=:]", true),
+            array("[v1.09azAZ-._~!$&'()*+,;=:", false),
+            array("v1.09azAZ-._~!$&'()*+,;=:]", false),
+            array("v1.09azAZ-._~!$&'()*+,;=:", true),
+            array("v1.09azAZ-._~!$&'()*+,;=", true),
+            array("v1.09azAZ-._~!$&'()*+,;", true),
+            array("v1.09azAZ-._~!$&'()*+,", true),
+            array("v1.09azAZ-._~!$&'()*+", true),
+            array("v1.09azAZ-._~!$&'()*", true),
+            array("v1.09azAZ-._~!$&'()", true),
+            array("v1.09azAZ-._~!$&'(", true),
+            array("v1.09azAZ-._~!$&'", true),
+            array("v1.09azAZ-._~!$&", true),
+            array("v1.09azAZ-._~!$", true),
+            array("v1.09azAZ-._~!", true),
+            array("v1.09azAZ-._~", true),
+            array("v1.09azAZ-._", true),
+            array("v1.09azAZ-.", true),
+            array("v1.09azAZ-", true),
+            array("v1.09azAZ", true),
+            array("v1.09azA", true),
+            array("v1.09az", true),
+            array("v1.09a", true),
+            array("v1.09", true),
+            array("v1.0", true),
+            array("v1.", false),
+            array("v1", false),
+            array("v", false),
+            array("", false),
+            array("vFF.Z", true),
+            array("vFG./", false),
+            array("v1./", false),
+            array("v1.?", false),
+            array("v1.#", false),
+            array("v1.[", false),
+            array("v1.]", false),
+            array("v1.@", false),
+        );
+    }
+
+    /**
+     * Ensures that getMessages() returns expected default value
+     *
+     * @return void
+     */
+    public function testGetMessages()
+    {
+        $this->assertEquals(array(), $this->validator->getMessages());
     }
 
     public function testEqualsMessageTemplates()

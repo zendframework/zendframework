21e15711be7a989c05660727c71e31ac07e597d2
Matthew Weier O'Phinney <matthew@zend.com>
2012-06-29 11:19:35 -0500

    [zen-60] remove dependency on composer autoloader
    - If composer autoloader is missing, simply use the standard autoloader

--- tests/_autoload.php
+++ tests/_autoload.php
@@ -3,12 +3,14 @@
  * Setup autoloading
  */
 if (!include_once __DIR__ . '/../vendor/autoload.php') {
-    throw new UnexpectedValueException('Could not find composer; did you run `php composer.phar install`?');
+    // if composer autoloader is missing, explicitly load the standard 
+    // autoloader by relativepath
+    require_once __DIR__ . '../library/Zend/Loader/StandardAutoloader.php';
 }
 
 $loader = new Zend\Loader\StandardAutoloader(array(
     Zend\Loader\StandardAutoloader::LOAD_NS => array(
-        'Zend'     => __DIR__ . '/../library',
+        'Zend'     => __DIR__ . '/../library/Zend', // present in case composer autoloader missing
         'ZendTest' => __DIR__ . '/Zend',
     ),
 ));

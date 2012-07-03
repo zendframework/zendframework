34a3384a35e8d62f9d60d290cfaeaae0e3539cd0
Denis Portnov <denixport@gmail.com>
2012-07-03 07:52:34 +0400

    filter plugin manager

---
+++ library/Zend/I18n/Filter/FilterPluginManager.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Filter
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace Zend\I18n\Filter;
+
+use Zend\ServiceManager\AbstractPluginManager;
+use Zend\Filter\FilterInterface;
+use Zend\I18n\Exception;
+
+/**
+ * Plugin manager implementation for the filter chain.
+ *
+ * Enforces that filters retrieved are either callbacks or instances of
+ * FilterInterface. Additionally, it registers a number of default filters
+ * available, as well as aliases for them.
+ *
+ * @category   Zend
+ * @package    Zend_Filter
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class FilterPluginManager extends AbstractPluginManager
+{
+    /**
+     * Default set of filters
+     * 
+     * @var array
+     */
+    protected $invokableClasses = array(
+        'numberformat' => 'Zend\I18n\Filter\NumberFormat',
+    );
+
+    /**
+     * Validate the plugin
+     *
+     * Checks that the filter loaded is either a valid callback or an instance
+     * of FilterInterface.
+     * 
+     * @param  mixed $plugin 
+     * @return void
+     * @throws Exception\RuntimeException if invalid
+     */
+    public function validatePlugin($plugin)
+    {
+        if ($plugin instanceof FilterInterface) {
+            // we're okay
+            return;
+        }
+        if (is_callable($plugin)) {
+            // also okay
+            return;
+        }
+
+        throw new Exception\RuntimeException(sprintf(
+            'Plugin of type %s is invalid; must implement %s\FilterInterface or be callable',
+            (is_object($plugin) ? get_class($plugin) : gettype($plugin)),
+            __NAMESPACE__
+        ));
+    }
+}

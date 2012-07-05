30b0f8c228a612e57daa19d292ab6631f6088711
Padraic Brady <padraic.brady@gmail.com>
2012-07-05 18:35:30 +0100

    Incorrect class name for EscapeUrl helper. Everyone has a Homer day...

--- library/Zend/View/Helper/EscapeUrl.php
+++ library/Zend/View/Helper/EscapeUrl.php
@@ -32,7 +32,7 @@ use Zend\View\Exception;
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class EscapeCss extends Escaper\AbstractHelper
+class EscapeUrl extends Escaper\AbstractHelper
 {
     /**@+
      * @const Recursion constants

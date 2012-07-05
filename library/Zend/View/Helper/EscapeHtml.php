10bd6bc051a62e5e12a5080c29b3c5f19bb9e1c2
Padraic Brady <padraic.brady@gmail.com>
2012-07-05 20:06:38 +0100

    Minor refactor of Escape helper recursion into parent AbstractHelper

--- library/Zend/View/Helper/EscapeHtml.php
+++ library/Zend/View/Helper/EscapeHtml.php
@@ -34,54 +34,16 @@ use Zend\View\Exception;
  */
 class EscapeHtml extends Escaper\AbstractHelper
 {
-    /**@+
-     * @const Recursion constants
-     */
-    const RECURSE_NONE   = 0x00;
-    const RECURSE_ARRAY  = 0x01;
-    const RECURSE_OBJECT = 0x02;
-    /**@-*/
-
+    
     /**
-     * Invoke this helper: escape a value
-     * 
-     * @param  mixed $value 
-     * @param  int $recurse Expects one of the recursion constants; used to decide whether or not to recurse the given value when escaping
-     * @return mixed Given a scalar, a scalar value is returned. Given an object, with the $recurse flag not allowing object recursion, returns a string. Otherwise, returns an array.
-     * @throws Exception\InvalidArgumentException
+     * Escape a value for current escaping strategy
+     *
+     * @param string $value
+     * @return string
      */
-    public function __invoke($value, $recurse = self::RECURSE_NONE)
+    protected function escape($value)
     {
-        if (is_string($value)) {
-            return $this->getEscaper()->escapeHtml($value);
-        }
-        if (is_array($value)) {
-            if (!(self::RECURSE_ARRAY & $recurse)) {
-                throw new Exception\InvalidArgumentException(
-                    'Array provided to Escape helper, but flags do not allow recursion'
-                );
-            }
-            foreach ($value as $k => $v) {
-                $value[$k] = $this->__invoke($v, $recurse);
-            }
-            return $value;
-        }
-        if (is_object($value)) {
-            if (!(self::RECURSE_OBJECT & $recurse)) {
-                // Attempt to cast it to a string
-                if (method_exists($value, '__toString')) {
-                    return $this->getEscaper()->escapeHtml((string) $value);
-                }
-                throw new Exception\InvalidArgumentException(
-                    'Object provided to Escape helper, but flags do not allow recursion'
-                );
-            }
-            if (method_exists($value, 'toArray')) {
-                return $this->__invoke($value->toArray(), $recurse | self::RECURSE_ARRAY);
-            }
-            return $this->__invoke((array) $value, $recurse | self::RECURSE_ARRAY);
-        }
-        // At this point, we have a scalar; simply return it
-        return $value;
+        return $this->getEscaper()->escapeHtml($value);
     }
+
 }

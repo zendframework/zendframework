67c2c697f6ba4e36828edcd2e5b4d56a46a962f3
Evan Coury <me@evancoury.com>
2012-07-05 04:02:46 -0700

    Remove accidental paste of extra docblock
    I accidentally the docblock.

--- library/Zend/I18n/Translator/TranslatorServiceFactory.php
+++ library/Zend/I18n/Translator/TranslatorServiceFactory.php
@@ -18,14 +18,6 @@
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Db
- */
 
 namespace Zend\I18n\Translator;
 

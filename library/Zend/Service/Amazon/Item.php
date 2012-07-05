acb9c52250a0fadb12c70ca75bac602f1cfd4105
Maks3w <github.maks3w@virtualplanets.net>
2012-07-05 20:20:13 +0200

    [Amazon] Some changes
    * Remove a test for getters/setters
    * Throw exception when item-list is empty

--- library/Zend/Service/Amazon/Item.php
+++ library/Zend/Service/Amazon/Item.php
@@ -118,14 +118,8 @@ class Item
      * 
      * @group ZF-9547
      */
-    public function __construct(DOMElement $dom = null)
+    public function __construct(DOMElement $dom)
     {
-        if (null === $dom) {
-            throw new Exception\InvalidArgumentException(sprintf(
-                '%s expects a populated DOMElement; received null',
-                __CLASS__
-            ));
-        }
         $xpath = new \DOMXPath($dom->ownerDocument);
         $xpath->registerNamespace('az', 'http://webservices.amazon.com/AWSECommerceService/2011-08-01');
         $this->ASIN = $xpath->query('./az:ASIN/text()', $dom)->item(0)->data;

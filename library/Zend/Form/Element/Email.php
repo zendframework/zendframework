0f937a27ff2d5d423eecb9cf446396dbe2883520
Christopher Martin <cgmartin@gmail.com>
2012-07-08 18:36:09 -0400

    [Docs] Form Email element docs and bugfix to match HTML5 spec

--- library/Zend/Form/Element/Email.php
+++ library/Zend/Form/Element/Email.php
@@ -24,7 +24,7 @@ namespace Zend\Form\Element;
 use Zend\Form\Element;
 use Zend\InputFilter\InputProviderInterface;
 use Zend\Validator\ValidatorInterface;
-use Zend\Validator\EmailAddress as EmailValidator;
+use Zend\Validator\Regex as RegexValidator;
 use Zend\Validator\Explode as ExplodeValidator;
 
 /**
@@ -51,24 +51,86 @@ class Email extends Element implements InputProviderInterface
     protected $validator;
 
     /**
-     * Get validator
+     * @var ValidatorInterface
+     */
+    protected $emailValidator;
+
+    /**
+     * Get primary validator
      *
      * @return ValidatorInterface
      */
     public function getValidator()
     {
         if (null === $this->validator) {
+            $emailValidator = $this->getEmailValidator();
             if (!empty($this->attributes['multiple'])) {
                 $this->validator = new ExplodeValidator(array(
-                    'validator' => new EmailValidator(),
+                    'validator' => $emailValidator,
                 ));
             } else {
-                $this->validator = new EmailValidator();
+                $this->validator = $emailValidator;
             }
         }
 
         return $this->validator;
     }
+
+    /**
+     * Sets the primary validator to use for this element
+     *
+     * @param  ValidatorInterface $validator
+     * @return Email
+     */
+    public function setValidator(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+        return $this;
+    }
+
+    /**
+     * Get the email validator to use for multiple or single
+     * email addresses.
+     *
+     * Note from the HTML5 Specs regarding the regex:
+     *
+     * "This requirement is a *willful* violation of RFC 5322, which
+     * defines a syntax for e-mail addresses that is simultaneously
+     * too strict (before the "@" character), too vague
+     * (after the "@" character), and too lax (allowing comments,
+     * whitespace characters, and quoted strings in manners
+     * unfamiliar to most users) to be of practical use here."
+     *
+     * The default Regex validator is in use to match that of the
+     * browser validation, but you are free to set a different
+     * (more strict) email validator such as Zend\Validator\Email
+     * if you wish.
+     *
+     * @return ValidatorInterface
+     */
+    public function getEmailValidator()
+    {
+        if (null === $this->emailValidator) {
+            $this->emailValidator = new RegexValidator(
+                '/^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/'
+            );
+        }
+        return $this->emailValidator;
+    }
+
+    /**
+     * Sets the email validator to use for multiple or single
+     * email addresses.
+     *
+     * @param  ValidatorInterface $validator
+     * @return Email
+     */
+    public function setEmailValidator(ValidatorInterface $validator)
+    {
+        $this->emailValidator = $validator;
+        return $this;
+    }
+
 
     /**
      * Provide default input rules for this element

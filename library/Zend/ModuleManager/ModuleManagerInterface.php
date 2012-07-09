745f9c950725d21338e7c307fef39523c795e88a
Maks3w <github.maks3w@virtualplanets.net>
2012-07-09 16:19:42 +0200

    [CS][Library] Set File Header
    http://framework.zend.com/wiki/display/ZFDEV2/Coding+Standards#CodingStandards-Files
    
    The following script replaces the content between PHP open tag and namespace declaration.
    
    for COMPONENT in $(ls -d *)
    do
        for FILE in $(find $COMPONENT -name "*.php")
        do
            BLOCK="\/\*\*\n \* Zend Framework \(http:\/\/framework\.zend\.com\/\)\n \*\n \* \@link      http:\/\/github\.com\/zendframework\/zf2 for the canonical source repository\n \* \@copyright Copyright \(c\) 2005-2012 Zend Technologies USA Inc\. \(http:\/\/www\.zend\.com\)\n \* \@license   http:\/\/framework\.zend\.com\/license\/new-bsd New BSD License\n \* \@package   Zend_$COMPONENT\n \*\/"
            perl -0777 -i -pe "s/(<\?php(\s*.*)*\nn)/<?php\n$BLOCK\n\nn/" $FILE
        done
    done

--- library/Zend/ModuleManager/ModuleManagerInterface.php
+++ library/Zend/ModuleManager/ModuleManagerInterface.php
@@ -3,7 +3,9 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @package   Zend_Module
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_ModuleManager
  */
 
 namespace Zend\ModuleManager;

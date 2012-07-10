71f9ee9f0169ce377829c74bbb859f90626f6f2a
Chris Martin <cgmartin@gmail.com>
2012-07-10 13:54:44 -0400

    [BugFix] ZF2-391 form element boolean attributes to support raw values

--- tests/Zend/Form/View/Helper/FormInputTest.php
+++ tests/Zend/Form/View/Helper/FormInputTest.php
@@ -409,6 +409,22 @@ class FormInputTest extends CommonTestCase
         } else {
             $this->assertNotContains($expect, $markup, sprintf("Disabled value for %s should not be rendered; received %s", $attribute, $markup));
         }
+
+        // ZF2-391 : Ability to use non-boolean values that match expected end-value
+        $element->setAttribute($attribute, $on);
+        $markup = $this->helper->render($element);
+        $expect = sprintf('%s="%s"', $attribute, $on);
+        $this->assertContains($expect, $markup, sprintf("Enabled value for %s should be '%s'; received %s", $attribute, $on, $markup));
+
+        $element->setAttribute($attribute, $off);
+        $markup = $this->helper->render($element);
+        $expect = sprintf('%s="%s"', $attribute, $off);
+
+        if ($off !== '') {
+            $this->assertContains($expect, $markup, sprintf("Disabled value for %s should be '%s'; received %s", $attribute, $off, $markup));
+        } else {
+            $this->assertNotContains($expect, $markup, sprintf("Disabled value for %s should not be rendered; received %s", $attribute, $markup));
+        }
     }
 
     public function testInvokeProxiesToRender()

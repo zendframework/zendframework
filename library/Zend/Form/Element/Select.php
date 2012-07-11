d5e9dac7ac07717be2958f2060957a3629110ad9
Chris Martin <cgmartin@gmail.com>
2012-07-11 12:28:44 -0400

    [Form] InArray validator added to Select element

--- library/Zend/Form/Element/Select.php
+++ library/Zend/Form/Element/Select.php
@@ -23,6 +23,9 @@ namespace Zend\Form\Element;
 
 use Traversable;
 use Zend\Form\Element;
+use Zend\InputFilter\InputProviderInterface;
+use Zend\Validator\InArray as InArrayValidator;
+use Zend\Validator\ValidatorInterface;
 
 /**
  * @category   Zend
@@ -41,4 +44,45 @@ class Select extends Element
     protected $attributes = array(
         'type' => 'select',
     );
+
+    /**
+    * @var ValidatorInterface
+    */
+    protected $validator;
+
+    /**
+    * Get validator
+    *
+    * @return ValidatorInterface
+    */
+    protected function getValidator()
+    {
+        if (null === $this->validator) {
+            $this->validator = new InArrayValidator(array(
+                'haystack' => (array) $this->getAttribute('options'),
+                'strict'   => false
+            ));
+        }
+        return $this->validator;
+    }
+
+    /**
+     * Provide default input rules for this element
+     *
+     * Attaches the captcha as a validator.
+     *
+     * @return array
+     */
+    public function getInputSpecification()
+    {
+        $spec = array(
+            'name' => $this->getName(),
+            'required' => true,
+            'validators' => array(
+                $this->getValidator()
+            )
+        );
+
+        return $spec;
+    }
 }

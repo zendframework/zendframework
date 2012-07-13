b59183e724aa437aea918987a09b15f606386e93
Ben Youngblood <bx.youngblood@gmail.com>
2012-07-13 14:47:40 -0700

    docblocks for prg plugin

--- library/Zend/Mvc/Controller/Plugin/PostRedirectGet.php
+++ library/Zend/Mvc/Controller/Plugin/PostRedirectGet.php
@@ -1,9 +1,25 @@
 <?php
 
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Mvc
+ */
+
 namespace Zend\Mvc\Controller\Plugin;
 
 use Zend\Mvc\Router\Exception\RuntimeException;
 
+/**
+ * Plugin to help facilitate Post/Redirect/Get (http://en.wikipedia.org/wiki/Post/Redirect/Get)
+ *
+ * @category Zend
+ * @package Zend_Mvc
+ * @subpackage Controller
+ */
 class PostRedirectGet extends AbstractPlugin
 {
     public function __invoke($redirect)

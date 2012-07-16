e060312c6546b68ff4193e770782b744b13fa54c
Ben Youngblood <bx.youngblood@gmail.com>
2012-07-16 02:01:17 -0700

    license header + class-level docblock in tests

--- tests/Zend/Mvc/Controller/Plugin/PostRedirectGetTest.php
+++ tests/Zend/Mvc/Controller/Plugin/PostRedirectGetTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Mvc
+ */
+
 namespace ZendTest\Mvc\Controller\Plugin;
 
 use PHPUnit_Framework_TestCase as TestCase;
@@ -13,6 +22,11 @@ use Zend\Stdlib\Parameters;
 use ZendTest\Mvc\Controller\TestAsset\SampleController;
 use ZendTest\Session\TestAsset\TestManager as SessionManager;
 
+/**
+ * @category   Zend
+ * @package    Zend_Mvc
+ * @subpackage UnitTests
+ */
 class PostRedirectGetTest extends TestCase
 {
     public $controller;

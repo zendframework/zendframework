cddcd8d7e61e6d75ed2778c6503fc57db2b21af8
Denis Portnov <denixport@gmail.com>
2012-07-17 04:49:54 +0400

    remove abstract adapter

--- library/Zend/Serializer/Adapter/AdapterInterface.php
+++ library/Zend/Serializer/Adapter/AdapterInterface.php
@@ -18,62 +18,20 @@ namespace Zend\Serializer\Adapter;
 interface AdapterInterface
 {
     /**
-     * Constructor
-     *
-     * @param  array|\Traversable $options Serializer options
-     */
-    public function __construct($options = array());
-
-    /**
-     * Set serializer options
-     *
-     * @param  array|\Traversable $options Serializer options
-     * @return AdapterInterface
-     */
-    public function setOptions($options);
-
-    /**
-     * Set a serializer option
-     *
-     * @param  string $name Option name
-     * @param  mixed $value Option value
-     * @return AdapterInterface
-     */
-    public function setOption($name, $value);
-
-    /**
-     * Get serializer options
-     *
-     * @return array
-     */
-    public function getOptions();
-
-    /**
-     * Get a serializer option
-     *
-     * @param  string $name
-     * @return mixed
-     * @throws AdapterInterface
-     */
-    public function getOption($name);
-
-    /**
      * Generates a storable representation of a value.
      *
      * @param  mixed $value Data to serialize
-     * @param  array $options Serialize options
      * @return string
      * @throws \Zend\Serializer\Exception\ExceptionInterface
      */
-    public function serialize($value, array $options = array());
+    public function serialize($value);
 
     /**
      * Creates a PHP value from a stored representation.
      *
      * @param  string $serialized Serialized string
-     * @param  array $options Unserialize options
      * @return mixed
      * @throws \Zend\Serializer\Exception\ExceptionInterface
      */
-    public function unserialize($serialized, array $options = array());
+    public function unserialize($serialized);
 }

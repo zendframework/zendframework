83bb330cd1f4f7bdb3bbd5ff0662b5d20b32dd0d
Ralph Schindler <ralph@ralphschindler.com>
2012-07-23 10:59:57 -0500

    Updating PHPUnit minimal version to 3.6.0

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -14,7 +14,7 @@
 error_reporting( E_ALL | E_STRICT );
 
 $phpUnitVersion = PHPUnit_Runner_Version::id();
-if ('@package_version@' !== $phpUnitVersion && version_compare($phpUnitVersion, '3.5.0', '<')) {
+if ('@package_version@' !== $phpUnitVersion && version_compare($phpUnitVersion, '3.6.0', '<')) {
     echo 'This version of PHPUnit (' . PHPUnit_Runner_Version::id() . ') is not supported in Zend Framework 2.x unit tests.' . PHP_EOL;
     exit(1);
 }

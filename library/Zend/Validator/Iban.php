37b749a56a9ca062cbbf0defa4c657796b919180
Mike <wickedymike@gmail.com>
2012-07-31 12:20:48 +0200

    * Include all known IBAN countries with correct validators * Add more unit tests * Allow exclusion of non-Sepa countries

--- library/Zend/Validator/Iban.php
+++ library/Zend/Validator/Iban.php
@@ -23,9 +23,10 @@ use Zend\Validator\Exception;
  */
 class Iban extends AbstractValidator
 {
-    const NOTSUPPORTED = 'ibanNotSupported';
-    const FALSEFORMAT  = 'ibanFalseFormat';
-    const CHECKFAILED  = 'ibanCheckFailed';
+    const NOTSUPPORTED     = 'ibanNotSupported';
+    const SEPANOTSUPPORTED = 'ibanSepaNotSupported';
+    const FALSEFORMAT      = 'ibanFalseFormat';
+    const CHECKFAILED      = 'ibanCheckFailed';
 
     /**
      * Validation failure message template definitions
@@ -34,6 +35,7 @@ class Iban extends AbstractValidator
      */
     protected $messageTemplates = array(
         self::NOTSUPPORTED => "Unknown country within the IBAN",
+        self::SEPANOTSUPPORTED => "Unknown country within the SEPA",
         self::FALSEFORMAT  => "The input has a false IBAN format",
         self::CHECKFAILED  => "The input has failed the IBAN check",
     );
@@ -46,50 +48,92 @@ class Iban extends AbstractValidator
     protected $countryCode;
 
     /**
+     * Optionally allow IBAN codes from non-SEPA countries. Defaults to true
+     * 
+     * @var bool
+     */
+    protected $allowNonSepa = true;
+    
+    /**
+     * The SEPA country codes
+     * 
+     * @var array<ISO 3166-1> 
+     */
+    protected static $sepaCountries = array(
+        'AT', 'BE', 'BG', 'CY', 'CZ', 'DK', 'FO', 'GL', 'EE', 'FI', 'FR', 'DE', 
+        'GI', 'GR', 'HU', 'IS', 'IE', 'IT', 'LV', 'LI', 'LT', 'LU', 'MT', 'MC', 
+        'NL', 'NO', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'CH', 'GB'
+    );
+    
+    /**
      * IBAN regexes by country code
      *
      * @var array
      */
-    protected static $ibanRegex = array(
-        'AD' => '/^AD[0-9]{2}[0-9]{8}[A-Z0-9]{12}$/',
-        'AT' => '/^AT[0-9]{2}[0-9]{5}[0-9]{11}$/',
-        'BA' => '/^BA[0-9]{2}[0-9]{6}[0-9]{10}$/',
-        'BE' => '/^BE[0-9]{2}[0-9]{3}[0-9]{9}$/',
-        'BG' => '/^BG[0-9]{2}[A-Z]{4}[0-9]{4}[0-9]{2}[A-Z0-9]{8}$/',
-        'CH' => '/^CH[0-9]{2}[0-9]{5}[A-Z0-9]{12}$/',
-        'CS' => '/^CS[0-9]{2}[0-9]{3}[0-9]{15}$/',
-        'CY' => '/^CY[0-9]{2}[0-9]{8}[A-Z0-9]{16}$/',
-        'CZ' => '/^CZ[0-9]{2}[0-9]{4}[0-9]{16}$/',
-        'DE' => '/^DE[0-9]{2}[0-9]{8}[0-9]{10}$/',
-        'DK' => '/^DK[0-9]{2}[0-9]{4}[0-9]{10}$/',
-        'EE' => '/^EE[0-9]{2}[0-9]{4}[0-9]{12}$/',
-        'ES' => '/^ES[0-9]{2}[0-9]{8}[0-9]{12}$/',
-        'FR' => '/^FR[0-9]{2}[0-9]{10}[A-Z0-9]{13}$/',
-        'FI' => '/^FI[0-9]{2}[0-9]{6}[0-9]{8}$/',
-        'GB' => '/^GB[0-9]{2}[A-Z]{4}[0-9]{14}$/',
-        'GI' => '/^GI[0-9]{2}[A-Z]{4}[A-Z0-9]{15}$/',
-        'GR' => '/^GR[0-9]{2}[0-9]{7}[A-Z0-9]{16}$/',
-        'HR' => '/^HR[0-9]{2}[0-9]{7}[0-9]{10}$/',
-        'HU' => '/^HU[0-9]{2}[0-9]{7}[0-9]{1}[0-9]{15}[0-9]{1}$/',
-        'IE' => '/^IE[0-9]{2}[A-Z0-9]{4}[0-9]{6}[0-9]{8}$/',
-        'IS' => '/^IS[0-9]{2}[0-9]{4}[0-9]{18}$/',
-        'IT' => '/^IT[0-9]{2}[A-Z]{1}[0-9]{10}[A-Z0-9]{12}$/',
-        'LI' => '/^LI[0-9]{2}[0-9]{5}[A-Z0-9]{12}$/',
-        'LU' => '/^LU[0-9]{2}[0-9]{3}[A-Z0-9]{13}$/',
-        'LT' => '/^LT[0-9]{2}[0-9]{5}[0-9]{11}$/',
-        'LV' => '/^LV[0-9]{2}[A-Z]{4}[A-Z0-9]{13}$/',
-        'MK' => '/^MK[0-9]{2}[A-Z]{3}[A-Z0-9]{10}[0-9]{2}$/',
-        'MT' => '/^MT[0-9]{2}[A-Z]{4}[0-9]{5}[A-Z0-9]{18}$/',
-        'NL' => '/^NL[0-9]{2}[A-Z]{4}[0-9]{10}$/',
-        'NO' => '/^NO[0-9]{2}[0-9]{4}[0-9]{7}$/',
-        'PL' => '/^PL[0-9]{2}[0-9]{8}[0-9]{16}$/',
-        'PT' => '/^PT[0-9]{2}[0-9]{8}[0-9]{13}$/',
-        'RO' => '/^RO[0-9]{2}[A-Z]{4}[A-Z0-9]{16}$/',
-        'SE' => '/^SE[0-9]{2}[0-9]{3}[0-9]{17}$/',
-        'SI' => '/^SI[0-9]{2}[0-9]{5}[0-9]{8}[0-9]{2}$/',
-        'SK' => '/^SK[0-9]{2}[0-9]{4}[0-9]{16}$/',
-        'TN' => '/^TN[0-9]{2}[0-9]{5}[0-9]{15}$/',
-        'TR' => '/^TR[0-9]{2}[0-9]{5}[A-Z0-9]{17}$/'
+    protected static $ibanRegex = array(    
+        'AD' => 'AD[0-9]{2}[0-9]{4}[0-9]{4}[A-Z0-9]{12}',
+        'AE' => 'AE[0-9]{2}[0-9]{3}[0-9]{16}',
+        'AL' => 'AL[0-9]{2}[0-9]{8}[A-Z0-9]{16}',
+        'AT' => 'AT[0-9]{2}[0-9]{5}[0-9]{11}',
+        'AZ' => 'AZ[0-9]{2}[A-Z]{4}[A-Z0-9]{20}',
+        'BA' => 'BA[0-9]{2}[0-9]{3}[0-9]{3}[0-9]{8}[0-9]{2}',
+        'BE' => 'BE[0-9]{2}[0-9]{3}[0-9]{7}[0-9]{2}',
+        'BG' => 'BG[0-9]{2}[A-Z]{4}[0-9]{4}[0-9]{2}[A-Z0-9]{8}',
+        'BH' => 'BH[0-9]{2}[A-Z]{4}[A-Z0-9]{14}',
+        'CH' => 'CH[0-9]{2}[0-9]{5}[A-Z0-9]{12}',
+        'CR' => 'CR[0-9]{2}[0-9]{3}[0-9]{14}',
+        'CY' => 'CY[0-9]{2}[0-9]{3}[0-9]{5}[A-Z0-9]{16}',
+        'CZ' => 'CZ[0-9]{2}[0-9]{20}',
+        'DE' => 'DE[0-9]{2}[0-9]{8}[0-9]{10}',
+        'DO' => 'DO[0-9]{2}[A-Z0-9]{4}[0-9]{20}',
+        'DK' => 'DK[0-9]{2}[0-9]{14}',
+        'EE' => 'EE[0-9]{2}[0-9]{2}[0-9]{2}[0-9]{11}[0-9]{1}',
+        'ES' => 'ES[0-9]{2}[0-9]{4}[0-9]{4}[0-9]{1}[0-9]{1}[0-9]{10}',
+        'FI' => 'FI[0-9]{2}[0-9]{6}[0-9]{7}[0-9]{1}',
+        'FO' => 'FO[0-9]{2}[0-9]{4}[0-9]{9}[0-9]{1}',
+        'FR' => 'FR[0-9]{2}[0-9]{5}[0-9]{5}[A-Z0-9]{11}[0-9]{2}',
+        'GB' => 'GB[0-9]{2}[A-Z]{4}[0-9]{6}[0-9]{8}',
+        'GE' => 'GE[0-9]{2}[A-Z]{2}[0-9]{16}',
+        'GI' => 'GI[0-9]{2}[A-Z]{4}[A-Z0-9]{15}',
+        'GL' => 'GL[0-9]{2}[0-9]{4}[0-9]{9}[0-9]{1}',
+        'GR' => 'GR[0-9]{2}[0-9]{3}[0-9]{4}[A-Z0-9]{16}',
+        'GT' => 'GT[0-9]{2}[A-Z0-9]{4}[A-Z0-9]{20}',
+        'HR' => 'HR[0-9]{2}[0-9]{7}[0-9]{10}',
+        'HU' => 'HU[0-9]{2}[0-9]{3}[0-9]{4}[0-9]{1}[0-9]{15}[0-9]{1}',
+        'IE' => 'IE[0-9]{2}[A-Z]{4}[0-9]{6}[0-9]{8}',
+        'IL' => 'IL[0-9]{2}[0-9]{3}[0-9]{3}[0-9]{13}',
+        'IS' => 'IS[0-9]{2}[0-9]{4}[0-9]{2}[0-9]{6}[0-9]{10}',
+        'IT' => 'IT[0-9]{2}[A-Z]{1}[0-9]{5}[0-9]{5}[A-Z0-9]{12}',
+        'KW' => 'KW[0-9]{2}[A-Z]{4}[0-9]{22}',
+        'KZ' => 'KZ[0-9]{2}[0-9]{3}[A-Z0-9]{13}',
+        'LB' => 'LB[0-9]{2}[0-9]{4}[A-Z0-9]{20}',
+        'LI' => 'LI[0-9]{2}[0-9]{5}[A-Z0-9]{12}',
+        'LT' => 'LT[0-9]{2}[0-9]{5}[0-9]{11}',
+        'LU' => 'LU[0-9]{2}[0-9]{3}[A-Z0-9]{13}',
+        'LV' => 'LV[0-9]{2}[A-Z]{4}[A-Z0-9]{13}',
+        'MC' => 'MC[0-9]{2}[0-9]{5}[0-9]{5}[A-Z0-9]{11}[0-9]{2}',
+        'MD' => 'MD[0-9]{2}[A-Z0-9]{20}',
+        'ME' => 'ME[0-9]{2}[0-9]{3}[0-9]{13}[0-9]{2}',
+        'MK' => 'MK[0-9]{2}[0-9]{3}[A-Z0-9]{10}[0-9]{2}',
+        'MR' => 'MR13[0-9]{5}[0-9]{5}[0-9]{11}[0-9]{2}',
+        'MT' => 'MT[0-9]{2}[A-Z]{4}[0-9]{5}[A-Z0-9]{18}',
+        'MU' => 'MU[0-9]{2}[A-Z]{4}[0-9]{2}[0-9]{2}[0-9]{12}[0-9]{3}[A-Z]{3}',
+        'NL' => 'NL[0-9]{2}[A-Z]{4}[0-9]{10}',
+        'NO' => 'NO[0-9]{2}[0-9]{4}[0-9]{6}[0-9]{1}',
+        'PK' => 'PK[0-9]{2}[A-Z]{4}[A-Z0-9]{16}',
+        'PL' => 'PL[0-9]{2}[0-9]{8}[0-9]{16}',
+        'PS' => 'PS[0-9]{2}[A-Z]{4}[A-Z0-9]{21}',
+        'PT' => 'PT[0-9]{2}[0-9]{4}[0-9]{4}[0-9]{11}[0-9]{2}',
+        'RO' => 'RO[0-9]{2}[A-Z]{4}[A-Z0-9]{16}',
+        'RS' => 'RS[0-9]{2}[0-9]{3}[0-9]{13}[0-9]{2}',
+        'SA' => 'SA[0-9]{2}[0-9]{2}[A-Z0-9]{18}',
+        'SE' => 'SE[0-9]{2}[0-9]{3}[0-9]{16}[0-9]{1}',
+        'SI' => 'SI[0-9]{2}[0-9]{5}[0-9]{8}[0-9]{2}',
+        'SK' => 'SK[0-9]{2}[0-9]{4}[0-9]{6}[0-9]{10}',
+        'SM' => 'SM[0-9]{2}[A-Z]{1}[0-9]{5}[0-9]{5}[A-Z0-9]{12}',
+        'TN' => 'TN59[0-9]{2}[0-9]{3}[0-9]{13}[0-9]{2}',
+        'TR' => 'TR[0-9]{2}[0-9]{5}[A-Z0-9]{1}[A-Z0-9]{16}',
+        'VG' => 'VG[0-9]{2}[A-Z]{4}[0-9]{16}',
     );
 
     /**
@@ -107,6 +151,10 @@ class Iban extends AbstractValidator
             $this->setCountryCode($options['country_code']);
         }
 
+        if (array_key_exists('allow_non_sepa', $options)) {
+            $this->setAllowNonSepa($options['allow_non_sepa']);
+        }
+        
         parent::__construct($options);
     }
 
@@ -119,7 +167,7 @@ class Iban extends AbstractValidator
     {
         return $this->countryCode;
     }
-
+    
     /**
      * Sets an optional country code by ISO 3166-1
      *
@@ -140,6 +188,32 @@ class Iban extends AbstractValidator
         }
 
         $this->countryCode = $countryCode;
+        return $this;
+    }
+
+    /**
+     * Returns the optional allow non-sepa countries setting
+     * 
+     * @return bool
+     */
+    public function getAllowNonSepa()
+    {
+        return $this->allowNonSepa;
+    }
+    
+    /**
+     * Sets the optional allow non-sepa countries setting
+     * 
+     * @param type $allowNonSepa 
+     * @return Iban provides a fluent interface
+     */
+    public function setAllowNonSepa($allowNonSepa = null)
+    {
+        if ($allowNonSepa !== null) {
+            $allowNonSepa = (bool) $allowNonSepa;
+        }
+        
+        $this->allowNonSepa = $allowNonSepa;
         return $this;
     }
 
@@ -170,8 +244,14 @@ class Iban extends AbstractValidator
             return false;
         }
 
-        if (!preg_match(self::$ibanRegex[$countryCode], $value)) {
-            $this->error(self::FALSEFORMAT);
+        if (!$this->allowNonSepa && !in_array($countryCode, self::$sepaCountries)) {
+            $this->setValue($countryCode);
+            $this->error(self::SEPANOTSUPPORTED);
+            return false;
+        }
+        
+        if (!preg_match('/^' . self::$ibanRegex[$countryCode] . '$/', $value)) {
+            $this->error(self::FALSEFORMAT . $value . '<>' .  self::$ibanRegex[$countryCode]);
             return false;
         }
 

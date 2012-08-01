77dbece4479ad28aae0501b3950469f86e9df362
Walter Tamboer <walter.tamboer@live.com>
2012-08-01 10:56:47 +0200

    Netbeans' code formatting sucks

--- library/Zend/Stdlib/Hydrator/Strategy/DefaultStrategy.php
+++ library/Zend/Stdlib/Hydrator/Strategy/DefaultStrategy.php
@@ -23,18 +23,18 @@ class DefaultStrategy implements StrategyInterface
      * @param mixed $value The original value.
      * @return mixed Returns the value that should be extracted.
      */
-	public function extract($value)
+    public function extract($value)
     {
         return $value;
     }
-	
+
     /**
      * Converts the given value so that it can be hydrated by the hydrator.
      * 
      * @param mixed $value The original value.
      * @return mixed Returns the value that should be hydrated.
      */
-	public function hydrate($value)
+    public function hydrate($value)
     {
         return $value;
     }

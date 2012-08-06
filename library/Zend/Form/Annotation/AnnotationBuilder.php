14f0efe5005529044451cedcf1f5c7bf9d7797c7
Matthew Weier O'Phinney <matthew@zend.com>
2012-08-06 13:18:13 -0500

    [#2079] CS cleanup
    - ran php-cs-fixer --level=psr2

--- library/Zend/Form/Annotation/AnnotationBuilder.php
+++ library/Zend/Form/Annotation/AnnotationBuilder.php
@@ -341,8 +341,8 @@ class AnnotationBuilder implements EventManagerAwareInterface
                 // add required attribute to form element
                 if ($inputSpec['required']){
                     if (!array_key_exists('attributes', $elementSpec['spec'])){
-	 	                $elementSpec['spec']['attributes'] = array();
-		            }
+                         $elementSpec['spec']['attributes'] = array();
+                    }
                     $elementSpec['spec']['attributes']['required'] = 'required';
                 }
             }

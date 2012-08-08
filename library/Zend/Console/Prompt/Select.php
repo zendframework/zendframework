9d64a16829a5b392bce444d6d449341f221db74d
Artur Bodera <abodera@gmail.com>
2012-08-08 17:36:13 +0200

    Cleanup of Console\Prompt\Select, allow Prompt\Char without text.
    - an extraneous newline was inserted by Select superclass Char, when there was no text.
    - a non-configurable prompt text has been removed from Select.
    - with $echo === true, Select will now display the value of option picked.

--- library/Zend/Console/Prompt/Select.php
+++ library/Zend/Console/Prompt/Select.php
@@ -77,28 +77,43 @@ class Select extends Char
      */
     public function show()
     {
-        /**
-         * Show prompt text and available options
-         */
+        // Show prompt text and available options
         $console = $this->getConsole();
         $console->writeLine($this->promptText);
         foreach ($this->options as $k => $v) {
-            $console->writeLine('  '.$k.') '.$v);
+            $console->writeLine('  ' . $k . ') ' . $v);
         }
 
-        /**
-         * Ask for selection
-         */
-        $mask = implode("",array_keys($this->options));
+        //  Prepare mask
+        $mask = implode("", array_keys($this->options));
         if ($this->allowEmpty) {
             $mask .= "\r\n";
         }
-        $this->setAllowedChars($mask);
-        $oldPrompt = $this->promptText;
-        $this->promptText = 'Pick one option: ';
-        $response = parent::show();
-        $this->promptText = $oldPrompt;
 
+        // Prepare other params for parent class
+        $this->setAllowedChars($mask);
+        $oldPrompt        = $this->promptText;
+        $oldEcho          = $this->echo;
+        $this->echo       = false;
+        $this->promptText = null;
+
+        // Retrieve a single character
+        $response = parent::show();
+
+        // Restore old params
+        $this->promptText = $oldPrompt;
+        $this->echo       = $oldEcho;
+
+        // Display selected option if echo is enabled
+        if ($this->echo) {
+            if (isset($this->options[$response])) {
+                $console->writeLine($this->options[$response]);
+            } else {
+                $console->writeLine();
+            }
+        }
+
+        $this->lastResponse = $response;
         return $response;
     }
 

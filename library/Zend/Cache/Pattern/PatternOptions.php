bcecc5865b249eeed63b379b5162f686168b31d0
Wesley Overdijk <r.w.overdijk@gmail.com>
2012-08-09 21:58:47 +0300

    Update library/Zend/Cache/Pattern/PatternOptions.php
    Removed strict check to just make it PSR-2

--- library/Zend/Cache/Pattern/PatternOptions.php
+++ library/Zend/Cache/Pattern/PatternOptions.php
@@ -750,7 +750,7 @@ class PatternOptions extends AbstractOptions
             $storage = StorageFactory::factory($storage);
         } elseif (is_string($storage)) {
             $storage = StorageFactory::adapterFactory($storage);
-        } elseif (($storage instanceof Storage) !== true) {
+        } elseif (!($storage instanceof Storage)) {
             throw new Exception\InvalidArgumentException(
                 'The storage must be an instanceof Zend\Cache\Storage\StorageInterface '
                 . 'or an array passed to Zend\Cache\Storage::factory '

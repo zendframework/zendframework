f22e636eac67b1bcd05a5879201edfc20a0851d8
Matthew Weier O'Phinney <matthew@zend.com>
2012-08-23 14:40:52 -0500

    Bumped version to 2.0.0rc5

--- library/Zend/Version/Version.php
+++ library/Zend/Version/Version.php
@@ -23,7 +23,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '2.0.0rc4';
+    const VERSION = '2.0.0rc5';
 
     /**
      * The latest stable version Zend Framework available

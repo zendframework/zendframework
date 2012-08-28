de807ffc3dd1a5ca0495e300724ace1e6d15ffae
Ralph Schindler <ralph.schindler@zend.com>
2012-08-28 18:01:25 -0500

    Zend\Db\RowGateway: * Added tests for handling multi-key primary key updates within save();

--- library/Zend/Db/RowGateway/AbstractRowGateway.php
+++ library/Zend/Db/RowGateway/AbstractRowGateway.php
@@ -165,6 +165,7 @@ abstract class AbstractRowGateway implements ArrayAccess, Countable, RowGatewayI
             if (($primaryKeyValue = $result->getGeneratedValue()) && count($this->primaryKeyColumn) == 1) {
                 $this->primaryKeyData = array($this->primaryKeyColumn[0] => $primaryKeyValue);
             } else {
+                // make primary key data available so that $where can be complete
                 $this->processPrimaryKeyData();
             }
             $rowsAffected = $result->getAffectedRows();

21d7de2f5004edc462e3680dd567286418558e6d
Matthew Weier O'Phinney <matthew@zend.com>
2012-09-04 17:31:10 -0500

    [2.0.0] Updated release version

--- library/Zend/Version/Version.php
+++ library/Zend/Version/Version.php
@@ -23,7 +23,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '2.0.0rc7';
+    const VERSION = '2.0.0';
 
     /**
      * The latest stable version Zend Framework available

73b01c93be4dd0e35f3e0e5c530ab2aef6ad1640
Matthew Weier O'Phinney <matthew@zend.com>
2012-09-04 10:33:29 -0500

    [#2293] Reverted files to release branch
    - README.md, ParameterReflection, and Version needed to be reverted to release
      branch in order to merge cleanly.

--- library/Zend/Version/Version.php
+++ library/Zend/Version/Version.php
@@ -23,7 +23,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '2.1.0dev';
+    const VERSION = '2.0.0rc7';
 
     /**
      * The latest stable version Zend Framework available

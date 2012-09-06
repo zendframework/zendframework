50725f36b6a6ddd9aac247f34e3fe370c17247e4
Chris Martin <cgmartin@gmail.com>
2012-09-06 12:04:13 -0400

    Moved the translation code around in FormRow for non-empty labels

--- library/Zend/Form/View/Helper/FormRow.php
+++ library/Zend/Form/View/Helper/FormRow.php
@@ -78,13 +78,6 @@ class FormRow extends AbstractHelper
         $inputErrorClass = $this->getInputErrorClass();
         $elementErrors   = $elementErrorsHelper->render($element);
 
-        // Translate the label
-        if (null !== ($translator = $this->getTranslator())) {
-            $label = $translator->translate(
-                $label, $this->getTranslatorTextDomain()
-            );
-        }
-
         // Does this element have errors ?
         if (!empty($elementErrors) && !empty($inputErrorClass)) {
             $classAttributes = ($element->hasAttribute('class') ? $element->getAttribute('class') . ' ' : '');
@@ -95,7 +88,14 @@ class FormRow extends AbstractHelper
 
         $elementString = $elementHelper->render($element);
 
-        if (!empty($label)) {
+        if (isset($label) && '' !== $label) {
+            // Translate the label
+            if (null !== ($translator = $this->getTranslator())) {
+                $label = $translator->translate(
+                        $label, $this->getTranslatorTextDomain()
+                );
+            }
+
             $label = $escapeHtmlHelper($label);
             $labelAttributes = $element->getLabelAttributes();
 

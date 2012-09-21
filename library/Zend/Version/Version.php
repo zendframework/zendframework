e34b567505ffd90df528d04100d451daf40d667f
Matthew Weier O'Phinney <matthew@zend.com>
2012-09-21 13:23:58 -0500

    Updated Zend\Version to 2.0.2

--- library/Zend/Version/Version.php
+++ library/Zend/Version/Version.php
@@ -23,7 +23,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '2.0.1';
+    const VERSION = '2.0.2';
 
     /**
      * Github Service Identifier for version information is retreived from

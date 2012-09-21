34e8ab8cabadc4ed3b13c8e312eb420ac926fe7a
Philipp Dobrigkeit <p.dobrigkeit@goalio.de>
2012-09-21 17:33:36 +0200

    Sadly I fell into the same trap with just testing one other value ;) Now I am testing for each level of the same precision

--- library/Zend/Validator/Step.php
+++ library/Zend/Validator/Step.php
@@ -127,7 +127,9 @@ class Step extends AbstractValidator
 
         $this->setValue($value);
 
-        if ($this->fmod($value - $this->baseValue, $this->step) !== 0.0) {
+        $fmod = $this->fmod($value - $this->baseValue, $this->step);
+
+        if ($fmod !== 0.0 && $fmod !== $this->step) {
             $this->error(self::NOT_STEP);
             return false;
         }
@@ -147,6 +149,10 @@ class Step extends AbstractValidator
         if ($y == 0.0) {
             return 1.0;
         }
-        return $x - $y * floor($x / $y);
+
+        //find the maximum precision from both input params to give accurate results
+        $precision = strlen(substr($x, strpos($x, '.')+1)) + strlen(substr($y, strpos($y, '.')+1));
+
+        return round($x - $y * floor($x / $y), $precision);
     }
 }
--- tests/ZendTest/Validator/StepTest.php
+++ tests/ZendTest/Validator/StepTest.php
@@ -96,7 +96,15 @@ class StepTest extends \PHPUnit_Framework_TestCase
             array(0.1, false),
             array(2.1, true),
             array(3.1, false),
+            array(4.2, true),
+            array(6.3, true),
+            array(8.4, true),
             array(10.5, true),
+            array(12.6, true),
+            array(14.7, true),
+            array(16.8, true),
+            array(18.9, true),
+            array(21.0, true),
             array('2.1', true),
             array('1.1', false),
             array(1.11, false),
@@ -106,6 +114,40 @@ class StepTest extends \PHPUnit_Framework_TestCase
         $validator = new Validator\Step(array(
             'baseValue' => 0,
             'step'      => 2.1
+        ));
+
+        foreach ($valuesExpected as $element) {
+            $this->assertEquals($element[1], $validator->isValid($element[0]),
+                'Test failed with ' . var_export($element, 1));
+        }
+    }
+
+    public function testDecimalStep2()
+    {
+        $valuesExpected = array(
+            array(0.01, true),
+            array(0.02, true),
+            array(0.03, true),
+            array(0.04, true),
+            array(0.05, true),
+            array(0.06, true),
+            array(0.07, true),
+            array(0.08, true),
+            array(0.09, true),
+            array(0.001, false),
+            array(0.002, false),
+            array(0.003, false),
+            array(0.004, false),
+            array(0.005, false),
+            array(0.006, false),
+            array(0.007, false),
+            array(0.008, false),
+            array(0.009, false)
+        );
+
+        $validator = new Validator\Step(array(
+            'baseValue' => 0,
+            'step'      => 0.01
         ));
 
         foreach ($valuesExpected as $element) {

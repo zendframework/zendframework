5e3c4a86db47cf541dd02e099fe8fe05e2790270
Ralph Schindler <ralph@ralphschindler.com>
2012-09-25 13:14:07 -0500

    First commit of oracle specific Zend\Db driver

---
+++ library/Zend/Db/Adapter/Driver/Oci8/Oci8.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Db
+ */
+
+namespace Zend\Db\Adapter\Driver\Oci8;
+
+use Zend\Db\Adapter\Driver\DriverInterface;
+use Zend\Db\Adapter\Exception;
+
+/**
+ * @category   Zend
+ * @package    Zend_Db
+ * @subpackage Adapter
+ */
+class Oci8 implements DriverInterface
+{
+
+    /**
+     * @var Connection
+     */
+    protected $connection = null;
+
+    /**
+     * @var Statement
+     */
+    protected $statementPrototype = null;
+
+    /**
+     * @var Result
+     */
+    protected $resultPrototype = null;
+
+    /**
+     * @var array
+     */
+    protected $options = array(
+
+    );
+
+    /**
+     * @param array|Connection|\mysqli $connection
+     * @param null|Statement $statementPrototype
+     * @param null|Result $resultPrototype
+     * @param array $options
+     */
+    public function __construct($connection, Statement $statementPrototype = null, Result $resultPrototype = null, array $options = array())
+    {
+        if (!$connection instanceof Connection) {
+            $connection = new Connection($connection);
+        }
+
+        $options = array_intersect_key(array_merge($this->options, $options), $this->options);
+
+        $this->registerConnection($connection);
+        $this->registerStatementPrototype(($statementPrototype) ?: new Statement());
+        $this->registerResultPrototype(($resultPrototype) ?: new Result());
+    }
+
+    /**
+     * Register connection
+     *
+     * @param  Connection $connection
+     * @return Mysqli
+     */
+    public function registerConnection(Connection $connection)
+    {
+        $this->connection = $connection;
+        $this->connection->setDriver($this); // needs access to driver to createStatement()
+        return $this;
+    }
+
+    /**
+     * Register statement prototype
+     *
+     * @param Statement $statementPrototype
+     */
+    public function registerStatementPrototype(Statement $statementPrototype)
+    {
+        $this->statementPrototype = $statementPrototype;
+        $this->statementPrototype->setDriver($this); // needs access to driver to createResult()
+    }
+
+    /**
+     * @return null|Statement
+     */
+    public function getStatementPrototype()
+    {
+        return $this->statementPrototype;
+    }
+
+    /**
+     * Register result prototype
+     *
+     * @param Result $resultPrototype
+     */
+    public function registerResultPrototype(Result $resultPrototype)
+    {
+        $this->resultPrototype = $resultPrototype;
+    }
+
+    /**
+     * @return null|Result
+     */
+    public function getResultPrototype()
+    {
+        return $this->resultPrototype;
+    }
+
+    /**
+     * Get database platform name
+     *
+     * @param  string $nameFormat
+     * @return string
+     */
+    public function getDatabasePlatformName($nameFormat = self::NAME_FORMAT_CAMELCASE)
+    {
+        if ($nameFormat == self::NAME_FORMAT_CAMELCASE) {
+            return 'Oracle';
+        } else {
+            return 'Oracle';
+        }
+    }
+
+    /**
+     * Check environment
+     */
+    public function checkEnvironment()
+    {
+        if (!extension_loaded('oci8')) {
+            throw new Exception\RuntimeException('The Oci8 extension is required for this adapter but the extension is not loaded');
+        }
+    }
+
+    /**
+     * @return Connection
+     */
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+
+    /**
+     * @param string $sqlOrResource
+     * @return Statement
+     */
+    public function createStatement($sqlOrResource = null)
+    {
+        $statement = clone $this->statementPrototype;
+        if (is_resource($sqlOrResource) && get_resource_type($sqlOrResource) == 'oci8 statement') {
+            $statement->setResource($sqlOrResource);
+        } else {
+            if (is_string($sqlOrResource)) {
+                $statement->setSql($sqlOrResource);
+            }
+            if (!$this->connection->isConnected()) {
+                $this->connection->connect();
+            }
+            $statement->initialize($this->connection->getResource());
+        }
+        return $statement;
+    }
+
+    /**
+     * @param resource $resource
+     * @return Result
+     */
+    public function createResult($resource, $isBuffered = null)
+    {
+        $result = clone $this->resultPrototype;
+        $result->initialize($resource, $this->connection->getLastGeneratedValue(), $isBuffered);
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPrepareType()
+    {
+        return self::PARAMETERIZATION_NAMED;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $type
+     * @return string
+     */
+    public function formatParameterName($name, $type = null)
+    {
+        return ':' . $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastGeneratedValue()
+    {
+        return $this->getConnection()->getLastGeneratedValue();
+    }
+
+}

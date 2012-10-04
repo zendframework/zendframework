7aaee694242e9499b9d1832ae98b9802631a4169
Christopher Martin <cgmartin@gmail.com>
2012-10-04 01:13:33 -0400

    New Form Elements for File Upload Progress tracking

---
+++ library/Zend/Form/Element/Upload/ApcProgress.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage Element
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+namespace Zend\Form\Element\Upload;
+
+/**
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage Element
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+class ApcProgress extends UploadProgress
+{
+    /**
+     * Get value for name
+     *
+     * @return string|int
+     */
+    public function getName()
+    {
+        return ini_get('apc.rfc1867_name');
+    }
+}

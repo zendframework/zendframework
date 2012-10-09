06ed0838619d67a3b375d9f128db9f8bbd2c0ab4
Michaël Gallego <mic.gallego@gmail.com>
2012-10-09 19:05:42 +0200

    Correctly hydrate when using base fieldset

--- library/Zend/Form/Form.php
+++ library/Zend/Form/Form.php
@@ -287,7 +287,14 @@ class Form extends Fieldset implements FormInterface
         }
 
         $data = $this->prepareBindData($data, $this->data);
-        $this->object = parent::bindValues($data);
+
+        // If there is a base fieldset, only hydrate beginning from the base fieldset
+        if ($this->baseFieldset !== null) {
+            $data = $data[$this->baseFieldset->getName()];
+            $this->object = $this->baseFieldset->bindValues($data);
+        } else {
+            $this->object = parent::bindValues($data);
+        }
     }
 
     /**

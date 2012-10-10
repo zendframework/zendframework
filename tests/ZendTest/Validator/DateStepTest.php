d63d80a5e28956038aad6b1cccf4f6d136c896e8
Matthew Weier O'Phinney <matthew@zend.com>
2012-10-10 10:35:49 -0500

    [#2717] CS fixes
    - Trailing whitespace

--- tests/ZendTest/Validator/DateStepTest.php
+++ tests/ZendTest/Validator/DateStepTest.php
@@ -110,7 +110,7 @@ class DateStepTest extends \PHPUnit_Framework_TestCase
         $this->assertObjectHasAttribute('messageTemplates', $validator);
         $this->assertAttributeEquals($validator->getOption('messageTemplates'), 'messageTemplates', $validator);
     }
-    
+
     public function testStepError()
     {
         $validator = new Validator\DateStep(array(
@@ -118,7 +118,7 @@ class DateStepTest extends \PHPUnit_Framework_TestCase
             'baseValue'    => '2012-01-23',
             'step' => new DateInterval("P10D"),
         ));
-        
+
         $this->assertFalse($validator->isValid('2012-13-13'));
     }
 }

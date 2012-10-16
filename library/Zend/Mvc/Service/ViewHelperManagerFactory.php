a4cb96439ada36544cecd1fc17c00a5648b60d39
Eric Boh <cossish@gmail.com>
2012-10-16 20:06:09 +0200

    Added isset check for the 'view_manager' key to prevent undefined index notice.

--- library/Zend/Mvc/Service/ViewHelperManagerFactory.php
+++ library/Zend/Mvc/Service/ViewHelperManagerFactory.php
@@ -84,9 +84,8 @@ class ViewHelperManagerFactory extends AbstractPluginManagerFactory
 
         $plugins->setFactory('basepath', function($sm) use($serviceLocator) {
             $config = $serviceLocator->get('Config');
-            $config = $config['view_manager'];
             $basePathHelper = new ViewHelper\BasePath;
-            if (isset($config['base_path'])) {
+            if (isset($config['view_manager']) && isset($config['view_manager']['base_path'])) {
                 $basePath = $config['base_path'];
             } else {
                 $basePath = $serviceLocator->get('Request')->getBasePath();

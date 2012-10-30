bb43e0e31589e29bc49c3074dbcc05e4ffc05235
blanchonvincent <blanchon.vincent@gmail.com>
2012-10-30 21:21:02 +0100

    Improve the update of ServiceListener of the tests lib

--- library/Zend/Test/PHPUnit/Mvc/Service/ServiceListenerFactory.php
+++ library/Zend/Test/PHPUnit/Mvc/Service/ServiceListenerFactory.php
@@ -6,45 +6,27 @@ use Zend\Mvc\Service\ServiceListenerFactory as BaseServiceListenerFactory;
 
 class ServiceListenerFactory extends BaseServiceListenerFactory
 {
-    protected $defaultServiceConfig = array(
+    protected $testServiceConfig = array(
         'invokables' => array(
-            'DispatchListener' => 'Zend\Mvc\DispatchListener',
-            'RouteListener'    => 'Zend\Mvc\RouteListener',
-            'Request'          => 'Zend\Http\PhpEnvironment\Request', // override
-            'Response'         => 'Zend\Http\PhpEnvironment\Response', // override
-            'ViewManager'      => 'Zend\Mvc\View\Http\ViewManager', // override
+            'Request'          => 'Zend\Http\PhpEnvironment\Request',
+            'Response'         => 'Zend\Http\PhpEnvironment\Response',
+            'ViewManager'      => 'Zend\Mvc\View\Http\ViewManager',
         ),
         'factories' => array(
-            'Application'             => 'Zend\Mvc\Service\ApplicationFactory',
-            'Config'                  => 'Zend\Mvc\Service\ConfigFactory',
-            'ControllerLoader'        => 'Zend\Mvc\Service\ControllerLoaderFactory',
-            'ControllerPluginManager' => 'Zend\Mvc\Service\ControllerPluginManagerFactory',
-            'ConsoleAdapter'          => 'Zend\Mvc\Service\ConsoleAdapterFactory',
-            'ConsoleRouter'           => 'Zend\Mvc\Service\RouterFactory',
-            'DependencyInjector'      => 'Zend\Mvc\Service\DiFactory',
-            'HttpRouter'              => 'Zend\Mvc\Service\RouterFactory',
-            'Router'                  => 'Zend\Test\PHPUnit\Mvc\Service\RouterFactory', // override
-            'ViewHelperManager'       => 'Zend\Mvc\Service\ViewHelperManagerFactory',
-            'ViewFeedRenderer'        => 'Zend\Mvc\Service\ViewFeedRendererFactory',
-            'ViewFeedStrategy'        => 'Zend\Mvc\Service\ViewFeedStrategyFactory',
-            'ViewJsonRenderer'        => 'Zend\Mvc\Service\ViewJsonRendererFactory',
-            'ViewJsonStrategy'        => 'Zend\Mvc\Service\ViewJsonStrategyFactory',
-            'ViewResolver'            => 'Zend\Mvc\Service\ViewResolverFactory',
-            'ViewTemplateMapResolver' => 'Zend\Mvc\Service\ViewTemplateMapResolverFactory',
-            'ViewTemplatePathStack'   => 'Zend\Mvc\Service\ViewTemplatePathStackFactory',
-        ),
-        'aliases' => array(
-            'Configuration'                          => 'Config',
-            'Console'                                => 'ConsoleAdapter',
-            'ControllerPluginBroker'                 => 'ControllerPluginManager',
-            'Di'                                     => 'DependencyInjector',
-            'Zend\Di\LocatorInterface'               => 'DependencyInjector',
-            'Zend\Mvc\Controller\PluginBroker'       => 'ControllerPluginBroker',
-            'Zend\Mvc\Controller\PluginManager'      => 'ControllerPluginManager',
-            'Zend\View\Resolver\TemplateMapResolver' => 'ViewTemplateMapResolver',
-            'Zend\View\Resolver\TemplatePathStack'   => 'ViewTemplatePathStack',
-            'Zend\View\Resolver\AggregateResolver'   => 'ViewResolver',
-            'Zend\View\Resolver\ResolverInterface'   => 'ViewResolver',
+            'Router'           => 'Zend\Test\PHPUnit\Mvc\Service\RouterFactory',
         ),
     );
+
+    public function __construct()
+    {
+        // merge basee config with specific tests config
+        $this->defaultServiceConfig = array_replace_recursive(
+            $this->defaultServiceConfig, $this->testServiceConfig
+        );
+
+        // delete the factory which has moved
+        unset($this->defaultServiceConfig['factories']['Request']);
+        unset($this->defaultServiceConfig['factories']['Response']);
+        unset($this->defaultServiceConfig['factories']['ViewManager']);
+    }
 }

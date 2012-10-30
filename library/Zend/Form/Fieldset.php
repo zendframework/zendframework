1dbac10c151e7074a2c7e8e96a825a79b664d978
Matthew Weier O'Phinney <matthew@zend.com>
2012-10-30 16:32:50 -0500

    [#2854] Remove interface implementation
    - FieldsetInterface was modified in an earlier PR to extend
      FormFactoryAwareInterface already.

--- library/Zend/Form/Fieldset.php
+++ library/Zend/Form/Fieldset.php
@@ -19,7 +19,7 @@ use Zend\Stdlib\PriorityQueue;
  * @category   Zend
  * @package    Zend_Form
  */
-class Fieldset extends Element implements FieldsetInterface, FormFactoryAwareInterface
+class Fieldset extends Element implements FieldsetInterface
 {
     /**
      * @var Factory

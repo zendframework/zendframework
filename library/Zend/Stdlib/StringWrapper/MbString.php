7ba3c5ffb4d6bd8e587abdfbe24266625356faf9
Marc Bennewitz <marc-bennewitz@arcor.de>
2012-11-29 21:09:12 +0100

    StringUtils: MbString wrapper use of 'mb_list_encodings'

--- library/Zend/Stdlib/StringWrapper/MbString.php
+++ library/Zend/Stdlib/StringWrapper/MbString.php
@@ -23,83 +23,7 @@ class MbString extends AbstractStringWrapper
      * @var string[]
      * @link http://php.net/manual/mbstring.supported-encodings.php
      */
-    protected $encodings = array(
-        'UCS-4',
-        'UCS-4BE',
-        'UCS-4LE',
-        'UCS-2',
-        'UCS-2BE',
-        'UCS-2LE',
-        'UTF-32',
-        'UTF-32BE',
-        'UTF-32LE',
-        'UTF-16',
-        'UTF-16BE',
-        'UTF-16LE',
-        'UTF-7',
-        'UTF7-IMAP',
-        'UTF-8',
-        'ASCII',
-        'EUC-JP',
-        'SJIS',
-        'EUCJP-WIN',
-        'SJIS-WIN',
-        'ISO-2022-JP',
-        'ISO-2022-JP-MS',
-        'CP932',
-        'CP51932',
-        'SJIS-MAC', 'MACJAPANESE',
-        'SJIS-Mobile#DOCOMO', 'SJIS-DOCOMO',
-        'SJIS-Mobile#KDDI', 'SJIS-KDDI',
-        'SJIS-Mobile#SOFTBANK', 'SJIS-SOFTBANK',
-        'UTF-8-Mobile#DOCOMO', 'UTF-8-DOCOMO',
-        'UTF-8-Mobile#KDDI-A',
-        'UTF-8-Mobile#KDDI-B', 'UTF-8-KDDI',
-        'UTF-8-Mobile#SOFTBANK', 'UTF-8-SOFTBANK',
-        'ISO-2022-JP-MOBILE#KDDI', 'ISO-2022-JP-KDDI',
-        'JIS',
-        'JIS-MS',
-        'CP50220',
-        'CP50220RAW',
-        'CP50221',
-        'CP50222',
-        'ISO-8859-1',
-        'ISO-8859-2',
-        'ISO-8859-3',
-        'ISO-8859-4',
-        'ISO-8859-5',
-        'ISO-8859-6',
-        'ISO-8859-7',
-        'ISO-8859-8',
-        'ISO-8859-9',
-        'ISO-8859-10',
-        'ISO-8859-13',
-        'ISO-8859-14',
-        'ISO-8859-15',
-        // 'ISO-8859-16',
-        'bYTE2BE',
-        'bYTE2LE',
-        'BYTE4BE',
-        'BYTE4LE',
-        'BASE64',
-        'HTML-ENTITIES',
-        '7BIT',
-        '8BIT',
-        'EUC-CN',
-        'CP936',
-        'GB18030',
-        'HZ',
-        'EUC-TW',
-        'CP950',
-        'BIG-5',
-        'EUC-KR',
-        'UHC', 'CP949',
-        'ISO-2022-KR',
-        'WINDOWS-1251', 'CP1251',
-        'WINDOWS-1252', 'CP1252',
-        'CP866', 'IBM866',
-        'KOI8-R',
-    );
+    protected $encodings = array();
 
     /**
      * Constructor
@@ -114,29 +38,7 @@ class MbString extends AbstractStringWrapper
             );
         }
 
-        // remove encodings not available before PHP-5.4
-        if (version_compare(PHP_VERSION, '5.4', '<')) {
-            unset(
-                $this->encodings['SJIS-MAC'],
-                $this->encodings['MACJAPANESE'],
-                $this->encodings['SJIS-Mobile#DOCOMO'],
-                $this->encodings['SJIS-DOCOMO'],
-                $this->encodings['SJIS-Mobile#KDDI'],
-                $this->encodings['SJIS-KDDI'],
-                $this->encodings['SJIS-Mobile#SOFTBANK'],
-                $this->encodings['SJIS-SOFTBANK'],
-                $this->encodings['UTF-8-Mobile#DOCOMO'],
-                $this->encodings['UTF-8-DOCOMO'],
-                $this->encodings['UTF-8-Mobile#KDDI-A'],
-                $this->encodings['UTF-8-Mobile#KDDI-B'],
-                $this->encodings['UTF-8-KDDI'],
-                $this->encodings['UTF-8-Mobile#SOFTBANK'],
-                $this->encodings['UTF-8-SOFTBANK'],
-                $this->encodings['ISO-2022-JP-MOBILE#KDDI'],
-                $this->encodings['ISO-2022-JP-KDDI'],
-                $this->encodings['GB18030']
-            );
-        }
+        $this->encodings = array_map('strtoupper', mb_list_encodings());
     }
 
     /**

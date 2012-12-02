c57ab8827675b9c2658bd820f6c625ffe906f113
Andreas Möller <a@softe.is>
2012-12-02 13:51:50 +0100

    Obey PSR-2

--- tests/ZendTest/EventManager/EventManagerTest.php
+++ tests/ZendTest/EventManager/EventManagerTest.php
@@ -277,10 +277,18 @@ class EventManagerTest extends \PHPUnit_Framework_TestCase
     public function testCanDetachListenerAggregates()
     {
         // setup some other event listeners, to ensure appropriate items are detached
-        $listenerFooBar1 = $this->events->attach('foo.bar', function() { return true; });
-        $listenerFooBar2 = $this->events->attach('foo.bar', function() { return true; });
-        $listenerFooBaz1 = $this->events->attach('foo.baz', function() { return true; });
-        $listenerOther   = $this->events->attach('other', function() { return true; });
+        $listenerFooBar1 = $this->events->attach('foo.bar', function() {
+            return true;
+        });
+        $listenerFooBar2 = $this->events->attach('foo.bar', function() {
+            return true;
+        });
+        $listenerFooBaz1 = $this->events->attach('foo.baz', function() {
+            return true;
+        });
+        $listenerOther   = $this->events->attach('other', function() {
+            return true;
+        });
 
         $aggregate = new TestAsset\MockAggregate();
         $this->events->attachAggregate($aggregate);
@@ -307,10 +315,18 @@ class EventManagerTest extends \PHPUnit_Framework_TestCase
     public function testCanDetachListenerAggregatesViaDetach()
     {
         // setup some other event listeners, to ensure appropriate items are detached
-        $listenerFooBar1 = $this->events->attach('foo.bar', function() { return true; });
-        $listenerFooBar2 = $this->events->attach('foo.bar', function() { return true; });
-        $listenerFooBaz1 = $this->events->attach('foo.baz', function() { return true; });
-        $listenerOther   = $this->events->attach('other', function() { return true; });
+        $listenerFooBar1 = $this->events->attach('foo.bar', function() {
+            return true;
+        });
+        $listenerFooBar2 = $this->events->attach('foo.bar', function() {
+            return true;
+        });
+        $listenerFooBaz1 = $this->events->attach('foo.baz', function() {
+            return true;
+        });
+        $listenerOther   = $this->events->attach('other', function() {
+            return true;
+        });
 
         $aggregate = new TestAsset\MockAggregate();
         $this->events->attach($aggregate);

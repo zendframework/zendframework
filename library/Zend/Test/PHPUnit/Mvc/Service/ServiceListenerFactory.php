694e396ced4f4a40c6db1bb2fd77fefe81905d6a
blanchonvincent <blanchon.vincent@gmail.com>
2012-12-05 18:43:43 +0100

    Improve ServiceListenerFactory for unit tests

--- library/Zend/Test/PHPUnit/Mvc/Service/ServiceListenerFactory.php
+++ library/Zend/Test/PHPUnit/Mvc/Service/ServiceListenerFactory.php
@@ -19,27 +19,25 @@ use Zend\Mvc\Service\ServiceListenerFactory as BaseServiceListenerFactory;
  */
 class ServiceListenerFactory extends BaseServiceListenerFactory
 {
-    protected $testServiceConfig = array(
-        'invokables' => array(
-            'Request'          => 'Zend\Http\PhpEnvironment\Request',
-            'Response'         => 'Zend\Http\PhpEnvironment\Response',
-            'ViewManager'      => 'Zend\Mvc\View\Http\ViewManager',
-        ),
-        'factories' => array(
-            'Router'           => 'Zend\Test\PHPUnit\Mvc\Service\RouterFactory',
-        ),
-    );
-
     public function __construct()
     {
         // merge basee config with specific tests config
         $this->defaultServiceConfig = array_replace_recursive(
-            $this->defaultServiceConfig, $this->testServiceConfig
+            $this->defaultServiceConfig,
+            array(
+                'factories' => array(
+                    'Request' => function($sm) {
+                        return new \Zend\Http\PhpEnvironment\Request();
+                    },
+                    'Response' => function($sm) {
+                        return new \Zend\Http\PhpEnvironment\Response();
+                    },
+                    'Router' => 'Zend\Test\PHPUnit\Mvc\Service\RouterFactory',
+                    'ViewManager' => function($sm) {
+                        return new \Zend\Mvc\View\Http\ViewManager();
+                    },
+                ),
+            )
         );
-
-        // delete the factory which has moved
-        unset($this->defaultServiceConfig['factories']['Request']);
-        unset($this->defaultServiceConfig['factories']['Response']);
-        unset($this->defaultServiceConfig['factories']['ViewManager']);
     }
 }

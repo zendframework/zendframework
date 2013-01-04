0f5df671b9ed8ecdbdbf7675483165839a88a12b
prolic <saschaprolic@googlemail.com>
2013-01-04 17:57:09 +0100

    added file and class headers

--- library/Zend/Mvc/ResponseSender/SimpleStreamResponseSender.php
+++ library/Zend/Mvc/ResponseSender/SimpleStreamResponseSender.php
@@ -1,10 +1,23 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Mvc
+ */
 
 namespace Zend\Mvc\ResponseSender;
 
 use Zend\Http\Header\MultipleHeaderInterface;
 use Zend\Http\Response\Stream;
 
+/**
+ * @category   Zend
+ * @package    Zend_Mvc
+ * @subpackage ResponseSender
+ */
 class SimpleStreamResponseSender implements ResponseSenderInterface
 {
 
@@ -60,8 +73,8 @@ class SimpleStreamResponseSender implements ResponseSenderInterface
     {
         $response = $event->getParam('response');
         if ($response instanceof Stream) {
-            $this->sendHeaders($event)
-                ->sendStream($event);
+            $this->sendHeaders($event);
+            $this->sendStream($event);
             $event->stopPropagation(true);
         }
         return $this;

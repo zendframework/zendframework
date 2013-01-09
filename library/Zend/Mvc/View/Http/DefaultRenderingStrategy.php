89425f4abdf5f04e46d3aaa37a5716ac6fa07f27
radnan <rafiadnan47@gmail.com>
2013-01-08 18:37:57 -0600

    render view one last time in case of render.error
    Try to render the view one last time in case a render.error is
    triggered. If the view continues to throw exceptions, bail out and
    just re-throw the exception.

--- library/Zend/Mvc/View/Http/DefaultRenderingStrategy.php
+++ library/Zend/Mvc/View/Http/DefaultRenderingStrategy.php
@@ -63,6 +63,7 @@ class DefaultRenderingStrategy implements ListenerAggregateInterface
     public function attach(EventManagerInterface $events)
     {
         $this->listeners[] = $events->attach(MvcEvent::EVENT_RENDER, array($this, 'render'), -10000);
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_RENDER_ERROR, array($this, 'render'), -10000);
     }
 
     /**
@@ -130,11 +131,16 @@ class DefaultRenderingStrategy implements ListenerAggregateInterface
         try {
             $view->render($viewModel);
         } catch(\Exception $ex) {
-            $application = $e->getApplication();
-            $events      = $application->getEventManager();
-            $e->setError(Application::ERROR_EXCEPTION)
-              ->setParam('exception', $ex);
-            $events->trigger(MvcEvent::EVENT_RENDER_ERROR, $e);
+            if ($e->getName() === MvcEvent::EVENT_RENDER_ERROR) {
+                throw $ex;
+            }
+            else {
+                $application = $e->getApplication();
+                $events      = $application->getEventManager();
+                $e->setError(Application::ERROR_EXCEPTION)
+                  ->setParam('exception', $ex);
+                $events->trigger(MvcEvent::EVENT_RENDER_ERROR, $e);
+            }
         }
 
         return $response;

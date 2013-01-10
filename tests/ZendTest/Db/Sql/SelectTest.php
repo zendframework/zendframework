f100d5b42816e2fd4b5f06cb23f43b3714bc3d93
sybeck2k <robertomigli@gmail.com>
2013-01-10 12:38:01 +0100

    Fixed typo

--- tests/ZendTest/Db/Sql/SelectTest.php
+++ tests/ZendTest/Db/Sql/SelectTest.php
@@ -769,7 +769,7 @@ class SelectTest extends \PHPUnit_Framework_TestCase
 
         // joins with a few keywords in the on clause
         $select28 = new Select;
-        $select28->from('foo')->join('zac', '(m = n AND c.x) BETWEEN x AND y.z OR (c.x < y.z AND c.z <= y.z AND c.x > y.z AND c.x >= y.z)');
+        $select28->from('foo')->join('zac', '(m = n AND c.x) BETWEEN x AND y.z OR (c.x < y.z AND c.x <= y.z AND c.x > y.z AND c.x >= y.z)');
         $sqlPrep28 = // same
         $sqlStr28 = 'SELECT "foo".*, "zac".* FROM "foo" INNER JOIN "zac" ON ("m" = "n" AND "c"."x") BETWEEN "x" AND "y"."z" OR ("c"."x" < "y"."z" AND "c"."x" <= "y"."z" AND "c"."x" > "y"."z" AND "c"."x" >= "y"."z")';
         $internalTests28 = array(

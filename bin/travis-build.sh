dfc2eb56c6086d053583b49ce8f1f612812e310f
Matthew Weier O'Phinney <matthew@zend.com>
2013-02-01 15:25:24 -0600

    Better travis build
    - Uses "$?" to determine success of a command; previous php-cs-fixer usage was
      not necessary, as travis terminal is color-enabled.
    - Echoes out the exit status for each command, to make it easier to determine
      which is responsible for a failed build.

--- bin/travis-build.sh
+++ bin/travis-build.sh
@@ -2,20 +2,30 @@
 cd "$(dirname $(dirname "$0"))"
 php ./tests/run-tests.php
 testresults=$?
-cslibrary=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 library); if [[ $cslibrary ]];then while read -r line;do echo -e "\e[00;31m$line\e[00m"; done <<< "$cslibrary"; false; fi;
-cstests=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 tests); if [[ $cstests ]]; then while read -r line; do echo -e "\e[00;31m$line\e[00m"; done <<< "$cstests"; false; fi;
-csbin=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 bin); if [[ $csbin ]]; then while read -r line; do echo -e "\e[00;31m$line\e[00m"; done <<< "$csbin"; false; fi;
+echo "Unit tests exited with $testresults"
 
-if [[ "$testresults" ]]; then
+php php-cs-fixer.phar fix -v --dry-run --level=psr2 library
+cslibrary=$?
+echo "Coding standards (library) exited with $cslibrary"
+
+php php-cs-fixer.phar fix -v --dry-run --level=psr2 tests
+cstests=$?
+echo "Coding standards (tests) exited with $cstests"
+
+php php-cs-fixer.phar fix -v --dry-run --level=psr2 bin
+csbin=$?
+echo "Coding standards (bin) exited with $csbin"
+
+if [[ $testresults ]]; then
     exit 1 ;
 fi
-if [[ "$cslibrary" ]]; then
+if [[ $cslibrary ]]; then
     exit 1 ;
 fi
-if [[ "$cstests" ]]; then
+if [[ $cstests ]]; then
     exit 1 ;
 fi
-if [[ "$csbin" ]]; then
+if [[ $csbin ]]; then
     exit 1 ;
 fi
 

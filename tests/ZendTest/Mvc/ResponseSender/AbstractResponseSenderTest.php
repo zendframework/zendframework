876f95da1d72c14c32c2ecef43d845eaf4be4ae6
Jamie <fantasticjamieburns@hotmail.com>
2013-02-04 06:10:04 +0000

    Status header is sent last
    Status header is sent last to prevent header() from overwriting the ZF
    status code when a Location header is used

--- tests/ZendTest/Mvc/ResponseSender/AbstractResponseSenderTest.php
+++ tests/ZendTest/Mvc/ResponseSender/AbstractResponseSenderTest.php
@@ -11,6 +11,7 @@
 namespace ZendTest\Mvc\ResponseSender;
 
 use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Http\Headers;
 use Zend\Http\Response;
 
 /**
@@ -52,5 +53,35 @@ class AbstractResponseSenderTest extends TestCase
 
         $responseSender->sendHeaders($mockSendResponseEvent);
         $this->assertEquals(array(), xdebug_get_headers());
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testSendHeadersSendsStatusLast()
+    {
+        if (!function_exists('xdebug_get_headers')) {
+            $this->markTestSkipped('Xdebug extension needed, skipped test');
+        }
+
+        $mockResponse = $this->getMock('Zend\Http\Response');
+        $mockResponse->expects($this->once())->method('getHeaders')->will($this->returnValue(Headers::fromString('Location: example.com')));
+        $mockResponse->expects($this->once())->method('renderStatusLine')->will($this->returnValue('X-Test: HTTP/1.1 202 Accepted'));
+
+        $mockSendResponseEvent = $this->getMock('Zend\Mvc\ResponseSender\SendResponseEvent', array('getResponse'));
+        $mockSendResponseEvent->expects($this->any())->method('getResponse')->will($this->returnValue($mockResponse));
+
+        $responseSender = $this->getMockForAbstractClass('Zend\Mvc\ResponseSender\AbstractResponseSender');
+        $responseSender->sendHeaders($mockSendResponseEvent);
+
+        $sentHeaders = xdebug_get_headers();
+
+        $this->assertCount(2, $sentHeaders);
+        $this->assertEquals('Location: example.com', $sentHeaders[0]);
+        $this->assertEquals(
+            'X-Test: HTTP/1.1 202 Accepted',
+            $sentHeaders[1],
+            'Status header is sent last to prevent header() from overwriting the ZF status code when a Location header is used'
+        );
     }
 }

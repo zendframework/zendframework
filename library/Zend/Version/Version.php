ce7c6907588c2ef50f87e390ed57a2d847298e2d
Matthew Weier O'Phinney <matthew@zend.com>
2013-02-06 15:25:59 -0600

    Bump version to 2.1.2dev

--- library/Zend/Version/Version.php
+++ library/Zend/Version/Version.php
@@ -19,7 +19,7 @@ final class Version
     /**
      * Zend Framework version identification - see compareVersion()
      */
-    const VERSION = '2.1.1';
+    const VERSION = '2.1.2dev';
 
     /**
      * Github Service Identifier for version information is retreived from

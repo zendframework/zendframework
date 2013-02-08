1624ade69fd4fdaeb36051a4dbab2c2eb130b05e
Ralph Schindler <ralph.schindler@zend.com>
2013-02-08 10:13:35 -0600

    Zend\Barcode - ensure plugin managers are not shared by default

--- library/Zend/Barcode/Barcode.php
+++ library/Zend/Barcode/Barcode.php
@@ -51,6 +51,7 @@ abstract class Barcode
     {
         if (!static::$objectPlugins instanceof ObjectPluginManager) {
             static::$objectPlugins = new ObjectPluginManager();
+            static::$objectPlugins->setShareByDefault(false);
         }
 
         return static::$objectPlugins;
@@ -65,6 +66,7 @@ abstract class Barcode
     {
         if (!static::$rendererPlugins instanceof RendererPluginManager) {
             static::$rendererPlugins = new RendererPluginManager();
+            static::$rendererPlugins->setShareByDefault(false);
         }
 
         return static::$rendererPlugins;

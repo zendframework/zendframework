6dc2cc53df577eae4890d54b4b7a68810a122345
Stewart Lord <stewey@ambitious.ca>
2013-02-12 01:02:17 -0800

    Cast a wider net for EOL translation. The to and subject headers were being independantly wrapped. Now we convert line-endings on $to, $subject, $body and $headers immediately before calling mail().

--- library/Zend/Mail/Transport/Sendmail.php
+++ library/Zend/Mail/Transport/Sendmail.php
@@ -127,6 +127,15 @@ class Sendmail implements TransportInterface
         $headers = $this->prepareHeaders($message);
         $params  = $this->prepareParameters($message);
 
+        // On *nix platforms, we need to replace \r\n with \n
+        // sendmail is not an SMTP server, it is a unix command - it expects LF
+        if (!$this->isWindowsOs()) {
+            $to      = str_replace("\r\n", "\n", $to);
+            $subject = str_replace("\r\n", "\n", $subject);
+            $body    = str_replace("\r\n", "\n", $body);
+            $headers = str_replace("\r\n", "\n", $headers);
+        }
+
         call_user_func($this->callable, $to, $subject, $body, $headers, $params);
     }
 
@@ -190,9 +199,8 @@ class Sendmail implements TransportInterface
     protected function prepareBody(Mail\Message $message)
     {
         if (!$this->isWindowsOs()) {
-            // On *nix platforms, we should replace \r\n with \n
-            // sendmail is not an SMTP server, it is a unix command - it expects LF
-            return str_replace("\r\n", "\n", $message->getBodyText());
+            // *nix platforms can simply return the body text
+            return $message->getBodyText();
         }
 
         // On windows, lines beginning with a full stop need to be fixed
@@ -218,10 +226,7 @@ class Sendmail implements TransportInterface
         $headers = clone $message->getHeaders();
         $headers->removeHeader('To');
         $headers->removeHeader('Subject');
-
-        // On *nix platforms, we should replace \r\n with \n
-        // sendmail is not an SMTP server, it is a unix command - it expects LF
-        return str_replace("\r\n", "\n", $headers->toString());
+        return $headers->toString();
     }
 
     /**

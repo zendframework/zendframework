a94f4973eea6f13fb14878ed426182006efff979
Matthew Weier O'Phinney <matthew@zend.com>
2013-03-05 15:37:14 -0600

    Fake scanners out
    - Use __halt_compiler(), and then define stub class, in order to trick
      classmap compilers into thinking a class exists.

--- library/Zend/Stdlib/ArrayObject.php
+++ library/Zend/Stdlib/ArrayObject.php
@@ -7,12 +7,14 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
+namespace Zend\Stdlib;
+
 /**
  * If the version is less than 5.3.4, we'll use Zend\Stdlib\ArrayObject\PhpLegacyCompatibility
  * which extends the native PHP ArrayObject implementation. For versions greater than or equal
  * to 5.3.4, we'll use Zend\Stdlib\ArrayObject\PhpReferenceCompatibility, which corrects
  * issues with how PHP handles references inside ArrayObject.
- * 
+ *
  * class_alias is a global construct, so we can alias either one to Zend\Stdlib\ArrayObject,
  * and from this point forward, that alias will be used.
  */
@@ -20,4 +22,10 @@ if (version_compare(PHP_VERSION, '5.3.4', 'lt')) {
     class_alias('Zend\Stdlib\ArrayObject\PhpLegacyCompatibility', 'Zend\Stdlib\ArrayObject');
 } else {
     class_alias('Zend\Stdlib\ArrayObject\PhpReferenceCompatibility', 'Zend\Stdlib\ArrayObject');
+}
+
+__halt_compiler();
+
+class ArrayObject extends \ArrayObject
+{
 }

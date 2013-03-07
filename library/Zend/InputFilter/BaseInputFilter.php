27314e9b286c23071f44f78d3980251a1b37fafc
ossinkine <ossinkine@ya.ru>
2013-03-07 17:07:55 +0600

    Update BaseInputFilter If value is required, it must be set in $this->data[]. If the value is set and it should not be empty, it must be invalid, even if not required.

--- library/Zend/InputFilter/BaseInputFilter.php
+++ library/Zend/InputFilter/BaseInputFilter.php
@@ -159,6 +159,13 @@ class BaseInputFilter implements InputFilterInterface, UnknownInputsCapableInter
         $inputs = $this->validationGroup ?: array_keys($this->inputs);
         foreach ($inputs as $name) {
             $input = $this->inputs[$name];
+            if ((!array_key_exists($name, $this->data)
+                    || (null === $this->data[$name]))
+                && !$input->isRequired()
+            ) {
+                $this->validInputs[$name] = $input;
+                continue;
+            }
             if (!array_key_exists($name, $this->data)
                 || (null === $this->data[$name])
                 || (is_string($this->data[$name]) && strlen($this->data[$name]) === 0)
@@ -170,11 +177,6 @@ class BaseInputFilter implements InputFilterInterface, UnknownInputsCapableInter
                     && isset($this->data[$name][0]['error']) && $this->data[$name][0]['error'] === UPLOAD_ERR_NO_FILE)
             ) {
                 if ($input instanceof InputInterface) {
-                    // - test if input is required
-                    if (!$input->isRequired()) {
-                        $this->validInputs[$name] = $input;
-                        continue;
-                    }
                     // - test if input allows empty
                     if ($input->allowEmpty()) {
                         $this->validInputs[$name] = $input;
@@ -182,7 +184,7 @@ class BaseInputFilter implements InputFilterInterface, UnknownInputsCapableInter
                     }
                 }
                 // make sure we have a value (empty) for validation
-                $this->data[$name] = '';
+                $this->data[$name] = null;
             }
 
             if ($input instanceof InputFilterInterface) {

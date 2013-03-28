df6e60886e63ebd3f083f308194d82287743bede
Chris de Kok <chris.de.kok@gmail.com>
2013-03-28 22:54:44 +0100

    + fix pgsql test for empty string

--- tests/ZendTest/Db/Adapter/Driver/Pgsql/ConnectionTest.php
+++ tests/ZendTest/Db/Adapter/Driver/Pgsql/ConnectionTest.php
@@ -28,9 +28,16 @@ class ConnectionTest extends \PHPUnit_Framework_TestCase
     public function testResource()
     {
         if (extension_loaded('pgsql')) {
-            $this->setExpectedException('Zend\Db\Adapter\Exception\RuntimeException');
-            // Error supressing needed else pg_connect will fatal
-            @$this->connection->getResource();
+            try {
+                // Error supressing needed else pg_connect will fatal
+                $resource = @$this->connection->getResource();
+                // pg_connect allows to connect with an empty string
+                $this->assertTrue(is_resource($resource));
+            } catch (\Zend\Db\Adapter\Exception\RuntimeException $exc) {
+                // If it throws an exception it has failed to connect
+                $this->setExpectedException('Zend\Db\Adapter\Exception\RuntimeException');
+                throw $exc;
+            }
         } else {
             $this->markTestSkipped('pgsql extension not loaded');
         }

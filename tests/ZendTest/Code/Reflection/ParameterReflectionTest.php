7334a7cf2e3e70d8a39047b55cda199cce335582
Marco Pivetta <ocramius@gmail.com>
2013-03-30 13:11:26 +0100

    Adding tests for parameter reflection for `callable`

--- tests/ZendTest/Code/Reflection/ParameterReflectionTest.php
+++ tests/ZendTest/Code/Reflection/ParameterReflectionTest.php
@@ -48,6 +48,17 @@ class ParameterReflectionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($type, $parameter->getType());
     }
 
+    public function testCallableTypeHint()
+    {
+        if (PHP_VERSION_ID < 50400) {
+            $this->markTestSkipped('`callable` is only supported in PHP >=5.4.0');
+        }
+
+        $parameter = new Reflection\ParameterReflection(array('ZendTest\Code\Reflection\TestAsset\CallableTypeHintClass', 'foo'), 'bar');
+
+        $this->assertEquals('callable', $parameter->getType());
+    }
+
     public function paramTypeTestProvider()
     {
         return array(

48521f5586e947618cddee24e3042093976827db
Chintan V. Patel <ch11cvaindian@gmail.com>
2013-04-04 17:47:09 +0530

    Revert back

--- library/Zend/Form/View/Helper/FormMultiCheckbox.php
+++ library/Zend/Form/View/Helper/FormMultiCheckbox.php
@@ -259,7 +259,7 @@ class FormMultiCheckbox extends FormInput
             $label           = '';  
             $inputAttributes = $attributes;
             $labelAttributes = $globalLabelAttributes;
-            $selected        = isset($inputAttributes['selected']) && $inputAttributes['type'] != 'radio' && $inputAttributes['selected'] != false ? true : false;
+            $selected        = isset($inputAttributes['selected']) && $inputAttributes['selected'] != false ? true : false;
             $disabled        = isset($inputAttributes['disabled']) && $inputAttributes['disabled'] != false ? true : false;
 
             if (is_scalar($optionSpec)) {

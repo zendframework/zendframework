27512bbd2ef3296fdf150325df2a0081be592990
Marco Pivetta <ocramius@gmail.com>
2013-04-18 22:39:31 +0200

    Fixing checks - was failing also with no CS failures

--- bin/check-cs.sh
+++ bin/check-cs.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 cd "$(dirname $(dirname "$0"))"
 
-libraryCS=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 ./library/)
-testsCS=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 ./tests/)
+libraryCS=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 ./library)
+testsCS=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 ./tests)
 
-output="$libraryCS
-$testsCS"
-
-if [[ "$output" ]];
+if [[ "$libraryCS" || "$testsCS"  ]];
 then
-    echo   -en '\E[31m'"$output\033[1m\033[0m";
+    echo   -en '\E[31m'"$libraryCS
+$testsCS\033[1m\033[0m";
     printf "\n";
     echo   -en '\E[31;47m'"\033[1mCoding standards check failed!\033[0m"   # Red
     printf "\n";
@@ -18,3 +16,5 @@ fi
 
 echo   -en '\E[32m'"\033[1mCoding standards check passed!\033[0m"   # Green
 printf "\n";
+
+echo $libraryCS$testsCS;

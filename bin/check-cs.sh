4349c8d273fe39cbcfc249d1faff52833e75a172
Marco Pivetta <ocramius@gmail.com>
2013-04-18 22:10:47 +0200

    Cleaning up CS fixer

--- bin/check-cs.sh
+++ bin/check-cs.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 cd "$(dirname $(dirname "$0"))"
 
-output=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 .)
-echo $output
-cs=0
-if [[ "$output" -ne "" ]];then
-    cs=2
-fi
-echo "Coding standards exited with status $cs"
+libraryCS=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 ./library/)
+testsCS=$(php php-cs-fixer.phar fix -v --dry-run --level=psr2 ./tests/)
 
-if [[ "$cs" -eq "2" ]];then
-    echo "Exiting with status 2 due to CS";
-    exit 2;
+output="$libraryCS
+$testsCS"
+
+if [[ "$output" ]];
+then
+    echo   -en '\E[31m'"$output\033[1m\033[0m";
+    printf "\n";
+    echo   -en '\E[31;47m'"\033[1mCoding standards check failed!\033[0m"   # Red
+    printf "\n";
+    exit   2;
 fi
 
-exit 0
+echo   -en '\E[32m'"\033[1mCoding standards check passed!\033[0m"   # Green
+printf "\n";

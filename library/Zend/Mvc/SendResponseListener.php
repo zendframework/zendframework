c7fe241d73bc617982d02e55d0fa74b712abe7b8
prolic <saschaprolic@googlemail.com>
2013-04-19 22:30:31 +0200

    allow default http responses to be sent in mvc stack

--- library/Zend/Mvc/SendResponseListener.php
+++ library/Zend/Mvc/SendResponseListener.php
@@ -15,6 +15,7 @@ use Zend\EventManager\EventManagerInterface;
 use Zend\EventManager\ListenerAggregateInterface;
 use Zend\Mvc\MvcEvent;
 use Zend\Mvc\ResponseSender\ConsoleResponseSender;
+use Zend\Mvc\ResponseSender\HttpResponseSender;
 use Zend\Mvc\ResponseSender\PhpEnvironmentResponseSender;
 use Zend\Mvc\ResponseSender\SimpleStreamResponseSender;
 use Zend\Mvc\ResponseSender\SendResponseEvent;
@@ -125,7 +126,7 @@ class SendResponseListener implements
     public function getEvent()
     {
         if (!$this->event instanceof SendResponseEvent) {
-            $this->event = new SendResponseEvent();
+            $this->setEvent(new SendResponseEvent());
         }
         return $this->event;
     }
@@ -160,6 +161,6 @@ class SendResponseListener implements
         $events->attach(SendResponseEvent::EVENT_SEND_RESPONSE, new PhpEnvironmentResponseSender(), -1000);
         $events->attach(SendResponseEvent::EVENT_SEND_RESPONSE, new ConsoleResponseSender(), -2000);
         $events->attach(SendResponseEvent::EVENT_SEND_RESPONSE, new SimpleStreamResponseSender(), -3000);
+        $events->attach(SendResponseEvent::EVENT_SEND_RESPONSE, new HttpResponseSender(), -4000);
     }
-
 }

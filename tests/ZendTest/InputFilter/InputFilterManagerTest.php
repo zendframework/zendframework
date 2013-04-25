da79087e41967c5386a6196fd89b036f14ff241c
Michaël Gallego <mic.gallego@gmail.com>
2013-04-25 18:47:27 +0200

    Add various plugin manager

---
+++ tests/ZendTest/InputFilter/InputFilterManagerTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Form
+ */
+
+namespace ZendTest\InputFilter;
+
+use Zend\ServiceManager\ServiceManager;
+use Zend\InputFilter\InputFilterPluginManager;
+
+/**
+ * @category   Zend
+ * @package    Zend_Stdlib
+ * @subpackage UnitTests
+ * @group      Zend_Stdlib
+ */
+class InputFilterManagerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var InputFilterPluginManager
+     */
+    protected $manager;
+
+    public function setUp()
+    {
+        $this->manager = new InputFilterPluginManager();
+    }
+
+    public function testRegisteringInvalidElementRaisesException()
+    {
+        $this->setExpectedException('Zend\InputFilter\Exception\RuntimeException');
+        $this->manager->setService('test', $this);
+    }
+
+    public function testLoadingInvalidElementRaisesException()
+    {
+        $this->manager->setInvokableClass('test', get_class($this));
+        $this->setExpectedException('Zend\InputFilter\Exception\RuntimeException');
+        $this->manager->get('test');
+    }
+}

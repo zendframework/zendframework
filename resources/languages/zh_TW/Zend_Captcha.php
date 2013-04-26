8300592dc93525e510d2e17a3ad6ee3ecb8cd217
atans <atans.chiu@gmail.com>
2013-04-26 17:41:42 +0800

    Add zh_TW translations

---
+++ resources/languages/zh_TW/Zend_Captcha.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Zend Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@zend.com so we can send you a copy immediately.
+ *
+ * @copyright  Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * ZH-Revision: 26.Apr.2013
+ */
+return array(
+    // Zend_Captcha_ReCaptcha
+    "Missing captcha fields" => "沒有找到驗證碼區域",
+    "Failed to validate captcha" => "驗證碼校驗失敗",
+    "Captcha value is wrong: %value%" => "驗證碼不匹配: %value%",
+
+    // Zend_Captcha_Word
+    "Empty captcha value" => "請輸入驗證碼",
+    "Captcha ID field is missing" => "沒有找到驗證碼區域",
+    "Captcha value is wrong" => "驗證碼不匹配",
+);

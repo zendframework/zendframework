f80b478bfc87b90e11038805fd8eae000e9eb4d9
Ralph Schindler <ralph.schindler@zend.com>
2013-04-29 18:08:09 -0500

    Updated CreateTableTest

--- library/Zend/Db/Sql/Ddl/CreateTable.php
+++ library/Zend/Db/Sql/Ddl/CreateTable.php
@@ -20,52 +20,146 @@ class CreateTable extends AbstractSql implements SqlInterface
     const TABLE = 'table';
     const COLUMNS = 'columns';
     const CONSTRAINTS = 'constraints';
-    const OPTIONS = 'options';
 
     protected $specifications = array(
-        self::TABLE => 'CREATE TABLE %1$s',
-        ' (',
+        self::TABLE => 'CREATE TABLE %1$s (',
         self::COLUMNS  => array(
             "\n    %1\$s" => array(
                 array(1 => '%1$s', 'combinedby' => ",\n    ")
             )
         ),
         self::CONSTRAINTS => array(
-            ",\n    %1\$s" => array(
+            "\n    %1\$s" => array(
                 array(1 => '%1$s', 'combinedby' => ",\n    ")
             )
-        ),
-        "\n)"
+        )
     );
 
+    /**
+     * @var bool
+     */
     protected $isTemporary = false;
+
+    /**
+     * @var string
+     */
     protected $table = '';
+
+    /**
+     * @var array
+     */
     protected $columns = array();
+
+    /**
+     * @var array
+     */
     protected $constraints = array();
 
+    /**
+     * @param string $table
+     * @param bool $isTemporary
+     */
     public function __construct($table = '', $isTemporary = false)
     {
         $this->table = $table;
     }
 
+    /**
+     * @param $temporary
+     * @return $this
+     */
     public function setTemporary($temporary)
     {
         $this->isTemporary = (bool) $temporary;
+        return $this;
     }
 
+    /**
+     * @return bool
+     */
+    public function isTemporary()
+    {
+        return $this->isTemporary;
+    }
+
+    /**
+     * @param $name
+     * @return $this
+     */
     public function setTable($name)
     {
         $this->table = $name;
+        return $this;
     }
 
+    /**
+     * @param Column\ColumnInterface $column
+     * @return $this
+     */
     public function addColumn(Column\ColumnInterface $column)
     {
         $this->columns[] = $column;
+        return $this;
     }
 
+    /**
+     * @param Constraint\ConstraintInterface $constraint
+     * @return $this
+     */
     public function addConstraint(Constraint\ConstraintInterface $constraint)
     {
         $this->constraints[] = $constraint;
+        return $this;
+    }
+
+    /**
+     * @param string|null $key
+     * @return array
+     */
+    public function getRawState($key = null)
+    {
+        $rawState = array(
+            self::TABLE      => $this->table,
+            self::COLUMNS    => $this->columns,
+            self::CONSTRAINTS => $this->constraints,
+        );
+        return (isset($key) && array_key_exists($key, $rawState)) ? $rawState[$key] : $rawState;
+    }
+
+    /**
+     * @param PlatformInterface $adapterPlatform
+     * @return string
+     */
+    public function getSqlString(PlatformInterface $adapterPlatform = null)
+    {
+        // get platform, or create default
+        $adapterPlatform = ($adapterPlatform) ?: new AdapterSql92Platform;
+
+        $sqls = array();
+        $parameters = array();
+
+        foreach ($this->specifications as $name => $specification) {
+            if (is_int($name)) {
+                $sqls[] = $specification;
+                continue;
+            }
+            $parameters[$name] = $this->{'process' . $name}($adapterPlatform, null, null, $sqls, $parameters);
+            if ($specification && is_array($parameters[$name]) && ($parameters[$name] != array(array()))) {
+                $sqls[$name] = $this->createSqlFromSpecificationAndParameters($specification, $parameters[$name]);
+            }
+            if (stripos($name, 'table') === false && $parameters[$name] !== array(array())) {
+                $sqls[] = ",\n";
+            }
+        }
+
+        // remove last ,
+        if (count($sqls) > 2) {
+            array_pop($sqls);
+        }
+
+
+        $sql = implode('', $sqls) . "\n)";
+        return $sql;
     }
 
     protected function processTable(PlatformInterface $adapterPlatform = null)
@@ -98,27 +192,5 @@ class CreateTable extends AbstractSql implements SqlInterface
         return array($sqls);
     }
 
-    public function getSqlString(PlatformInterface $adapterPlatform = null)
-    {
-        // get platform, or create default
-        $adapterPlatform = ($adapterPlatform) ?: new AdapterSql92Platform;
-
-        $sqls = array();
-        $parameters = array();
-
-        foreach ($this->specifications as $name => $specification) {
-            if (is_int($name)) {
-                $sqls[] = $specification;
-                continue;
-            }
-            $parameters[$name] = $this->{'process' . $name}($adapterPlatform, null, null, $sqls, $parameters);
-            if ($specification && is_array($parameters[$name])) {
-                $sqls[$name] = $this->createSqlFromSpecificationAndParameters($specification, $parameters[$name]);
-            }
-        }
-
-        $sql = implode('', $sqls);
-        return $sql;
-    }
 
 }
--- tests/ZendTest/Db/Sql/Ddl/CreateTableTest.php
+++ tests/ZendTest/Db/Sql/Ddl/CreateTableTest.php
@@ -11,66 +11,111 @@
 namespace ZendTest\Db\Sql\Ddl;
 
 use Zend\Db\Sql\Ddl\CreateTable;
+use Zend\Db\Sql\Ddl\Column\Column;
 
 class CreateTableTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @covers Zend\Db\Sql\Ddl\CreateTable::setTemporary
-     * @todo   Implement testSetTemporary().
      */
     public function testSetTemporary()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $ct = new CreateTable();
+        $this->assertSame($ct, $ct->setTemporary(false));
+        $this->assertFalse($ct->isTemporary());
+        $ct->setTemporary(true);
+        $this->assertTrue($ct->isTemporary());
+        $ct->setTemporary('yes');
+        $this->assertTrue($ct->isTemporary());
+    }
+
+    /**
+     * @covers Zend\Db\Sql\Ddl\CreateTable::isTemporary
+     */
+    public function testIsTemporary()
+    {
+        $ct = new CreateTable();
+        $this->assertFalse($ct->isTemporary());
+        $ct->setTemporary(true);
+        $this->assertTrue($ct->isTemporary());
     }
 
     /**
      * @covers Zend\Db\Sql\Ddl\CreateTable::setTable
-     * @todo   Implement testSetTable().
      */
     public function testSetTable()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $ct = new CreateTable();
+        $this->assertEquals('', $ct->getRawState('table'));
+        $ct->setTable('test');
+        return $ct;
+    }
+
+    /**
+     * @covers Zend\Db\Sql\Ddl\CreateTable::getRawState
+     * @depends testSetTable
+     */
+    public function testRawStateViaTable(CreateTable $ct)
+    {
+        $this->assertEquals('test', $ct->getRawState('table'));
     }
 
     /**
      * @covers Zend\Db\Sql\Ddl\CreateTable::addColumn
-     * @todo   Implement testAddColumn().
      */
     public function testAddColumn()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $column = $this->getMock('Zend\Db\Sql\Ddl\Column\ColumnInterface');
+        $ct = new CreateTable;
+        $this->assertSame($ct, $ct->addColumn($column));
+        return $ct;
+    }
+
+    /**
+     * @covers Zend\Db\Sql\Ddl\CreateTable::getRawState
+     * @depends testAddColumn
+     */
+    public function testRawStateViaColumn(CreateTable $ct)
+    {
+        $state = $ct->getRawState('columns');
+        $this->assertInternalType('array', $state);
+        $column = array_pop($state);
+        $this->assertInstanceOf('Zend\Db\Sql\Ddl\Column\ColumnInterface', $column);
     }
 
     /**
      * @covers Zend\Db\Sql\Ddl\CreateTable::addConstraint
-     * @todo   Implement testAddConstraint().
      */
     public function testAddConstraint()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $constraint = $this->getMock('Zend\Db\Sql\Ddl\Constraint\ConstraintInterface');
+        $ct = new CreateTable;
+        $this->assertSame($ct, $ct->addConstraint($constraint));
+        return $ct;
+    }
+
+    /**
+     * @covers Zend\Db\Sql\Ddl\CreateTable::getRawState
+     * @depends testAddConstraint
+     */
+    public function testRawStateViaConstraint(CreateTable $ct)
+    {
+        $state = $ct->getRawState('constraints');
+        $this->assertInternalType('array', $state);
+        $constraint = array_pop($state);
+        $this->assertInstanceOf('Zend\Db\Sql\Ddl\Constraint\ConstraintInterface', $constraint);
     }
 
     /**
      * @covers Zend\Db\Sql\Ddl\CreateTable::getSqlString
-     * @todo   Implement testGetSqlString().
      */
     public function testGetSqlString()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-          'This test has not been implemented yet.'
-        );
+        $ct = new CreateTable('foo');
+        $this->assertEquals("CREATE TABLE \"foo\" (\n)", $ct->getSqlString());
+
+        $ct = new CreateTable('foo');
+        $ct->addColumn(new Column('bar'));
+        $this->assertEquals("CREATE TABLE \"foo\" (\n    \"bar\" INTEGER NOT NULL\n)", $ct->getSqlString());
     }
 }

327c67089412b2e9e6e0a1a52bb4cc6a116ded8e
Filippo Tessarotto <zoeslam@gmail.com>
2013-05-09 16:40:13 +0200

    Form InputFilterSpecification: incorrect propagation

---
+++ tests/ZendTest/Form/TestAsset/InputFilterProviderWithFieldset.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Form
+ */
+
+namespace ZendTest\Form\TestAsset;
+
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilterProviderInterface;
+
+class InputFilterProviderWithFieldset extends Form implements InputFilterProviderInterface
+{
+    public function __construct($name = null, $options = array())
+    {
+        parent::__construct($name, $options);
+
+        $this->add(array(
+            'name' => 'foo',
+            'options' => array(
+                'label' => 'Foo'
+            ),
+        ));
+
+        $this->add(new BasicFieldset());
+    }
+
+    public function getInputFilterSpecification()
+    {
+        return array(
+            'foo' => array(
+                'required' => true,
+            )
+        );
+    }
+}

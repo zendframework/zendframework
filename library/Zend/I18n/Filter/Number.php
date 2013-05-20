3e9bf9d6325253ae8383db0a9f63305c4bb07f02
Andreas Möller <am@localheinz.com>
2013-05-20 17:46:17 +0200

    Extend NumberFormat filter

---
+++ library/Zend/I18n/Filter/Number.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\I18n\Filter;
+
+use Zend\I18n\Exception;
+use Zend\Stdlib\ErrorHandler;
+
+class Number extends NumberFormat
+{
+
+    /**
+     * Defined by Zend\Filter\FilterInterface
+     *
+     * @see    Zend\Filter\FilterInterface::filter()
+     * @param  mixed $value
+     * @return mixed
+     */
+    public function filter($value)
+    {
+        if (!is_int($value)
+            || !is_float($value)) {
+
+            ErrorHandler::start();
+
+            $result = $this->getFormatter()->parse(
+                $value,
+                $this->getType()
+            );
+
+            ErrorHandler::stop();
+
+            if (false !== $result) {
+                return $result;
+            }
+        }
+
+        return $value;
+
+    }
+}
---
+++ tests/ZendTest/I18n/Filter/NumberTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_I18n
+ */
+
+namespace ZendTest\I18n\Filter;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\I18n\Filter\Number as NumberFilter;
+use NumberFormatter;
+
+class NumberTest extends TestCase
+{
+    public function testConstructWithOptions()
+    {
+        $filter = new NumberFilter(array(
+            'locale' => 'en_US',
+            'style'  => NumberFormatter::DECIMAL
+        ));
+
+        $this->assertEquals('en_US', $filter->getLocale());
+        $this->assertEquals(NumberFormatter::DECIMAL, $filter->getStyle());
+    }
+
+    public function testConstructWithParameters()
+    {
+        $filter = new NumberFilter('en_US', NumberFormatter::DECIMAL);
+
+        $this->assertEquals('en_US', $filter->getLocale());
+        $this->assertEquals(NumberFormatter::DECIMAL, $filter->getStyle());
+    }
+
+    /**
+     * @param $locale
+     * @param $style
+     * @param $type
+     * @param $value
+     * @param $expected
+     * @dataProvider formattedToNumberProvider
+     */
+    public function testFormattedToNumber($locale, $style, $type, $value, $expected)
+    {
+        $filter = new NumberFilter($locale, $style, $type);
+        $this->assertSame($expected, $filter->filter($value));
+    }
+
+    public static function formattedToNumberProvider()
+    {
+        return array(
+            array(
+                'en_US',
+                NumberFormatter::DEFAULT_STYLE,
+                NumberFormatter::TYPE_DOUBLE,
+                '1,234,567.891',
+                1234567.891,
+            ),
+            array(
+                'de_DE',
+                NumberFormatter::DEFAULT_STYLE,
+                NumberFormatter::TYPE_DOUBLE,
+                '1.234.567,891',
+                1234567.891,
+            ),
+            array(
+                'ru_RU',
+                NumberFormatter::DEFAULT_STYLE,
+                NumberFormatter::TYPE_DOUBLE,
+                '1 234 567,891',
+                1234567.891,
+            ),
+        );
+    }
+}

91f9b67a24efb792d74d7b994ec9e641784f7946
Kyle Spraggs <theman@spiffyjr.me>
2013-05-24 11:37:17 -0500

    Update StrategyInterface.php

--- library/Zend/Stdlib/Hydrator/Strategy/StrategyInterface.php
+++ library/Zend/Stdlib/Hydrator/Strategy/StrategyInterface.php
@@ -15,17 +15,17 @@ interface StrategyInterface
      * Converts the given value so that it can be extracted by the hydrator.
      *
      * @param mixed   $value The original value.
-     * @param object $object The original object.
+     * @param object $object (optional) The original object for context.
      * @return mixed Returns the value that should be extracted.
      */
-    public function extract($value, $object = null);
+    public function extract($value);
 
     /**
      * Converts the given value so that it can be hydrated by the hydrator.
      *
      * @param mixed $value The original value.
-     * @param array  $data The original data.
+     * @param array  $data (optional) The original data for context.
      * @return mixed Returns the value that should be hydrated.
      */
-    public function hydrate($value, array $data = null);
+    public function hydrate($value);
 }

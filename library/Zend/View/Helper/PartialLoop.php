6d783a788e8fad44a6b0296e198a0bf2f2d601a6
Max Tsepkov <max@yogi.pw>
2013-05-27 22:26:57 +0600

    PartialLoop: rewrite input check even better

--- library/Zend/View/Helper/PartialLoop.php
+++ library/Zend/View/Helper/PartialLoop.php
@@ -44,22 +44,14 @@ class PartialLoop extends Partial
             return $this;
         }
 
-        if (!( is_array($values)
-            || $values instanceof Traversable
-            || (is_object($values) && method_exists($values, 'toArray'))
-        )) {
-            throw new Exception\InvalidArgumentException('PartialLoop helper requires iterable data');
-        }
-
-        if (is_object($values)
-            && (!$values instanceof Traversable)
-            && method_exists($values, 'toArray')
-        ) {
-            $values = $values->toArray();
-        }
-
-        if ($values instanceof Iterator) {
+        if (is_array($values)) {
+            // it's ok. do nothing
+        } else if ($values instanceof Traversable) {
             $values = ArrayUtils::iteratorToArray($values);
+        } else if (is_object($values) && method_exists($values, 'toArray')) {
+            $values = $values->toArray();
+        } else {
+            throw new Exception\InvalidArgumentException('PartialLoop helper requires iterable data');
         }
 
         // reset the counter if it's called again

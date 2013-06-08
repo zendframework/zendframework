58475a8ba514da29aeeed90d394edafa26b64d41
Marc Bennewitz <marc-bennewitz@arcor.de>
2013-06-08 20:32:16 +0200

    Log formatter: Use JSON_UNESCAPE_[SLASHES|UNICODE] to generate better readable error messages

--- library/Zend/Log/Formatter/Base.php
+++ library/Zend/Log/Formatter/Base.php
@@ -76,13 +76,21 @@ class Base implements FormatterInterface
             return $value;
         }
 
+        // better readable JSON
+        static $jsonFlags;
+        if ($jsonFlags === null) {
+            $jsonFlags = 0;
+            $jsonFlags |= defined('JSON_UNESCAPED_SLASHES') ? JSON_UNESCAPED_SLASHES : 0;
+            $jsonFlags |= defined('JSON_UNESCAPED_UNICODE') ? JSON_UNESCAPED_UNICODE : 0;
+        }
+
         ErrorHandler::start();
         if ($value instanceof DateTime) {
             $value = $value->format($this->getDateTimeFormat());
         } elseif ($value instanceof Traversable) {
-            $value = json_encode(iterator_to_array($value));
+            $value = json_encode(iterator_to_array($value), $jsonFlags);
         } elseif (is_array($value)) {
-            $value = json_encode($value);
+            $value = json_encode($value, $jsonFlags);
         } elseif (is_object($value) && !method_exists($value, '__toString')) {
             $value = sprintf('object(%s) %s', get_class($value), json_encode($value));
         } elseif (is_resource($value)) {

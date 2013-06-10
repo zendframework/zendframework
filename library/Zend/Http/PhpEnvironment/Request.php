b8abbb9ed4a279de4febdefa00dab7c6f684592a
mbn18 <miki@epoch.co.il>
2013-06-10 23:55:18 +0300

    Update Request.php

--- library/Zend/Http/PhpEnvironment/Request.php
+++ library/Zend/Http/PhpEnvironment/Request.php
@@ -261,12 +261,29 @@ class Request extends HttpRequest
         // Set the host
         if ($this->getHeaders()->get('host')) {
             $host = $this->getHeaders()->get('host')->getFieldValue();
+
             // works for regname, IPv4 & IPv6
             if (preg_match('|\:(\d+)$|', $host, $matches)) {
                 $host = substr($host, 0, -1 * (strlen($matches[1]) + 1));
                 $port = (int) $matches[1];
             }
-        } elseif (isset($this->serverParams['SERVER_NAME'])) {
+
+            // set up a validator that check if the hostname is legal (not spoofed)
+            $hostnameValidator = new \Zend\Validator\Hostname(
+                array(
+                    'allow'=>\Zend\Validator\Hostname::ALLOW_ALL,
+                    'useIdnCheck'=>false,
+                    'useTldCheck'=>false
+                )
+            );
+            // If invalid. Reset the host & port
+            if (!$hostnameValidator->isValid($host)) {
+                $host = null;
+                $port = null;
+            }
+        }
+
+        if (!$host && isset($this->serverParams['SERVER_NAME'])) {
             $host = $this->serverParams['SERVER_NAME'];
             if (isset($this->serverParams['SERVER_PORT'])) {
                 $port = (int) $this->serverParams['SERVER_PORT'];

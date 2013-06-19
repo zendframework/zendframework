58154ba4de0e0e7afa3f53beadbdc86cb78009aa
Richard Jennings <rich@trueaxiom.co.uk>
2013-06-19 16:06:38 +0100

    use DateTime::getTimestamp() as argument to IntlDateFormatter::format() for PHP installation consistency

--- library/Zend/Form/View/Helper/FormDateSelect.php
+++ library/Zend/Form/View/Helper/FormDateSelect.php
@@ -92,8 +92,8 @@ class FormDateSelect extends FormMonthSelectHelper
 
         $result = array();
         for ($day = 1; $day <= 31; $day++) {
-            $key   = $keyFormatter->format($date);
-            $value = $valueFormatter->format($date);
+            $key   = $keyFormatter->format($date->getTimestamp());
+            $value = $valueFormatter->format($date->getTimestamp());
             $result[$key] = $value;
 
             $date->modify('+1 day');
--- library/Zend/Form/View/Helper/FormMonthSelect.php
+++ library/Zend/Form/View/Helper/FormMonthSelect.php
@@ -257,8 +257,8 @@ class FormMonthSelect extends AbstractHelper
 
         $result = array();
         for ($month = 1; $month <= 12; $month++) {
-            $key   = $keyFormatter->format($date);
-            $value = $valueFormatter->format($date);
+            $key   = $keyFormatter->format($date->getTimestamp());
+            $value = $valueFormatter->format($date->getTimestamp());
             $result[$key] = $value;
 
             $date->modify('+1 month');

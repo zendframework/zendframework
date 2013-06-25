53db6b99234c2adba9fb24ed0ba571d122d7217e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2013-06-25 19:40:21 +0700

    remove bloated LICENSE at header because already linked using @license

--- bin/pluginmap_generator.php
+++ bin/pluginmap_generator.php
@@ -1,18 +1,9 @@
 #!/usr/bin/env php
 <?php
 /**
- * Zend Framework
+ * Zend Framework (http://framework.zend.com/)
  *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://framework.zend.com/license/new-bsd
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@zend.com so we can send you a copy immediately.
- *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
  * @category   Zend
  * @package    Zend_Loader
  * @subpackage Exception

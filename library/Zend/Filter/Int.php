dd4f1ddad8861c1f17b0aa6ac63df95d6a2003a2
Paulo Eduardo <pauloelr@gmail.com>
2013-06-26 21:34:11 -0300

    Fix for Int Filter
    Found one more.

--- library/Zend/Filter/Int.php
+++ library/Zend/Filter/Int.php
@@ -21,6 +21,14 @@ class Int extends AbstractFilter
      */
     public function filter($value)
     {
+        if(!is_scalar($value)){
+            throw new Exception\InvalidArgumentException(sprintf(
+                '%s expects parameter to be scalar, "%s" given',
+                __METHOD__,
+                (is_object($value) ? get_class($value) : gettype($value))
+            ));
+        }
+        
         return (int) ((string) $value);
     }
 }

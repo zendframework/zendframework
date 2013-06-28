fc69a35d9794b5e96fe6901c4a03f4b6ae2bb2ab
Michaël Gallego <mic.gallego@gmail.com>
2013-06-28 15:54:12 +0200

    Remove ZendTest from Composer

--- tests/_autoload.php
+++ tests/_autoload.php
@@ -3,17 +3,12 @@
  * Setup autoloading
  */
 
-if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    include_once __DIR__ . '/../vendor/autoload.php';
-} else {
-    // if composer autoloader is missing, explicitly add the ZF library path
-    require_once __DIR__ . '/../library/Zend/Loader/StandardAutoloader.php';
-    $loader = new Zend\Loader\StandardAutoloader(
-        array(
-             Zend\Loader\StandardAutoloader::LOAD_NS => array(
-                 'Zend'     => __DIR__ . '/../library/Zend',
-                 'ZendTest' => __DIR__ . '/ZendTest',
-             ),
-        ));
-    $loader->register();
-}
+require_once __DIR__ . '/../library/Zend/Loader/StandardAutoloader.php';
+$loader = new Zend\Loader\StandardAutoloader(
+    array(
+         Zend\Loader\StandardAutoloader::LOAD_NS => array(
+             'Zend'     => __DIR__ . '/../library/Zend',
+             'ZendTest' => __DIR__ . '/ZendTest',
+         ),
+    ));
+$loader->register();

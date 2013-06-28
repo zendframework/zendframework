ebc7f4190daf5f667dfa8ceaefce66c15c0b7211
Chris R <git@gitgate.ch>
2013-06-28 14:01:13 +0200

    Update LoggerAwareInterface.php
    trailing spaces removed

--- library/Zend/Log/LoggerAwareInterface.php
+++ library/Zend/Log/LoggerAwareInterface.php
@@ -23,11 +23,11 @@ interface LoggerAwareInterface
      * @return void
      */
     public function setLogger(LoggerInterface $logger);
-    
+
     /**
      * Get logger instance. Currently commented out as this would possibly break
      * existing implementations.
-     * 
+     *
      * @return null|LoggerInterface
      */
     // public function getLogger();

3bf60a31e4e3079942f437e74a523cf2bf32821e
Michaël Gallego <mic.gallego@gmail.com>
2013-06-28 17:06:11 +0200

    Feedback

--- tests/_autoload.php
+++ tests/_autoload.php
@@ -5,12 +5,11 @@
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
-require_once __DIR__ . '/../library/Zend/Loader/StandardAutoloader.php';
 $loader = new Zend\Loader\StandardAutoloader(
     array(
         Zend\Loader\StandardAutoloader::LOAD_NS => array(
-            'Zend'     => __DIR__ . '/../library/Zend',
             'ZendTest' => __DIR__ . '/ZendTest',
         ),
-    ));
+    )
+);
 $loader->register();

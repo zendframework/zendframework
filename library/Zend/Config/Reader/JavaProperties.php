9bf1b40bd3a8a1667494216ca4031214b1af27ed
Matthew Vargeson <mvargeson@soliantconsulting.com>
2013-07-13 14:24:44 -0700

    Conform to ZF2 contribution coding standards

--- library/Zend/Config/Reader/JavaProperties.php
+++ library/Zend/Config/Reader/JavaProperties.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
 
 namespace Zend\Config\Reader;
 
@@ -99,11 +106,13 @@ class JavaProperties implements ReaderInterface
         $key = "";
         $isWaitingOtherLine = false;
         foreach ($lines as $i => $line) {
-            if(empty($line) || (!$isWaitingOtherLine && strpos($line, "#") === 0)) {
+        	// Ignore empty lines and commented lines
+            if (empty($line) || (!$isWaitingOtherLine && strpos($line, "#") === 0)) {
                 continue;
             }
 
-            if(!$isWaitingOtherLine) {
+            // Add a new key-value pair or append value to a previous pair
+            if (!$isWaitingOtherLine) {
                 $key = substr($line, 0, strpos($line, ':'));
                 $value = substr($line, strpos($line, ':') + 1, strlen($line));
             }
@@ -111,8 +120,8 @@ class JavaProperties implements ReaderInterface
                 $value .= $line;
             }
 
-            /* Check if ends with single '\' */
-            if(strrpos($value, "\\") === strlen($value)-strlen("\\")) {
+            // Check if ends with single '\' (indicating another line is expected)
+            if (strrpos($value, "\\") === strlen($value)-strlen("\\")) {
                 $value = substr($value,0,strlen($value)-1);
                 $isWaitingOtherLine = true;
             }

ebb5e8b6e7370087620f7287d1551b9df624ca1b
Matthew Vargeson <mvargeson@soliantconsulting.com>
2013-07-13 13:48:45 -0700

    Add unit tests and test assets for Zend\Config\Reader\JavaProperties

---
+++ tests/ZendTest/Config/Reader/JavaPropertiesTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Config
+ */
+
+namespace ZendTest\Config\Reader;
+
+use Zend\Config\Reader\JavaProperties as JavaProperties;
+
+/**
+ * @category   Zend
+ * @package    Zend_Config
+ * @subpackage UnitTests
+ * @group      Zend_Config
+ */
+class JavaPropertiesTest extends AbstractReaderTestCase
+{
+    public function setUp()
+    {
+        $this->reader = new JavaProperties();
+    }
+
+    /**
+     * getTestAssetPath(): defined by AbstractReaderTestCase.
+     *
+     * @see    AbstractReaderTestCase::getTestAssetPath()
+     * @return string
+     */
+    protected function getTestAssetPath($name)
+    {
+        return __DIR__ . '/TestAssets/JavaProperties/' . $name . '.properties';
+    }
+
+    public function testFromFile()
+    {
+        $arrayJavaProperties = $this->reader->fromFile($this->getTestAssetPath('include-target'));
+        
+        $this->assertNotEmpty($arrayJavaProperties);
+        $this->assertEquals($arrayJavaProperties['single.line'], 'test');
+        $this->assertEquals($arrayJavaProperties['path'], '\test\with\slashes');
+        $this->assertEquals($arrayJavaProperties['multiple'], 'line test');
+    }
+
+    public function testIncludeAsElement()
+    {
+        $arrayJavaProperties = $this->reader->fromFile($this->getTestAssetPath('include-base'));
+        
+        $this->assertNotEmpty($arrayJavaProperties);
+        $this->assertEquals($arrayJavaProperties['single.line'], 'test');
+        $this->assertEquals($arrayJavaProperties['path'], '\test\with\slashes');
+        $this->assertEquals($arrayJavaProperties['multiple'], 'line test');
+    }
+/*
+    public function testInvalidJavaPropertiesFile()
+    {
+        $this->setExpectedException('Zend\Config\Exception\RuntimeException');
+        $arrayJavaProperties = $this->reader->fromFile($this->getTestAssetPath('invalid'));
+    }
+*/
+    public function testFromString()
+    {
+        $JavaProperties = <<<'ASSET'
+single.line:test
+path:\\test\\with\\slashes
+multiple:line \
+test
+ASSET;
+
+        $arrayJavaProperties = $this->reader->fromString($JavaProperties);
+        
+        $this->assertNotEmpty($arrayJavaProperties);
+        $this->assertEquals($arrayJavaProperties['single.line'], 'test');
+        $this->assertEquals($arrayJavaProperties['path'], '\test\with\slashes');
+        $this->assertEquals($arrayJavaProperties['multiple'], 'line test');
+    }
+/*
+    public function testInvalidString()
+    {
+        $JavaProperties = 'failing|test';
+
+        $this->setExpectedException('Zend\Config\Exception\RuntimeException');
+        $arrayJavaPropterties = $this->reader->fromString($JavaProperties);
+    }
+*/
+}

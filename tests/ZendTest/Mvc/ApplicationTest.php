5c70772642323e8d2f2665d5bb921095568e65ee
Matthew Weier O'Phinney <matthew@zend.com>
2013-07-18 09:41:02 -0500

    [#4831] Fix run() return values
    - Always return the application instance. (and fix docblock to indicate this)
    - If an alternate response object is returned by an event, give it to
      the application instance.

--- tests/ZendTest/Mvc/ApplicationTest.php
+++ tests/ZendTest/Mvc/ApplicationTest.php
@@ -332,14 +332,15 @@ class ApplicationTest extends TestCase
         });
 
         $result = $this->application->run();
-        $this->assertSame($response, $result);
+        $this->assertSame($this->application, $result);
+        $this->assertSame($response, $result->getResponse());
     }
 
     public function testControllerIsDispatchedDuringRun()
     {
         $this->setupPathController();
 
-        $response = $this->application->run();
+        $response = $this->application->run()->getResponse();
         $this->assertContains('PathController', $response->getContent());
         $this->assertContains('dispatch', $response->toString());
     }
@@ -496,7 +497,8 @@ class ApplicationTest extends TestCase
         });
 
         $result = $this->application->run();
-        $this->assertSame($response, $result, var_export($result, 1));
+        $this->assertSame($this->application, $result, get_class($result));
+        $this->assertSame($response, $result->getResponse(), get_class($result));
     }
 
     /**

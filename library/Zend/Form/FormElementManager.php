cf1d95e868c2cc6c34b399f7728028e728f2dd0b
Richard Jennings <rich@trueaxiom.co.uk>
2013-07-19 21:13:07 +0100

    revert setting validator and filter plugin managers on default chains from form manager

--- library/Zend/Form/FormElementManager.php
+++ library/Zend/Form/FormElementManager.php
@@ -89,20 +89,11 @@ class FormElementManager extends AbstractPluginManager
             $factory = $element->getFormFactory();
             $factory->setFormElementManager($this);
 
-            if (!$this->serviceLocator instanceof ServiceLocatorInterface) {
-                return;
-            }
-            if ($this->serviceLocator->has('InputFilterManager')) {
+            if ($this->serviceLocator instanceof ServiceLocatorInterface
+                && $this->serviceLocator->has('InputFilterManager')
+            ) {
                 $inputFilters = $this->serviceLocator->get('InputFilterManager');
                 $factory->getInputFilterFactory()->setInputFilterManager($inputFilters);
-            }
-            if ($this->serviceLocator->has('ValidatorManager')) {
-                $factory->getInputFilterFactory()->getDefaultValidatorChain()
-                    ->setPluginManager($this->serviceLocator->get('ValidatorManager'));
-            }
-            if ($this->serviceLocator->has('FilterManager')) {
-                $factory->getInputFilterFactory()->getDefaultFilterChain()
-                    ->setPluginManager($this->serviceLocator->get('FilterManager'));
             }
         }
     }
@@ -131,5 +122,63 @@ class FormElementManager extends AbstractPluginManager
             'Plugin of type %s is invalid; must implement Zend\Form\ElementInterface',
             (is_object($plugin) ? get_class($plugin) : gettype($plugin))
         ));
+    }
+
+    /**
+     * Retrieve a service from the manager by name
+     *
+     * Allows passing an array of options to use when creating the instance.
+     * createFromInvokable() will use these and pass them to the instance
+     * constructor if not null and a non-empty array.
+     *
+     * @param  string $name
+     * @param  string|array $options
+     * @param  bool $usePeeringServiceManagers
+     * @return object
+     */
+    public function get($name, $options = array(), $usePeeringServiceManagers = true)
+    {
+        if (is_string($options)) {
+            $options = array('name' => $options);
+        }
+        return parent::get($name, $options, $usePeeringServiceManagers);
+    }
+
+    /**
+     * Attempt to create an instance via an invokable class
+     *
+     * Overrides parent implementation by passing $creationOptions to the
+     * constructor, if non-null.
+     *
+     * @param  string $canonicalName
+     * @param  string $requestedName
+     * @return null|\stdClass
+     * @throws Exception\ServiceNotCreatedException If resolved class does not exist
+     */
+    protected function createFromInvokable($canonicalName, $requestedName)
+    {
+        $invokable = $this->invokableClasses[$canonicalName];
+
+        if (null === $this->creationOptions
+            || (is_array($this->creationOptions) && empty($this->creationOptions))
+        ) {
+            $instance = new $invokable();
+        } else {
+            if (isset($this->creationOptions['name'])) {
+                $name = $this->creationOptions['name'];
+            } else {
+                $name = $requestedName;
+            }
+
+            if (isset($this->creationOptions['options'])) {
+                $options = $this->creationOptions['options'];
+            } else {
+                $options = $this->creationOptions;
+            }
+
+            $instance = new $invokable($name, $options);
+        }
+
+        return $instance;
     }
 }

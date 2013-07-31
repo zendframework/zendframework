45e87eb5f8e22bb46027e1b3127be9ce7a77bd62
tux-rampage <staff@pro-unreal.de>
2013-07-31 18:49:35 +0200

    Changed runtime definition to exclude methods without parameters from Aware interfaces

--- library/Zend/Di/Definition/RuntimeDefinition.php
+++ library/Zend/Di/Definition/RuntimeDefinition.php
@@ -302,8 +302,9 @@ class RuntimeDefinition implements DefinitionInterface
                 preg_match($interfaceInjectorPattern, $rIface->getName(), $matches);
                 if ($matches) {
                     foreach ($rIface->getMethods() as $rMethod) {
-                        if ($rMethod->getName() === '__construct') {
+                        if (($rMethod->getName() === '__construct') || (count($rMethod->getParameters()) < 1)) {
                             // constructor not allowed in interfaces
+                            // Don't call interface methods without a parameter (Some aware interfaces define setters in ZF2)
                             continue;
                         }
                         $def['methods'][$rMethod->getName()] = Di::METHOD_IS_AWARE;
---
+++ tests/ZendTest/Di/TestAsset/AwareClasses/B.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Di
+ */
+
+namespace ZendTest\Di\TestAsset\AwareClasses;
+
+class B implements NoParamsAwareInterface
+{
+    /**
+     * @see \ZendTest\Di\TestAsset\AwareClasses\NoParamsAwareInterface::getSomething()
+     */
+    public function getSomething()
+    {
+    }
+
+    /**
+     * @see \ZendTest\Di\TestAsset\AwareClasses\NoParamsAwareInterface::setSomething()
+     */
+    public function setSomething(A $something)
+    {
+    }
+}

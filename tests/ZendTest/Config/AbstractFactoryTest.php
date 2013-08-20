9f0d08777fb4d417b5b0054d6f5564c5e854af78
Chris Raidler <chris@raidler.com>
2013-08-20 17:47:35 +0200

    added call to attachDefaultListeners if method has been defined

---
+++ tests/ZendTest/Config/AbstractFactoryTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * AbstractFactoryTest.php
+ *
+ * @author Chris Raidler <chris@raidler.com>
+ * @copyright Copyright 2012 - 2013, raidler dot com
+ */
+namespace ZendTest\Config;
+
+use Zend\Config\AbstractConfigFactory;
+use Zend\Config\Config;
+use Zend\Mvc\Service\ServiceManagerConfig;
+use Zend\ServiceManager\ServiceManager;
+
+/**
+ * Class AbstractFactoryTest
+ */
+class AbstractFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Zend\Mvc\Application
+     */
+    protected $application;
+
+    /**
+     * @var \Zend\ServiceManager\ServiceManager
+     */
+    protected $serviceManager;
+
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        $config = array(
+            'MyModule\Config' => array(
+                'foo' => array(
+                    'bar'
+                )
+            )
+        );
+
+        $appConfig = array(
+            'modules' => array(),
+            'module_listener_options' => array(
+                'config_cache_enabled' => false,
+                'cache_dir'            => 'data/cache',
+                'module_paths'         => array(),
+            ),
+        );
+
+        $sm = $this->serviceManager = new ServiceManager(
+            new ServiceManagerConfig(array(
+                'abstract_factories' => array(
+                    'Zend\Config\AbstractConfigFactory',
+                )
+            ))
+        );
+
+        $sm->setService('ApplicationConfig', $appConfig);
+        $sm->setService('Config', $config);
+    }
+
+    public function testCanCreateService()
+    {
+        $sm = $this->serviceManager;
+        $factory = new AbstractConfigFactory();
+
+        $this->assertFalse($factory->canCreateServiceWithName($sm, 'mymodulefail', 'MyModule\Fail'));
+        $this->assertTrue($factory->canCreateServiceWithName($sm, 'mymoduleconfig', 'MyModule\Config'));
+    }
+
+    /**
+     * @depends testCanCreateService
+     */
+    public function testCreateService()
+    {
+        $sm = $this->serviceManager;
+        $this->assertInstanceOf('Zend\Config\Config', $sm->get('MyModule\Config'));
+    }
+
+    /**
+     * @depends testCreateService
+     */
+    public function testServiceManager()
+    {
+        $sm = $this->serviceManager;
+        $this->assertInstanceOf('Zend\Config\Config', $sm->get('MyModule\Config'));
+    }
+}

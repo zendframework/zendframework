14109f04837f541ff1a97506ee0028b0bfd2e13a
Clemens Sahs <clemens.sahs@slides-worker.org>
2013-09-06 10:06:22 +0200

    add missing linebreak in test

--- tests/ZendTest/Http/Header/SetCookieTest.php
+++ tests/ZendTest/Http/Header/SetCookieTest.php
@@ -155,6 +155,7 @@ class SetCookieTest extends \PHPUnit_Framework_TestCase
         $target = 'Set-Cookie: myname="myvalue"; Expires=Wed, 13-Jan-2021 22:23:01 GMT;'
             . ' Domain=docs.foo.com; Path=/accounts;'
             . ' Secure; HttpOnly';
+        $target.= "\n";
         $target.= 'Set-Cookie: othername="othervalue"';
         $this->assertEquals($target, $headerLine);
     }

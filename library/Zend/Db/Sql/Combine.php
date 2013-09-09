391172b547a51d3d72e0e91ac0a92ea55825b055
turrsis <turrsis@gmail.com>
2013-09-09 18:41:36 +0300

    implement multiple combine statement

---
+++ library/Zend/Db/Sql/Combine.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Db\Sql;
+
+use Zend\Db\Adapter\Platform\PlatformInterface;
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\Adapter\Driver\DriverInterface;
+use Zend\Db\Adapter\StatementContainerInterface;
+use Zend\Db\Adapter\Platform\Sql92 as AdapterSql92Platform;
+use Zend\Db\Adapter\ParameterContainer;
+
+class Combine extends AbstractSql implements SqlInterface, PreparableSqlInterface
+{
+    const COMBINE_UNION = 'union';
+    const COMBINE_EXCEPT = 'except';
+    const COMBINE_INTERSECT = 'intersect';
+
+    protected $combine = array();
+
+    const SPECIFICATION_COMBINE = '%1$s (%2$s) ';
+
+    /**
+     * Create combine clause
+     *
+     * @param Select $select
+     * @param string $type
+     * @param string $modifier
+     * @return self
+     */
+    public function combine($select, $type = self::COMBINE_UNION, $modifier = '')
+    {
+        if (is_array($select)) {
+            foreach($select as $combine) {
+                if ($combine instanceof Select) {
+                    $combine = array($combine);
+                }
+                $this->combine(
+                    $combine[0],
+                    isset($combine[1]) ? $combine[1] : $type,
+                    isset($combine[2]) ? $combine[2] : $modifier
+                );
+            }
+            return $this;
+        }
+        if (!$select instanceof Select) {
+            throw new Exception\InvalidArgumentException('$select must be a array or instance of Select.');
+        }
+        $this->combine[] = array(
+            'select' => $select,
+            'type' => $type,
+            'modifier' => $modifier
+        );
+        return $this;
+    }
+
+    /**
+     * Create union clause
+     *
+     * @param Select $select
+     * @param string $modifier
+     * @return self
+     */
+    public function union(Select $select, $modifier = '')
+    {
+        return $this->combine($select, self::COMBINE_UNION, $modifier);
+    }
+
+    /**
+     * Create except clause
+     *
+     * @param Select $select
+     * @param string $modifier
+     * @return self
+     */
+    public function except(Select $select, $modifier = '')
+    {
+        return $this->combine($select, self::COMBINE_EXCEPT, $modifier);
+    }
+
+    /**
+     * Create intersect clause
+     *
+     * @param Select $select
+     * @param string $modifier
+     * @return self
+     */
+    public function intersect(Select $select, $modifier = '')
+    {
+        return $this->combine($select, self::COMBINE_INTERSECT, $modifier);
+    }
+
+    public function getSqlString(PlatformInterface $adapterPlatform = null)
+    {
+        $adapterPlatform = ($adapterPlatform) ?: new AdapterSql92Platform;
+        return $this->buildSqlString($adapterPlatform);
+    }
+
+    public function prepareStatement(AdapterInterface $adapter, StatementContainerInterface $statementContainer = null)
+    {
+        $statementContainer = ($statementContainer) ?: $adapter->getDriver()->createStatement();
+        $parameterContainer = $statementContainer->getParameterContainer();
+
+        if (!$parameterContainer instanceof ParameterContainer) {
+            $parameterContainer = new ParameterContainer();
+            $statementContainer->setParameterContainer($parameterContainer);
+        }
+
+        $sql = $this->buildSqlString($adapter->getPlatform(), $adapter->getDriver(), $parameterContainer);
+        return $statementContainer->setSql($sql);
+    }
+
+    protected function buildSqlString(PlatformInterface $platform, DriverInterface $driver = null, ParameterContainer $parameterContainer = null)
+    {
+        if (!$this->combine) {
+            return null;
+        }
+
+        $sql = '';
+        foreach ($this->combine as $i => $combine) {
+            $type = $i == 0
+                    ? ''
+                    : strtoupper($combine['type'] . ($combine['modifier'] ? ' ' . $combine['modifier'] : ''));
+            $select = $this->processSubSelect($combine['select'], $platform, $driver, $parameterContainer);
+            $sql .= sprintf(
+                self::SPECIFICATION_COMBINE,
+                $type,
+                $select
+            );
+        }
+        return trim($sql, ' ');
+    }
+
+    public function getRawState()
+    {
+        return $this->combine;
+    }
+}
---
+++ tests/ZendTest/Db/Sql/CombineTest.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Db\Sql;
+
+use Zend\Db\Sql\Combine;
+use Zend\Db\Sql\Select;
+
+class CombineTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Combine
+     */
+    protected $combine;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->combine = new Combine;
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    public function testGetSqlString()
+    {
+        $this->combine
+                ->union(new Select('t1'))
+                ->intersect(new Select('t2'))
+                ->except(new Select('t3'))
+                ->union(new Select('t4'));
+
+        $this->assertEquals(
+            '(SELECT "t1".* FROM "t1") INTERSECT (SELECT "t2".* FROM "t2") EXCEPT (SELECT "t3".* FROM "t3") UNION (SELECT "t4".* FROM "t4")',
+            $this->combine->getSqlString()
+        );
+    }
+
+    public function testGetSqlStringWithModifier()
+    {
+        $this->combine
+                ->union(new Select('t1'))
+                ->union(new Select('t2'), 'ALL');
+
+        $this->assertEquals(
+            '(SELECT "t1".* FROM "t1") UNION ALL (SELECT "t2".* FROM "t2")',
+            $this->combine->getSqlString()
+        );
+    }
+
+    public function testGetSqlStringFromArray()
+    {
+        $this->combine->combine(array(
+            array(new Select('t1')),
+            array(new Select('t2'), Combine::COMBINE_INTERSECT, 'ALL'),
+            array(new Select('t3'), Combine::COMBINE_EXCEPT),
+        ));
+
+        $this->assertEquals(
+            '(SELECT "t1".* FROM "t1") INTERSECT ALL (SELECT "t2".* FROM "t2") EXCEPT (SELECT "t3".* FROM "t3")',
+            $this->combine->getSqlString()
+        );
+
+        $this->combine = new Combine();
+        $this->combine->combine(array(
+            new Select('t1'),
+            new Select('t2'),
+            new Select('t3'),
+        ));
+
+        $this->assertEquals(
+            '(SELECT "t1".* FROM "t1") UNION (SELECT "t2".* FROM "t2") UNION (SELECT "t3".* FROM "t3")',
+            $this->combine->getSqlString()
+        );
+    }
+
+    public function testGetSqlStringEmpty()
+    {
+        $this->assertSame(
+            null,
+            $this->combine->getSqlString()
+        );
+    }
+
+    public function testPrepareStatementWithModifier()
+    {
+        $select1 = new Select('t1');
+        $select1->where(array('x1'=>10));
+        $select2 = new Select('t2');
+        $select2->where(array('x2'=>20));
+
+        $this->combine->combine(array(
+            $select1,
+            $select2
+        ));
+
+        $adapter = new \Zend\Db\Adapter\Adapter(array('driver'=>'mysqli'));
+
+        $statement = $this->combine->prepareStatement($adapter);
+        $this->assertInstanceOf('Zend\Db\Adapter\StatementContainerInterface', $statement);
+        $this->assertEquals(
+            "(SELECT `t1`.* FROM `t1` WHERE `x1` = ?) UNION (SELECT `t2`.* FROM `t2` WHERE `x2` = ?)",
+            $statement->getSql()
+        );
+    }
+
+    public function testGetRawState()
+    {
+        $select = new Select('t1');
+        $this->combine->combine($select);
+        $this->assertSame(
+            array(array(
+                'select'   => $select,
+                'type'     => Combine::COMBINE_UNION,
+                'modifier' => ''
+            ),),
+            $this->combine->getRawState()
+        );
+    }
+}

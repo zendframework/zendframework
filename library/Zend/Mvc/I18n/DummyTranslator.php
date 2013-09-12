e5e7e65cecc62b31a8c2ebf255b971e7689921e8
Ben Scholzen <mail@dasprids.de>
2013-09-12 19:25:26 +0200

    Use dummy translator when no translator is configured

---
+++ library/Zend/Mvc/I18n/DummyTranslator.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Mvc\I18n;
+
+use Zend\Validator\Translator\TranslatorInterface as ValidatorTranslatorInterface;
+
+class DummyTranslator implements ValidatorTranslatorInterface
+{
+    /**
+     * translate(): defined by ValidatorTranslatorInterface()
+     *
+     * @see    ValidatorTranslatorInterface::translate()
+     * @param  string $message
+     * @param  string $textDomain
+     * @param  string $locale
+     * @return string
+     */
+    public function translate($message, $textDomain = 'default', $locale = null)
+    {
+        return $message;
+    }
+}

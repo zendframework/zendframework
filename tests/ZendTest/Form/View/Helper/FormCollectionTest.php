3272364f262b6ae3cd46a2015c544f9362ab1c38
dpommeranz <dpommeranz@gmx.de>
2013-10-07 18:12:59 +0200

    collections should be rendered in fieldsets if shouldWrap is set to true even if no label (legend) is given.

--- tests/ZendTest/Form/View/Helper/FormCollectionTest.php
+++ tests/ZendTest/Form/View/Helper/FormCollectionTest.php
@@ -176,4 +176,15 @@ class FormCollectionTest extends TestCase
 
         $this->assertContains('>translated legend<', $markup);
     }
+
+    public function testShouldWrapWithoutLabel()
+    {
+        $form = $this->getForm();
+        $collection = $form->get('colors');
+        $collection->setLabel('');
+        $this->helper->setShouldWrap(true);
+
+        $markup = $this->helper->render($collection);
+        $this->assertContains('<fieldset>', $markup);
+    }
 }

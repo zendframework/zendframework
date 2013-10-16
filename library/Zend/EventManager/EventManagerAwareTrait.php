7be61621af3a35f317f5025ecb7d70b1d54ac01c
Evan Coury <me@evancoury.com>
2013-10-16 11:55:59 -0700

    Deprecate ProvidesEvents trait.
    This trait was added before we had a naming convention for traits, and
    has since been replaced by EventManagerAwareTrait. To help people
    understand this, we should have the actual code in the correct trait,
    and proxy the old one to the new one with a comment explaining what's
    going on.

--- library/Zend/EventManager/EventManagerAwareTrait.php
+++ library/Zend/EventManager/EventManagerAwareTrait.php
@@ -13,5 +13,51 @@ use \Zend\EventManager\ProvidesEvents;
 
 trait EventManagerAwareTrait
 {
-    use ProvidesEvents;
+    /**
+     * @var EventManagerInterface
+     */
+    protected $events;
+
+    /**
+     * Set the event manager instance used by this context
+     *
+     * @param  EventManagerInterface $events
+     * @return mixed
+     */
+    public function setEventManager(EventManagerInterface $events)
+    {
+        $identifiers = array(__CLASS__, get_class($this));
+        if (isset($this->eventIdentifier)) {
+            if ((is_string($this->eventIdentifier))
+                || (is_array($this->eventIdentifier))
+                || ($this->eventIdentifier instanceof Traversable)
+            ) {
+                $identifiers = array_unique(array_merge($identifiers, (array) $this->eventIdentifier));
+            } elseif (is_object($this->eventIdentifier)) {
+                $identifiers[] = $this->eventIdentifier;
+            }
+            // silently ignore invalid eventIdentifier types
+        }
+        $events->setIdentifiers($identifiers);
+        $this->events = $events;
+        if (method_exists($this, 'attachDefaultListeners')) {
+            $this->attachDefaultListeners();
+        }
+        return $this;
+    }
+
+    /**
+     * Retrieve the event manager
+     *
+     * Lazy-loads an EventManager instance if none registered.
+     *
+     * @return EventManagerInterface
+     */
+    public function getEventManager()
+    {
+        if (!$this->events instanceof EventManagerInterface) {
+            $this->setEventManager(new EventManager());
+        }
+        return $this->events;
+    }
 }

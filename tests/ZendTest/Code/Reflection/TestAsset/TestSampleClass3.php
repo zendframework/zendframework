6636d17ba5f8a7fc828366701f2793f0b0c331c6
Tom Oram <tom@scl.co.uk>
2013-10-25 23:11:42 +0100

    Oops, paste error

--- tests/ZendTest/Code/Reflection/TestAsset/TestSampleClass3.php
+++ tests/ZendTest/Code/Reflection/TestAsset/TestSampleClass3.php
@@ -1,6 +1,6 @@
 <?php
+/**
  * Zend Framework (http://framework.zend.com/)
- <?php echo $this->partial('application/stats/engines/Generic_Stats.phtml', ['stats' => $this->stats]); ?>
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
  * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)

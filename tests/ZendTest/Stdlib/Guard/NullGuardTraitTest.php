b393e2f80daa1f30386d0cae0b9de0ab4bd0d20d
George Cooksey <texdc3@gmail.com>
2013-10-27 17:05:20 -0500

    Add Guard Traits
    The GuardUtils can be used now.  The traits are for php >= 5.4 only.

---
+++ tests/ZendTest/Stdlib/Guard/NullGuardTraitTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Stdlib\Guard;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use ZendTest\Stdlib\TestAsset\GuardedObject;
+
+/**
+ * @requires PHP 5.4
+ * @group    Zend_StdLib_Guard
+ * @covers   Zend\Stdlib\Guard\NullGuardTrait
+ */
+class NullGuardTraitTest extends TestCase
+{
+
+    public function setUp()
+    {
+        if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+            $this->markTestSkipped('Only valid for php >= 5.4');
+        }
+    }
+
+    public function testGuardAgainstNullThrowsException()
+    {
+        $object = new GuardedObject;
+        $this->setExpectedException(
+            'Zend\Stdlib\Exception\InvalidArgumentException',
+            'Argument cannot be null'
+        );
+        $object->setNotNull(null);
+    }
+
+    public function testGuardAgainstNullAllowsNonNull()
+    {
+        $object = new GuardedObject;
+        $this->assertNull($object->setNotNull('foo'));
+    }
+
+}

51a8b82c1d3e0c16eb01ae690858af45fb058011
Maximaximum <maximaximums@gmail.com>
2013-10-29 10:56:18 +0200

    Create Zend_Captcha.php

---
+++ resources/languages/uk/Zend_Captcha.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright  Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * EN-Revision: 30.Jul.2011
+ */
+return array(
+    // Zend\Captcha\ReCaptcha
+    "Missing captcha fields" => "Бракує полів captcha",
+    "Failed to validate captcha" => "Captcha не пройшла перевірку",
+    "Captcha value is wrong: %value%" => "Неправильне значення captcha: %value%",
+
+    // Zend\Captcha\Word
+    "Empty captcha value" => "Порожнє значення captcha",
+    "Captcha ID field is missing" => "Бракує поля captcha ID",
+    "Captcha value is wrong" => "Неправильне значення captcha",
+);

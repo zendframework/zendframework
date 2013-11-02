6c5a0857486a7cc66575590ce89ab1062f2b8ed6
jkavalik <jkavalik@gmail.com>
2013-11-02 23:39:35 +0100

    test for using logger default errorhandler with streamwriter

--- tests/ZendTest/Log/LoggerTest.php
+++ tests/ZendTest/Log/LoggerTest.php
@@ -14,6 +14,7 @@ use ErrorException;
 use Zend\Log\Logger;
 use Zend\Log\Processor\Backtrace;
 use Zend\Log\Writer\Mock as MockWriter;
+use Zend\Log\Writer\Stream as StreamWriter;
 use Zend\Log\Filter\Mock as MockFilter;
 use Zend\Stdlib\SplPriorityQueue;
 use Zend\Validator\Digits as DigitsFilter;
@@ -384,5 +385,27 @@ class LoggerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf('Zend\Log\Logger', $logger->info('Hi', array('extra' => '')));
         fclose($stream);
+    }
+
+    public function testErrorHandlerWithStreamWriter()
+    {
+        $options = array(
+                         'errorhandler' => true,
+                   );
+        $logger = new Logger($options);
+        $stream = fopen('php://memory', 'w+');
+        $streamWriter = new StreamWriter($stream);
+        // error handler does not like this feature so turn it off
+        $streamWriter->setConvertWriteErrorsToExceptions(false);
+        $logger->addWriter($streamWriter);
+
+        // we raise two notices - both should be logged
+        echo $test;
+        echo $second;
+
+        rewind($stream);
+        $contents = stream_get_contents($stream);
+        $this->assertContains('test', $contents);
+        $this->assertContains('second', $contents);
     }
 }

c35dbd99935be4d60b4ca198601e8454d90fd293
scribles <scribles@scribles.com>
2013-11-09 02:43:21 -0800

    fix formatting

--- tests/ZendTest/Db/Adapter/Driver/Pdo/ResultTest.php
+++ tests/ZendTest/Db/Adapter/Driver/Pdo/ResultTest.php
@@ -9,12 +9,12 @@ class ResultTest extends \PHPUnit_Framework_TestCase
     {
         $stub = $this->getMock('PDOStatement');
         $stub
-	    ->expects($this->any())
-	    ->method('fetch')
-	    ->will($this->returnCallback(function() {return uniqid();}));
+            ->expects($this->any())
+            ->method('fetch')
+            ->will($this->returnCallback(function () {return uniqid();}));
 
-	$result = new Result();
-	$result->initialize($stub, null);
+        $result = new Result();
+        $result->initialize($stub, null);
 
         $this->assertEquals($result->current(), $result->current());
     }

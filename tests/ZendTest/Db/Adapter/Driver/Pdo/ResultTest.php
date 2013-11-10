8522ba6736c60ff457576c3776ad590448248332
scribles <scribles@scribles.com>
2013-11-09 17:46:00 -0800

    Add license and comments

--- tests/ZendTest/Db/Adapter/Driver/Pdo/ResultTest.php
+++ tests/ZendTest/Db/Adapter/Driver/Pdo/ResultTest.php
@@ -1,10 +1,23 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
 namespace ZendTest\Db\Adapter\Driver\Pdo;
 
 use Zend\Db\Adapter\Driver\Pdo\Result;
 
 class ResultTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Tests current method returns same data on consecutive calls.
+     *
+     * @covers Zend\Db\Adapter\Driver\Pdo\Result::current
+     */
     public function testCurrent()
     {
         $stub = $this->getMock('PDOStatement');

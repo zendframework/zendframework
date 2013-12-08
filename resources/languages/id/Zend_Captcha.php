06bc7a199670b9592c4d3bfebe9ffc4aa993ee36
Abdul Malik Ikhsan <samsonasik@gmail.com>
2013-12-08 15:32:26 +0700

    Added resources/languages/id for Indonesian translation

---
+++ resources/languages/id/Zend_Captcha.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright  Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * EN-Revision: 30.Jul.2011
+ */
+return array(
+    // Zend\Captcha\ReCaptcha
+    "Missing captcha fields" => "Tidak ada bidang captcha",
+    "Failed to validate captcha" => "Gagal memvalidasi captcha",
+    "Captcha value is wrong: %value%" => "Nilai captcha salah: %value%",
+
+    // Zend\Captcha\Word
+    "Empty captcha value" => "Nilai captcha kosong",
+    "Captcha ID field is missing" => "Bidang Captcha ID tidak ada",
+    "Captcha value is wrong" => "Nilai Captcha salah",
+);

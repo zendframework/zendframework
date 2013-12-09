970ca280ba651256aaa1ba2f4a2276e7d71c0c3b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2013-12-09 18:26:21 +0700

    change mode from "r" to "r+"

--- tests/ZendTest/Log/LoggerTest.php
+++ tests/ZendTest/Log/LoggerTest.php
@@ -369,7 +369,7 @@ class LoggerTest extends \PHPUnit_Framework_TestCase
 
     public function testLogExtraArrayKeyWithNonArrayValue()
     {
-        $stream = fopen("php://memory", "r");
+        $stream = fopen("php://memory", "r+");
         $options = array(
             'writers' => array(
                 array(

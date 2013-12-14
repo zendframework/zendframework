02cafd24e68c36af72966fc74fee29c5c034dccb
Abdul Malik Ikhsan <samsonasik@gmail.com>
2013-12-14 23:54:06 +0700

    make 'listeners' key can be configured outside application.config.php because in ZF2 Skeleton App use : Zend\Mvc\Application::init(require 'config/application.config.php')

--- library/Zend/Mvc/Application.php
+++ library/Zend/Mvc/Application.php
@@ -247,10 +247,13 @@ class Application implements
     public static function init($configuration = array())
     {
         $smConfig = isset($configuration['service_manager']) ? $configuration['service_manager'] : array();
-        $listeners = isset($configuration['listeners']) ? $configuration['listeners'] : array();
         $serviceManager = new ServiceManager(new Service\ServiceManagerConfig($smConfig));
         $serviceManager->setService('ApplicationConfig', $configuration);
         $serviceManager->get('ModuleManager')->loadModules();
+
+        $config    = $this->getConfig();
+        $listeners = isset($config['listeners']) ? $config['listeners'] : array();
+
         return $serviceManager->get('Application')->bootstrap($listeners);
     }
 

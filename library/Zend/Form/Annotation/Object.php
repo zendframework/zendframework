ca59a6cc26b5289f258e90880cf20649d8335450
Ralph Schindler <ralph.schindler@zend.com>
2014-01-02 11:31:48 -0600

    Back-port of copyright changes to master
    Diff based on PR #5654 (git diff a961e19..fd935cf), applied skipping unmatched files

--- library/Zend/Form/Annotation/Object.php
+++ library/Zend/Form/Annotation/Object.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright  Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
@@ -16,7 +16,7 @@ namespace Zend\Form\Annotation;
  * Use this annotation to specify an object to use as the bound object of a form or fieldset
  *
  * @Annotation
- * @copyright  Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Object extends AbstractStringAnnotation
--- resources/languages/de/Zend_Validate.php
+++ resources/languages/de/Zend_Validate.php
@@ -10,7 +10,7 @@
  * obtain it through the world-wide-web, please send an email
  * to license@zend.com so we can send you a copy immediately.
  *
- * @copyright  Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright  Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 

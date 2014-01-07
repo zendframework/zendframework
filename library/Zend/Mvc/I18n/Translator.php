1b49fce554f390656db231efa523c956603228bb
Matthew Weier O'Phinney <matthew@zend.com>
2014-01-07 15:53:33 -0600

    Make MVC translator a proxy
    - Added `__call()` implementation to proxy unknown method calls to the
      underlying translator instance.
    - Added a `getTranslator()` method to allow direct access to the
      underlying translator instance.
    - Added unit tests for the above functionality.

--- library/Zend/Mvc/I18n/Translator.php
+++ library/Zend/Mvc/I18n/Translator.php
@@ -10,6 +10,7 @@
 namespace Zend\Mvc\I18n;
 
 use Zend\I18n\Translator\TranslatorInterface as I18nTranslatorInterface;
+use Zend\Mvc\Exception;
 use Zend\Validator\Translator\TranslatorInterface as ValidatorTranslatorInterface;
 
 class Translator implements
@@ -29,11 +30,59 @@ class Translator implements
         $this->translator = $translator;
     }
 
+    /**
+     * Proxy unknown method calls to underlying translator instance
+     *
+     * Note: this method is only implemented to keep backwards compatibility
+     * with pre-2.3.0 code.
+     *
+     * @deprecated
+     * @param string $method 
+     * @param array $args 
+     * @return mixed
+     */
+    public function __call($method, array $args)
+    {
+        if (!method_exists($this->translator, $method)) {
+            throw new Exception\BadMethodCallException(sprintf(
+                'Unable to call method "%s"; does not exist in translator',
+                $method
+            ));
+        }
+        return call_user_func_array(array($this->translator, $method), $args);
+    }
+
+    /**
+     * @return I18nTranslatorInterface
+     */
+    public function getTranslator()
+    {
+        return $this->translator;
+    }
+
+    /**
+     * Translate a message using the given text domain and locale
+     * 
+     * @param string $message 
+     * @param string $textDomain 
+     * @param string $locale 
+     * @return string
+     */
     public function translate($message, $textDomain = 'default', $locale = null)
     {
         return $this->translator->translate($message, $textDomain, $locale);
     }
 
+    /**
+     * Provide a pluralized translation of the given string using the given text domain and locale
+     * 
+     * @param string $singular 
+     * @param string $plural 
+     * @param string $number 
+     * @param string $textDomain 
+     * @param string $locale 
+     * @return string
+     */
     public function translatePlural($singular, $plural, $number, $textDomain = 'default', $locale = null)
     {
         return $this->translator->translatePlural($singular, $plural, $number, $textDomain, $locale);

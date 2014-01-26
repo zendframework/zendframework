0136e2152b68f0008aa85e7d5e17e393fcaf3ba5
Roumen Damianoff <roumen@dawebs.com>
2014-01-26 22:05:42 +0200

    Updated Bulgarian translation

--- resources/languages/bg/Zend_Captcha.php
+++ resources/languages/bg/Zend_Captcha.php
@@ -11,12 +11,12 @@
  * EN-Revision: 30.Jul.2011
  */
 return array(
-    // Zend_Captcha_ReCaptcha
+    // Zend\Captcha\ReCaptcha
     "Missing captcha fields" => "Непопълнена стойност на капча",
     "Failed to validate captcha" => "Не може да валидира капча",
     "Captcha value is wrong: %value%" => "Стойността на капча е грешна: %value%",
 
-    // Zend_Captcha_Word
+    // Zend\Captcha\Word
     "Empty captcha value" => "Непопълнена стойност на капча",
     "Captcha ID field is missing" => "Липсващо капча ID поле",
     "Captcha value is wrong" => "Стойността на капча е грешна",

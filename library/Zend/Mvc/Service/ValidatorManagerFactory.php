84b66d5fd4532906970805ca434000d3f79ef619
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-01-29 22:58:28 +0700

    remove unneeded createService functions because actually do same with base class

--- library/Zend/Mvc/Service/ValidatorManagerFactory.php
+++ library/Zend/Mvc/Service/ValidatorManagerFactory.php
@@ -9,21 +9,7 @@
 
 namespace Zend\Mvc\Service;
 
-use Zend\ServiceManager\ServiceLocatorInterface;
-
 class ValidatorManagerFactory extends AbstractPluginManagerFactory
 {
     const PLUGIN_MANAGER_CLASS = 'Zend\Validator\ValidatorPluginManager';
-
-    /**
-     * Create and return the validator plugin manager
-     *
-     * @param  ServiceLocatorInterface $serviceLocator
-     * @return \Zend\Validator\ValidatorPluginManager
-     */
-    public function createService(ServiceLocatorInterface $serviceLocator)
-    {
-        $plugins = parent::createService($serviceLocator);
-        return $plugins;
-    }
 }

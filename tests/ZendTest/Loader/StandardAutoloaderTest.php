70cd8a57e71b399896adc3b7bb01b4752e79bf69
Matthew Weier O'Phinney <matthew@zend.com>
2014-02-04 16:51:59 -0600

    [#5783] Assertion on return value
    - per @Ocramius

--- tests/ZendTest/Loader/StandardAutoloaderTest.php
+++ tests/ZendTest/Loader/StandardAutoloaderTest.php
@@ -195,7 +195,8 @@ class StandardAutoloaderTest extends \PHPUnit_Framework_TestCase
         $loader = new StandardAutoloader();
         $loader->registerNamespace('ZendTest\Loader\TestAsset\Parent', __DIR__ . '/TestAsset/Parent');
         $loader->registerNamespace('ZendTest\Loader\TestAsset\Parent\Child', __DIR__ . '/TestAsset/Child');
-        $loader->autoload('ZendTest\Loader\TestAsset\Parent\Child\Subclass');
+        $result = $loader->autoload('ZendTest\Loader\TestAsset\Parent\Child\Subclass');
+        $this->assertTrue($result !== false);
         $this->assertTrue(class_exists('ZendTest\Loader\TestAsset\Parent\Child\Subclass', false));
     }
 }

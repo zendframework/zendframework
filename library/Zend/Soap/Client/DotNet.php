90e7a7d93644d080c95d6561981bcfa4b4a3d755
Arnaud LEMAIRE <alemaire@cap-tic.fr>
2014-02-13 02:20:21 +0200

    [FIX] return null value

--- library/Zend/Soap/Client/DotNet.php
+++ library/Zend/Soap/Client/DotNet.php
@@ -216,7 +216,7 @@ class DotNet extends SOAPClient
     protected function _preProcessResult($result)
     {
         $resultProperty = $this->getLastMethod() . 'Result';
-        if (isset($result->$resultProperty)) {
+        if (property_exists($result, $resultProperty)) {
             return $result->$resultProperty;
         }
         return $result;

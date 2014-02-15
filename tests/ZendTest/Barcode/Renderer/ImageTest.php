36d07666455d1a4a08cbd6d40414800bb98132e6
Steve Rhoades <sedonami@gmail.com>
2014-02-15 14:36:26 -0800

    Resolves #4708 - remove method used for testing, different solution chosen.

--- tests/ZendTest/Barcode/Renderer/ImageTest.php
+++ tests/ZendTest/Barcode/Renderer/ImageTest.php
@@ -354,23 +354,4 @@ class ImageTest extends TestCommon
             $this->markTestSkipped('TTF (FreeType) support is required in order to run this test');
         }
     }
-
-    protected function check_transparent($im) {
-
-    $width = imagesx($im); // Get the width of the image
-    $height = imagesy($im); // Get the height of the image
-
-    // We run the image pixel by pixel and as soon as we find a transparent pixel we stop and return true.
-    for($i = 0; $i < $width; $i++) {
-        for($j = 0; $j < $height; $j++) {
-            $rgba = imagecolorat($im, $i, $j);
-            if(($rgba & 0x7F000000) >> 24) {
-                return true;
-            }
-        }
-    }
-
-    // If we dont find any pixel the function will return false.
-    return false;
-}
 }

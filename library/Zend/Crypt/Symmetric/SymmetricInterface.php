0a35cbdaf9e2127a818037ff5482095ce275c33e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-02-25 04:23:12 +0700

    feedback on Crypt : no @return at interface

--- library/Zend/Crypt/Symmetric/SymmetricInterface.php
+++ library/Zend/Crypt/Symmetric/SymmetricInterface.php
@@ -13,82 +13,49 @@ interface SymmetricInterface
 {
     /**
      * @param string $data
-     * @return string
      */
     public function encrypt($data);
     
     /**
      * @param string $data
-     * @return string
      */
     public function decrypt($data);
 
     /**
      * @param string $key
-     * @return Mcrypt
      */
     public function setKey($key);
     
-    /**
-     * @return null|string
-     */
     public function getKey();
 
-    /**
-     * @return int
-     */
     public function getKeySize();
 
-    /**
-     * @return string
-     */
     public function getAlgorithm();
 
     /**
      * @param  string $algo 
-     * @return Mcrypt
      */
     public function setAlgorithm($algo);
 
-    /**
-     * @return string
-     */
     public function getSupportedAlgorithms();
 
     /**
      * @param string|false $salt
-     * @return Mcrypt
      */
     public function setSalt($salt);
     
-    /**
-     * @return null|string
-     */
     public function getSalt();
 
-    /**
-     * @return int
-     */
     public function getSaltSize();
 
-    /**
-     * @return int
-     */
     public function getBlockSize();
 
     /**
      * @param string $mode
-     * @return Mcrypt
      */
     public function setMode($mode);
 
-    /**
-     * @return string
-     */
     public function getMode();
 
-    /**
-     * @return string
-     */
     public function getSupportedModes();
 }

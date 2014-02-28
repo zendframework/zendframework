877b72bdc44cb5a31805fa2b83c351dfb54ee2cf
Michael Schiller <michael.schiller@check24.de>
2014-02-28 13:52:50 +0100

    [BUGFIX] DI fails with CompilerDefinition; solves #5738 - Test - assertions on false added

--- tests/ZendTest/Di/Definition/CompilerDefinitionTest.php
+++ tests/ZendTest/Di/Definition/CompilerDefinitionTest.php
@@ -139,8 +139,18 @@ class CompilerDefinitionTest extends TestCase
         $definition->addDirectory(__DIR__ . '/../TestAsset/SetterInjection');
         $definition->addDirectory(__DIR__ . '/../TestAsset/CompilerClasses');
         $definition->compile();
+        
+        // constructor injection
         $this->assertTrue($definition->hasMethodParameters('ZendTest\Di\TestAsset\ConstructorInjection\B', '__construct'));
+        // setter injection
         $this->assertTrue($definition->hasMethodParameters('ZendTest\Di\TestAsset\SetterInjection\B', 'setA'));
+        // setter injection with method from derived class
         $this->assertTrue($definition->hasMethodParameters('ZendTest\Di\TestAsset\CompilerClasses\D', 'setB'));
+        // class does not exist
+        $this->assertFalse($definition->hasMethodParameters('ZendTest\Di\TestAsset\ConstructorInjection\BB', '__construct'));
+        // method not existing
+        $this->assertFalse($definition->hasMethodParameters('ZendTest\Di\TestAsset\SetterInjection\B', 'setB'));
+        // method exists but has no parameters
+        $this->assertFalse($definition->hasMethodParameters('ZendTest\Di\TestAsset\SetterInjection\StaticSetter', 'setFoo'));
     }
 }

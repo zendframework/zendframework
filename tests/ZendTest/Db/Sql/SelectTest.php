df1790a0d2df2a79736be91c1dead960782a0ebe
Abdul Malik Ikhsan <samsonasik@gmail.com>
2014-03-08 08:52:43 +0700

    fix T_OBJECT_OPERATOR on php 5.3

--- tests/ZendTest/Db/Sql/SelectTest.php
+++ tests/ZendTest/Db/Sql/SelectTest.php
@@ -1164,7 +1164,8 @@ class SelectTest extends \PHPUnit_Framework_TestCase
         $select46b->from('bar')->where('c = d');
         $select46->combine($select46b);
 
-        $select46 = (new Select)->from(array('sub' => $select46))->order('id DESC');
+        $selectcombined = new Select();
+        $select46 = $selectcombined->from(array('sub' => $select46))->order('id DESC');
         $sqlPrep46 = // same
         $sqlStr46 = 'SELECT "sub".* FROM (( SELECT "foo".* FROM "foo" WHERE a = b ) UNION ( SELECT "bar".* FROM "bar" WHERE c = d )) AS "sub" ORDER BY "id" DESC';
         $internalTests46 = array(

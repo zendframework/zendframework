be5228f6c1cd16ebe1bdca4a751af8e7d16315aa
Brett Mc Bride <brett@deakin.edu.au>
2014-03-27 13:49:38 +1100

    Fix for issue 6048, where an apparent race condition occurs when concurrent filesystem caches try to clear expired items. To avoid this causing an exception for a user session, arrange for Filesystem::clearExpired to trigger a 'clearExpired.exception' event. This should give the same functionality as currently exists. Also, have the ExceptionHandler plugin listen for the clearExpired.exception event, and suppress it as per other exceptions.

--- library/Zend/Cache/Storage/Adapter/Filesystem.php
+++ library/Zend/Cache/Storage/Adapter/Filesystem.php
@@ -131,6 +131,8 @@ class Filesystem extends AbstractAdapter implements
      * Remove expired items
      *
      * @return bool
+     * 
+     * @triggers clearExpired.exception(ExceptionEvent)
      */
     public function clearExpired()
     {
@@ -161,7 +163,10 @@ class Filesystem extends AbstractAdapter implements
         }
         $error = ErrorHandler::stop();
         if ($error) {
-            throw new Exception\RuntimeException("Failed to clear expired items", 0, $error);
+            $e = new Exception\RuntimeException("Failed to clear expired items", 0, $error);
+            $result = false;
+            $args = new \ArrayObject();
+            return $this->triggerException(__FUNCTION__, $args, $result, $e);
         }
 
         return true;

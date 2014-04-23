ade0734a84937bc8088f24dd0954136955aee674
demichl68 <michel@petiton.de>
2014-04-23 15:40:02 +0200

    * added MutableCreationOptionsAwareTrait

---
+++ library/Zend/ServiceManager/MutableCreationOptionsAwareTrait.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\ServiceManager;
+
+/**
+ * Trait for MutableCreationOptionsAware Factories
+ */
+trait MutableCreationOptionsAwareTrait
+{
+    /**
+     * @var array
+     */
+    protected $creationOptions = [];
+
+    /**
+     * Set creation options
+     *
+     * @param array $creationOptions
+     * @return void
+     */
+    public function setCreationOptions(array $creationOptions)
+    {
+        $this->creationOptions = $creationOptions;
+    }
+
+
+    /**
+     * Get creation options
+     *
+     * @return array
+     */
+    public function getCreationOptions()
+    {
+        return $this->creationOptions;
+    }
+}

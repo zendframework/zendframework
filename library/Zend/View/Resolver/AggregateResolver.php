c98907ddb98224f91b805ce953dfb836dc432442
Vitalii Nagara <duke@ejoom.com>
2014-04-25 23:48:26 +0300

    Added posibility to render view with short reference.
    It makes add rendering view the same as php include file which contains
    in the same folder.

--- library/Zend/View/Resolver/AggregateResolver.php
+++ library/Zend/View/Resolver/AggregateResolver.php
@@ -80,6 +80,20 @@ class AggregateResolver implements Countable, IteratorAggregate, ResolverInterfa
         return $this;
     }
 
+    private function findResourceInQueue($name, Renderer $renderer = null)
+    {
+        foreach ($this->queue as $resolver) {
+            $resource = $resolver->resolve($name, $renderer);
+            if ($resource) {
+                // Resource found; return it
+                $this->lastSuccessfulResolver = $resolver;
+                return $resource;
+            }
+        }
+
+        return false;
+    }
+
     /**
      * Resolve a template/pattern name to a resource the renderer can consume
      *
@@ -97,16 +111,23 @@ class AggregateResolver implements Countable, IteratorAggregate, ResolverInterfa
             return false;
         }
 
-        foreach ($this->queue as $resolver) {
-            $resource = $resolver->resolve($name, $renderer);
-            if (!$resource) {
-                // No resource found; try next resolver
-                continue;
-            }
-
-            // Resource found; return it
-            $this->lastSuccessfulResolver = $resolver;
+        $resource = $this->findResourceInQueue($name, $renderer);
+        if ($resource) {
             return $resource;
+        }
+
+        // Here we look in the same name space (folder)
+        if ($renderer !== null) {
+            $helper = $renderer->plugin('view_model');
+            $currentTemplate = $helper->getCurrent()->getTemplate();
+            $pos = strrpos($currentTemplate, '/');
+            if ($pos > 0) {
+                $fullName = substr($currentTemplate, 0, $pos) . '/' . $name;
+                $resource = $this->findResourceInQueue($fullName, $renderer);
+                if ($resource) {
+                    return $resource;
+                }
+            }
         }
 
         $this->lastLookupFailure = static::FAILURE_NOT_FOUND;

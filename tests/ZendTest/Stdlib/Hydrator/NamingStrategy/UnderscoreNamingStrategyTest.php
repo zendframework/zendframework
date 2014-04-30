44ecdcb9b314144dc3a19bd14964f4c09735a145
Ben Scholzen <mail@dasprids.de>
2014-04-30 19:13:00 +0200

    Hotfix for UnderscoreNamingStrategy for 2.3, will be fixed properly in 2.4

---
+++ tests/ZendTest/Stdlib/Hydrator/NamingStrategy/UnderscoreNamingStrategyTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Stdlib\Hydrator\NamingStrategy;
+
+use Zend\Stdlib\Hydrator\NamingStrategy\UnderscoreNamingStrategy;
+
+/**
+ * Unit tests for {@see \Zend\Stdlib\Hydrator\NamingStrategy\UnderscoreNamingStrategy}
+ *
+ * @covers \Zend\Stdlib\Hydrator\NamingStrategy\UnderscoreNamingStrategy
+ * @group Zend_Stdlib
+ */
+class UnderscoreNamingStrategyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNameHydratesToCamelCase()
+    {
+        $strategy = new UnderscoreNamingStrategy();
+        $this->assertEquals('fooBarBaz', $strategy->hydrate('foo_bar_baz'));
+    }
+
+    public function testNameExtractsToUnderscore()
+    {
+        $strategy = new UnderscoreNamingStrategy();
+        $this->assertEquals('foo_bar_baz', $strategy->extract('fooBarBaz'));
+    }
+}

913d2345a9a201af367965d4b49bdddc479353d4
Frank <trainmaster@gmx.net>
2014-05-06 18:32:20 +0200

    Fix parameter format causing duplicate mails
    Recently I've experienced mails being duplicated and send to the sender when using the sendmail transport. After consulting my hosting company they told me there shouldn't been a space between "-f" and the envelope sender address. Removing the space character fixed the problem. No more duplicated mails being send to the sender.
    
    Furthermore I tested Swiftmailer and its sendmail transport layer. Absolutely no problems with it.
    
    Looking at the php docs there isn't a space character either, see http://www.php.net/manual/en/function.mail.php#example-3471. Furthermore I found http://stackoverflow.com/questions/4205228/basic-mail-function-php-additional-f-parameter-question and consulted the sendmail docs too: http://www.sendmail.org/~ca/email/man/sendmail.html.
    
    However, there are other examples around with a space character: http://stackoverflow.com/questions/179014/how-to-change-envelope-from-address-using-php-mail. Are they wrong?
    
    Since I'm not familiar with sendmail but actually trusting my hosting company I'm proposing to remove the space character.

--- library/Zend/Mail/Transport/Sendmail.php
+++ library/Zend/Mail/Transport/Sendmail.php
@@ -248,7 +248,7 @@ class Sendmail implements TransportInterface
 
         $sender = $message->getSender();
         if ($sender instanceof AddressInterface) {
-            $parameters .= ' -f ' . $sender->getEmail();
+            $parameters .= ' -f' . $sender->getEmail();
             return $parameters;
         }
 
@@ -256,7 +256,7 @@ class Sendmail implements TransportInterface
         if (count($from)) {
             $from->rewind();
             $sender      = $from->current();
-            $parameters .= ' -f ' . $sender->getEmail();
+            $parameters .= ' -f' . $sender->getEmail();
             return $parameters;
         }
 

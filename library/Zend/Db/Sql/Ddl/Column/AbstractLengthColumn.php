25f5bacb5817873d4e20c70f16edb54d618dad90
Kevin Abel <kevin.abel.0@gmail.com>
2014-05-08 23:16:56 -0500

    Add abstract Ddl Column classes to handle variable length data types

---
+++ library/Zend/Db/Sql/Ddl/Column/AbstractLengthColumn.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Db\Sql\Ddl\Column;
+
+abstract class AbstractLengthColumn extends Column
+{
+    /**
+     * @var int
+     */
+    protected $length;
+
+    /**
+     * @param null|string $name
+     * @param int $length
+     */
+    public function __construct($name, $length)
+    {
+        parent::__construct($name);
+        $this->length = $length;
+    }
+
+    /**
+     * @param  int $length
+     * @return self
+     */
+    public function setLength($length)
+    {
+        $this->length = $length;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLength()
+    {
+        return $this->length;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLengthExpression()
+    {
+        return (string) $this->length;
+    }
+
+    /**
+     * @return array
+     */
+    public function getExpressionData()
+    {
+        $spec = $this->specification;
+
+        $params   = array();
+        $params[] = $this->name;
+        $params[] = $this->type;
+
+        if ($this->getLengthExpression()) {
+            $params[1] .= '(' . $this->getLengthExpression() . ')';
+        }
+
+        $types = array(self::TYPE_IDENTIFIER, self::TYPE_LITERAL);
+
+        if (!$this->isNullable) {
+            $params[1] .= ' NOT NULL';
+        }
+
+        if ($this->default !== null) {
+            $spec    .= ' DEFAULT %s';
+            $params[] = $this->default;
+            $types[]  = self::TYPE_VALUE;
+        }
+
+        return array(array(
+            $spec,
+            $params,
+            $types,
+        ));
+    }
+}
---
+++ library/Zend/Db/Sql/Ddl/Column/AbstractPrecisionColumn.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Db\Sql\Ddl\Column;
+
+abstract class AbstractPrecisionColumn extends AbstractLengthColumn
+{
+    /**
+     * @var int
+     */
+    protected $decimal;
+
+    /**
+     * @param null|string $name
+     * @param int $digits
+     * @param null|int $decimal
+     */
+    public function __construct($name, $digits, $decimal = null)
+    {
+        parent::__construct($name, $digits);
+        $this->decimal = $decimal;
+    }
+
+    /**
+     * @param  int $digits
+     * @return self
+     */
+    public function setDigits($digits)
+    {
+        return $this->setLength($digits);
+    }
+
+    /**
+     * @return int
+     */
+    public function getDigits()
+    {
+        return $this->getLength();
+    }
+
+    /**
+     * @param int $decimal
+     * @return self
+     */
+    public function setDecimal($decimal)
+    {
+        $this->decimal = $decimal;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDecimal()
+    {
+        return $this->decimal;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLengthExpression()
+    {
+        $expr = $this->length;
+        if ($this->decimal !== null) {
+            $expr .= ',' . $this->decimal;
+        }
+        return $expr;
+    }
+}

9af3b400e19b59308f47ab4eac8a1ca1054e059a
Kevin Abel <kevin.abel.0@gmail.com>
2014-05-11 21:00:57 -0500

    Use the parent logic exclusively for expression data on Primary/Unique

--- library/Zend/Db/Sql/Ddl/Constraint/PrimaryKey.php
+++ library/Zend/Db/Sql/Ddl/Constraint/PrimaryKey.php
@@ -14,23 +14,5 @@ class PrimaryKey extends AbstractConstraint
     /**
      * @var string
      */
-    protected $specification = 'PRIMARY KEY (%s)';
-
-    /**
-     * @return array
-     */
-    public function getExpressionData()
-    {
-        $colCount     = count($this->columns);
-        $newSpecParts = array_fill(0, $colCount, '%s');
-        $newSpecTypes = array_fill(0, $colCount, self::TYPE_IDENTIFIER);
-
-        $newSpec = sprintf($this->specification, implode(', ', $newSpecParts));
-
-        return array(array(
-            $newSpec,
-            $this->columns,
-            $newSpecTypes,
-        ));
-    }
+    protected $specification = 'PRIMARY KEY';
 }
--- library/Zend/Db/Sql/Ddl/Constraint/UniqueKey.php
+++ library/Zend/Db/Sql/Ddl/Constraint/UniqueKey.php
@@ -14,42 +14,5 @@ class UniqueKey extends AbstractConstraint
     /**
      * @var string
      */
-    protected $specification = 'CONSTRAINT UNIQUE KEY %s(...)';
-
-    /**
-     * @param  string $column
-     * @param  null|string $name
-     */
-    public function __construct($column, $name = null)
-    {
-        $this->setColumns($column);
-        $this->name = $name;
-    }
-
-    /**
-     * @return array
-     */
-    public function getExpressionData()
-    {
-        $colCount = count($this->columns);
-
-        $values   = array();
-        $values[] = ($this->name) ? $this->name : '';
-
-        $newSpecTypes = array(self::TYPE_IDENTIFIER);
-        $newSpecParts = array();
-
-        for ($i = 0; $i < $colCount; $i++) {
-            $newSpecParts[] = '%s';
-            $newSpecTypes[] = self::TYPE_IDENTIFIER;
-        }
-
-        $newSpec = str_replace('...', implode(', ', $newSpecParts), $this->specification);
-
-        return array(array(
-            $newSpec,
-            array_merge($values, $this->columns),
-            $newSpecTypes,
-        ));
-    }
+    protected $specification = 'UNIQUE';
 }

f7ad8a7fbc10c8131e6b025e84705201d7114484
Kevin Abel <kevin.abel.0@gmail.com>
2014-05-11 21:04:21 -0500

    Add a test on Integer Ddl Column class to check column constraint PK

--- tests/ZendTest/Db/Sql/Ddl/Column/IntegerTest.php
+++ tests/ZendTest/Db/Sql/Ddl/Column/IntegerTest.php
@@ -10,6 +10,7 @@
 namespace ZendTest\Db\Sql\Ddl\Column;
 
 use Zend\Db\Sql\Ddl\Column\Integer;
+use Zend\Db\Sql\Ddl\Constraint\PrimaryKey;
 
 class IntegerTest extends \PHPUnit_Framework_TestCase
 {
@@ -30,6 +31,17 @@ class IntegerTest extends \PHPUnit_Framework_TestCase
         $column = new Integer('foo');
         $this->assertEquals(
             array(array('%s %s NOT NULL', array('foo', 'INTEGER'), array($column::TYPE_IDENTIFIER, $column::TYPE_LITERAL))),
+            $column->getExpressionData()
+        );
+
+        $column = new Integer('foo');
+        $column->addConstraint(new PrimaryKey());
+        $this->assertEquals(
+            array(
+                array('%s %s NOT NULL', array('foo', 'INTEGER'), array($column::TYPE_IDENTIFIER, $column::TYPE_LITERAL)),
+                ' ',
+                array('PRIMARY KEY', array(), array())
+            ),
             $column->getExpressionData()
         );
     }

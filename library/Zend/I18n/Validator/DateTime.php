b3c5b9ebb42c8ec1c4dfa420f1a0e9d8f6733348
Mark Garrett <mark@moderndeveloperllc.com>
2014-05-21 15:49:12 -0500

    Suggested changes from @DASPRiD

--- library/Zend/I18n/Validator/DateTime.php
+++ library/Zend/I18n/Validator/DateTime.php
@@ -6,6 +6,7 @@
  * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
+
 namespace Zend\I18n\Validator;
 
 use Locale;
--- tests/ZendTest/I18n/Validator/DateTimeTest.php
+++ tests/ZendTest/I18n/Validator/DateTimeTest.php
@@ -6,6 +6,7 @@
  * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
+
 namespace ZendTest\I18n\Validator;
 
 use Zend\I18n\Validator\DateTime as DateTimeValidator;

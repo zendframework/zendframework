d18b1071bf250cf37691d32e15ddbf72c2f703c4
arekkas <aeneas.rekkas@serlo.org>
2014-06-25 12:32:21 +0200

    Update header

--- tests/ZendTest/Mvc/View/Console/RouteNotFoundStrategyTest.php
+++ tests/ZendTest/Mvc/View/Console/RouteNotFoundStrategyTest.php
@@ -1,13 +1,11 @@
 <?php
 /**
- * Athene2 - Advanced Learning Resources Manager
- *
- * @author    Aeneas Rekkas (aeneas.rekkas@serlo.org)
- * @license   LGPL-3.0
- * @license   http://opensource.org/licenses/LGPL-3.0 The GNU Lesser General Public License, version 3.0
- * @link      https://github.com/serlo-org/athene2 for the canonical source repository
- * @copyright Copyright (c) 2013-2014 Gesellschaft für freie Bildung e.V. (http://www.open-education.eu/)
- */
+* Zend Framework (http://framework.zend.com/)
+*
+* @link http://github.com/zendframework/zf2 for the canonical source repository
+* @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+* @license http://framework.zend.com/license/new-bsd New BSD License
+*/
 namespace ZendTest\Mvc\View\Console;
 
 use PHPUnit_Framework_TestCase as TestCase;

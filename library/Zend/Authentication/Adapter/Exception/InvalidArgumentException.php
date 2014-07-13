8ffbdcf795250165dbfc257be0f101e41628825b
Chad Gray <chadwickgray@gmail.com>
2014-07-13 07:57:33 -0400

    PHPCS Fixes for Zend\Authentication

--- library/Zend/Authentication/Adapter/Exception/InvalidArgumentException.php
+++ library/Zend/Authentication/Adapter/Exception/InvalidArgumentException.php
@@ -11,8 +11,6 @@ namespace Zend\Authentication\Adapter\Exception;
 
 use Zend\Authentication\Exception;
 
-class InvalidArgumentException
-    extends Exception\InvalidArgumentException
-    implements ExceptionInterface
+class InvalidArgumentException extends Exception\InvalidArgumentException implements ExceptionInterface
 {
 }
--- library/Zend/Authentication/Exception/ExceptionInterface.php
+++ library/Zend/Authentication/Exception/ExceptionInterface.php
@@ -10,4 +10,5 @@
 namespace Zend\Authentication\Exception;
 
 interface ExceptionInterface
-{}
+{
+}

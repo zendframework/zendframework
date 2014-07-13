5291fd58d1260798ee1bbba6830b3c0d76264b69
Chad Gray <chadwickgray@gmail.com>
2014-07-13 08:06:50 -0400

    PHPCS fixes for Zend\InputFilter

--- library/Zend/InputFilter/BaseInputFilter.php
+++ library/Zend/InputFilter/BaseInputFilter.php
@@ -245,7 +245,7 @@ class BaseInputFilter implements
                 && $input->isRequired()
                 && $input->allowEmpty()
             ) {
-                if (!($input instanceOf EmptyContextInterface && $input->continueIfEmpty())) {
+                if (!($input instanceof EmptyContextInterface && $input->continueIfEmpty())) {
                     $this->validInputs[$name] = $input;
                     continue;
                 }

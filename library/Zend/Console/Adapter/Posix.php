44274fb5bbaa5a82e0f2571188d1af4022897915
Marco Pivetta <ocramius@gmail.com>
2014-08-06 22:03:07 +0200

    #6532 - removing redundant indentation on sprintf+exception throw

--- library/Zend/Console/Adapter/Posix.php
+++ library/Zend/Console/Adapter/Posix.php
@@ -392,12 +392,11 @@ class Posix extends AbstractAdapter
 
         if ($color !== null) {
             if (!isset(static::$ansiColorMap[$type][$color])) {
-                throw new Exception\BadMethodCallException(
-                    sprintf(
-                        'Unknown color "%s". Please use one of the Zend\Console\ColorInterface constants or use Zend\Console\Color\Xterm256::calculate',
-                        $color
-                    )
-                );
+                throw new Exception\BadMethodCallException(sprintf(
+                    'Unknown color "%s". Please use one of the Zend\Console\ColorInterface constants '
+                    . 'or use Zend\Console\Color\Xterm256::calculate',
+                    $color
+                ));
             }
 
             return static::$ansiColorMap[$type][$color];

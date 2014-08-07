f7cbd14585ddb800bc89b9174aa87b250355acb9
Wojciech Nowogrodzki <w.nowogrodzki@praca.pl>
2014-08-07 09:29:08 +0200

    Mime Part class rewind fix

--- library/Zend/Mime/Part.php
+++ library/Zend/Mime/Part.php
@@ -131,7 +131,12 @@ class Part
         if ($this->isStream) {
             $encodedStream = $this->getEncodedStream($EOL);
             $encodedStreamContents = stream_get_contents($encodedStream);
-            rewind($encodedStream);
+            $streamMetaData = stream_get_meta_data($encodedStream);
+            if (isset($streamMetaData['seekable'])) {
+                if ($streamMetaData['seekable']) {
+                    rewind($encodedStream);
+                }
+            }
             return $encodedStreamContents;
         }
         return Mime::encode($this->content, $this->encoding, $EOL);

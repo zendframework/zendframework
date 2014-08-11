b110e8b8184e4aebe385a04f51b6ecd24b4e21d8
Oleg <olegkulik1985@gmail.com>
2014-08-11 11:24:37 +0300

    - fix some column definitions (blob, text, integer, bigint, varchar, char) - add column def (datetime) - add Index constraint type - add AlterTableDecorator for Platform - adjust tests to be compatible with new definitions

---
+++ library/Zend/Db/Sql/Platform/Mysql/Ddl/AlterTableDecorator.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Db\Sql\Platform\Mysql\Ddl;
+
+use Zend\Db\Adapter\Platform\PlatformInterface;
+use Zend\Db\Sql\Ddl\AlterTable;
+use Zend\Db\Sql\Platform\PlatformDecoratorInterface;
+
+class AlterTableDecorator extends AlterTable implements PlatformDecoratorInterface
+{
+    /**
+     * @var AlterTable
+     */
+    protected $alterTable;
+
+    /**
+     * @param AlterTable $subject
+     */
+    public function setSubject($subject)
+    {
+        $this->alterTable = $subject;
+    }
+
+    /**
+     * @param  null|PlatformInterface $platform
+     * @return string
+     */
+    public function getSqlString(PlatformInterface $platform = null)
+    {
+        // localize variables
+        foreach (get_object_vars($this->alterTable) as $name => $value) {
+            $this->{$name} = $value;
+        }
+        return parent::getSqlString($platform);
+    }
+
+    protected function processAddColumns(PlatformInterface $adapterPlatform = null)
+    {
+        $sqls = array();
+        foreach ($this->addColumns as $i => $column) {
+            $stmtContainer = $this->processExpression($column, $adapterPlatform);
+            $sql           = $stmtContainer->getSql();
+            $columnOptions = $column->getOptions();
+
+            foreach ($columnOptions as $coName => $coValue) {
+                switch (strtolower(str_replace(array('-', '_', ' '), '', $coName))) {
+                    case 'identity':
+                    case 'serial':
+                    case 'autoincrement':
+                        $sql .= ' AUTO_INCREMENT';
+                        break;
+                    case 'comment':
+                        $sql .= ' COMMENT \'' . $coValue . '\'';
+                        break;
+                    case 'columnformat':
+                    case 'format':
+                        $sql .= ' COLUMN_FORMAT ' . strtoupper($coValue);
+                        break;
+                    case 'storage':
+                        $sql .= ' STORAGE ' . strtoupper($coValue);
+                        break;
+                }
+            }
+            $sqls[$i] = $sql;
+        }
+        return array($sqls);
+    }
+
+    protected function processChangeColumns(PlatformInterface $adapterPlatform = null)
+    {
+        $sqls = array();
+        foreach ($this->changeColumns as $name => $column) {
+            $stmtContainer = $this->processExpression($column, $adapterPlatform);
+            $sql           = $stmtContainer->getSql();
+            $columnOptions = $column->getOptions();
+
+            foreach ($columnOptions as $coName => $coValue) {
+                switch (strtolower(str_replace(array('-', '_', ' '), '', $coName))) {
+                    case 'identity':
+                    case 'serial':
+                    case 'autoincrement':
+                        $sql .= ' AUTO_INCREMENT';
+                        break;
+                    case 'comment':
+                        $sql .= ' COMMENT \'' . $coValue . '\'';
+                        break;
+                    case 'columnformat':
+                    case 'format':
+                        $sql .= ' COLUMN_FORMAT ' . strtoupper($coValue);
+                        break;
+                    case 'storage':
+                        $sql .= ' STORAGE ' . strtoupper($coValue);
+                        break;
+                }
+            }
+            $sqls[] = array(
+                $adapterPlatform->quoteIdentifier($name),
+                $sql
+            );
+        }
+
+        return array($sqls);
+    }
+}

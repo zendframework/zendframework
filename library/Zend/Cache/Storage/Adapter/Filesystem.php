8235694ef5cc53da807c6b07fc5fbb11ed24a425
Marc Bennewitz <marc-bennewitz@arcor.de>
2014-08-13 20:33:33 +0200

    fixes #6435: Race Condition in filesystem cache on prepare directory structure

--- library/Zend/Cache/Storage/Adapter/Filesystem.php
+++ library/Zend/Cache/Storage/Adapter/Filesystem.php
@@ -1413,8 +1413,16 @@ class Filesystem extends AbstractAdapter implements
             }
 
             if (!$res) {
-                $oct = ($perm === false) ? '777' : decoct($perm);
                 $err = ErrorHandler::stop();
+
+                // Issue 6435:
+                // mkdir could fail because of a race condition it was already created by another process
+                // after the first file_exists above
+                if (file_exists($pathname)) {
+                    return;
+                }
+
+                $oct = ($perm === false) ? '777' : decoct($perm);
                 throw new Exception\RuntimeException("mkdir('{$pathname}', 0{$oct}, true) failed", 0, $err);
             }
 
@@ -1452,6 +1460,14 @@ class Filesystem extends AbstractAdapter implements
                 }
 
                 if (!$res) {
+
+                    // Issue 6435:
+                    // mkdir could fail because of a race condition it was already created by another process
+                    // after the first file_exists above ... go to the next path part.
+                    if (file_exists($path)) {
+                        continue;
+                    }
+
                     $oct = ($perm === false) ? '777' : decoct($perm);
                     ErrorHandler::stop();
                     throw new Exception\RuntimeException(

e459bb4200305b61500c77a713c11aaa277e45ea
Marcos Lois Bermúdez <marcos.lois@gmail.com>
2014-09-01 21:57:18 +0200

    Create Zend_Captcha.php
    Added spanish translations.

---
+++ resources/languages/es/Zend_Captcha.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright  Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ */
+
+/**
+ * EN-Revision: 30.Jul.2011
+ */
+return array(
+    // Zend\Captcha\ReCaptcha
+    "Missing captcha fields" => "Faltan campos de captcha",
+    "Failed to validate captcha" => "La validación de captcha falló",
+    "Captcha value is wrong: %value%" => "El valor de captcha es erróneo: %value%",
+
+    // Zend\Captcha\Word
+    "Empty captcha value" => "Valor de captcha vacío",
+    "Captcha ID field is missing" => "Falta el campo ID de captcha",
+    "Captcha value is wrong" => "El valor de captcha es erróneo",
+);

e3eee77681d04b473d04a991f72e06f960729f98
Richard Holloway <richard@richardjh.org>
2014-09-02 23:17:38 +0100

    Added password prompt to Console

---
+++ library/Zend/Console/Prompt/Password.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Console\Prompt;
+
+class Password extends AbstractPrompt
+{
+    /**
+     * @var string
+     */
+    protected $promptText = 'Password : ';
+
+    /**
+     * @var bool
+     */
+    protected $echo = true;
+
+    /**
+     * @var string
+     */
+    protected $password = '';
+
+    /**
+     * Ask the user for a password
+     *
+     * @param string $promptText   The prompt text to display in console
+     * @param bool   $echo         Display the selection after user presses key
+     */
+    public function __construct(
+        $promptText = 'Password : ',
+        $echo = true
+    ) {
+
+        $this->setPromptText($promptText);
+        $this->setEcho($echo);
+        $this->password = '';
+    }
+
+    /**
+     * Show the prompt to user and return a single char.
+     *
+     * @return string
+     */
+    public function show()
+    {
+        $this->getConsole()->write($this->promptText);
+
+        /**
+         * Read characters from console
+         */
+        while (true) {
+            $char = $this->getConsole()->readChar();
+
+            if (PHP_EOL == $char) {
+
+                break;
+            }
+
+            if ($this->echo) {
+                echo "*";
+            }
+            $this->password .= $char;
+        }
+
+        return $this->password;
+    }
+
+    /**
+     * @param string $promptText
+     */
+    public function setPromptText($promptText)
+    {
+        $this->promptText = $promptText;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPromptText()
+    {
+        return $this->promptText;
+    }
+
+    /**
+     * @param bool $echo
+     */
+    public function setEcho($echo)
+    {
+        $this->echo = (bool) $echo;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getEcho()
+    {
+        return $this->echo;
+    }
+}
---
+++ tests/ZendTest/Console/Prompt/PasswordTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Console\Prompt;
+
+use Zend\Console\Prompt\Password;
+use ZendTest\Console\TestAssets\ConsoleAdapter;
+
+/**
+ * @group      Zend_Console
+ */
+class PasswordTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ConsoleAdapter
+     */
+    protected $adapter;
+
+    public function setUp()
+    {
+        $this->adapter = new ConsoleAdapter();
+        $this->adapter->stream = fopen('php://memory', 'w+');
+    }
+
+    public function tearDown()
+    {
+        fclose($this->adapter->stream);
+    }
+
+    public function testCanPromptPassword()
+    {
+        fwrite($this->adapter->stream, 'secret');
+
+        $char = new Password();
+        $char->setEcho(false);
+        $char->setConsole($this->adapter);
+        ob_start();
+        $response = $char->show();
+        $text = ob_get_clean();
+        $this->assertEquals($text, "Password : \n");
+        $this->assertEquals('secret', $response);
+    }
+
+    public function testCanPromptPasswordWithNewQuestion()
+    {
+        fwrite($this->adapter->stream, 'sh its a secret');
+
+        $char = new Password("What is the secret?");
+        $char->setEcho(false);
+        $char->setConsole($this->adapter);
+        ob_start();
+        $response = $char->show();
+        $text = ob_get_clean();
+        $this->assertEquals($text, "What is the secret\n");
+        $this->assertEquals('sh its a secret', $response);
+    }
+}

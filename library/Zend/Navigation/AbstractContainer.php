8e7b14d46e2a0a64762af376139fd0d7a7d2a17e
Tomoaki Kosugi <kipspro@gmail.com>
2014-10-31 20:44:15 +0900

    Fix #4517

--- library/Zend/Navigation/AbstractContainer.php
+++ library/Zend/Navigation/AbstractContainer.php
@@ -484,7 +484,7 @@ abstract class AbstractContainer implements Countable, RecursiveIterator
      */
     public function hasChildren()
     {
-        return $this->hasPages();
+        return $this->current()->hasPages();
     }
 
     /**

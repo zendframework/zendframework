6d59e88bfdcfbb8ea39e18a5a4bfce61b407fcf3
Marco Pivetta <ocramius@gmail.com>
2014-11-12 12:54:46 +0100

    #5142 - Expecting exceptions when passing invalid select statements to `Zend\Db\Sql\Combine#combine()`

--- tests/ZendTest/Db/Sql/CombineTest.php
+++ tests/ZendTest/Db/Sql/CombineTest.php
@@ -29,12 +29,11 @@ class CombineTest extends \PHPUnit_Framework_TestCase
         $this->combine = new Combine;
     }
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
+    public function testRejectsInvalidStatement()
     {
+        $this->setExpectedException('Zend\Db\Sql\Exception\InvalidArgumentException');
+
+        $this->combine->combine('foo');
     }
 
     public function testGetSqlString()

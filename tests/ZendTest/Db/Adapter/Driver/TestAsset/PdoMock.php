6a962706bd5ee8ea861bd8dd77602fea2ab5878d
Marco Pivetta <ocramius@gmail.com>
2014-11-12 12:30:43 +0100

    #5001 - license headers and PSR-2 compliance for `PdoMock`

--- tests/ZendTest/Db/Adapter/Driver/TestAsset/PdoMock.php
+++ tests/ZendTest/Db/Adapter/Driver/TestAsset/PdoMock.php
@@ -1,13 +1,23 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
 namespace ZendTest\Db\Adapter\Driver\TestAsset;
 
 /**
- * @todo Use a real mock object
+ * Stub class
  */
 class PdoMock extends \PDO
 {
     public function __construct()
-    {}
+    {
+
+    }
 
     public function beginTransaction()
     {
@@ -20,7 +30,8 @@ class PdoMock extends \PDO
     }
 
     public function getAttribute($attribute)
-    {}
+    {
+    }
 
     public function rollBack()
     {

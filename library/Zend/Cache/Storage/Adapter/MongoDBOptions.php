8daf979e2efdd7f45752b0e4be8d68d510e5dca0
Marco Pivetta <ocramius@gmail.com>
2014-11-13 23:17:41 +0100

    #6078 - minor CS fixes/cleanups in `MongoDBOptions` implementation

--- library/Zend/Cache/Storage/Adapter/MongoDBOptions.php
+++ library/Zend/Cache/Storage/Adapter/MongoDBOptions.php
@@ -13,37 +13,42 @@ class MongoDBOptions extends AdapterOptions
 {
     /**
      * The namespace separator
+     *
      * @var string
      */
-    protected $namespaceSeparator = ':';
+    private $namespaceSeparator = ':';
 
     /**
      * The redis resource manager
      *
      * @var null|RedisResourceManager
      */
-    protected $resourceManager;
+    private $resourceManager;
 
     /**
      * The resource id of the resource manager
      *
      * @var string
      */
-    protected $resourceId = 'default';
+    private $resourceId = 'default';
 
     /**
      * Set namespace separator
      *
      * @param  string $namespaceSeparator
-     * @return RedisOptions
+     *
+     * @return self
      */
     public function setNamespaceSeparator($namespaceSeparator)
     {
         $namespaceSeparator = (string) $namespaceSeparator;
+
         if ($this->namespaceSeparator !== $namespaceSeparator) {
             $this->triggerOptionEvent('namespace_separator', $namespaceSeparator);
+
             $this->namespaceSeparator = $namespaceSeparator;
         }
+
         return $this;
     }
 
@@ -61,14 +66,17 @@ class MongoDBOptions extends AdapterOptions
      * Set the mongodb resource manager to use
      *
      * @param null|MongoDBResourceManager $resourceManager
-     * @return MongoDBOptions
+     *
+     * @return self
      */
     public function setResourceManager(MongoDBResourceManager $resourceManager = null)
     {
         if ($this->resourceManager !== $resourceManager) {
             $this->triggerOptionEvent('resource_manager', $resourceManager);
+
             $this->resourceManager = $resourceManager;
         }
+
         return $this;
     }
 
@@ -79,10 +87,7 @@ class MongoDBOptions extends AdapterOptions
      */
     public function getResourceManager()
     {
-        if (!$this->resourceManager) {
-            $this->resourceManager = new MongoDBResourceManager();
-        }
-        return $this->resourceManager;
+        return $this->resourceManager ?: $this->resourceManager = new MongoDBResourceManager();
     }
 
     /**
@@ -99,15 +104,19 @@ class MongoDBOptions extends AdapterOptions
      * Set the redis resource id
      *
      * @param string $resourceId
-     * @return MongoDBOptions
+     *
+     * @return self
      */
     public function setResourceId($resourceId)
     {
         $resourceId = (string) $resourceId;
+
         if ($this->resourceId !== $resourceId) {
             $this->triggerOptionEvent('resource_id', $resourceId);
+
             $this->resourceId = $resourceId;
         }
+
         return $this;
     }
 
@@ -115,12 +124,14 @@ class MongoDBOptions extends AdapterOptions
      * set mongo options
      *
      * @param array $libOptions
-     * @return MongoDBOptions
+     *
+     * @return self
      */
     public function setLibOptions(array $libOptions)
     {
         $this->triggerOptionEvent('lib_option', $libOptions);
         $this->getResourceManager()->setLibOptions($this->getResourceId(), $libOptions);
+
         return $this;
     }
 }

6dac2352dc70bbb22cd748270bb4f979aa8187d1
Marco Pivetta <ocramius@gmail.com>
2014-11-20 00:08:34 +0100

    #6537 - minor CS fixes

--- library/Zend/Mime/Part.php
+++ library/Zend/Mime/Part.php
@@ -129,14 +129,14 @@ class Part
     public function getContent($EOL = Mime::LINEEND)
     {
         if ($this->isStream) {
-            $encodedStream = $this->getEncodedStream($EOL);
+            $encodedStream         = $this->getEncodedStream($EOL);
             $encodedStreamContents = stream_get_contents($encodedStream);
-            $streamMetaData = stream_get_meta_data($encodedStream);
-            if (isset($streamMetaData['seekable'])) {
-                if ($streamMetaData['seekable']) {
-                    rewind($encodedStream);
-                }
+            $streamMetaData        = stream_get_meta_data($encodedStream);
+
+            if (isset($streamMetaData['seekable']) && $streamMetaData['seekable']) {
+                rewind($encodedStream);
             }
+
             return $encodedStreamContents;
         }
         return Mime::encode($this->content, $this->encoding, $EOL);

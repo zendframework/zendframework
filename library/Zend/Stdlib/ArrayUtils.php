80475b61e62ea71d3a265a6f7e0549dce7232335
Witold Wasiczko <witold.wasiczko@rst.com.pl>
2014-11-19 15:28:39 +0100

    Add missing file

--- library/Zend/Stdlib/ArrayUtils.php
+++ library/Zend/Stdlib/ArrayUtils.php
@@ -257,8 +257,12 @@ abstract class ArrayUtils
     public static function merge(array $a, array $b, $preserveNumericKeys = false)
     {
         foreach ($b as $key => $value) {
+            $isRemove = $value instanceof ArrayUtilsRemoveKey;
+
             if (isset($a[$key]) || array_key_exists($key, $a)) {
-                if (!$preserveNumericKeys && is_int($key)) {
+                if ($isRemove) {
+                    unset($a[$key]);
+                } elseif (!$preserveNumericKeys && is_int($key)) {
                     $a[] = $value;
                 } elseif (is_array($value) && is_array($a[$key])) {
                     $a[$key] = static::merge($a[$key], $value, $preserveNumericKeys);
@@ -266,7 +270,9 @@ abstract class ArrayUtils
                     $a[$key] = $value;
                 }
             } else {
-                $a[$key] = $value;
+                if (!$isRemove) {
+                    $a[$key] = $value;
+                }
             }
         }
 

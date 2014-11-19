7b0973ba9166c7f70063796f68bd4b773ef4f325
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2014-11-19 21:36:06 +0100

    Resolved the issue with the Zend\Code\Scanner\ClassScanner not properly identifying the ";" as the end of a abstract method.

--- library/Zend/Code/Scanner/ClassScanner.php
+++ library/Zend/Code/Scanner/ClassScanner.php
@@ -554,6 +554,7 @@ class ClassScanner implements ScannerInterface
             // @todo find a way to test this
             die('Massive Failure, test this');
         }
+
         $m = new MethodScanner(
             array_slice($this->tokens, $info['tokenStart'], $info['tokenEnd'] - $info['tokenStart'] + 1),
             $this->nameInformation
@@ -848,6 +849,10 @@ class ClassScanner implements ScannerInterface
                                 //goto no break needed
                             case '}':
                                 $braceCount--;
+                                goto SCANNER_CLASS_BODY_MEMBER_CONTINUE;
+
+                            case ';':
+                                $infos[$infoIndex]['tokenEnd'] = $tokenIndex;
                                 goto SCANNER_CLASS_BODY_MEMBER_CONTINUE;
                         }
                     }

f3000351d9166cfac977b09a4c179389c6356b80
Marco Pivetta <ocramius@gmail.com>
2014-11-22 09:09:05 +0100

    #6825 - minor CS fixes (removing unneeded parenthetical)

--- library/Zend/Navigation/AbstractContainer.php
+++ library/Zend/Navigation/AbstractContainer.php
@@ -484,7 +484,7 @@ abstract class AbstractContainer implements Countable, RecursiveIterator
      */
     public function hasChildren()
     {
-        return ($this->valid() && $this->current()->hasPages());
+        return $this->valid() && $this->current()->hasPages();
     }
 
     /**

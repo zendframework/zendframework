c6566f05c9337380a443500148c609c9a16353af
Marco Pivetta <ocramius@gmail.com>
2014-11-29 05:13:34 +0100

    #6814 #6946 - checking against `PHP_VERSION_ID` as `version_compare()` is unreliable

--- tests/ZendTest/File/ClassFileLocatorTest.php
+++ tests/ZendTest/File/ClassFileLocatorTest.php
@@ -135,12 +135,11 @@ class ClassFileLocatorTest extends \PHPUnit_Framework_TestCase
 
     public function testIterationShouldNotCountFQCNScalarResolutionConstantAsClass()
     {
-        if (version_compare(PHP_VERSION, "5.5", "<")) {
+        if (PHP_VERSION_ID < 50500) {
             $this->markTestSkipped('Only applies to PHP >=5.5');
         }
 
-        $locator = new ClassFileLocator(__DIR__ .'/TestAsset');
-        foreach ($locator as $file) {
+        foreach (new ClassFileLocator(__DIR__ .'/TestAsset') as $file) {
             if (! preg_match('/ClassNameResolutionCompatibility\.php$/', $file->getFilename())) {
                 continue;
             }

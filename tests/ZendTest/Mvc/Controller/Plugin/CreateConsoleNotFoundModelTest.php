0f65074b2e659c41d46cadbee58ca2fdc1d091bf
fabiocarneiro <fahecs@gmail.com>
2014-12-04 12:16:02 -0200

    add tests for new functionality

---
+++ tests/ZendTest/Mvc/Controller/Plugin/CreateConsoleNotFoundModelTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ZendTest\Mvc\Controller\Plugin;
+
+use PHPUnit_framework_TestCase as TestCase;
+use Zend\Console\Request;
+use Zend\Console\Response;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\Console\RouteMatch;
+use ZendTest\Mvc\Controller\TestAsset\SampleController;
+
+class CreateConsoleNotFoundModelTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->request = new Request;
+        $event         = new MvcEvent;
+
+        $event->setRequest($this->request);
+        $event->setResponse(new Response);
+        $event->setRouteMatch(new RouteMatch(array()));
+
+        $this->controller = new SampleController;
+        $this->controller->setEvent($event);
+
+        $this->plugin = $this->controller->plugin('createConsoleNotFoundModel');
+    }
+
+    public function testIfCanReturnModelWithErrorMessageAndErrorLevel()
+    {
+        $response = $this->controller->getEvent()->getResponse();
+        $model    = $this->plugin->__invoke($response);
+
+        $this->assertInstanceOf('Zend\View\Model\ConsoleModel', $model);
+        $this->assertSame("Page not found", $model->getResult());
+        $this->assertSame(1, $model->getErrorLevel());
+    }
+} 
---
+++ tests/ZendTest/Mvc/Controller/Plugin/CreateHttpNotFoundModelTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ZendTest\Mvc\Controller\Plugin;
+
+use PHPUnit_framework_TestCase as TestCase;
+use Zend\Http\Request;
+use Zend\Http\PhpEnvironment\Response;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\Http\RouteMatch;
+use ZendTest\Mvc\Controller\TestAsset\SampleController;
+
+class CreateHttpNotFoundModelTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->request = new Request;
+        $event         = new MvcEvent;
+
+        $event->setRequest($this->request);
+        $event->setResponse(new Response);
+        $event->setRouteMatch(new RouteMatch(array()));
+
+        $this->controller = new SampleController;
+        $this->controller->setEvent($event);
+
+        $this->plugin = $this->controller->plugin('createHttpNotFoundModel');
+    }
+
+    public function testIfCanReturnModelWithErrorMessageAndSetResponseStatusCode()
+    {
+        $response = $this->controller->getEvent()->getResponse();
+        $model    = $this->plugin->__invoke($response);
+
+        $this->assertInstanceOf('Zend\View\Model\ViewModel', $model);
+        $this->assertSame("Page not found", $model->getVariable('content'));
+        $this->assertSame(404, $response->getStatusCode());
+    }
+} 

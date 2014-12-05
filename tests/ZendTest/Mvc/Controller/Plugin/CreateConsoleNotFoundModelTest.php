3db24968bbb10499ddda72229d70ea17fdc522c5
Marco Pivetta <ocramius@gmail.com>
2014-12-05 15:39:00 +0100

    #6956 - Converted test for `Zend\Mvc\Controller\Plugin\CreateConsoleNotFoundModel` into a unit test

--- tests/ZendTest/Mvc/Controller/Plugin/CreateConsoleNotFoundModelTest.php
+++ tests/ZendTest/Mvc/Controller/Plugin/CreateConsoleNotFoundModelTest.php
@@ -3,36 +3,18 @@
 namespace ZendTest\Mvc\Controller\Plugin;
 
 use PHPUnit_framework_TestCase as TestCase;
-use Zend\Console\Request;
-use Zend\Console\Response;
-use Zend\Mvc\MvcEvent;
-use Zend\Mvc\Router\Console\RouteMatch;
-use ZendTest\Mvc\Controller\TestAsset\SampleController;
+use Zend\Mvc\Controller\Plugin\CreateConsoleNotFoundModel;
 
 class CreateConsoleNotFoundModelTest extends TestCase
 {
-    public function setUp()
+    public function testCanReturnModelWithErrorMessageAndErrorLevel()
     {
-        $this->request = new Request;
-        $event         = new MvcEvent;
+        $plugin = new CreateConsoleNotFoundModel();
 
-        $event->setRequest($this->request);
-        $event->setResponse(new Response);
-        $event->setRouteMatch(new RouteMatch(array()));
+        $model = $plugin->__invoke();
 
-        $this->controller = new SampleController;
-        $this->controller->setEvent($event);
-
-        $this->plugin = $this->controller->plugin('createConsoleNotFoundModel');
-    }
-
-    public function testIfCanReturnModelWithErrorMessageAndErrorLevel()
-    {
-        $response = $this->controller->getEvent()->getResponse();
-        $model    = $this->plugin->__invoke($response);
-
-        $this->assertInstanceOf('Zend\View\Model\ConsoleModel', $model);
-        $this->assertSame("Page not found", $model->getResult());
+        $this->assertInstanceOf('Zend\\View\\Model\\ConsoleModel', $model);
+        $this->assertSame('Page not found', $model->getResult());
         $this->assertSame(1, $model->getErrorLevel());
     }
 }

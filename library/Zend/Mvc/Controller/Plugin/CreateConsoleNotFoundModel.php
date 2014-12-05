f0e6ba81ae4e2238fbd449584471fae01b615b77
Marco Pivetta <ocramius@gmail.com>
2014-12-05 15:15:44 +0100

    #6956 - adding license headers, minor cs fixes (spacing)

--- library/Zend/Mvc/Controller/Plugin/CreateConsoleNotFoundModel.php
+++ library/Zend/Mvc/Controller/Plugin/CreateConsoleNotFoundModel.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
 
 namespace Zend\Mvc\Controller\Plugin;
 
@@ -11,12 +18,16 @@ use Zend\Stdlib\ResponseInterface;
 class CreateConsoleNotFoundModel extends AbstractPlugin
 {
     /**
+     * Create a console view model representing a "not found" action
+     *
      * @param  ResponseInterface $response
+     *
      * @return ConsoleModel
      */
     public function __invoke($response)
     {
         $viewModel = new ConsoleModel();
+
         $viewModel->setErrorLevel(1);
         $viewModel->setResult('Page not found');
 

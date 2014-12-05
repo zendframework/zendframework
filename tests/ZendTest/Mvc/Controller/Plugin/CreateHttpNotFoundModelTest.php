c80d5be3739f8495858f63632d3497ddf51ac19f
Marco Pivetta <ocramius@gmail.com>
2014-12-05 15:21:18 +0100

    #6956 - removing dead code

--- tests/ZendTest/Mvc/Controller/Plugin/CreateHttpNotFoundModelTest.php
+++ tests/ZendTest/Mvc/Controller/Plugin/CreateHttpNotFoundModelTest.php
@@ -3,30 +3,11 @@
 namespace ZendTest\Mvc\Controller\Plugin;
 
 use PHPUnit_framework_TestCase as TestCase;
-use Zend\Http\Request;
 use Zend\Http\Response;
 use Zend\Mvc\Controller\Plugin\CreateHttpNotFoundModel;
-use Zend\Mvc\MvcEvent;
-use Zend\Mvc\Router\Http\RouteMatch;
-use ZendTest\Mvc\Controller\TestAsset\SampleController;
 
 class CreateHttpNotFoundModelTest extends TestCase
 {
-    public function setUp()
-    {
-        $this->request = new Request();
-        $event         = new MvcEvent;
-
-        $event->setRequest($this->request);
-        $event->setResponse(new Response);
-        $event->setRouteMatch(new RouteMatch(array()));
-
-        $this->controller = new SampleController;
-        $this->controller->setEvent($event);
-
-        $this->plugin = $this->controller->plugin('createHttpNotFoundModel');
-    }
-
     public function testBuildsModelWithErrorMessageAndSetsResponseStatusCode()
     {
         $response = new Response();

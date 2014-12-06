bd4d10e7173b3c6cbb0f125808605f1506b7163c
Marco Pivetta <ocramius@gmail.com>
2014-12-06 03:22:07 +0100

    #6903 - importing `MergeReplaceKeyInterface` interface

--- library/Zend/Stdlib/ArrayUtils.php
+++ library/Zend/Stdlib/ArrayUtils.php
@@ -11,6 +11,7 @@ namespace Zend\Stdlib;
 
 use Traversable;
 use Zend\Stdlib\ArrayUtils\MergeRemoveKey;
+use Zend\Stdlib\ArrayUtils\MergeReplaceKeyInterface;
 
 /**
  * Utility class for testing and manipulation of PHP arrays.
@@ -258,7 +259,7 @@ abstract class ArrayUtils
     public static function merge(array $a, array $b, $preserveNumericKeys = false)
     {
         foreach ($b as $key => $value) {
-            if ($value instanceof ArrayUtils\MergeReplaceKeyInterface) {
+            if ($value instanceof MergeReplaceKeyInterface) {
                 $a[$key] = $value->getData();
             } elseif (isset($a[$key]) || array_key_exists($key, $a)) {
                 if ($value instanceof MergeRemoveKey) {

d5f26e4c79e0e92eb122e237e1d323556f9bfe08
Marco Pivetta <ocramius@gmail.com>
2014-12-06 03:20:49 +0100

    #6903 - adding docblocks to the new `MergeReplaceKeyInterface`

--- library/Zend/Stdlib/ArrayUtils/MergeReplaceKeyInterface.php
+++ library/Zend/Stdlib/ArrayUtils/MergeReplaceKeyInterface.php
@@ -9,7 +9,13 @@
 
 namespace Zend\Stdlib\ArrayUtils;
 
+/**
+ * Marker interface: can be used to replace keys completely in {@see ArrayUtils::merge()} operations
+ */
 interface MergeReplaceKeyInterface
 {
+    /**
+     * @return mixed
+     */
     public function getData();
 }

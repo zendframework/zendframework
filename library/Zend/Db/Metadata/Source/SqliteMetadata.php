71eafc43cfac38c8fbcd8f4937e9b90e0eb1f7d5
Jaap van Otterdijk <jaap@youngguns.nl>
2014-12-08 08:40:26 +0100

    remove unneeded method call

--- library/Zend/Db/Metadata/Source/SqliteMetadata.php
+++ library/Zend/Db/Metadata/Source/SqliteMetadata.php
@@ -78,7 +78,6 @@ class SqliteMetadata extends AbstractSource
         $this->prepareDataHierarchy('columns', $schema, $table);
         $this->prepareDataHierarchy('sqlite_columns', $schema, $table);
 
-        $this->adapter->getPlatform();
         $results = $this->fetchPragma('table_info', $table, $schema);
 
         $columns = array();

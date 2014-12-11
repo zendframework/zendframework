bae59190a50f4087a0eb5ab96d9ff8c9d338882b
Marco Pivetta <ocramius@gmail.com>
2014-12-11 05:02:03 +0100

    #7006 - `sprintf` over string concatenation

--- library/Zend/Stdlib/AbstractOptions.php
+++ library/Zend/Stdlib/AbstractOptions.php
@@ -107,11 +107,11 @@ abstract class AbstractOptions implements ParameterObjectInterface
         }
 
         if ($this->__strictMode__) {
-            throw new Exception\BadMethodCallException(
-                'The option "' . $key . '" does not '
-                . 'have a matching ' .  'set' . str_replace(' ', '', ucwords(str_replace('_', ' ', $key)))
-                . ' setter method which must be defined'
-            );
+            throw new Exception\BadMethodCallException(sprintf(
+                'The option "%s" does not have a matching "%s" setter method which must be defined',
+                $key,
+                'set' . str_replace(' ', '', ucwords(str_replace('_', ' ', $key)))
+            ));
         }
     }
 

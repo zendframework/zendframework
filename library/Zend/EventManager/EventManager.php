34bb24acfa3e375e56ba11b19ad5a14f460ce00f
Jaap van Otterdijk <jaap@youngguns.nl>
2014-12-12 21:54:56 +0100

    listeners can be any kind of traversable

--- library/Zend/EventManager/EventManager.php
+++ library/Zend/EventManager/EventManager.php
@@ -506,7 +506,7 @@ class EventManager implements EventManagerInterface
      * Used to inject shared listeners and wildcard listeners.
      *
      * @param  PriorityQueue $masterListeners
-     * @param  PriorityQueue $listeners
+     * @param  \Traversable|array $listeners
      * @return void
      */
     protected function insertListeners($masterListeners, $listeners)

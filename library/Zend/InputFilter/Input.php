ef53725087194947578a3b825fd8d7622a95fa1a
Holger Schletz <holger.schletz@web.de>
2014-12-15 13:15:56 +0100

    Forked changes from #6784.

--- library/Zend/InputFilter/Input.php
+++ library/Zend/InputFilter/Input.php
@@ -319,6 +319,13 @@ class Input implements InputInterface, EmptyContextInterface
      */
     public function isValid($context = null)
     {
+        $value     = $this->getValue();
+        $empty     = ($value === null || $value === '');
+
+        if ($empty && $this->allowEmpty()) {
+            return true;
+        }
+
         // Empty value needs further validation if continueIfEmpty is set
         // so don't inject NotEmpty validator which would always
         // mark that as false
@@ -326,7 +333,7 @@ class Input implements InputInterface, EmptyContextInterface
             $this->injectNotEmptyValidator();
         }
         $validator = $this->getValidatorChain();
-        $value     = $this->getValue();
+
         $result    = $validator->isValid($value, $context);
         if (!$result && $this->hasFallback()) {
             $this->setValue($this->getFallbackValue());

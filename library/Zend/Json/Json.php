a5239ea8c25b36fd603c82928759a2cedf04d0f8
Bernhard Efler <efler@raum42.at>
2014-12-15 11:48:50 +0100

    change PHP Version Check to PHP_VERSION_ID

--- library/Zend/Json/Json.php
+++ library/Zend/Json/Json.php
@@ -121,7 +121,7 @@ class Json
 
             $encodeOptions = JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP;
 
-            if (version_compare(phpversion(), '5.4', '>=') && $prettyPrint) {
+            if (PHP_VERSION_ID >= 50400 && $prettyPrint) {
                 $encodeOptions |= JSON_PRETTY_PRINT;
                 $prettyPrint = false;
             }

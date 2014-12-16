b056310a9adf8eb5ce00459ebacf5d62bccf4f04
Rob Allen <rob@akrabat.com>
2014-12-16 21:55:06 +0000

    InjectTemplateListenerFactory tests.

---
+++ tests/ZendTest/Mvc/Service/InjectTemplateListenerFactoryTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Mvc\Service;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\ServiceManager\ServiceManager;
+use Zend\Mvc\View\Http\InjectTemplateListener;
+use Zend\Mvc\Service\InjectTemplateListenerFactory;
+
+class InjectTemplateListenerFactoryTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->services = new ServiceManager();
+        $this->factory  = new InjectTemplateListenerFactory();
+    }
+
+    public function testFactoryCanCreateInjectTemplateListener()
+    {
+        $this->services->setService('Config', array());
+
+        $listener = $this->factory->createService($this->services);
+        $this->assertInstanceOf('Zend\Mvc\View\Http\InjectTemplateListener', $listener);
+    }
+
+    public function testFactoryCanSetControllerMap()
+    {
+        $this->services->setService('Config', array(
+            'view_manager' => array(
+                'controller_map' => array(
+                    'SomeModule' => 'some/module',
+                ),
+            ),
+        ));
+
+        $listener = $this->factory->createService($this->services);
+
+        $this->assertEquals('some/module', $listener->mapController("SomeModule"));
+    }
+}

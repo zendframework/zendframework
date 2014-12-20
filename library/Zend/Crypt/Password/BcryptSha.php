110db6eb135022c01ccbb1a13fe87f4d5594a8fc
Padraic Brady <padraic.brady@gmail.com>
2014-12-20 17:42:48 +0000

    Create a new Bcrypt password hashing class using SHA2 to pre-hash longer password >72 chars to acceptable length

---
+++ library/Zend/Crypt/Password/BcryptSha.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\Crypt\Password;
+
+use Traversable;
+use Zend\Math\Rand;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Crypt\Hash;
+
+/**
+ * Bcrypt algorithm using crypt() function of PHP with password
+ * hashed using SHA2 to allow for passwords >72 characters.
+ */
+class BcryptSha extends Bcrypt
+{
+
+    /**
+     * BcryptSha
+     *
+     * @param  string $password
+     * @throws Exception\RuntimeException
+     * @return string
+     */
+    public function create($password)
+    {
+        $hashedPassword = Hash::compute('sha256', $password);
+        return parent::create($hashedPassword);
+    }
+
+    /**
+     * Verify if a password is correct against a hash value
+     *
+     * @param  string $password
+     * @param  string $hash
+     * @throws Exception\RuntimeException when the hash is unable to be processed
+     * @return bool
+     */
+    public function verify($password, $hash)
+    {
+        $hashedPassword = Hash::compute('sha256', $password);
+        return parent::verify($hashedPassword, $hash);
+    }
+
+}

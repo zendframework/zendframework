893ffa61b97f9f5b7e069c5aa1729968387423b9
Tim Klever <tklever@live.com>
2015-01-26 15:19:21 -0700

    Added Zend License

--- library/Zend/Paginator/PaginatorIterator.php
+++ library/Zend/Paginator/PaginatorIterator.php
@@ -1,9 +1,20 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
 
 namespace Zend\Paginator;
 
 use OuterIterator;
 
+/**
+ * Class allowing for the continuous iteration of a Zend\Paginator\Paginator instance.
+ * Useful for representing remote paginated data sources as a single Iterator
+ */
 class PaginatorIterator implements OuterIterator
 {
     /**

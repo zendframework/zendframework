e5e367b745faec2461f7249ce051aa5c5f2cb4c4
Ralf Eggert <ralf@travello.com>
2015-02-13 14:47:14 +0100

    Added file header to ZendTest\Code\Generator\TestAsset\TestSampleSingleClassMultiLines

--- tests/ZendTest/Code/Generator/TestAsset/TestSampleSingleClassMultiLines.php
+++ tests/ZendTest/Code/Generator/TestAsset/TestSampleSingleClassMultiLines.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * File header here
+ * Zend Framework (http://framework.zend.com/)
  *
- * @author Ralph Schindler <ralph.schindler@zend.com>
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace ZendTest\Code\Generator\TestAsset;

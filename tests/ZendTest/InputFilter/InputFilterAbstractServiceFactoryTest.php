9e07cf191af000a5c52a51016c6973dcba591cf6
BENOIT Julien <jbenoit.jb@gmail.com>
2015-02-23 14:28:04 +0100

    Change copyright/license headers

--- tests/ZendTest/InputFilter/InputFilterAbstractServiceFactoryTest.php
+++ tests/ZendTest/InputFilter/InputFilterAbstractServiceFactoryTest.php
@@ -1,7 +1,10 @@
 <?php
 /**
- * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace ZendTest\InputFilter;

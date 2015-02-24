6a0e452b8fe5edd70f1162a98932208a4313693c
Steve Rhoades <sedonami@gmail.com>
2015-02-24 10:36:11 -0800

    adding Zend copyright

--- library/Zend/Code/Generator/TraitUsageGenerator.php
+++ library/Zend/Code/Generator/TraitUsageGenerator.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: steverhoades
- * Date: 2/24/15
- * Time: 10:02 AM
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 namespace Zend\Code\Generator;
 

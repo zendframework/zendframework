1c3cbcd67032a33340a11dea7e557a75fe1233a1
Matthew Weier O'Phinney <matthew@zend.com>
2015-02-24 10:38:58 -0600

    [#6783] Incorporate feedback
    - Update copyright date
    - Simplify original assigment of flag

--- library/Zend/Form/Annotation/ContinueIfEmpty.php
+++ library/Zend/Form/Annotation/ContinueIfEmpty.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
@@ -33,14 +33,12 @@ class ContinueIfEmpty
      */
     public function __construct(array $data)
     {
-        if (!isset($data['value'])) {
-            $data['value'] = false;
-        }
+        $continueIfEmpty = (isset($data['value']))
+            ? $data['value']
+            : false;
 
-        $continueIfEmpty = $data['value'];
-
-        if (!is_bool($continueIfEmpty)) {
-            $filter   = new BooleanFilter();
+        if (! is_bool($continueIfEmpty)) {
+            $filter = new BooleanFilter();
             $continueIfEmpty = $filter->filter($continueIfEmpty);
         }
 

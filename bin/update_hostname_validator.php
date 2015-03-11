3c1f8d8a2325d14675acf7a0d30654559e6b27d4
Patrick Guiran <pguiran@prismamedia.com>
2015-03-11 15:18:44 +0100

    use https://data.iana.org/TLD/tlds-alpha-by-domain.txt thx @moderndeveloperllc

--- bin/update_hostname_validator.php
+++ bin/update_hostname_validator.php
@@ -9,19 +9,21 @@ Zend\Loader\AutoloaderFactory::factory(array(
 
 use Zend\Dom\Query as DomQuery;
 use Zend\Http\ClientStatic;
+use Zend\Validator\Hostname;
 
-define('IANA_URL', 'http://www.iana.org/domains/root/db', true);
+define('IANA_URL', 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt', true);
 define('ZF2_HOSTNAME_VALIDATOR_FILE', __DIR__.'/../library/Zend/Validator/Hostname.php', true);
 
 if (!file_exists(ZF2_HOSTNAME_VALIDATOR_FILE) || !is_readable(ZF2_HOSTNAME_VALIDATOR_FILE)) {
     printf('Error: cannont read file "%s"'.PHP_EOL, ZF2_HOSTNAME_VALIDATOR_FILE);
-    exit (1);
+    exit(1);
 }
 
 if (!is_writable(ZF2_HOSTNAME_VALIDATOR_FILE)) {
     printf('Error: Cannot update file "%s"'.PHP_EOL, ZF2_HOSTNAME_VALIDATOR_FILE);
     exit(1);
 }
+
 /** get online page of official TLDs **/
 $response = ClientStatic::get(IANA_URL);
 if (!$response->isSuccess()) {
@@ -29,11 +31,34 @@ if (!$response->isSuccess()) {
     exit(1);
 }
 
+/**
+ * TLDs are puny encoded. We need a decodePunycode function to
+ * translate TLDs ot UTF-8 :
+ * - use idn_to_utf8 if available
+ * - otherwise, use Zend\Validator\Hostname::decodePunycode()
+ */
+$decodePunycode = null;
+if (function_exists('idn_to_utf8')) {
+    $decodePunycode = 'idn_to_utf8';
+}
+if ($decodePunycode == null) {
+    $hostnameValidator = new Hostname();
+    $reflection = new \ReflectionClass(get_class($hostnameValidator));
+    $method = $reflection->getMethod('decodePunycode');
+    $method->setAccessible(true);
+
+    $decodePunycode = function ($encode) use ($hostnameValidator, $method) {
+        if (strpos($encode, 'xn--') === 0) {
+            return $method->invokeArgs($hostnameValidator, array(substr($encode, 4)));
+        }
+        return $encode;
+    };
+}
+
 /** Get new TLDs from the fetched page **/
 $newValidTlds = array();
-$dom = new DomQuery($response->getBody());
-foreach ($dom->execute('span.domain.tld > a') as $node) {
-    $newValidTlds []= str_repeat(' ', 8)."'".substr($node->nodeValue, 1)."',".PHP_EOL;
+foreach (preg_grep('/^[^#]/', explode(PHP_EOL, $response->getBody())) as $line) {
+    $newValidTlds []= str_repeat(' ', 8) ."'". call_user_func($decodePunycode, strtolower($line)) ."',".PHP_EOL;
 }
 
 /** Get file content **/
@@ -62,7 +87,6 @@ foreach ($fileLines as $line) {
             }
         }
     }
-
 }
 
 if (!$insertDone) {

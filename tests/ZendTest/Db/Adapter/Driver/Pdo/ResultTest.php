ed92a8dff3ab3dfccfb50bb84924d639a3dfbe66
Yordi Pauptit <yordipauptit@hotmail.com>
2015-03-19 00:40:27 +0100

    added support for other pdo fetch modes

--- tests/ZendTest/Db/Adapter/Driver/Pdo/ResultTest.php
+++ tests/ZendTest/Db/Adapter/Driver/Pdo/ResultTest.php
@@ -11,8 +11,15 @@ namespace ZendTest\Db\Adapter\Driver\Pdo;
 
 use Zend\Db\Adapter\Driver\Pdo\Result;
 
+/**
+ * Class ResultTest
+ * @package ZendTest\Db\Adapter\Driver\Pdo
+ *
+ * @group result-pdo
+ */
 class ResultTest extends \PHPUnit_Framework_TestCase
 {
+
     /**
      * Tests current method returns same data on consecutive calls.
      *
@@ -30,5 +37,33 @@ class ResultTest extends \PHPUnit_Framework_TestCase
         $result->initialize($stub, null);
 
         $this->assertEquals($result->current(), $result->current());
+    }
+
+    /**
+     * @expectedException \Zend\Db\Adapter\Exception\InvalidArgumentException
+     */
+    public function testFetchModeException()
+    {
+        $result = new Result();
+        $result->setFetchMode(11);
+    }
+
+    /**
+     * Tests whether the fetch mode was set properly and
+     */
+    public function testFetchModeAnonymousObject()
+    {
+        $stub = $this->getMock('PDOStatement');
+        $stub
+            ->expects($this->any())
+            ->method('fetch')
+            ->will($this->returnCallback(function () {return uniqid();}));
+
+        $result = new Result();
+        $result->initialize($stub, null);
+        $result->setFetchMode(\PDO::FETCH_OBJ);
+
+        $this->assertEquals(5, $result->getFetchMode());
+        $this->assertInstanceOf('stdClass', $result->current());
     }
 }

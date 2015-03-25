5e3a3d0cc997dca62dda82d8b2b010c5558506bf
Matthew Weier O'Phinney <matthew@zend.com>
2015-03-25 13:48:16 -0500

    Re-added Null filter for BC
    Re-added the Null filter for BC purposes; it now emits an `E_USER_DEPRECATED`
    notice on instantiation, warning the user to migrate.

---
+++ tests/ZendTest/Filter/NullTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\Filter;
+
+use Zend\Filter\Null as NullFilter;
+
+/**
+ * @group      Zend_Filter
+ */
+class NullTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        if (version_compare(PHP_VERSION, '7.0', '>=')) {
+            $this->markTestSkipped('Cannot test Null filter under PHP 7; reserved keyword');
+        }
+    }
+
+    public function testRaisesNoticeOnInstantiation()
+    {
+        $this->setExpectedException('PHPUnit_Framework_Error_Deprecated');
+        new NullFilter();
+    }
+}
